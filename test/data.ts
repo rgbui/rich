5443502738b7cb369f2502acf6cce3d74b5c1dc7
kanhai <760489033@qq.com>
2021-01-24 22:46:20 +0800

    feat test

--- test/data.ts
+++ test/data.ts
@@ -17,7 +17,14 @@ export var data = {
                         { name: 'text', content: '我就这样喽' }]
                     }
                 ]
-            }]
+            }, {
+                name: "row",
+                childs: [
+                    { name: 'todo', content: '我可以选择吗', checked: true }
+                ]
+            },
+            { name: 'row', childs: [{ name: 'image', widthPercent: 50, src: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fblog%2F201508%2F10%2F20150810150356_hnves.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1614066907&t=93f1dfde3d75b35f1153af1eda01e314' }] }
+            ]
         }
     ]
 }
--- test/index.tsx
+++ test/index.tsx
@@ -9,14 +9,12 @@ import { KanHai } from "../src";
 import { data } from "./data";
 
 ReactDOM.render(
-    <div className='editor' style={{ width: '300px', height: '200px' }}></div>,
+    <div className='editor' style={{ width: '500px', height: '200px' }}></div>,
     document.body.appendChild(document.createElement('div')),
     async () => {
         var ele = document.querySelector('.editor') as HTMLElement;
         var page = new KanHai.Page(ele);
-        console.log(data);
         await page.load(data);
-        console.log(page);
         await page.render();
     }
 );
