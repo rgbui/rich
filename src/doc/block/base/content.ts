d39da541c3fcaad05243920b95a46cbeb81139f6
kanhai <760489033@qq.com>
2021-01-27 00:28:56 +0800

    fix Content display type

--- src/doc/block/base/content.ts
+++ src/doc/block/base/content.ts
@@ -4,11 +4,11 @@ import { BlockComposition } from "./composition/block";
 /***
  * 内容型的block
  */
-export class Content extends   BlockComposition{
+export class Content extends BlockComposition {
     /***
      * 是否是行block,就是独占一行
      */
-    isPerLine: boolean;
+    display: 'inline' | 'inline-block';
     /***
      * 指内容的摆放位置
      */
