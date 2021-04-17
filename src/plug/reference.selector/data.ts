51f5580030fd8421d4790214edaf4c11175f5444
kanhai <760489033@qq.com>
2021-04-17 22:29:46 +0800

    feat reference block selector

---
+++ src/plug/reference.selector/data.ts
@@ -0,0 +1,11 @@
+
+
+export var ReferenceSelectorData = [
+    {
+        text: '基本',
+        childs: [
+            { text: '文本', pic: '', url: '/textspan', description: '文本', label: '/文本', labels: [] },
+            { text: '大标题', pic: '', url: '/head1', description: '文本', label: '/文本', labels: [] }
+        ]
+    }
+]
---
+++ src/plug/reference.selector/index.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { createPortal } from "react-dom";
+import { Point } from "../../common/point";
+import { ReferenceSelectorData } from "./data";
+
+export class ReferenceSelector extends React.Component {
+    private node: HTMLElement;
+    constructor(props) {
+        super(props);
+        this.node = document.body.appendChild(document.createElement('div'));
+    }
+    renderSelectors() {
+        return ReferenceSelectorData.map(group => {
+            return <div className='sy-reference-selector-group' key={group.text}>
+                <div className='sy-reference-selector-head'>{group.text}</div>
+                <div className='sy-reference-selector-blocks'>{
+                    group.childs.map(child => {
+                        return <div className='sy-reference-selector-block' key={child.url}>
+                            <div className='sy-reference-selector-info'>
+                                <span>{child.text}</span>
+                                <em>{child.description}</em>
+                            </div>
+                            <label>{child.label}</label>
+                        </div>
+                    })
+                }</div>
+            </div>
+        })
+    }
+    render() {
+        var style: Record<string, any> = {
+            top: this.pos.y,
+            left: this.pos.x
+        }
+        return createPortal(<div>
+            {this.visible && <div className='sy-reference-selector' style={style}>{this.renderSelectors()}</div>}
+        </div>, this.node);
+    }
+    private visible: boolean = false;
+    private pos: Point = new Point(0, 0);
+    private command: string = '';
+    private selectIndex: number = 0;
+    get isVisible() {
+        return this.visible;
+    }
+    open(point: Point) {
+        this.pos = point;
+        this.visible = true;
+        this.forceUpdate();
+    }
+    onInputFilter(text: string) {
+        var cs = text.match(/@[^\s]+$/g);
+        var command = cs[0];
+        if (command) {
+            this.command = command;
+            this.forceUpdate();
+        }
+        else {
+            this.command = '';
+            this.close();
+        }
+    }
+    get selectBlockData() {
+        return null;
+    }
+    close() {
+        this.visible = false;
+        this.forceUpdate();
+    }
+    /**
+     * 向上选择内容
+     */
+    keydown() {
+        this.selectIndex -= 1;
+    }
+    /**
+     * 向下选择内容
+     */
+    keyup() {
+        this.selectIndex += 1;
+    }
+}
