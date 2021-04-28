dcb9bbd1b1c044290d30e215dbd3365c6cd6a579
kanhai <760489033@qq.com>
2021-04-28 09:26:20 +0800

    fix 文本选择器

--- src/plug/text.menu/text.tool.tsx
+++ src/plug/text.menu/text.tool.tsx
@@ -33,16 +33,16 @@ export class TextTool extends React.Component<{ page: Page }>{
         return createPortal(
             <div>
                 {this.visible == true && <div className='sy-tool-text-menu' style={style}>
-                    <div className='sy-tool-text-menu-item sy-tool-text-menu-devide'><span>Text</span><Icon icon='arrow-down:sy'></Icon></div>
-                    <div className='sy-tool-text-menu-item sy-tool-text-menu-devide'><Icon icon='link:sy'></Icon><Icon icon='arrow-down:sy'></Icon></div>
-                    <div className='sy-tool-text-menu-item sy-tool-text-menu-devide'><Icon icon='comment:sy'></Icon></div>
-                    <div className='sy-tool-text-menu-item'><Icon icon='bold:sy'></Icon></div>
-                    <div className='sy-tool-text-menu-item'><Icon icon='italic:sy'></Icon></div>
-                    <div className='sy-tool-text-menu-item'><Icon icon='underline:sy'></Icon></div>
-                    <div className='sy-tool-text-menu-item'><Icon icon='delete-line:sy'></Icon></div>
-                    <div className='sy-tool-text-menu-item'><Icon icon='code:sy'></Icon></div>
-                    <div className='sy-tool-text-menu-item'><Icon icon={Equation}></Icon></div>
-                    <div className='sy-tool-text-menu-item'><span>A</span><Icon icon='arrow-down:sy'></Icon></div>
+                    <div className='sy-tool-text-menu-item sy-tool-text-menu-devide' onClick={e => this.onOpenBlockSelector()}><span>Text</span><Icon icon='arrow-down:sy'></Icon></div>
+                    <div className='sy-tool-text-menu-item sy-tool-text-menu-devide' onClick={e => this.onOpenLink()}><Icon icon='link:sy'></Icon><Icon icon='arrow-down:sy'></Icon></div>
+                    <div className='sy-tool-text-menu-item sy-tool-text-menu-devide' onClick={e => this.onOpenComment()}><Icon icon='comment:sy'></Icon></div>
+                    <div className='sy-tool-text-menu-item' onClick={e => this.onExcute('bold')}><Icon icon='bold:sy'></Icon></div>
+                    <div className='sy-tool-text-menu-item' onClick={e => this.onExcute('italic')}><Icon icon='italic:sy'></Icon></div>
+                    <div className='sy-tool-text-menu-item' onClick={e => this.onExcute('underline')}><Icon icon='underline:sy'></Icon></div>
+                    <div className='sy-tool-text-menu-item' onClick={e => this.onExcute('delete-line')}><Icon icon='delete-line:sy'></Icon></div>
+                    <div className='sy-tool-text-menu-item' onClick={e => this.onExcute('code')}><Icon icon='code:sy'></Icon></div>
+                    <div className='sy-tool-text-menu-item' onClick={e => this.onExcute('equation')}><Icon icon={Equation}></Icon></div>
+                    <div className='sy-tool-text-menu-item' onClick={e => this.onOpenFontColor()}><span>A</span><Icon icon='arrow-down:sy'></Icon></div>
                     <div className='sy-tool-text-menu-item'><Icon icon={Mention}></Icon></div>
                 </div>}
             </div>,
@@ -51,4 +51,20 @@ export class TextTool extends React.Component<{ page: Page }>{
     componentWillUnmount() {
         this.node.remove();
     }
+    onExcute(command: string) {
+
+    }
+    onOpenFontColor() {
+
+    }
+    onOpenLink() {
+
+    }
+    onOpenComment() {
+
+    }
+    onOpenBlockSelector() {
+
+    }
+
 }
