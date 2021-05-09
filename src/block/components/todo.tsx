b070e99350757bba05b0ec707bf2f1bc337e37be
kanhai <760489033@qq.com>
2021-05-09 22:07:55 +0800

    实现todo申明切换选中状态的不同样式

--- src/block/components/todo.tsx
+++ src/block/components/todo.tsx
@@ -5,32 +5,44 @@ import { TextSpan } from "./textspan";
 
 import { observable, prop, url, view } from "../factory/observable";
 import { ChildsArea, TextArea } from "../base/appear";
+import { BlockCssName, FontCss } from "../pattern/css";
+import { CssSelectorType } from "../pattern/type";
+import { BlockRenderRange } from "../base/enum";
+
 @url('/todo')
 export class ToDo extends TextSpan {
+    init() {
+        this.pattern.declare<FontCss>('checked', CssSelectorType.pseudo, {
+            cssName: BlockCssName.font,
+            textDecoration: 'line-through'
+        });
+    }
     @prop()
     checked: boolean = false;
     @observable('onChange')
     onChange(event: Event) {
         var input = event.target as HTMLInputElement;
-        //this.setState({ checked: input.checked });
+        this.onUpdateProps({ checked: input.checked }, BlockRenderRange.self);
+    }
+    get patternState() {
+        if (this.checked == true) return 'checked';
+        return 'default';
     }
 }
 @view('/todo')
 export class ToDoView extends BaseComponent<ToDo>{
     render() {
-        var style: Record<string, any> = {
-            textDecoration: this.block.checked ? 'line-through' : 'none'
-        }
+
         if (this.block.childs.length > 0) {
             return <span className='sy-block-todo' style={this.block.visibleStyle}>
                 <input onMouseDown={e => e.nativeEvent.stopPropagation()} type='checkbox' checked={this.block.checked} onChange={e => this.block.onChange(e.nativeEvent)} />
-                <span style={style} ref={e => this.block.childsEl = e}><ChildsArea childs={this.block.childs}></ChildsArea></span>
+                <span ref={e => this.block.childsEl = e}><ChildsArea childs={this.block.childs}></ChildsArea></span>
             </span>
         }
         else {
             return <span className='sy-block-todo' style={this.block.visibleStyle}>
-                <input type='checkbox' checked={this.block.checked} onChange={e => this.block.onChange(e.nativeEvent)} />
-                <span style={style}> <TextArea html={this.block.htmlContent}></TextArea></span>
+                <input onMouseDown={e => e.nativeEvent.stopPropagation()} type='checkbox' checked={this.block.checked} onChange={e => this.block.onChange(e.nativeEvent)} />
+                <span > <TextArea html={this.block.htmlContent}></TextArea></span>
             </span>
         }
     }
