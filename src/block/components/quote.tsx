8dce137bf0511e3187b0d2720c07f948d5071bc2
kanhai <760489033@qq.com>
2021-06-02 09:31:42 +0800

    完善quote

--- src/block/components/quote.tsx
+++ src/block/components/quote.tsx
@@ -1,7 +1,6 @@
 import { BaseComponent } from "../base/component";
 import React from 'react';
 import { url, view } from "../factory/observable";
-import { Content } from "../base/common/content";
 import { TextSpan } from "./textspan";
 import { TextArea } from "../base/appear";
 @url('/quote')
@@ -11,13 +10,13 @@ export class Quote extends TextSpan {
 @view('/quote')
 export class QuoteView extends BaseComponent<Quote>{
     render() {
-          return <div className='sy-block-quote'>
-        {this.block.childs.length > 0 && <span className='sy-appear-text-line' style={this.block.visibleStyle} ref={e => this.block.childsEl = e}>{this.block.childs.map(x =>
-            <x.viewComponent key={x.id} block={x}></x.viewComponent>
-        )}</span>}
-        {this.block.childs.length == 0 && <span className='sy-appear-text-line' style={this.block.visibleStyle}>
-            <TextArea html={this.block.htmlContent} placeholder={'键入文字或"/"选择'}></TextArea>
-        </span>}
-    </div>
+        return <div className='sy-block-quote'>
+            {this.block.childs.length > 0 && <span className='sy-appear-text-line' style={this.block.visibleStyle} ref={e => this.block.childsEl = e}>{this.block.childs.map(x =>
+                <x.viewComponent key={x.id} block={x}></x.viewComponent>
+            )}</span>}
+            {this.block.childs.length == 0 && <span className='sy-appear-text-line' style={this.block.visibleStyle}>
+                <TextArea html={this.block.htmlContent} placeholder={'键入文字或"/"选择'}></TextArea>
+            </span>}
+        </div>
     }
 }
