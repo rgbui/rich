f7f493f4abbc41a0d31b4772bcdd01b8373c2e65
kanhai <760489033@qq.com>
2021-06-19 23:48:51 +0800

    调整导入index的引用

--- index.ts
+++ index.ts
@@ -1,16 +1,14 @@
 
 import "./src/util/array";
-
-
 import "./src/assert/theme.less";
 
-import "./src/kit/style.less";
+
 import "./src/component/style.less";
-import "./src/page/style.less";
 import './src/assert/font/iconfont.css';
 
 import { Page } from "./src/page";
 import './blocks/declare';
+import "./src/block/declare";
 import "./src/extensions/declare";
 /***
  * import components
