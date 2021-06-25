7c6f6ee55f0bf1f412eab7bdc433efa2a26e96bc
kanhai <760489033@qq.com>
2021-06-25 22:22:09 +0800

    调整简单表格单元格类似于col，不会直接存文本

--- blocks/table/cell.tsx
+++ blocks/table/cell.tsx
@@ -34,14 +34,10 @@ export class TableCellView extends BlockView<TableCell>{
         var style: Record<string, any> = {
 
         };
-        if (this.block.childs.length > 0)
-            return <td style={style}
-                rowSpan={this.block.rowspan == 1 ? undefined : this.block.rowspan}
-                colSpan={this.block.colspan == 1 ? undefined : this.block.colspan}
-                ref={e => this.block.childsEl = e}
-            ><ChildsArea childs={this.block.childs}></ChildsArea></td>
-        else return <td style={style} rowSpan={this.block.rowspan} colSpan={this.block.colspan}>
-            <TextArea html={this.block.htmlContent}></TextArea>
-        </td>
+        return <td style={style}
+            rowSpan={this.block.rowspan == 1 ? undefined : this.block.rowspan}
+            colSpan={this.block.colspan == 1 ? undefined : this.block.colspan}
+            ref={e => this.block.childsEl = e}
+        ><ChildsArea childs={this.block.childs}></ChildsArea></td>
     }
 }
