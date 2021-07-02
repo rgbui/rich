9c66963be47cc3e34a992ed6517cf6ba354dae8a
rgbui <760489033@qq.com>
2021-07-02 19:42:54 +0800

    dom parse create block

---
+++ src/kit/input/dom.block.ts
@@ -0,0 +1,10 @@
+export function DomBlock(doc: Document) {
+        /**
+         * 
+         * find img video autio 
+         * ignore input,select,textarea
+         * 
+         * key tags title hr  h1~h6  pre  ol-ul-li table b del
+         * https://zhuanlan.zhihu.com/p/81132589
+         */
+}
