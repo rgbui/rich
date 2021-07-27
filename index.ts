796373305c229ed9c057383bbffa3a6817c561d3
kanhai <760489033@qq.com>
2021-07-27 23:23:27 +0800

    调整rich的index申明结构

--- index.ts
+++ index.ts
@@ -3,11 +3,10 @@ import "./util/array";
 import "./src/assert/theme.less";
 import "./component/style.less";
 import './src/assert/font/iconfont.css';
-import { Page } from "./src/page";
 import './blocks/declare';
 import "./src/block/declare";
 import "./extensions/declare";
-export var SY = {
-    Page
-}
+
+
+import "./src/page/index";
 
