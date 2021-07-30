6ee0799fd799d2e6dccf28c67339d8c3752f434c
rgbui <760489033@qq.com>
2021-07-30 16:35:17 +0800

    新增confirm确认框

---
+++ component/confirm.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Singleton } from "../extensions/Singleton";
+import { SyExtensionsComponent } from "../extensions/sy.component";
+import { Button } from "./button";
+class SyConfirm extends SyExtensionsComponent {
+    private msg: string;
+    private description: string;
+    private visible: boolean = false;
+    open(msg: string, description?: string) {
+        this.msg = msg;
+        this.description = description;
+        this.visible = true;
+        this.forceUpdate();
+    }
+    close() {
+        this.visible = false;
+        this.forceUpdate()
+    }
+    onConfirm() {
+        this.emit('confirm');
+        this.close()
+    }
+    onCancel() {
+        this.emit('cancel');
+        this.close()
+    }
+    render() {
+        return this.visible && <div className='shy-confirm-box'>
+            <div className='shy-confirm-mask' onMouseDown={e => this.onCancel()}></div>
+            <div className='shy-confirm'>
+                <div className='syh-confirm-msg'>{this.msg}</div>
+                {this.description && <div className='syh-confirm-description'>{this.description}</div>}
+                <div className='syh-confirm-buttons'>
+                    <Button onClick={e => this.onConfirm()}>保存</Button>
+                    <Button onClick={e => this.onCancel()} ghost>取消</Button>
+                </div>
+            </div>
+        </div>
+    }
+}
+var syConfirm: SyConfirm;
+export async function Confirm(msg: string) {
+    syConfirm = await Singleton<SyConfirm>(SyConfirm);
+    return new Promise((resolve, reject) => {
+        syConfirm.only('confirm', () => resolve(true))
+        syConfirm.only('cancel', () => resolve(false))
+    })
+}
