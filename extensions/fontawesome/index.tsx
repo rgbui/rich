02ede3ab8f7e85600a561ff1b53767bfc7f44ab2
kanhai <760489033@qq.com>
2021-08-04 09:47:32 +0800

    新增fontawesome

---
+++ extensions/fontawesome/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export class FontaweSomeView extends React.Component<{ change: (data: { code: string, color?: string }) => void }> {
+
+    render() {
+        return <div></div>
+    }
+}
