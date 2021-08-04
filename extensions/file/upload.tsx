9b63ce216760877878d31ffc0949e0e04e9348be
kanhai <760489033@qq.com>
2021-08-04 09:47:07 +0800

    新增文件上传

--- extensions/file/upload.tsx
+++ extensions/file/upload.tsx
@@ -1,11 +1,34 @@
 import React from "react";
 import { Button } from "../../component/button";
+import { OpenFileDialoug } from "../../component/file";
+import { Sp } from "../../i18n";
+import { LangID } from "../../i18n/declare";
+import { Directive } from "../../util/bus/directive";
+import { bus } from "../../util/bus/event.bus";
+export class UploadView extends React.Component<{ mine: 'image' | 'file', change: (url: string) => void }> {
+    async uploadFile() {
+        var file = await OpenFileDialoug();
+        if (file) {
+            var r = await bus.fireAsync(Directive.uploadFile, file, (event) => {
 
-export class Upload extends React.Component {
+            });
+            if (r.ok) {
+                if (r.data.url) {
+                    this.props.change(r.data.url);
+                }
+            }
+        }
+    }
     render() {
+        var id = LangID.UploadFile;
+        if (this.props.mine == 'image') id = LangID.UploadImage
         return <div className='shy-upload'>
-            <Button block>上传文件</Button>
-            <div className='shy-upload-remark'></div>
+            <div className='shy-upload-remark'>
+                <Sp id={LangID.UploadRemark}></Sp>
+            </div>
+            <Button block onClick={e => this.uploadFile()}><Sp id={id}></Sp></Button>
+            <div className='shy-upload-error'>
+            </div>
         </div>
     }
 }
