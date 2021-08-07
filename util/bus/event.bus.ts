e26cc3d8d3af8894c07e8795e471dd6c9a3dd7f4
kanhai <760489033@qq.com>
2021-08-07 23:48:57 +0800

    eventBus增加only绑定事件，判断事件是否绑定has

--- util/bus/event.bus.ts
+++ util/bus/event.bus.ts
@@ -11,6 +11,10 @@ class EventBus {
     on(directive: Directive, action: (...args: any[]) => any) {
         this._events.push({ directive, action });
     }
+    only(directive: Directive, action: (...args: any[]) => any) {
+        this._events.removeAll(g => g.directive == directive);
+        this._events.push({ directive, action });
+    }
     fire(directive: Directive, ...args: any[]) {
         let es = this._events.filter(ev => ev.directive == directive);
         var rs: any[] = [];
@@ -34,18 +38,25 @@ class EventBus {
         if (es.length == 1) return rs[0]
         else return rs;
     }
+    has(directive: Directive) {
+        return this._events.exists(g => g.directive == directive)
+    }
 }
 /**
  * 订阅通知活动事件申明
  */
 interface EventBus {
     on(directive: Directive.UploadFile, handler: (file: File, uploadProgress?: (event: ProgressEvent) => void) => Promise<{ ok: boolean, data: { url: string } }>): void;
+    only(directive: Directive.UploadFile, handler: (file: File, uploadProgress?: (event: ProgressEvent) => void) => Promise<{ ok: boolean, data: { url: string } }>): void;
     fireAsync(directive: Directive.UploadFile, file: File, uploadProgress?: (event: ProgressEvent) => void): Promise<{ ok: boolean, data: { url: string } }>;
     on(directive: Directive.GalleryQuery, fn: (type: GalleryType, word: string) => Promise<OuterPic[]>): void;
+    only(directive: Directive.GalleryQuery, fn: (type: GalleryType, word: string) => Promise<OuterPic[]>): void;
     fireAsync(directive: Directive.GalleryQuery, type: GalleryType, word: string): Promise<OuterPic[]>;
     on(directive: Directive.UsersQuery, fn: () => Promise<User[]>): void;
+    only(directive: Directive.UsersQuery, fn: () => Promise<User[]>): void;
     fireAsync(directive: Directive.UsersQuery): Promise<User[]>;
     on(directive: Directive.PagesQuery, fn: (word: string) => Promise<LinkPage[]>): void;
+    only(directive: Directive.PagesQuery, fn: (word: string) => Promise<LinkPage[]>): void;
     fireAsync(directive: Directive.PagesQuery, word: string): Promise<LinkPage[]>;
 }
 export let richBus = new EventBus()
