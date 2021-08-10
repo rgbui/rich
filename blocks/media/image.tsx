36ee28035a0971575429f44e0d263f1095da3f1e
kanhai <760489033@qq.com>
2021-08-10 09:40:05 +0800

    完善图片block

--- blocks/media/image.tsx
+++ blocks/media/image.tsx
@@ -3,12 +3,16 @@ import { prop, url, view } from "../../src/block/factory/observable";
 import React from 'react';
 import { BlockAppear } from "../../src/block/enum";
 import { SolidArea } from "../../src/block/partial/appear";
-import { Content } from "../../src/block/element/content";
 import { Rect } from "../../src/common/point";
+import { ResourceArguments } from "../../extensions/icon/declare";
+import { Block } from "../../src/block";
+import ImageError from "../../src/assert/svg/imageError.svg";
+import { LangID } from "../../i18n/declare";
+import { Sp } from "../../i18n/view";
 @url('/image')
-export class Image extends Content {
+export class Image extends Block {
     @prop()
-    src: string;
+    src: ResourceArguments;
     appear = BlockAppear.solid;
     getVisibleContentBound() {
         var img = this.el.querySelector('.sy-block-image-content img');
@@ -17,20 +21,34 @@ export class Image extends Content {
 }
 @view('/image')
 export class ImageView extends BlockView<Image>{
+    errorUrl: string;
+    isLoadError: boolean = false;
+    onError(e: React.SyntheticEvent<HTMLImageElement, Event>) {
+        this.isLoadError = true;
+        this.errorUrl = this.block.src?.url;
+        this.forceUpdate();
+    }
     render() {
-        var align = this.block.align || 'center';
-        var contentStyle: Record<string, any> = {
-            width: (this.block.widthPercent || 100) + "%"
-        }
-        var style: Record<string, any> = {};
-        style.alignItems = align;
-        if (align == 'left') style.alignItems = 'flex-start'
-        else if (align == 'right') style.alignItems = 'flex-end'
-        return <div className='sy-block-image' style={style}>
-            <div className='sy-block-image-content' style={contentStyle}>
-                <SolidArea content={
-                    this.block.src && <img src={this.block.src} />
-                }></SolidArea>
+        return <div className='sy-block-image' style={this.block.visibleStyle} >
+            <div className='sy-block-image-content' >
+                {this.isLoadError && <div style={{ background: 'rgb(242, 241, 238)' }} className='sy-block-image-error'>
+                    <ImageError></ImageError>
+                    <p style={{
+                        fontSize: '14px',
+                        lineHeight: '20px',
+                        color: 'rgba(55, 53, 47, 0.6)',
+                        margin: '0px 0px 14px'
+                    }}><Sp id={LangID.imageErrorLoadTip}></Sp></p>
+                    <p><a><Sp id={LangID.learnMore}></Sp></a></p>
+                    <p style={{
+                        fontSize: '12px',
+                        lineHeight: '16px',
+                        color: 'rgba(55, 53, 47, 0.4)'
+                    }}>{this.errorUrl}</p>
+                </div>}
+                {!this.isLoadError && <SolidArea content={
+                    this.block.src && <img onError={e => this.onError(e)} src={this.block?.src?.url} />
+                }></SolidArea>}
             </div>
         </div>
     }
