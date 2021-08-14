b66262de74bdbf9d6808897b4f30592b6597f01f
kanhai <760489033@qq.com>
2021-08-15 00:45:16 +0800

    增加组件loading

---
+++ component/loading.tsx
@@ -0,0 +1,8 @@
+import React, { CSSProperties } from "react";
+import lg from "../src/assert/img/loading.gif";
+export function Loading(props: { children?: React.ReactNode, style?: CSSProperties }) {
+    return <div className='shy-loading' style={props.style || {}}>
+        <img src={lg}></img>
+        <div className='shy-loading-content'>{props.children}</div>
+    </div>
+}
