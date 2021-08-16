43d6cdfe0f8904a619127ee42b197f04e24c9090
kanhai <760489033@qq.com>
2021-08-16 08:46:38 +0800

    调整audioPicker

--- extensions/file/audio.picker.tsx
+++ extensions/file/audio.picker.tsx
@@ -1,80 +1,32 @@
-import React, { CSSProperties } from "react";
+import React from "react";
 import { Tip } from "../../component/tip";
 import { LangID } from "../../i18n/declare";
-import { Point,  Rect, RectUtility } from "../../src/common/point";
 import { OutsideUrl } from "../link/outside";
 import { EventsComponent } from "../events.component";
 import { UploadView } from "./upload";
 import Link from "../../src/assert/svg/link.svg";
 import Upload from "../../src/assert/svg/upload.svg";
 import { ResourceArguments } from "../icon/declare";
-import { Singleton } from "../Singleton";
+
 import { PopoverPosition } from "../popover/position";
+import { PopoverSingleton } from "../popover/popover";
+import { Tab } from "../../component/tab";
+import { Icon } from "../../component/icon";
 class AudioPicker extends EventsComponent {
-    point: Point = new Point(0, 0);
-    visible: boolean = false;
-    mode: 'upload' | 'link' = 'upload';
-    private el: HTMLElement;
-    open(pos: PopoverPosition) {
-        this.visible = true;
-        this.point = pos.roundArea.leftTop;
-        this.forceUpdate(() => {
-            if (this.el) {
-                var b = Rect.from(this.el.getBoundingClientRect());
-                pos.elementArea = b;
-                var newPoint = RectUtility.cacPopoverPosition(pos);
-                if (!this.point.equal(newPoint)) {
-                    this.point = newPoint;
-                    this.forceUpdate();
-                }
-            }
-        })
-    }
-    onChangeMode(mode: AudioPicker['mode']) {
-        this.mode = mode;
-        this.forceUpdate()
-    }
-    onChange(mode: AudioPicker['mode'], data: any) {
-        this.emit('select', { name: mode, ...data });
-        this.close();
-    }
-    close() {
-        this.visible = false;
-        this.forceUpdate();
-    }
-    onClose() {
-        this.close();
-        this.emit('close');
-    }
-    private _mousedown: (event: MouseEvent) => void;
-    componentDidMount() {
-        document.addEventListener('mousedown', this._mousedown = this.globalMousedown.bind(this), true);
-    }
-    componentWillUnmount() {
-        if (this._mousedown) document.removeEventListener('mousedown', this._mousedown, true);
-    }
-    private globalMousedown(event: MouseEvent) {
-        var target = event.target as HTMLElement;
-        if (this.el?.contains(target)) return;
-        if (this.visible == true) {
-            this.onClose()
-        }
+    onChange(data: any)
+    {
+        this.emit('select', { ...data });
     }
     render() {
-        var style: CSSProperties = {
-            top: this.point.y,
-            left: this.point.x
-        }
-        return <div ref={e => this.el = e}>{this.visible && <div className='shy-audio-picker' style={style}>
-            <div className='shy-audio-picker-head'>
-                <Tip id={LangID.IconUpload}><a onMouseDown={e => this.onChangeMode('upload')}><Upload></Upload></a></Tip>
-                <Tip id={LangID.IconLink}><a onMouseDown={e => this.onChangeMode('link')}><Link></Link></a></Tip>
-            </div>
-            <div className='shy-audio-picker-content'>
-                {this.mode == 'upload' && <UploadView mine='audio' change={e => this.onChange(this.mode, { url: e })}></UploadView>}
-                {this.mode == 'link' && <OutsideUrl change={e => this.onChange(this.mode, { url: e })}></OutsideUrl>}
-            </div>
-        </div>}
+        return <div className='shy-audio-picker'>
+            <Tab keeplive>
+                <Tab.Page item={<Tip placement='bottom' id={LangID.UploadAudio}><Icon size={30} icon={Upload}></Icon></Tip>}>
+                    <UploadView mine='file' change={e => this.onChange({ name: 'upload', url: e })}></UploadView>
+                </Tab.Page>
+                <Tab.Page item={<Tip placement='bottom' id={LangID.IconLink}><Icon size={30} icon={Link}></Icon></Tip>}>
+                    <OutsideUrl change={e => this.onChange({ name: 'link', url: e })}></OutsideUrl>
+                </Tab.Page>
+            </Tab>
         </div>
     }
 }
@@ -82,18 +34,17 @@ class AudioPicker extends EventsComponent {
 interface AudioPicker {
     only(name: 'select', fn: (data: ResourceArguments) => void);
     emit(name: 'select', data: ResourceArguments);
-    only(name: 'close', fn: () => void);
-    emit(name: 'close');
 }
 
 export async function useAudioPicker(pos: PopoverPosition) {
-    let audioPicker = await Singleton<AudioPicker>(AudioPicker);
-    audioPicker.open(pos);
+    let popover = await PopoverSingleton(AudioPicker);
+    let audioPicker = await popover.open<AudioPicker>(pos);
     return new Promise((resolve: (data: ResourceArguments) => void, reject) => {
         audioPicker.only('select', (data) => {
+            popover.close();
             resolve(data);
         })
-        audioPicker.only('close', () => {
+        popover.only('close', () => {
             resolve(null)
         })
     })
