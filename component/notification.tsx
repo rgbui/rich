8c90a8b18a88ec70f525a619bffe8b5fc5b6f1fa
kanhai <760489033@qq.com>
2021-08-21 23:58:34 +0800

    新增消息通知组件

---
+++ component/notification.tsx
@@ -0,0 +1,14 @@
+import { EventsComponent } from "./events.component";
+class Notify extends EventsComponent {
+
+}
+
+export enum NotifyType {
+    warn,
+    error,
+    info
+}
+
+export async function useOpenNotify() {
+
+}
