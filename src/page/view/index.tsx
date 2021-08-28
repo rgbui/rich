e909241260dfcd4391d1f11f81f15663e38bae7e
kanhai <760489033@qq.com>
2021-08-28 21:31:10 +0800

    page view 目录调整

--- src/page/view/index.tsx
+++ src/page/view/index.tsx
@@ -1,14 +1,14 @@
 import { Component } from "react";
 import React from 'react';
-import { Page } from "./index";
-import { PageLayoutView } from "../layout/view";
+import { Page } from "../index";
+import { PageLayoutView } from "../../layout/view";
 
-import { ChildsArea } from "../block/partial/appear";
-import { BlockSelector } from "../../extensions/block";
-import { AtSelector } from "../../extensions/at";
-import { TextTool } from "../../extensions/text.tool/text.tool";
+import { ChildsArea } from "../../block/view/appear";
+import { BlockSelector } from "../../../extensions/block";
+import { AtSelector } from "../../../extensions/at";
+import { TextTool } from "../../../extensions/text.tool/text.tool";
 import ReactDOM from "react-dom";
-import { KitView } from "../kit/view";
+import { KitView } from "../../kit/view";
 /**
  * mousedown --> mouseup --> click --> mousedown --> mouseup --> click --> dblclick
  * 对于同时支持这4个事件的浏览器，事件执行顺序为focusin > focus > focusout > blur
