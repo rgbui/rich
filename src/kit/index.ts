4d95da509e67123915ad3d443e974607eb44f211
kanhai <760489033@qq.com>
2021-08-28 21:32:36 +0800

    新增PageMouse在kit中，用来管理对页面文档的鼠标操作事件

--- src/kit/index.ts
+++ src/kit/index.ts
@@ -10,6 +10,7 @@ import { SelectionExplorer } from "./selection/explorer";
 import { Selector } from "./selector";
 import { KitView } from "./view";
 import "./style.less";
+import { PageMouse } from "./mouse";
 export class Kit extends Events {
     page: Page;
     constructor(page: Page) {
@@ -21,114 +22,15 @@ export class Kit extends Events {
         this.selector = new Selector(this);
         this.explorer = new SelectionExplorer(this);
         this.textInput = new TextInput(this);
+        this.mouse = new PageMouse(this);
         this.bar = new Bar(this);
     }
+    mouse: PageMouse;
     textInput: TextInput;
     selector: Selector;
     explorer: SelectionExplorer;
     bar: Bar;
-    private isDown: boolean = false;
-    private isMove: boolean = false;
-    private downEvent: MouseEvent;
-    private downAnchor: Anchor;
-    get isMousedown() {
-        return this.isDown;
-    }
     view: KitView;
-    private lastMouseupDate: number;
-    private lastMouseupEvent: MouseEvent;
-    acceptMousedown(event: MouseEvent) {
-        var block = this.page.getVisibleBlockByMouse(event);
-        this.downEvent = event;
-        this.isDown = true;
-        if (block && block.isSupportAnchor)
-        {
-            var anchor = block.visibleAnchor(Point.from(this.downEvent));
-            if (anchor) {
-                this.downAnchor = anchor;
-                if (this.page.keyboardPlate.isShift() && this.explorer.isOnlyAnchor) {
-                    if (this.page.isInLineBlock(this.downAnchor.block, this.explorer.activeAnchor.block))
-                        this.explorer.onShiftFocusAnchor(this.downAnchor);
-                    else {
-                        var blocks = this.page.searchBlocksBetweenAnchor(this.explorer.activeAnchor, this.downAnchor, { rowOrCol: true, lineBlock: true });
-                        if (Array.isArray(blocks) && blocks.length > 0) {
-                            this.explorer.onSelectBlocks(blocks);
-                        }
-                    }
-                }
-                else
-                    this.explorer.onFocusAnchor(this.downAnchor);
-            }
-        }
-        if (!this.downAnchor) this.explorer.onCancelSelection();
-    }
-    acceptMousemove(event: MouseEvent) {
-        var ele = event.target as HTMLElement;
-        if (this.isDown == true) {
-            var downPoint = Point.from(this.downEvent);
-            if (downPoint.remoteBy(Point.from(event), 5)) {
-                this.isMove = true;
-                if (!this.downAnchor) this.selector.setStart(downPoint)
-            }
-            if (this.isMove == true) {
-                if (!this.downAnchor) {
-                    this.selector.setMove(Point.from(event));
-                    var blocks = this.page.searchBlocksBetweenMouseRect(this.downEvent, event, { lineBlock: true });
-                    if (Array.isArray(blocks) && blocks.length > 0) {
-                        this.explorer.onSelectBlocks(blocks);
-                    }
-                }
-                else {
-                    var block = this.page.getVisibleBlockByMouse(event);
-                    if (block && !block.isLayout) {
-                        var anchor = block.visibleAnchor(Point.from(event));
-                        if (anchor) {
-                            if (this.page.isInLineBlock(this.downAnchor.block, anchor.block)) {
-                                this.explorer.onShiftFocusAnchor(anchor);
-                            }
-                            else {
-                                var blocks = this.page.searchBlocksBetweenMouseRect(this.downEvent, event, { lineBlock: true });
-                                if (Array.isArray(blocks) && blocks.length > 0) {
-                                    this.explorer.onSelectBlocks(blocks);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        //判断当前的ele是否在bar自已本身内
-        if (this.bar.barEle.contains(ele)) return;
-        var hoverBlock;
-        if (this.page.el.contains(ele)) {
-            var block = this.page.searchBlockByMouse(event);
-            if (block && !block.isRow && !block.isCol) hoverBlock = block;
-        }
-        this.bar.onHoverBlock(hoverBlock, event);
-    }
-    acceptMouseup(event: MouseEvent) {
-        if (this.isDown) {
-            this.emit('mouseup', event);
-            if (this.isMove) {
-                if (!this.downAnchor) this.selector.close();
-                this.isMove = false;
-            }
-            delete this.downAnchor;
-            delete this.downEvent;
-            this.isDown = false;
-            if (this.explorer.isOnlyAnchor && this.explorer.activeAnchor.isText && this.lastMouseupDate && Date.now() - this.lastMouseupDate < 700) {
-                if (this.lastMouseupEvent && Point.from(this.lastMouseupEvent).nearBy(Point.from(event), 0)) {
-                    var block = this.explorer.activeAnchor.block;
-                    var contentRowBlock = block.closest(x => !x.isLine);
-                    this.explorer.onSelectBlocks([contentRowBlock]);
-                }
-            }
-            this.lastMouseupEvent = event;
-            this.lastMouseupDate = Date.now();
-            if (this.explorer.isOnlyAnchor)
-                this.textInput.onFocus();
-        }
-    }
 }
 
 export interface Kit {
