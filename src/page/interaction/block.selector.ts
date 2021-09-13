2b7dd743cff12f655df592227df9478dd4f49e09
kanhai <760489033@qq.com>
2021-09-13 22:35:35 +0800

    移除blockSelector

--- src/page/interaction/block.selector.ts
+++
@@ -1,8 +0,0 @@
-import { Page } from "..";
-import { BlockSelector } from "../../../extensions/block";
-export function PageBlockSelector(page: Page, selector: BlockSelector) {
-    selector.on('error', err => page.onError(err));
-    selector.on('select', (blockData, value) => {
-        page.kit.textInput.onBlockSelectorInsert(blockData, value);
-    });
-}
--- src/page/interaction/detector.ts
+++
@@ -1,8 +0,0 @@
-import { Page } from "..";
-import { InputDetector } from "../../../extensions/input.detector/detector";
-
-export function PageInputDetector(page: Page, detector: InputDetector) {
-    detector.on('input', (rule, value, lastValue) => {
-        page.kit.textInput.onInputDetector(rule, value, lastValue);
-    });
-}
