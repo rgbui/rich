cbbbf6a4317d694d5c14bd04ecf4203369988491
kanhai <760489033@qq.com>
2021-09-17 00:02:52 +0800

    调整block回退删除处理

---
+++ src/kit/input/handle/back.delete.ts
@@ -0,0 +1,107 @@
+import { TextInput } from "..";
+import { dom } from "../../../common/dom";
+import { KeyboardCode } from "../../../common/keys";
+import { Exception, ExceptionType } from "../../../error/exception";
+import { Anchor } from "../../selection/anchor";
+import { InputDeleteStore } from "../store";
+export async function backspaceDeleteHandle(tp: TextInput) {
+    var anchor = tp.explorer.activeAnchor;
+    anchor.inputting();
+    if (await backspaceSolidBlock(tp)) return;
+    else if (await backspaceCrossBlock(tp)) return;
+    else if (await backspaceBlock(tp)) return;
+}
+
+export async function backspaceSolidBlock(tp: TextInput) {
+    var anchor = tp.explorer.activeAnchor;
+    if (anchor.isSolid) {
+        var block = anchor.block;
+        tp.kit.explorer.onCursorMove(KeyboardCode.ArrowLeft);
+        tp.onStartInput(tp.explorer.activeAnchor);
+        await block.onDelete()
+        return true;
+    }
+}
+
+export async function backspaceCrossBlock(tp: TextInput) {
+    var anchor = tp.explorer.activeAnchor;
+    if (anchor.isText && anchor.at == 0) {
+        var block = anchor.block;
+        var prevAnchor = block.visiblePrevAnchor;
+        if (prevAnchor) {
+            if (tp.kit.page.textAnchorIsAdjoin(anchor, prevAnchor)) {
+                tp.explorer.onFocusAnchor(prevAnchor);
+                tp.onStartInput(tp.explorer.activeAnchor);
+                await backspaceDeleteHandle(tp);
+            }
+            else {
+                tp.kit.explorer.onCursorMove(KeyboardCode.ArrowLeft);
+                tp.onStartInput(tp.explorer.activeAnchor);
+                if (block.isCanAutomaticallyDeleted) await block.onDelete()
+                else {
+                    /**
+                     * 这里判断是否为跨行
+                     * 如果跨行，那么此时block所在的行的内容是否合并到新行中
+                     ***/
+                }
+            }
+        }
+        else {
+            /**
+             * 此时光标无法再回退了
+             * 这应该是文档的第一行首位元素
+             */
+            tp.page.onBackspaceToTopPage();
+        }
+        return true;
+    }
+}
+export async function backspaceBlock(tp: TextInput) {
+    var anchor = tp.explorer.activeAnchor;
+    if (anchor.isText && anchor.at > 0) {
+        var block = anchor.block;
+        var dm = dom(anchor.view);
+        var textNode = dm.prevFind(g => {
+            if (g instanceof Text) return true;
+            else return false;
+        }) as Text;
+        if (textNode) {
+            var value = textNode.textContent;
+            tp.deleteInputText = value.slice(value.length - 1) + tp.deleteInputText;
+            textNode.textContent = value.slice(0, value.length - 1);
+            anchor.at -= 1;
+            if (textNode.textContent.length == 0) {
+                dom(textNode).removeEmptyNode();
+            }
+            var action: () => Promise<void>;
+            if (anchor.at == 0) {
+                action = async () => {
+                    var existsDelete: boolean = false;
+                    if (anchor.block.isLine) {
+                        var newAnchor: Anchor;
+                        if (block.prev) newAnchor = block.prev.visibleBackAnchor;
+                        else if (block.next) newAnchor = block.next.visibleHeadAnchor;
+                        var pa = block.parent;
+                        if (block.isCanAutomaticallyDeleted) { await block.delete(); existsDelete = true; }
+                        if (!newAnchor) newAnchor = pa.visibleHeadAnchor;
+                        tp.explorer.onFocusAnchor(newAnchor);
+                        tp.onStartInput(tp.explorer.activeAnchor);
+                    }
+                    var checkEmpty = () => {
+                        var currentAnchor = tp.explorer.activeAnchor;
+                        tp.explorer.onFocusAnchor(currentAnchor);
+                        if (currentAnchor.at == 0 && currentAnchor.elementAppear.isEmpty) currentAnchor.setEmpty();
+                        else currentAnchor.removeEmpty();
+                    }
+                    if (existsDelete == true) tp.page.onUpdated(async () => {
+                        checkEmpty();
+                    })
+                    else checkEmpty();
+                }
+            }
+            await InputDeleteStore(block, anchor.elementAppear, tp.cursorStartAt, tp.deleteInputText, anchor.at == 0 ? true : false, action);
+            tp.followAnchor(tp.explorer.activeAnchor);
+        } else throw new Exception(ExceptionType.notFoundTextEle);
+        return true;
+    }
+}
