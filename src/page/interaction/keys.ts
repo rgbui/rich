f4a5e99af8dc5ee813a50851d15e11bb3836c2bc
kanhai <760489033@qq.com>
2021-09-27 23:58:55 +0800

    增加ctrl+Y的浏览器的默认效果

--- src/page/interaction/keys.ts
+++ src/page/interaction/keys.ts
@@ -2,14 +2,12 @@ import { Page } from "..";
 import { KeyboardCode, KeyboardPlate } from "../../common/keys";
 
 export function PageKeys(page: Page, keyboardPlate: KeyboardPlate) {
-    keyboardPlate.listener((kt) => kt.is(KeyboardCode.Backspace, KeyboardCode.Delete), async kt => {
-        // if (page.kit.explorer.hasSelectionRange)
-        //     page.kit.explorer.onDeleteSelection();
-    });
-    keyboardPlate.listener(kt => kt.isCtrl(KeyboardCode.Z), async kt => {
+    keyboardPlate.listener(kt => kt.isCtrl(KeyboardCode.Z), async (event, kt) => {
+        event.preventDefault();
         page.snapshoot.onUndo();
     });
-    keyboardPlate.listener(kt => kt.isCtrl(KeyboardCode.Y), async kt => {
+    keyboardPlate.listener(kt => kt.isCtrl(KeyboardCode.Y), async (event, kt) => {
+        event.preventDefault();
         page.snapshoot.onRedo();
     });
 }
