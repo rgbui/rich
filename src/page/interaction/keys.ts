c2e2372650d375e0443b2e1e96d57fec249a3165
kanhai <760489033@qq.com>
2021-09-28 01:40:40 +0800

    支持不同的系统实现快捷键撤消与重做的功能

--- src/page/interaction/keys.ts
+++ src/page/interaction/keys.ts
@@ -1,13 +1,16 @@
 import { Page } from "..";
+import { UA } from "../../../util/ua";
 import { KeyboardCode, KeyboardPlate } from "../../common/keys";
 
 export function PageKeys(page: Page, keyboardPlate: KeyboardPlate) {
-    keyboardPlate.listener(kt => kt.isCtrl(KeyboardCode.Z), async (event, kt) => {
+    keyboardPlate.listener(kt => UA.isMacOs && kt.isMeta(KeyboardCode.Z) || !UA.isMacOs && kt.isCtrl(KeyboardCode.Z), (event, kt) => {
         event.preventDefault();
         page.snapshoot.onUndo();
     });
-    keyboardPlate.listener(kt => kt.isCtrl(KeyboardCode.Y), async (event, kt) => {
+    keyboardPlate.listener(kt => UA.isMacOs && kt.isMeta(KeyboardCode.Y) || !UA.isMacOs && kt.isCtrl(KeyboardCode.Y), (event, kt) => {
         event.preventDefault();
         page.snapshoot.onRedo();
+    }, (event, kt) => {
+        event.preventDefault();
     });
 }
