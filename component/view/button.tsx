f80db7dde17f73fbea450e04c5b01c10193c92bc
kanhai <760489033@qq.com>
2021-10-05 21:52:17 +0800

    增加了button的大小模式normal,medium

--- component/view/button.tsx
+++ component/view/button.tsx
@@ -1,13 +1,14 @@
 import React, { CSSProperties } from "react";
 import { Icon } from "./icon";
 export class Button extends React.Component<{
-    children?: JSX.Element | string,
+    children?: JSX.Element | string | React.ReactNode,
     icon?: string | SvgrComponent | JSX.Element,
     disabled?: boolean,
     onClick?: (event: React.MouseEvent) => void,
     block?: boolean,
     ghost?: boolean,
-    style?: CSSProperties
+    style?: CSSProperties,
+    size?: 'normal' | 'medium' | 'larger'
 }>{
     private _loading: boolean;
     set loading(loading: boolean) {
@@ -17,7 +18,7 @@ export class Button extends React.Component<{
     render() {
         var props = this.props;
         var btn = <button style={this.props.style || {}}
-            className={'shy-button' + (props.ghost ? " shy-button-ghost" : "")}
+            className={'shy-button' + (props.ghost ? " shy-button-ghost" : "")+(this.props.size?" shy-button-"+this.props.size:"")}
             disabled={props.disabled ? props.disabled : false}
             onClick={e => props.onClick ? props.onClick(e) : undefined}
         >{props.icon && <Icon icon={props.icon}></Icon>}<span>{props.children}</span></button>;
