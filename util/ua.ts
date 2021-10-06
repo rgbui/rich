39489fbc6cd85d9d016af65497a423a6e38a5e2f
kanhai <760489033@qq.com>
2021-10-06 10:52:46 +0800

    新增对设备的信息获取

--- util/ua.ts
+++ util/ua.ts
@@ -4,5 +4,10 @@ import UAParser from 'ua-parser-js';
 var parser = new UAParser();
 var result = parser.getResult();
 export var UA = {
-    isMacOs: result.os.name.indexOf("Mac") > -1
+    isMacOs: result.os.name.indexOf("Mac") > -1,
+    browser: result.browser,
+    device: result.device,
+    os: result.os,
+    engine: result.engine,
+    cpu: result.cpu,
 }
