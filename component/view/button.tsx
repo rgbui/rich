c8b02c0a7ae8c0ed02c95f2d1be0bff4eda20bf2
kanhai <760489033@qq.com>
2021-10-21 00:00:07 +0800

    增加button的disabled功能

--- component/view/button.tsx
+++ component/view/button.tsx
@@ -15,11 +15,16 @@ export class Button extends React.Component<{
         this._loading = loading;
         this.forceUpdate()
     }
+    private _disabled: boolean;
+    set disabled(disabled: boolean) {
+        this._disabled = disabled;
+        this.forceUpdate()
+    }
     render() {
         var props = this.props;
         var btn = <button style={this.props.style || {}}
-            className={'shy-button' + (props.ghost ? " shy-button-ghost" : "")+(this.props.size?" shy-button-"+this.props.size:"")}
-            disabled={props.disabled ? props.disabled : false}
+            className={'shy-button' + (props.ghost ? " shy-button-ghost" : "") + (this.props.size ? " shy-button-" + this.props.size : "")}
+            disabled={props.disabled || this._disabled ? props.disabled : false}
             onClick={e => props.onClick ? props.onClick(e) : undefined}
         >{props.icon && <Icon icon={props.icon}></Icon>}<span>{props.children}</span></button>;
         if (props.block) return <div style={this.props.style || {}} className='shy-button-block'>{btn}</div>
