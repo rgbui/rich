529070d4aa031bfe1655348c4078386f92a48f8d
kanhai <760489033@qq.com>
2021-11-06 01:44:52 +0800

    修改time类型

--- src/common/cursor.ts
+++ src/common/cursor.ts
@@ -16,7 +16,7 @@ class Cursor {
         }
         return this._el;
     }
-    private time: number;
+    private time: NodeJS.Timeout;
     show(cursor: CursorName) {
         if (this.time) {
             clearTimeout(this.time);
