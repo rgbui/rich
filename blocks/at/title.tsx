febeb6ff712da015a227d3d6402277b9354b917e
kanhai <760489033@qq.com>
2021-11-09 00:12:10 +0800

    修复文档标题输入时光标显示不正确的问题

--- blocks/at/title.tsx
+++ blocks/at/title.tsx
@@ -12,6 +12,7 @@ import { Rect } from "../../src/common/point";
 import { messageChannel } from "../../util/bus/event.bus";
 import { Directive } from "../../util/bus/directive";
 import { BlockAppear } from "../../src/block/appear";
+import lodash from "lodash";
 
 @url('/title')
 export class Title extends Block {
@@ -54,8 +55,17 @@ export class TitleView extends BlockView<Title>{
     }
     updatePageInfo = (id: string, pageInfo: { text: string, icon?: IconArguments }) => {
         if (this.block.pageInfo.id == id) {
-            Object.assign(this.block.pageInfo, pageInfo);
-            this.forceUpdate();
+            var isUpdate: boolean = false;
+            if (pageInfo.text != this.block.pageInfo.text) {
+                this.block.pageInfo.text = pageInfo.text;
+                isUpdate = true;
+            }
+            if (typeof pageInfo.icon != 'undefined' && !lodash.isEqual(pageInfo.icon, this.block.pageInfo.icon)) {
+                this.block.pageInfo.icon = pageInfo.icon;
+                isUpdate = true;
+            }
+            if (isUpdate)
+                this.forceUpdate();
         }
     }
     willUnmount() {
@@ -69,7 +79,7 @@ export class TitleView extends BlockView<Title>{
                     {this.block.isShowIcon && this.block.pageInfo.icon && <span onMouseDown={e => this.block.onChangeIcon(e)} className='sy-block-page-info-head-icon'>
                         <Icon size={36} icon={this.block.pageInfo.icon}></Icon>
                     </span>}
-                    <span className='sy-block-page-info-head-title'><TextArea
+                    <span className='sy-block-page-info-head-title'><TextArea placeholder='输入标题'
                         rf={e => this.block.elementAppear({ el: e, appear: BlockAppear.text, prop: 'pageInfo.text' })}
                         html={this.block.pageInfo.text}></TextArea></span>
                 </div>
