92d7a1065629fb65f254f02cf05ac29ca30af0c1
rgbui <760489033@qq.com>
2021-11-23 11:26:29 +0800

    完善video

--- blocks/media/video.tsx
+++ blocks/media/video.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ResourceArguments } from "../../extensions/icon/declare";
 import { Sp } from "../../i18n/view";
 import { LangID } from "../../i18n/declare";
-import { BlockDisplay } from "../../src/block/enum";
+import { BlockDisplay, BlockRenderRange } from "../../src/block/enum";
 import { prop, url, view } from "../../src/block/factory/observable";
 import { BlockView } from "../../src/block/view";
 import { Rect } from "../../src/common/point";
@@ -10,6 +10,8 @@ import VideoSvg from "../../src/assert/svg/video.svg";
 import { useVideoPicker } from "../../extensions/file/video.picker";
 import { Block } from "../../src/block";
 import { SolidArea } from "../../src/block/view/appear";
+import { Directive } from "../../util/bus/directive";
+import { messageChannel } from "../../util/bus/event.bus";
 @url('/video')
 export class Video extends Block {
     @prop()
@@ -21,6 +23,33 @@ export class Video extends Block {
         var r = await useVideoPicker({ roundArea: bound });
         if (r) {
             await this.onUpdateProps({ src: r });
+        }
+    }
+    async didMounted() {
+        try {
+            if (this.createSource == 'InputBlockSelector' && !this.src) {
+                var r = await useVideoPicker({ roundArea: Rect.fromEle(this.el) });
+                if (r) {
+                    await this.onUpdateProps({ src: r }, BlockRenderRange.self);
+                }
+            }
+            if (this.initialData && this.initialData.file) {
+                var d = await messageChannel.fireAsync(Directive.UploadWorkspaceFile, this.initialData.file, (event) => {
+                    console.log(event, 'ev');
+                });
+                if (d.ok && d.data.url) {
+                    await this.onUpdateProps({ src: { url: d.data.url, name: 'upload' } }, BlockRenderRange.self);
+                }
+            }
+            if (this.initialData && this.initialData.url) {
+                var d = await messageChannel.fireAsync(Directive.UploadWorkspaceFileUrl, this.initialData.url);
+                if (d.ok && d.data.url) {
+                    await this.onUpdateProps({ src: { url: d.data.url, name: 'download', source: this.initialData.url } }, BlockRenderRange.self);
+                }
+            }
+        }
+        catch (ex) {
+            console.error(ex);
         }
     }
 }
