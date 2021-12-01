deffc5f167f6a1e247f82ccf6b2ed9b0643345bc
rgbui <760489033@qq.com>
2021-12-01 19:33:09 +0800

    实现flow mind

--- blocks/flow/mind/index.tsx
+++ blocks/flow/mind/index.tsx
@@ -5,7 +5,9 @@ import { url, view } from "../../../src/block/factory/observable";
 import { BlockDisplay } from "../../../src/block/enum";
 import { Block } from "../../../src/block";
 import { ChildsArea } from "../../../src/block/view/appear";
-
+import { Rect } from "../../../src/common/point";
+import { FlowMindLine } from "./line";
+import './style.less';
 @url('/flow/mind')
 export class FlowMind extends Block {
     blocks: { childs: Block[], subChilds: Block[] } = { childs: [], subChilds: [] };
@@ -20,18 +22,36 @@ export class FlowMind extends Block {
 }
 @view('/flow/mind')
 export class FlowMindView extends BlockView<FlowMind>{
-    renderNode() {
-        return <></>;
+    didMount() {
+        this.updateFlowLine();
     }
-
+    updateFlowLine() {
+        if (this.flowMindLine && this.block.blocks.subChilds.length > 0) {
+            var origin = Rect.fromEle(this.block.el).rightMiddle;
+            var points = this.block.blocks.subChilds.map(sub => {
+                return Rect.fromEle(sub.el.parentNode as HTMLElement).leftMiddle;
+            });
+            this.flowMindLine.updateView(origin, points);
+        }
+    }
+    renderItems() {
+        return <div className='sy-flow-mind-items'>
+            <ChildsArea childs={this.block.childs}></ChildsArea>
+        </div>
+    }
+    renderSubChilds() {
+        return <div className='sy-flow-mind-sub-items'>
+            {this.block.blocks.subChilds.map(child => {
+                return <div className='sy-flow-mind-sub-item' key={child.id}><child.viewComponent></child.viewComponent></div>
+            })}
+        </div>
+    }
+    flowMindLine: FlowMindLine;
     render() {
         return <div className='sy-flow-mind' style={this.block.visibleStyle} >
-            <div className='sy-flow-mind-text'>
-                {this.renderNode()}
-            </div>
-            <div className='sy-flow-mind-subs'>
-                <ChildsArea childs={this.block.blocks.subChilds}></ChildsArea>
-            </div>
+            {this.renderItems()}
+            {this.renderSubChilds()}
+            <FlowMindLine ref={e => this.flowMindLine = e}></FlowMindLine>
         </div>
     }
 }
---
+++ blocks/flow/mind/line.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Point, RectUtility, Rect } from "../../../src/common/point";
+export class FlowMindLine extends React.Component {
+    private origin: Point;
+    private points: Point[] = [];
+    private range: Rect;
+    updateView(origin: Point, points: Point[]) {
+        this.origin = origin;
+        this.points = points;
+        this.range = RectUtility.getPointsBound([this.origin, ...points]).extend(30);
+        this.forceUpdate();
+    }
+    renderLines() {
+        return <g>{this.points.map((point, index) => {
+            return <path key={index} d={`M${this.origin.join(' ')} C ${this.origin.move(40, 0).join(' ')}, ${point.move(-40, 0).join(' ')}, ${point.join(' ')}`} >
+            </path>
+        })}</g>
+    }
+    render() {
+        if (!this.origin) return <div style={{ display: 'none' }}></div>
+        return <svg viewBox={`${this.range.left}px ${this.range.left}px ${this.range.left}px ${this.range.left}px `}>{this.renderLines()}</svg>
+    }
+}
