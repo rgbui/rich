ce46a76b0b593f571557722438e6277ded4af7ce
kanhai <760489033@qq.com>
2021-12-05 02:48:40 +0800

    增加如果单元格是空单元格，那么点击将自动创建空白的文本块textSpan

--- blocks/table/cell.tsx
+++ blocks/table/cell.tsx
@@ -1,9 +1,11 @@
 import { BlockView } from "../../src/block/view";
 import React from 'react';
 import { prop, url, view } from "../../src/block/factory/observable";
-import { ChildsArea, TextArea } from "../../src/block/view/appear";
+import { ChildsArea } from "../../src/block/view/appear";
 import { BlockDisplay } from "../../src/block/enum";
 import { Block } from "../../src/block";
+import { ActionDirective } from "../../src/history/declare";
+import { BlockUrlConstant } from "../../src/block/constant";
 @url('/table/cell')
 export class TableCell extends Block {
     @prop()
@@ -12,17 +14,29 @@ export class TableCell extends Block {
     colspan: number = 1;
     display = BlockDisplay.block;
     partName = 'cell';
-    get isCol() {
+    get isCell() {
         return true;
     }
 }
 @view('/table/cell')
 export class TableCellView extends BlockView<TableCell>{
+    async mousedown(event: React.MouseEvent) {
+        if (this.block.childs.length == 0) {
+            event.stopPropagation()
+            await this.block.onAction(ActionDirective.onCreateBlockByEnter, async () => {
+                var newBlock = await this.block.page.createBlock(BlockUrlConstant.TextSpan, {}, this.block);
+                newBlock.mounted(() => {
+                    this.block.page.kit.explorer.onFocusAnchor(newBlock.createAnchor());
+                })
+            });
+        }
+    }
     render() {
         var style: Record<string, any> = {
 
         };
         return <td style={style}
+            onMouseDown={e => this.mousedown(e)}
             rowSpan={this.block.rowspan == 1 ? undefined : this.block.rowspan}
             colSpan={this.block.colspan == 1 ? undefined : this.block.colspan}
             ref={e => this.block.childsEl = e}
