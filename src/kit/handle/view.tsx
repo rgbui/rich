3330ee44175acc4bdd4bf70b6696eed00b64e753
kanhai <760489033@qq.com>
2021-12-05 17:57:12 +0800

    调整拖动多个的块的代码

--- src/kit/handle/view.tsx
+++ src/kit/handle/view.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import ReactDOM from 'react-dom';
 import { Point } from "../../common/point";
 import { Icon } from "../../../component/view/icon";
 import { Handle } from ".";
-import { DropDirection } from "./direction";
 import { Tip } from "../../../component/view/tip";
 import { LangID } from "../../../i18n/declare";
 import DragHandle from "../../assert/svg/DragHandle.svg";
-
+import { MouseDragger } from "../../common/dragger";
+import { ghostView } from "../../common/ghost";
 export class HandleView extends React.Component<{ handle: Handle }>{
     constructor(props) {
         super(props);
@@ -17,88 +16,53 @@ export class HandleView extends React.Component<{ handle: Handle }>{
         return this.props.handle;
     }
     el: HTMLElement;
-    componentDidMount() {
-        this.el = ReactDOM.findDOMNode(this) as HTMLElement;
-        document.addEventListener('mousemove', this.onMousemove);
-        document.addEventListener('mouseup', this.onMouseup);
-    }
-    componentWillUnmount() {
-        document.removeEventListener('mousemove', this.onMousemove);
-        document.removeEventListener('mouseup', this.onMouseup);
-    }
-    private point = new Point(0, 0);
     isDown: Boolean;
     isDrag: boolean = false;
     private onMousedown(event: MouseEvent) {
         if (this.toolTip) this.toolTip.close();
-        this.point = Point.from(event);
         this.isDown = true;
         this.isDrag = false;
-        this.shipBlock();
-    }
-    private onMousemove = async (event: MouseEvent) => {
-        if (this.isDown == true) {
-            if (!this.isDrag && this.point.remoteBy(Point.from(event), 5)) {
-                this.isDrag = true;
-                this.shipMoveBlock();
-            }
-            if (this.isDrag == true) {
-                this.moveDrag(event);
-            }
+        var self = this;
+        if (event) {
+            MouseDragger<{ item: HTMLElement }>({
+                event,
+                dis: 5,
+                moveStart(ev, data) {
+                    if (self.handle.kit.explorer.hasSelectionRange && self.handle.handleBlock && self.handle.kit.explorer.selectedBlocks.exists(c => c.find(g => g == self.handle.handleBlock, true) ? true : false)) {
+                        var cs = self.handle.kit.explorer.selectedBlocks.map(c => c.handleBlock);
+                        cs.each(c => {
+                            if (!self.handle.dragBlocks.some(s => s == c)) self.handle.dragBlocks.push(c)
+                        });
+                    }
+                    else self.handle.dragBlocks = [self.handle.handleBlock]
+                    self.isDrag = true;
+                    ghostView.load(self.handle.dragBlocks.map(b => b.el), { point: Point.from(ev) })
+                },
+                moving(ev, data, isend) {
+                    ghostView.move(Point.from(ev));
+                },
+                moveEnd(ev, isMove, data) {
+                    try {
+                        if (self.isDrag == true) self.handle.onDropBlock()
+                        else self.handle.onClickBlock(ev);
+                    }
+                    catch (ex) {
+                        self.handle.kit.emit('error', ex);
+                    }
+                    finally {
+                        self.isDrag = false;
+                        self.isDown = false;
+                        self.handle.onDropEnd();
+                        ghostView.unload();
+                    }
+                }
+            })
         }
-    }
-    private onMouseup = async (event: MouseEvent) => {
-        if (this.isDown == true) {
-            try {
-                if (this.isDrag == true) this.handle.onDropBlock()
-                else this.handle.onClickBlock(event);
-            }
-            catch (ex) {
-                this.handle.kit.emit('error', ex);
-            }
-            finally {
-                this.landBlock();
-                this.handle.onDropEnd();
-                this.isDrag = false;
-                this.isDown = false;
-                this.handle.dropDirection = DropDirection.none;
-                this.handle.dropBlock = null;
-            }
-        }
-    }
-    private dragCopyEle: HTMLElement;
-    private shipBlock() {
-        if (this.handle.kit.explorer.hasSelectionRange && this.handle.handleBlock && this.handle.kit.explorer.selectedBlocks.exists(c => c.find(g => g == this.handle.handleBlock, true) ? true : false)) {
-            this.handle.dragBlocks = this.handle.kit.explorer.selectedBlocks.map(c => c);
-        }
-        else this.handle.dragBlocks = [this.handle.handleBlock]
-    }
-    private shipMoveBlock() {
-        if (this.handle.dragBlocks.length > 0) {
-            var dragBlocks = this.handle.dragBlocks;
-            var cloneNode = dragBlocks.first().el.cloneNode(true);
-            this.dragCopyEle.innerHTML = '';
-            this.dragCopyEle.style.display = 'block';
-            var bound = dragBlocks.first().el.getBoundingClientRect();
-            this.dragCopyEle.style.width = bound.width + 'px';
-            this.dragCopyEle.style.height = bound.height + 'px';
-            this.dragCopyEle.appendChild(cloneNode);
-        }
-    }
-    private moveDrag(event: MouseEvent) {
-        this.dragCopyEle.style.display = 'block';
-        this.dragCopyEle.style.top = event.y + 'px';
-        this.dragCopyEle.style.left = event.x + 'px';
-    }
-    private landBlock() {
-        this.dragCopyEle.style.display = 'none';
-        this.dragCopyEle.innerHTML = '';
     }
     handleEle: HTMLElement;
     toolTip: Tip;
     render() {
         return <div>
-            <div className='shy-selector-drag-copy' ref={e => this.dragCopyEle = e}></div>
             <div className='shy-selector-bar'
                 ref={e => this.handleEle = e}
                 onMouseDown={e => this.onMousedown(e.nativeEvent)}>
