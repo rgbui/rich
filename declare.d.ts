d09bf3837166fcee7c072a3c3ff0add6a842b3bb
kanhai <760489033@qq.com>
2021-12-08 22:31:17 +0800

    移除iconPark

--- declare.d.ts
+++ declare.d.ts
@@ -34,9 +34,12 @@ declare module "*.json" {
   export default file;
 }
 
+
 /**
  * 自动在HTMLElement上面申明一个接收拖放元素的函数
  */
 interface HTMLElement {
-  receive_drop_elements?: (type: 'pageItem', elements: any[]) => void;
+  shy_drop_move?: (type: string, data: any) => void;
+  shy_drop_over?: (type: string, data: any) => void;
+  shy_end?: () => void;
 }
--- scripts/iconpark/packages/vue-next/src/icons/DEFAULT_ICON_CONFIGS.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @file DEFAULT_ICON_CONFIGS
- */
-
-import {DEFAULT_ICON_CONFIGS} from '../runtime';
-
-export default DEFAULT_ICON_CONFIGS;
