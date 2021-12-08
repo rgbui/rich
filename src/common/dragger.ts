78b22a7638af0bce8d1bb2250a24d4ab94a8199a
kanhai <760489033@qq.com>
2021-12-08 23:35:18 +0800

    实现跨不同的容器拖拽

--- src/common/dragger.ts
+++ src/common/dragger.ts
@@ -102,8 +102,13 @@ export function MouseDragger<T = Record<string, any>>(options: {
             window.getSelection ? window.getSelection().removeAllRanges() : (document as any).selection.empty();
             if (scope.isMove == true) {
                 if (options.cursor) MouseCursor.show(options.cursor);
-                if (typeof options.move == 'function') options.move(event, data)
-                if (typeof options.moving == 'function') options.moving(event, data, false);
+                try {
+                    if (typeof options.move == 'function') options.move(event, data)
+                    if (typeof options.moving == 'function') options.moving(event, data, false);
+                }
+                catch (ex) {
+                    console.error(ex);
+                }
                 if (options.isCross) {
                     var cp = dom(event.target as HTMLElement).closest(x => typeof (x as HTMLElement).shy_drop_move == 'function') as HTMLElement;
                     if (cp) {
@@ -114,8 +119,14 @@ export function MouseDragger<T = Record<string, any>>(options: {
             }
             else {
                 if (Point.from(options.event).remoteBy(Point.from(event), options.dis)) {
-                    if (typeof options.moveStart == 'function') options.moveStart(scope.event, data, crossData);
-                    scope.isMove = true;
+                    try {
+                        if (typeof options.moveStart == 'function') options.moveStart(scope.event, data, crossData); scope.isMove = true;
+                    }
+                    catch (ex) {
+                        console.error(ex);
+                        scope.isMove = false;
+                        scope.isDown = false;
+                    }
                 }
             }
         }
@@ -123,10 +134,36 @@ export function MouseDragger<T = Record<string, any>>(options: {
     up = (event: MouseEvent) => {
         if (scope.isDown == true) {
             if (options.cursor) MouseCursor.hide();
-            if (scope.isMove && typeof options.moving == 'function') options.moving(event, data, true);
-            if (typeof options.moveEnd == 'function') options.moveEnd(event, scope.isMove, data)
-            scope.isMove = false;
-            scope.isDown = false;
+            try {
+                if (scope.isMove && typeof options.moving == 'function') options.moving(event, data, true);
+                if (typeof options.moveEnd == 'function') options.moveEnd(event, scope.isMove, data)
+            }
+            catch (ex) {
+                console.error(ex);
+            }
+            /**
+             * 确保即使出错了，也能正常的释放
+             */
+            finally {
+                scope.isMove = false;
+                scope.isDown = false;
+                if (options.isCross) {
+                    try {
+                        var cp = dom(event.target as HTMLElement).closest(x => typeof (x as HTMLElement).shy_drop_move == 'function') as HTMLElement;
+                        if (cp) {
+                            cp.shy_drop_over(crossData.type, crossData.data);
+                        }
+                        if (crossPanels.length > 0) {
+                            for (let i = 0; i < crossPanels.length; i++) {
+                                crossPanels[i].shy_end()
+                            }
+                        }
+                    }
+                    catch (ex) {
+                        console.error(ex);
+                    }
+                }
+            }
         }
         document.removeEventListener('mousemove', move);
         document.removeEventListener('mouseup', up)
