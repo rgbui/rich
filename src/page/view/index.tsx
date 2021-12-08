9e4dc35b5e4705dc3cb53b7eabfdff1b80fa0083
kanhai <760489033@qq.com>
2021-12-08 23:35:49 +0800

    实现拖动pageItem创建文档链接

--- src/page/view/index.tsx
+++ src/page/view/index.tsx
@@ -25,16 +25,61 @@ export class PageView extends Component<{ page: Page }>{
     el: HTMLElement;
     componentDidMount() {
         this.el = ReactDOM.findDOMNode(this) as HTMLElement;
+        this.observeOutsideDrop();
         document.addEventListener('mousedown', this._mousedown = this.page.onGlobalMousedown.bind(this));
         document.addEventListener('mousemove', (this._mousemove = this.page.onMousemove.bind(this.page)));
         document.addEventListener('mouseup', (this._mouseup = this.page.onMouseup.bind(this.page)));
         document.addEventListener('keyup', (this._keyup = this.page.onKeyup.bind(this.page)), true);
+    }
+    observeOutsideDrop() {
+        var isMove: boolean = false;
+        var self = this;
+        var handle = self.page.kit.handle;
+        this.el.shy_drop_move = function (type, data) {
+            if (!isMove) {
+                switch (type) {
+                    case 'pageItem':
+                        handle.isDown = true;
+                        handle.isDrag = true;
+                        break;
+                }
+                isMove = true;
+            }
+        }
+        this.el.shy_drop_over = async function (type, data) {
+            if (isMove) {
+                switch (type) {
+                    case 'pageItem':
+                        if (handle.dropBlock) {
+                            var ds = Array.isArray(data) ? data : [data];
+                            await self.page.onBatchDargCreateBlocks(ds.map(d => {
+                                return {
+                                    url: '/link',
+                                    icon: d.icon,
+                                    pageId: d.id,
+                                    sn: d.sn,
+                                    text: d.text
+                                }
+                            }), handle.dropBlock, handle.dropDirection)
+                            handle.onDropEnd();
+                        }
+                        break;
+                }
+            }
+        }
+        this.el.shy_end = function () {
+            isMove = false;
+            handle.onDropEnd();
+        }
     }
     componentWillUnmount() {
         document.removeEventListener('mousedown', this._mousedown);
         document.removeEventListener('mouseup', this._mouseup);
         document.removeEventListener('mousemove', this._mousemove);
         document.removeEventListener('keyup', this._keyup, true);
+        delete this.el.shy_drop_move;
+        delete this.el.shy_drop_over;
+        delete this.el.shy_end;
     }
     render() {
         var pageStyle: Record<string, any> = {
