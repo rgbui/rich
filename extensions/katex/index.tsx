2b05835fda898784e7c6e932ee4f484ef27199d7
rgbui <760489033@qq.com>
2021-12-11 14:56:25 +0800

    实现katexInput组件

---
+++ extensions/katex/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { EventsComponent } from "../../component/lib/events.component";
+import { Textarea } from "../../component/view/input";
+import { PopoverSingleton } from "../popover/popover";
+import { PopoverPosition } from "../popover/position";
+
+class KatexInput extends EventsComponent {
+    content: string;
+    open(content: string) {
+        this.content = content;
+    }
+    render() {
+        var self = this;
+        function change(content: string) {
+            self.content = content;
+            self.emit('input', self.content);
+        }
+        return <div className="shy-katex-form">
+            <Textarea value={this.content} onChange={e => change(e)}></Textarea>
+        </div>
+    }
+}
+
+interface KatexInput {
+    only(name: 'input', fn: (data: string) => void);
+    emit(name: 'input', data: string);
+}
+
+export async function listenKatexInput(pos: PopoverPosition, content: string, listen?: (data) => void) {
+    let popover = await PopoverSingleton(KatexInput);
+    let katexInput = await popover.open(pos);
+    katexInput.open(content);
+    return new Promise((resolve: (data: string) => void, reject) => {
+        katexInput.only('input', (data) => {
+            //popover.close();
+            // resolve(data);
+            if (listen) listen(data)
+        })
+        popover.only('close', () => {
+            resolve(katexInput.content)
+        })
+    })
+}
