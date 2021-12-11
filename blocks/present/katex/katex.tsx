77fe84421a944e5ea08e30a0f818f6a01cc6afc0
rgbui <760489033@qq.com>
2021-12-11 14:56:37 +0800

    完善katex

--- blocks/present/katex/katex.tsx
+++ blocks/present/katex/katex.tsx
@@ -3,9 +3,10 @@ import katex from 'katex';
 import React from 'react';
 import { prop, url, view } from "../../../src/block/factory/observable";
 import "../../../node_modules/katex/dist/katex.min.css";
-import { SolidArea } from "../../../src/block/view/appear";
 import { Block } from "../../../src/block";
-import { BlockDisplay } from "../../../src/block/enum";
+import { BlockDisplay, BlockRenderRange } from "../../../src/block/enum";
+import { listenKatexInput } from "../../../extensions/katex";
+import { Rect } from "../../../src/common/point";
 
 @url('/katex')
 export class Katex extends Block {
@@ -18,14 +19,23 @@ export class Katex extends Block {
         });
         return html;
     }
+    async open(event: React.MouseEvent) {
+        event.stopPropagation();
+        var old = this.content;
+        var newValue = await listenKatexInput({ roundArea: Rect.fromEle(event.target as HTMLElement) }, this.content, (data) => {
+            this.content = data;
+            this.forceUpdate()
+        });
+        if (newValue) {
+            this.onManualUpdateProps({ cotnent: old }, { content: newValue });
+        }
+    }
 }
 @view('/katex')
 export class KatexView extends BlockView<Katex>{
     render() {
         return <div className='sy-block-katex' style={this.block.visibleStyle}>
-            <div className='sy-block-katex-content'>
-                <SolidArea rf={e => this.block.elementAppear({ el: e })}><span dangerouslySetInnerHTML={{ __html: this.block.htmlContent }}></span>
-                </SolidArea>
+            <div onMouseDown={e => this.block.open(e)} className='sy-block-katex-content' dangerouslySetInnerHTML={{ __html: this.block.htmlContent }}>
             </div>
         </div>
     }
