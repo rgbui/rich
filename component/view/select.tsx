65f11522cb4ebad87c8cbcd6961e153f4ddedf53
kanhai <760489033@qq.com>
2021-12-15 02:14:18 +0800

    实现select下拉的效果（不在提供输入框）

--- component/view/select.tsx
+++ component/view/select.tsx
@@ -1,10 +1,12 @@
 import React, { CSSProperties } from "react";
+import SvgDown from "../../src/assert/svg/chevronDown.svg";
 export class Select extends React.Component<{
     disabled?: boolean,
     value?: any,
     options: { text: string, value: any }[],
     onChange?: (value: any) => void,
-    style?:CSSProperties
+    style?: CSSProperties,
+    dropAlign?:'left'|'right'
 }>{
     private toggle: boolean = false;
     mousedown(event: MouseEvent) {
@@ -34,9 +36,16 @@ export class Select extends React.Component<{
             self.toggle = !self.toggle;
             self.forceUpdate()
         }
-        return <div className='shy-select' style={this.props.style||{}} ref={e => this.el = e}>
-            <div className='shy-select-selection' onClick={e => props.disabled ? undefined : (setToggle())}><input defaultValue={props.options.find(g => g.value == props.value)?.text} /></div>
-            {this.toggle && <div className='shy-select-drop'>
+        var dropStyle:CSSProperties={left:0};
+        if(this.props.dropAlign=='right'){
+            dropStyle={right:0};
+        }
+        return <div className='shy-select' style={this.props.style || {}} ref={e => this.el = e}>
+            <div className='shy-select-selection' onClick={e => props.disabled ? undefined : (setToggle())}>
+                <span>{props.options.find(g => g.value == props.value)?.text}</span><SvgDown></SvgDown>
+                {/*<input defaultValue={props.options.find(g => g.value == props.value)?.text} /> */}
+            </div>
+            {this.toggle && <div className='shy-select-drop' style={dropStyle} >
                 {props.options.map((op, index) => {
                     return <a key={index} className={props.value == op.value ? "hover" : ""} onClick={e => click(op)}><span>{op.text}</span></a>
                 })}
