2ce801eccfec54f25fae894e669fc42c40b7e022
kanhai <760489033@qq.com>
2021-12-15 00:01:22 +0800

    调整keys的触发方式，默认自动阻止默认事件，键盘冒泡事件传播

--- src/page/interaction/keys.ts
+++ src/page/interaction/keys.ts
@@ -4,23 +4,14 @@ import { KeyboardCode, KeyboardPlate } from "../../common/keys";
 
 export function PageKeys(page: Page, keyboardPlate: KeyboardPlate) {
     keyboardPlate.listener(kt => UA.isMacOs && kt.isMeta(KeyboardCode.Z) || !UA.isMacOs && kt.isCtrl(KeyboardCode.Z), (event, kt) => {
-        event.preventDefault();
         page.onUndo();
-    }, (event, kt) => {
-        event.preventDefault();
     });
     keyboardPlate.listener(kt => UA.isMacOs && kt.isMeta(KeyboardCode.Y) || !UA.isMacOs && kt.isCtrl(KeyboardCode.Y), (event, kt) => {
-        event.preventDefault();
         page.onRedo();
-    }, (event, kt) => {
-        event.preventDefault();
     });
     keyboardPlate.listener(kt => kt.isCtrl(KeyboardCode.S),
         (event, kt) => {
-            event.preventDefault();
             page.onSave();
-        }, (event, kt) => {
-            event.preventDefault();
         }
     );
 }
