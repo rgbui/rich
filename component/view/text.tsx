1019f975f73e9408c2e85599eb43554ab0903915
kanhai <760489033@qq.com>
2021-12-21 00:17:05 +0800

    增加reamk text,error text 显示

--- component/view/text.tsx
+++ component/view/text.tsx
@@ -2,3 +2,7 @@ import React from "react";
 export function Remark(props: { children: React.ReactNode }) {
     return <div className='shy-remark'>{props.children}</div>
 }
+
+export function ErrorText(props: { children: React.ReactNode }) {
+    return <span className='shy-text-error'>{props.children}</span>
+}
