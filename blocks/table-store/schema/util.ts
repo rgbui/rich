0f9227c87271bb38b67dc3b9f023f0929aef8276
kanhai <760489033@qq.com>
2021-12-26 22:08:25 +0800

    添加根据fieldType获取相对应的图标

---
+++ blocks/table-store/schema/util.ts
@@ -0,0 +1,41 @@
+import { FieldType } from "./field.type";
+import checkbox from "../../../src/assert/svg/types.checkbox.svg";
+import select from "../../../src/assert/svg/types.select.svg";
+import string from "../../../src/assert/svg/types.string.svg";
+import multipleSelect from "../../../src/assert/svg/types.multiple.select.svg";
+import file from "../../../src/assert/svg/types.file.svg";
+import link from "../../../src/assert/svg/types.link.svg";
+import number from "../../../src/assert/svg/types.number.svg";
+import date from "../../../src/assert/svg/types.date.svg";
+import email from "../../../src/assert/svg/types.email.svg";
+import phone from "../../../src/assert/svg/types.phone.svg";
+import person from "../../../src/assert/svg/types.person.svg";
+
+export function getTypeSvg(type: FieldType) {
+    switch (type) {
+        case FieldType.bool:
+            return checkbox
+        case FieldType.option:
+            return select
+        case FieldType.options:
+            return multipleSelect
+        case FieldType.file:
+            return file
+        case FieldType.link:
+            return link
+        case FieldType.phone:
+            return phone
+        case FieldType.email:
+            return email;
+        case FieldType.date:
+            return date;
+        case FieldType.users:
+            return person;
+        case FieldType.text:
+            return string;
+        case FieldType.number:
+            return number;
+        default:
+            return string;
+    }
+}
