99b37aa308b136d09e7029d11d49834fe58e1e5b
kanhai <760489033@qq.com>
2021-12-27 23:16:18 +0800

    实现formula公式选择器

---
+++ extensions/tablestore/formula/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { TableSchema } from "../../../blocks/table-store/schema/meta";
+import { EventsComponent } from "../../../component/lib/events.component";
+import { Input } from "../../../component/view/input";
+import { PopoverSingleton } from "../../popover/popover";
+import { PopoverPosition } from "../../popover/position";
+class FormulaSelector extends EventsComponent {
+    schema: TableSchema;
+    render(): React.ReactNode {
+        return <div className="shy-formula-selector">
+            <Input value={this.formula} onChange={e => this.formula = e} onEnter={e => this.onSave(e)}></Input>
+        </div>
+    }
+    onSave(e) {
+        this.formula = e;
+        this.forceUpdate();
+        this.emit('save', this.formula);
+    }
+    formula: string;
+    open(schema: TableSchema, formula: string) {
+        this.schema = schema;
+        this.formula = formula;
+    }
+}
+export async function useFormula(pos: PopoverPosition, options: {
+    schema: TableSchema,
+    formula: string
+}) {
+    let popover = await PopoverSingleton(FormulaSelector);
+    let fv = await popover.open(pos);
+    fv.open(options.schema, options.formula);
+    return new Promise((resolve: (data: string) => void, reject) => {
+        fv.only('save', (data: string) => {
+            popover.close();
+            resolve(data);
+        });
+        fv.only('close', () => {
+            popover.close();
+            resolve(null);
+        })
+        popover.only('close', () => {
+            resolve(null)
+        })
+    })
+}
