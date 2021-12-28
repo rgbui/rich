a6e5771b257573d3f0bce16536c53501f2700c8a
kanhai <760489033@qq.com>
2021-12-28 21:43:56 +0800

    调整当前用户user的申明

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,8 +1,11 @@
+import { IconArguments } from "../../extensions/icon/declare";
 
 
 export type User = {
     id: string,
+    inc?: number,
     name?: string,
-    avatar?: string,
-    clientId?: string
+    avatar?: IconArguments,
+    email?: string,
+    phone?: string
 }
