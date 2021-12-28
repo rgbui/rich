57f443fbaf4eac237ac5709a381f2ac171f00fb9
kanhai <760489033@qq.com>
2021-12-28 21:54:42 +0800

    调整tablestore 添加数据弹窗

--- extensions/tablestore/field/index.tsx
+++ extensions/tablestore/field/index.tsx
@@ -3,16 +3,18 @@ import { PopoverPosition } from "../../popover/position";
 import { PopoverSingleton } from "../../popover/popover";
 import { FieldType } from "../../../blocks/table-store/schema/field.type";
 import { TableFieldView } from "./view";
+import { FieldConfig } from "../../../blocks/table-store/schema/field";
 export async function useTableStoreAddField(pos: PopoverPosition,
     option: {
         text: string, type: FieldType,
-        check?: (newText: string) => string
+        check?: (newText: string) => string,
+        config?: Record<string, any>
     }) {
     let popover = await PopoverSingleton(TableFieldView);
     let fv = await popover.open(pos);
     fv.open(option, option.check);
-    return new Promise((resolve: (data: { text: string, type: FieldType }) => void, reject) => {
-        fv.only('save', (data: { text: string, type: FieldType }) => {
+    return new Promise((resolve: (data: { text: string, type: FieldType, config?: FieldConfig }) => void, reject) => {
+        fv.only('save', (data: { text: string, type: FieldType, config?: FieldConfig }) => {
             popover.close();
             resolve(data);
         });
--- extensions/tablestore/field/view.tsx
+++ extensions/tablestore/field/view.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Field, FieldConfig } from "../../../blocks/table-store/schema/field";
 import { FieldType } from "../../../blocks/table-store/schema/field.type";
+import { TableSchema } from "../../../blocks/table-store/schema/meta";
 import { EventsComponent } from "../../../component/lib/events.component";
 import { Button } from "../../../component/view/button";
 import { Col, Row, Space } from "../../../component/view/grid";
-import { Input } from "../../../component/view/input";
+import { Input, Textarea } from "../../../component/view/input";
 import { Select } from "../../../component/view/select";
 import { Remark, ErrorText } from "../../../component/view/text";
+import { Directive } from "../../../util/bus/directive";
+import { messageChannel } from "../../../util/bus/event.bus";
 import { TableFieldTypes } from "./type";
 
 export class TableFieldView extends EventsComponent {
@@ -21,7 +25,60 @@ export class TableFieldView extends EventsComponent {
             }
         }
         self.forceUpdate();
-        self.emit('save', { text: self.text, type: self.type });
+        self.emit('save', { text: self.text, type: self.type, config: self.config });
+    }
+    renderRelation() {
+        if (this.type != FieldType.relation) return <></>
+        return <Row>
+            <Col><Remark>关联表格:</Remark></Col>
+            <Col><Select
+                onChange={e => this.config.relationTableId = e}
+                value={this.config.relationTableId}
+                options={this.relationDatas.map(r => { return { text: r.text, value: r.id } })}
+                style={{ width: '100%' }}>
+            </Select>
+            </Col>
+        </Row >
+    }
+    renderRollup() {
+        if (this.type != FieldType.rollup) return <></>
+        return <>
+            <Row>
+                <Col><Remark>关联表格:</Remark></Col>
+                <Col><Select
+                    value={this.config.rollupTableId}
+                    options={this.relationDatas.map(r => { return { text: r.text, value: r.id } })}
+                    onChange={e => { this.config.rollupTableId = e; this.loadTypeDatas() }}
+                    style={{ width: '100%' }}
+                >
+                </Select>
+                </Col>
+            </Row >
+            {this.rollFields && this.rollFields[this.config.rollupTableId] && <><Row>
+                <Col><Remark>统计表格列:</Remark></Col>
+                <Col><Select value={this.config.rollupFieldId} options={this.rollFields[this.config.rollupTableId].map(c => {
+                    return { text: c.text, value: c.id }
+                })}
+                    onChange={e => { this.config.rollupFieldId = e; this.loadTypeDatas() }}
+                    style={{ width: '100%' }}></Select>
+                </Col>
+            </Row >
+                {this.config.rollupFieldId && <Row>
+                    <Col><Remark>对数据进行统计:</Remark></Col>
+                    <Col><Select onChange={e => this.config.rollupStat = e} value={this.config.rollupStat} options={[]} style={{ width: '100%' }}> </Select>
+                    </Col>
+                </Row>}
+            </>
+            }
+        </>
+    }
+    renderFormula() {
+        if (this.type != FieldType.formula) return <></>
+        return <Row>
+            <Col><Remark>公式:</Remark></Col>
+            <Col><Textarea value={this.config.formula} onEnter={e => this.config.formula = e}></Textarea>
+            </Col>
+        </Row >
     }
     render() {
         var self = this;
@@ -34,11 +91,14 @@ export class TableFieldView extends EventsComponent {
                 <Col><Remark>表格列类型:</Remark></Col>
                 <Col><Select style={{ width: '100%' }}
                     value={this.type}
-                    onChange={e => { this.type = e; self.forceUpdate(); }}
+                    onChange={e => this.changeType(e)}
                     options={TableFieldTypes}>
                 </Select>
                 </Col>
             </Row>
+            {this.renderRelation()}
+            {this.renderRollup()}
+            {this.renderFormula()}
             <Row style={{ marginTop: 10 }}>
                 <Col align="end">
                     <Space>
@@ -50,14 +110,54 @@ export class TableFieldView extends EventsComponent {
             </Row>
         </div>
     }
+    async changeType(type: FieldType) {
+        this.type = type;
+        this.forceUpdate();
+    }
+    private relationDatas: TableSchema[];
+    private rollFields: Record<string, Field[]>;
+    async loadTypeDatas(force?: boolean) {
+        var isUpdate: boolean = false;
+        if (this.type == FieldType.relation) {
+            if (!Array.isArray(this.relationDatas)) {
+                var data = await messageChannel.fireAsync(Directive.QueryWorkspaceTableSchemas);
+                this.relationDatas = data.list;
+                isUpdate = true;
+
+            }
+        }
+        else if (this.type == FieldType.rollup) {
+            if (!Array.isArray(this.relationDatas)) {
+                var data = await messageChannel.fireAsync(Directive.QueryWorkspaceTableSchemas);
+                this.relationDatas = data.list;
+                isUpdate = true;
+            }
+            if (!(this.rollFields && this.rollFields[this.config.rollupTableId])) {
+                if (!this.rollFields) { this.rollFields = {} }
+                if (!this.rollFields[this.config.rollupTableId]) {
+                    var rd = await messageChannel.fireAsync(Directive.getSchemaFields, this.config.rollupTableId as string);
+                    this.rollFields[this.config.rollupTableId] = rd.fields;
+                    isUpdate = true;
+                }
+            }
+        }
+        if (force == true && isUpdate) {
+            this.forceUpdate()
+        }
+    }
     private error: string = '';
     private text: string = '';
     private type: FieldType;
     private check: (text: string) => string;
-    open(option: { text: string, type: FieldType }, check: (newText: string) => string) {
+    private config: FieldConfig = {};
+    async open(option: { text: string, type: FieldType, config?: FieldConfig }, check: (newText: string) => string) {
         this.text = option.text;
         this.type = option.type;
         this.check = check;
+        this.config = option.config || {};
+        this.relationDatas = null;
+        this.rollFields = null;
+        await this.loadTypeDatas();
         this.forceUpdate();
     }
 }
