4e50727f3018017b579d7f02b4ea8c117142501e
kanhai <760489033@qq.com>
2022-01-02 02:24:48 +0800

    实现block pen块

---
+++ blocks/board/pen/index.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from "react";
+import { Block } from "../../../src/block";
+import { url, view } from "../../../src/block/factory/observable";
+import { BlockView } from "../../../src/block/view";
+@url('/pen')
+export class Pen extends Block {
+
+}
+@view('/pen')
+export class PenView extends BlockView<Pen>{
+    render(): ReactNode {
+        return <div className="sy-block-pen" style={this.block.visibleStyle}>
+        </div>
+    }
+}
