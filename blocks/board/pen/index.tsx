f51cd93f4fce59984a3301d0ed9410fe0b28e525
kanhai <760489033@qq.com>
2022-01-03 11:49:29 +0800

    添加signature_pad

--- blocks/board/pen/index.tsx
+++ blocks/board/pen/index.tsx
@@ -2,6 +2,8 @@ import React, { ReactNode } from "react";
 import { Block } from "../../../src/block";
 import { url, view } from "../../../src/block/factory/observable";
 import { BlockView } from "../../../src/block/view";
+// https://github.com/szimek/signature_pad
+
 @url('/pen')
 export class Pen extends Block {
 
