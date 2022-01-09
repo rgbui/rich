91d528f87db5858261a92cf5453200246aa62ca0
kanhai <760489033@qq.com>
2022-01-09 19:12:09 +0800

    增加vector line

---
+++ src/common/vector/line.ts
@@ -0,0 +1,22 @@
+
+import { Point } from "./point";
+export class Line {
+    x1: number;
+    x2: number;
+    y1: number;
+    y2: number;
+    constructor(x: number | Point, y: number | Point, x1?: number, y2?: number) {
+        if (x instanceof Point && y instanceof Point) {
+            this.x1 = x.x;
+            this.y1 = x.y;
+            this.x2 = y.x;
+            this.y2 = y.y;
+        }
+        else if (arguments.length == 4 && typeof x == 'number' && typeof y == 'number') {
+            this.x1 = x;
+            this.y1 = y;
+            this.x2 = x1;
+            this.y2 = y2;
+        }
+    }
+}
