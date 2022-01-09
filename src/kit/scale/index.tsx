2a3d0892ff9702d5b340c428e53639f45b6e4f99
kanhai <760489033@qq.com>
2022-01-09 17:10:19 +0800

    调整board缩放条样式

--- src/kit/scale/index.tsx
+++ src/kit/scale/index.tsx
@@ -3,13 +3,15 @@ import { ReactNode } from "react";
 import { Kit } from "..";
 import PlusSvg from "../../assert/svg/plus.2.svg";
 import MinusSvg from "../../assert/svg/minus.svg";
+import FitSvg from "../../assert/svg/fit.svg";
 import { Icon } from "../../../component/view/icon";
 import { Point } from "../../common/point";
 export class BoardScale extends React.Component<{ kit: Kit }>{
     render(): ReactNode {
         return <div className="shy-board-scale">
+            <Icon icon={FitSvg} mousedown={e=>this.props.kit.page.onFitZoom()}></Icon>
             <Icon icon={PlusSvg} mousedown={e => this.props.kit.page.onZoom(25, Point.from(e))} ></Icon>
-            <span>{(this.props.kit.page.scale * 100).toFixed(1)}%</span>
+            <span>{(this.props.kit.page.scale * 100).toFixed(0)}%</span>
             <Icon icon={MinusSvg} mousedown={e => this.props.kit.page.onZoom(-25, Point.from(e))} ></Icon>
         </div>
     }
