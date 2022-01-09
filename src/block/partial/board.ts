c514b796b38e681daf2e0481633c174b6e9430d4
kanhai <760489033@qq.com>
2022-01-09 19:15:06 +0800

    计算每个block在board中需要显示的操作点

--- src/block/partial/board.ts
+++ src/block/partial/board.ts
@@ -1,22 +1,68 @@
+
 import { Block } from "..";
-import { Rect } from "../../common/vector/point";
+import { Point, PointArrow, Rect, RectUtility } from "../../common/vector/point";
+import { Polygon } from "../../common/vector/polygon";
+
+export enum BoardPointType {
+    path,
+    resizePort,
+    connectPort,
+}
 
 export class Block$Board {
     getBlockPicker(this: Block) {
-        var pickers: {}[] = [];
+        var pickers: { type: BoardPointType, arrows: PointArrow[], point?: Point, poly?: Polygon }[] = [];
         var width = this.fixedWidth;
         var height = this.fixedHeight;
         var rect = new Rect(0, 0, width, height);
         var gm = this.globalWindowMatrix;
-        var gs = gm.resolveMatrixs();
+        // var gs = gm.resolveMatrixs();
         /**
          * 这里基本没有skew，只有scale,rotate,translate
          * scale 水平和垂直相等
          */
-        var dis = 20;
-        var extendRect = rect.extend(dis / gm.getScaling().x);
 
-
+        var s = gm.getScaling().x;
+        var extendRect = rect.extend(20 / s);
+        var pathRects = RectUtility.getRectLineRects(rect, 1 / s);
+        pickers.push(...rect.points.map((p, i) => {
+            var arrows: PointArrow[] = [];
+            if (i == 0) arrows = [PointArrow.top, PointArrow.left];
+            else if (i == 1) arrows = [PointArrow.top, PointArrow.right];
+            else if (i == 2) arrows = [PointArrow.bottom, PointArrow.right]
+            else if (i == 3) arrows = [PointArrow.bottom, PointArrow.left]
+            return {
+                type: BoardPointType.resizePort,
+                arrows,
+                point: gm.transform(p)
+            }
+        }))
+        if (!this.isFrame) {
+            pickers.push(...extendRect.centerPoints.map((p, i) => {
+                var arrows: PointArrow[] = [];
+                if (i == 0) arrows = [PointArrow.top, PointArrow.center];
+                else if (i == 1) arrows = [PointArrow.middle, PointArrow.right];
+                else if (i == 2) arrows = [PointArrow.bottom, PointArrow.center]
+                else if (i == 3) arrows = [PointArrow.middle, PointArrow.left]
+                return {
+                    type: BoardPointType.connectPort,
+                    arrows,
+                    point: gm.transform(p)
+                }
+            }))
+        }
+        pickers.push(...pathRects.map((pr, i) => {
+            var arrows: PointArrow[] = [];
+            if (i == 0) arrows = [PointArrow.top, PointArrow.center];
+            else if (i == 1) arrows = [PointArrow.middle, PointArrow.right];
+            else if (i == 2) arrows = [PointArrow.bottom, PointArrow.center]
+            else if (i == 3) arrows = [PointArrow.middle, PointArrow.left]
+            return {
+                type: BoardPointType.path,
+                arrows,
+                poly: new Polygon(...pr.points.map(p => gm.transform(p)))
+            }
+        }))
         return pickers;
     }
 }
