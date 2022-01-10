fce33acc258a3d0d5169a20c03e73ea72951fb8a
kanhai <760489033@qq.com>
2022-01-10 23:39:27 +0800

    渲染line movePort

--- src/kit/picker/view.tsx
+++ src/kit/picker/view.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM, { createPortal } from "react-dom";
 import { BlockPicker } from ".";
+import { Line } from "../../../blocks/board/line/line";
 import { Block } from "../../block";
 import { BoardPointType } from "../../block/partial/board";
 import "./style.less";
@@ -21,6 +22,9 @@ export class BlockPickerView extends React.Component<{ picker: BlockPicker }> {
                 switch (pi.type) {
                     case BoardPointType.path:
                         return <path onMouseDown={e => this.picker.onResizeBlock(block, pi.arrows, e)} style={{ cursor: i == 0 || i == 2 ? "ns-resize" : "ew-resize" }} d={pi.poly.pathString()} key={i}></path>
+                        break;
+                    case BoardPointType.movePort:
+                        return <circle onMouseDown={e => this.picker.onMovePortBlock(block as Line, pi.arrows, e)} className="move-port" key={i} cx={pi.point.x} cy={pi.point.y} r={connectR}  ></circle>
                         break;
                     case BoardPointType.connectPort:
                         return <circle onMouseDown={e => this.picker.onCreateBlockConnect(block, pi.arrows, e)} className="connect" key={i} cx={pi.point.x} cy={pi.point.y} r={connectR}  ></circle>
