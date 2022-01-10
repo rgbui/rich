863d57e3142bf3e935383238ab4d016b15c6babd
kanhai <760489033@qq.com>
2022-01-10 23:24:08 +0800

    调整board中的线条

--- blocks/board/line/line.tsx
+++ blocks/board/line/line.tsx
@@ -1,16 +1,33 @@
 import React, { ReactNode } from "react";
 import { Block } from "../../../src/block";
 import { prop, url, view } from "../../../src/block/factory/observable";
+import { BoardPointType } from "../../../src/block/partial/board";
 import { BlockView } from "../../../src/block/view";
-import { Point, Rect } from "../../../src/common/vector/point";
+import { Point, PointArrow } from "../../../src/common/vector/point";
+import { Polygon } from "../../../src/common/vector/polygon";
 import "./style.less";
 export type PortLocation = {
-    x: number | 'left' | 'right' | 'center',
-    y: number | 'top' | 'bottom' | "middle",
+    x: number | PointArrow,
+    y: number | PointArrow,
     blockId?: string
 }
 @url('/line')
 export class Line extends Block {
+    getBlockPicker() {
+        var pickers: { type: BoardPointType, arrows: PointArrow[], point?: Point, poly?: Polygon }[] = [];
+        var gm = this.globalWindowMatrix;
+        pickers.push({
+            type: BoardPointType.movePort,
+            arrows: [PointArrow.from],
+            point: gm.transform(this.cacPortLocationPos(this.from))
+        });
+        pickers.push({
+            type: BoardPointType.movePort,
+            arrows: [PointArrow.to],
+            point: gm.transform(this.cacPortLocationPos(this.to))
+        });
+        return pickers;
+    }
     @prop()
     from: PortLocation = { x: 0, y: 0 };
     @prop()
@@ -19,19 +36,15 @@ export class Line extends Block {
         var point = new Point();
         if (pl.blockId) {
             var block = this.page.find(g => g.id == pl.blockId);
-            var bound = block.getVisibleContentBound();
-            var np = this.page.getRelativePoint(bound.leftTop);
-            var ng = this.globalMatrix;
-            np = Point.from(ng.inverseTransform(np))
-            bound.moveTo(np);
-            if (pl.x == 'left') point.x = bound.x;
-            else if (pl.x == 'center') point.x = bound.x + bound.width / 2;
-            else if (pl.x == 'right') { point.x = bound.x + bound.width; }
-            else { point.x = bound.x + pl.x; }
-            if (pl.y == 'top') point.y = bound.y;
-            else if (pl.y == 'middle') point.y = bound.y + bound.height / 2;
-            else if (pl.y == 'bottom') point.y = bound.y + bound.height;
-            else point.y = bound.y + pl.y;
+            var pickers = block.getBlockPicker();
+            var ps = typeof pl.x == 'string' && typeof pl.y == 'string' ? pickers.findAll(x => x.type == BoardPointType.connectPort) : pickers.findAll(x => x.type == BoardPointType.path);
+            if (typeof pl.x == 'string' && typeof pl.y == 'string') {
+                var pi = ps.find(g => g.arrows.every(s => [pl.x, pl.y].includes(s)));
+                return this.globalWindowMatrix.inverseTransform(pi.point)
+            }
+            else {
+
+            }
         }
         else {
             point.x = pl.x as number;
@@ -48,16 +61,16 @@ export class LineView extends BlockView<Line>{
         if (from.equal(to)) {
             to = from.move(20, 20);
         }
-        var [dx, dy] = to.diff(from);
-        var rect = new Rect(from, to);
+        var poly = new Polygon(from, to);
+        var rect = poly.bound;
         var re = rect.extend(30);
         return <div className="sy-block-line" style={this.block.visibleStyle}>
             <svg viewBox={`${re.x} ${re.y} ${re.width} ${re.height}`} style={{
                 width: re.width,
                 height: re.height,
-                marginLeft: -30,
-                marginRight: -30
-            }}><path d={`M0 0,${dx} ${dy}`}></path>
+                marginLeft: re.x,
+                marginTop: re.y
+            }}><path d={`M${from.x} ${from.y},${to.x} ${to.y}`}></path>
             </svg>
         </div>
     }
