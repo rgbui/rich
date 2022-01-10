aabc36985eba482611a2596e0a5233a133605764
kanhai <760489033@qq.com>
2022-01-11 00:01:52 +0800

    修复线的移动点circle太小的问题

--- src/kit/picker/view.tsx
+++ src/kit/picker/view.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM, { createPortal } from "react-dom";
 import { BlockPicker } from ".";
 import { Line } from "../../../blocks/board/line/line";
 import { Block } from "../../block";
@@ -22,13 +21,10 @@ export class BlockPickerView extends React.Component<{ picker: BlockPicker }> {
                 switch (pi.type) {
                     case BoardPointType.path:
                         return <path onMouseDown={e => this.picker.onResizeBlock(block, pi.arrows, e)} style={{ cursor: i == 0 || i == 2 ? "ns-resize" : "ew-resize" }} d={pi.poly.pathString()} key={i}></path>
-                        break;
                     case BoardPointType.movePort:
-                        return <circle onMouseDown={e => this.picker.onMovePortBlock(block as Line, pi.arrows, e)} className="move-port" key={i} cx={pi.point.x} cy={pi.point.y} r={connectR}  ></circle>
-                        break;
+                        return <circle onMouseDown={e => this.picker.onMovePortBlock(block as Line, pi.arrows, e)} className="move-port" key={i} cx={pi.point.x} cy={pi.point.y} r={r}   ></circle>
                     case BoardPointType.connectPort:
                         return <circle onMouseDown={e => this.picker.onCreateBlockConnect(block, pi.arrows, e)} className="connect" key={i} cx={pi.point.x} cy={pi.point.y} r={connectR}  ></circle>
-                        break;
                     case BoardPointType.resizePort:
                         return <circle
                             key={i}
