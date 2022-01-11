a493c2b901e040360483f86e50d77e547648cb29
kanhai <760489033@qq.com>
2022-01-12 01:07:06 +0800

    将onResizeBlock移到块下面

--- src/kit/picker/index.ts
+++ src/kit/picker/index.ts
@@ -59,69 +59,9 @@ export class BlockPicker {
         });
         this.view.forceUpdate();
     }
-    onResizeBlock(block: Block,
-        arrows: PointArrow[],
-        event: React.MouseEvent) {
+    onResizeBlock(block: Block, arrows: PointArrow[], event: React.MouseEvent) {
         event.stopPropagation();
-        var matrix = block.matrix.clone();
-        var gm = block.globalWindowMatrix.clone();
-        var w = block.fixedWidth;
-        var h = block.fixedHeight;
-        var self = this;
-        var fp = gm.inverseTransform(Point.from(event));
-        var s = gm.getScaling().x;
-        var minW = 50 / s;
-        var minH = 50 / s;
-        MouseDragger({
-            event,
-            moving(ev, data, isEnd) {
-                var tp = gm.inverseTransform(Point.from(ev));
-                var ma = new Matrix();
-                var [dx, dy] = tp.diff(fp);
-                var bw = w;
-                var bh = h;
-                if (arrows.includes(PointArrow.top)) {
-                    if (bh - dy < minH) dy = bh - minH;
-                }
-                else if (arrows.includes(PointArrow.bottom)) {
-                    if (bh + dy < minH) dy = minH - bh;
-                }
-                if (arrows.includes(PointArrow.left)) {
-                    if (bw - dx < minW) dx = bw - minW;
-                }
-                else if (arrows.includes(PointArrow.right)) {
-                    if (bw + dx < minW) dx = minW - bw;
-                }
-                if (arrows.includes(PointArrow.top)) {
-                    ma.translate(0, dy);
-                    bh -= dy;
-                }
-                else if (arrows.includes(PointArrow.bottom)) {
-                    bh += dy;
-                }
-                if (arrows.includes(PointArrow.left)) {
-                    ma.translate(dx, 0);
-                    bw -= dx;
-                }
-                else if (arrows.includes(PointArrow.right)) {
-                    bw += dx;
-                }
-                block.matrix = matrix.appended(ma);
-                block.fixedHeight = bh;
-                block.fixedWidth = bw;
-                block.forceUpdate();
-                self.view.forceUpdate();
-                if (isEnd) {
-                    block.onAction(ActionDirective.onResizeBlock, async () => {
-                        if (!matrix.equals(block.matrix)) block.updateMatrix(matrix, block.matrix);
-                        block.manualUpdateProps(
-                            { fixedWidth: w, fixedHeight: h },
-                            { fixedWidth: block.fixedWidth, fixedHeight: block.fixedHeight }
-                        )
-                    })
-                }
-            }
-        });
+        block.onResizeBoardSelector(arrows, event);
     }
     async onCreateBlockConnect(block: Block, arrows: PointArrow[], event: React.MouseEvent) {
         event.stopPropagation();
