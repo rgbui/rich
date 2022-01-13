daa726190c20927a9c73873bd7359632a4c343fc
kanhai <760489033@qq.com>
2022-01-13 23:18:32 +0800

    包装paper库

--- src/paper/index.ts
+++ src/paper/index.ts
@@ -1,10 +1,17 @@
 ///<reference path='./lib/paper.d.ts'/>
 
+var paper
 export async function loadPaper() {
-    var r = await import(
-        /* webpackChunkName: 'paper' */
-        /* webpackPrefetch: true */
-        './lib/paper-full.js'
-    );
-    console.log(r);
+    if (typeof paper == 'undefined') {
+        var r = await import(
+            /* webpackChunkName: 'paper' */
+            /* webpackPrefetch: true */
+            './lib/paper-full.js'
+        );
+        paper=r.paper;
+        paper.install(window);
+        var canvas = document.createElement('canvas');
+        paper.setup(canvas);
+    }
+    return paper;
 }
