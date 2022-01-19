c9200baf757856018281709678c4df7d25e4627a
kanhai <760489033@qq.com>
2022-01-20 01:16:14 +0800

    实现board edit stroke 效果

--- extensions/board.edit.tool/stroke.tsx
+++ extensions/board.edit.tool/stroke.tsx
@@ -21,13 +21,21 @@ var colors: ColorType[] = [
     { color: 'rgb(101, 44, 179)' },
     { color: 'rgb(255, 249, 177)' }
 ]
-export function ShapeStroke(props) {
+export function ShapeStroke(props: {
+    stroke: string;
+    strokeWidth: number;
+    strokeOpacity: number;
+    strokeDasharray: 'none' | 'dash' | 'dash-circle';
+    change: (name: string, value: any) => void
+}) {
     var [visible, setDropVisible] = React.useState(false);
     return <div className="shy-shape-stroke">
         <div className="shy-shape-stroke-current" onMouseDown={e => setDropVisible(e => e ? false : true)}></div>
         {visible && <div className="shy-shape-stroke-drops">
             <div className="shy-shape-stroke-types">
-                <a>
+                <a className={props.strokeDasharray == 'none' ? "hover" : ""}
+                    onMouseDown={e => props.change('strokeDasharray', 'none')}
+                >
                     <svg viewBox="0 0 24 32" xmlns="http://www.w3.org/2000/svg">
                         <g fill="none" fillRule="evenodd">
                             <path d="M-18-5h60v40h-60z"></path>
@@ -35,12 +43,14 @@ export function ShapeStroke(props) {
                         </g>
                     </svg>
                 </a>
-                <a>
+                <a className={props.strokeDasharray == 'dash' ? "hover" : ""}
+                    onMouseDown={e => props.change('strokeDasharray', 'dash')}>
                     <svg viewBox="0 0 24 32" xmlns="http://www.w3.org/2000/svg">
                         <path d="M0 14h6v2H0zm9 0h6v2H9zm9 0h6v2h-6z" fill="currentColor" fillRule="evenodd"></path>
                     </svg>
                 </a>
-                <a>
+                <a className={props.strokeDasharray == 'dash-circle' ? "hover" : ""}
+                    onMouseDown={e => props.change('strokeDasharray', 'dash-circle')}>
                     <svg viewBox="0 0 24 32" xmlns="http://www.w3.org/2000/svg">
                         <g fill="currentColor" transform="translate(0 14)" fillRule="evenodd">
                             <rect width="2" height="2" rx="1"></rect>
@@ -54,13 +64,13 @@ export function ShapeStroke(props) {
                 </a>
             </div>
             <div className="shy-shape-stroke-opacity">
-                <MeasureView showValue={false} value={10} onChange={e => { }}></MeasureView>
+                <MeasureView min={1} max={10} showValue={false} value={props.strokeOpacity} onChange={e => { props.change('strokeOpacity', e) }}></MeasureView>
             </div>
             <div className="shy-shape-stroke-width">
-                <MeasureView showValue={false} value={10} onChange={e => { }}></MeasureView>
+                <MeasureView min={1} max={30} showValue={false} value={props.strokeWidth} onChange={e => { props.change('strokeWidth', e) }}></MeasureView>
             </div>
             <div className="shy-shape-stroke-colors">{colors.map(c => {
-                return <a key={c.color} style={{ backgroundColor: c.color }}></a>
+                return <a key={c.color} className={props.stroke == c.color ? "selected" : ""} onMouseDown={e => { props.change('stroke', c.color) }} style={{ backgroundColor: c.color }}></a>
             })}</div>
         </div>}
     </div>
