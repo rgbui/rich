3e6795fa63aadba78c7fff49a634f09ab42ed592
kanhai <760489033@qq.com>
2022-01-27 20:20:30 +0800

    实现gallery

--- blocks/data-grid/view/gallery/index.tsx
+++ blocks/data-grid/view/gallery/index.tsx
@@ -1,56 +1,32 @@
+
 import React from "react";
-import { Block } from "../../../../src/block";
 import { url, view } from "../../../../src/block/factory/observable";
 import { BlockView } from "../../../../src/block/view";
+import { ChildsArea } from "../../../../src/block/view/appear";
+import { TableStoreBase } from "../base/table";
+import "./style.less";
 
 @url('/data-grid/gallery')
-export class TableStoreBoard extends Block {
-    // @prop()
-    // schemaId: string;
-    // schema: TableSchema;
-    // blocks = { childs: [], columns: [] };
-    // data: any[] = [];
-    // index: number;
-    // size: number;
-    // total: number;
-    // get blockKeys() {
-    //     return ['childs', 'columns'];
-    // }
-    // async loadSchema() {
-    //     if (this.schemaId) {
-    //         var schemaData = await this.page.emitAsync(PageDirective.schemaLoad, this.schemaId);
-    //         this.schema = new TableSchema(schemaData);
-    //         console.log('schemaData', schemaData)
-    //     }
-    //     else {
-    //         this.page.onError(new Exception(ExceptionType.tableSchemaNotEmpty, '表格schema不为空'))
-    //     }
-    // }
-    // isLoadData: boolean = false;
-    // async loadData() {
-    //     if (this.schema) {
-    //         var r = await this.page.emitAsync(PageDirective.schemaTableLoad, this.schema.id, {
-    //             index: this.index,
-    //             size: this.size
-    //         });
-    //         this.data = Array.isArray(r.list) ? r.list : [];
-    //         this.total = r?.total || 0;
-    //     }
-    // }
-    // async createRows() {
-    //     this.blocks.childs = [];
-    //     for (let i = 0; i < this.data.length; i++) {
-    //         var row = this.data[i];
-    //         // var rowBlock: TableStoreRow = await BlockFactory.createBlock('/data-grid/row', this.page, { dataRow: row }, this) as TableStoreRow;
-    //         // this.blocks.rows.push(rowBlock);
+export class TableStoreGallery extends TableStoreBase {
 
-    //     }
-    // }
 }
 @view('/data-grid/gallery')
-export class TableStoreBoardView extends BlockView<TableStoreBoard>{
+export class TableStoreGalleryView extends BlockView<TableStoreGallery>{
+    renderRows() {
+        var childs = this.block.childs;
+        var rowCount = Math.ceil(childs.length / 3);
+        var eles: JSX.Element[] = [];
+        for (let i = 0; i < rowCount; i++) {
+            var cs = [childs[i * 3 + 0]];
+            if (i * 3 + 1 < this.block.childs.length) cs.push(childs[i * 3 + 1])
+            if (i * 3 + 2 < this.block.childs.length) cs.push(childs[i * 3 + 2]);
+            eles.push(<div className='sy-data-grid-gallery-row' key={i}><ChildsArea childs={cs}></ChildsArea></div>)
+        }
+        return eles;
+    }
     render() {
-        return <div className='sy-data-grid-board'>
+        return <div className='sy-data-grid-gallery'>
+            {this.renderRows()}
         </div>
     }
 }
