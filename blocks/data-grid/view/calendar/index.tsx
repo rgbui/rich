df8f7e2ca9f6b50bb8eb2b12e85f9dcbedd6e5fd
kanhai <760489033@qq.com>
2022-01-27 20:21:02 +0800

    完善calendar块

--- blocks/data-grid/view/calendar/index.tsx
+++ blocks/data-grid/view/calendar/index.tsx
@@ -1,19 +1,27 @@
 import { Block } from "../../../../src/block";
-import { prop, url, view } from "../../../../src/block/factory/observable";
+import { url, view } from "../../../../src/block/factory/observable";
 import { BlockView } from "../../../../src/block/view";
 import React from 'react';
-import { TableSchema } from "../../schema/meta";
 @url('/data-grid/calendar')
 export class TableStoreCalendar extends Block {
-    @prop()
-    schemaId: string;
-    schema: TableSchema;
+
 }
 @view('/data-grid/calendar')
 export class TableStoreCalendarView extends BlockView<TableStoreCalendar>{
     render() {
-        return <div className='sy-tablestore-calendar'>
+        return <div className='sy-data-grid-calendar'>
+            <div className="sy-data-grid-calendar-head">
+                <div className="sy-data-grid-calendar-head-operator">
+                    <label>日</label>
+                    <label>周</label>
+                    <label>年</label>
+                </div>
+                <label>今天</label>
+                <div className="sy-data-grid-calendar-head-date"></div>
+            </div>
+            <div className="sy-data-grid-calendar-cells">
 
+            </div>
         </div>
     }
 }
