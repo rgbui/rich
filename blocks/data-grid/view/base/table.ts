b8cadb25a06f1a56f86fddf359e46794afcf0569
kanhai <760489033@qq.com>
2022-01-27 20:21:35 +0800

    调整tabeStoreBase块

--- blocks/data-grid/view/base/table.ts
+++ blocks/data-grid/view/base/table.ts
@@ -1,10 +1,12 @@
 import { channel } from "../../../../net/channel";
 import { Block } from "../../../../src/block";
+import { BlockFactory } from "../../../../src/block/factory/block.factory";
 import { prop } from "../../../../src/block/factory/observable";
 import { Exception, ExceptionType } from "../../../../src/error/exception";
 import { TableSchema } from "../../schema/meta";
 import { ViewField } from "../../schema/view";
 import { TableStoreTurns } from "../../turn";
+import { TableStoreItem } from "../item";
 
 export class TableStoreBase extends Block {
     @prop()
@@ -34,5 +36,35 @@ export class TableStoreBase extends Block {
     }
     async getWillTurnData(url: string) {
         return await TableStoreTurns.turn(this, url);
+    }
+    data: any;
+    isLoadData: boolean = false;
+    index: number = 1;
+    size: number = 50;
+    total: number = 0;
+    async loadData() {
+        if (this.schema) {
+            var r = await this.schema.list({ page: this.index, size: this.size });
+            if (r.data) {
+                this.data = Array.isArray(r.data.list) ? r.data.list : [];
+                this.total = r.data?.total || 0;
+            }
+        }
+    }
+    async createItem() {
+        this.blocks.childs = [];
+        for (let i = 0; i < this.data.length; i++) {
+            var row = this.data[i];
+            var rowBlock: TableStoreItem = await BlockFactory.createBlock('/data-grid/item', this.page, { mark: i, dataRow: row }, this) as TableStoreItem;
+            this.blocks.childs.push(rowBlock);
+            await rowBlock.createElements();
+        }
+    }
+    async didMounted() {
+        await this.loadSchema();
+        await this.loadViewFields();
+        await this.loadData();
+        await this.createItem();
+        this.view.forceUpdate();
     }
 }
