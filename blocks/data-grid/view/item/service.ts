2e808648d61e19c4a303d849aca0b4e8cef7f37c
kanhai <760489033@qq.com>
2022-01-30 23:57:32 +0800

    修复创建create field block报错的问题

--- blocks/data-grid/view/item/service.ts
+++ blocks/data-grid/view/item/service.ts
@@ -11,43 +11,43 @@ export async function createFieldBlock(viewField: ViewField, row: Record<string,
             cellContent = await BlockFactory.createBlock('/field/text', page, {
                 value: viewField.getValue(row),
                 viewField
-            }, this);
+            }, block);
             break;
         case FieldType.autoIncrement:
         case FieldType.number:
             cellContent = await BlockFactory.createBlock('/field/number', page, {
                 value: viewField.getValue(row),
                 viewField
-            }, this);
+            }, block);
             break;
         case FieldType.modifyDate:
         case FieldType.createDate:
         case FieldType.date:
+            console.log(row,viewField?.field?.name);
             cellContent = await BlockFactory.createBlock('/field/date', page, {
                 value: viewField.getValue(row),
                 viewField
-            }, this);
+            }, block);
             break;
         case FieldType.option:
             cellContent = await BlockFactory.createBlock('/field/option', page, {
                 value: viewField.getValue(row),
                 viewField
-            }, this);
+            }, block);
             break;
         case FieldType.creater:
         case FieldType.modifyer:
             cellContent = await BlockFactory.createBlock('/field/user', page, {
                 viewField,
                 value: viewField.getValue(row),
-            }, this);
+            }, block);
             break;
         case FieldType.bool:
             cellContent = await BlockFactory.createBlock('/field/check', page, {
                 viewField,
                 value: viewField.getValue(row),
-            }, this);
+            }, block);
             break;
-
     }
     return cellContent;
 }
