b576660c59d854842299a7e99fabecf58b8fda10
kanhai <760489033@qq.com>
2022-01-30 23:53:08 +0800

    添加origin field功能方法

--- blocks/data-grid/element/field/origin.field.ts
+++ blocks/data-grid/element/field/origin.field.ts
@@ -2,7 +2,8 @@ import React from "react";
 import { Block } from "../../../../src/block";
 import { BlockDisplay } from "../../../../src/block/enum";
 import { ViewField } from "../../schema/view";
-import { TableStoreCell } from "../../view/table/part/cell";
+import { TableStoreItem } from "../../view/item";
+
 export class OriginField extends Block {
     display = BlockDisplay.block;
     value: any;
@@ -10,31 +11,24 @@ export class OriginField extends Block {
         return false;
     }
     get handleBlock(): Block {
-        return this.parent.parent;
+        return this.parent;
     }
     async changeProps(oldProps: Record<string, any>, newProps: Record<string, any>) {
         if (newProps && Object.keys(newProps).length > 0) {
             if (typeof newProps['value'] != 'undefined') {
-                var cell = this.closest(x => x instanceof TableStoreCell) as TableStoreCell;
-                if (cell) {
-                    await cell.onUpdateCellValue(newProps['value']);
-                }
+                await this.item.onUpdateCellValue(this.viewField, newProps['value'])
             }
         }
     }
     async onUpdateCellFieldSchema(props: Record<string, any>) {
-        var cell = this.closest(x => x instanceof TableStoreCell) as TableStoreCell;
-        if (cell) {
-            await cell.onUpdateCellFieldSchema(props);
-        }
+        this.item.onUpdateFieldSchema(this.viewField, props);
+    }
+    get item() {
+        return this.parent as TableStoreItem
     }
     viewField: ViewField;
     get field() {
         if (this.viewField) return this.viewField.field;
-        var cell = this.closest(x => x instanceof TableStoreCell) as TableStoreCell;
-        if (cell) {
-            return cell.field;
-        }
     }
     onCellMousedown(event: React.MouseEvent) {
 
