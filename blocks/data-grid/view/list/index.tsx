ae15a863ee50f4e5685c05a018da34cebb3dfca1
kanhai <760489033@qq.com>
2022-02-01 23:17:27 +0800

    完善list块

--- blocks/data-grid/view/list/index.tsx
+++ blocks/data-grid/view/list/index.tsx
@@ -3,6 +3,7 @@ import { url, view } from "../../../../src/block/factory/observable";
 import { BlockView } from "../../../../src/block/view";
 import { ChildsArea } from "../../../../src/block/view/appear";
 import { DataGridView } from "../base/table";
+import "./style.less";
 @url('/data-grid/list')
 export class TableStoreList extends DataGridView {
 
@@ -11,6 +12,7 @@ export class TableStoreList extends DataGridView {
 export class TableStoreListView extends BlockView<TableStoreList>{
     render() {
         return <div className='sy-data-grid-list'>
+            <div className="sy-data-grid-list-head"></div>
             <ChildsArea childs={this.block.childs}></ChildsArea>
         </div>
     }
