fa5b4ddb148c85d660bdc30ed1a6b82613824014
kanhai <760489033@qq.com>
2022-02-01 16:58:45 +0800

    添加用户头像

--- blocks/data-grid/element/field/user.tsx
+++ blocks/data-grid/element/field/user.tsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { Avatar } from "../../../../component/view/avator/face";
 import { url, view } from "../../../../src/block/factory/observable";
 import { BlockView } from "../../../../src/block/view";
 import { OriginField } from "./origin.field";
 @url('/field/user')
 export class FieldUser extends OriginField {
-
+   
 }
 @view('/field/user')
 export class FieldTextView extends BlockView<FieldUser>{
     render() {
-        return <div className='sy-field-text'>{this.block.htmlContent}</div>
+        return <div className='sy-field-text'>
+            {this.block.value && <Avatar userid={this.block.value}></Avatar>}
+        </div>
     }
 }
