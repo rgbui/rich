dbc4b8a728c73f859ebae74c9f452a9ee2cacaa7
kanhai <760489033@qq.com>
2022-02-01 17:00:08 +0800

    完善表格item块

--- blocks/data-grid/view/item/index.tsx
+++ blocks/data-grid/view/item/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { MenuItemType } from "../../../../component/view/menu/declare";
 import { Block } from "../../../../src/block";
+import { BlockDirective } from "../../../../src/block/enum";
 import { url, view } from "../../../../src/block/factory/observable";
 import { BlockView } from "../../../../src/block/view";
 import { ChildsArea } from "../../../../src/block/view/appear";
+import { util } from "../../../../util/util";
+import { ViewField } from "../../schema/view";
 import { DataGridView } from "../base/table";
 import { createFieldBlock } from "./service";
-
+import trash from "../../../../src/assert/svg/trash.svg";
 import "./style.less";
-
+import { langProvider } from "../../../../i18n/provider";
+import { LangID } from "../../../../i18n/declare";
 @url('/data-grid/item')
 export class TableStoreItem extends Block {
     dataRow: Record<string, any> = {};
@@ -17,11 +22,46 @@ export class TableStoreItem extends Block {
     get fields() {
         return (this.parent as DataGridView).fields;
     }
+    get dataGrid() {
+        if (this.parent instanceof DataGridView) {
+            return this.parent;
+        }
+    }
     async createElements() {
         for (let i = 0; i < this.fields.length; i++) {
             var field = this.fields[i];
             var block = await createFieldBlock(field, this.dataRow, this);
             this.blocks.childs.push(block);
+        }
+    }
+    async onUpdateCellValue(viewField: ViewField, value: any) {
+        value = util.clone(value);
+        this.dataRow[viewField.field.name] = value;
+        await this.schema.rowUpdate({
+            dataId: this.dataRow.id,
+            data: { [viewField.field.name]: value }
+        })
+    }
+    async onUpdateFieldSchema(viewField: ViewField, data) {
+        data = util.clone(data);
+        viewField.field.update(data);
+        await this.schema.fieldUpdate({ fieldId: viewField.field.id, data })
+    }
+    async onGetContextMenus(): Promise<MenuItemType<string | BlockDirective>[]> {
+        var items: MenuItemType<BlockDirective>[] = [];
+        items.push({
+            name: BlockDirective.delete,
+            icon: trash,
+            text: langProvider.getText(LangID.menuDelete),
+            label: "delete"
+        });
+        return items;
+    }
+    async onClickContextMenu(item: MenuItemType<BlockDirective | string>, event: MouseEvent) {
+        switch (item.name) {
+            case BlockDirective.delete:
+                this.dataGrid.onRemoveItem(this.dataRow.id);
+                break;
         }
     }
 }
