2e1b1d32ee56d8afa9252a937b2e944fef64c14c
rgbui <760489033@qq.com>
2022-02-10 14:01:39 +0800

    完善表格列排序功能

--- extensions/tablestore/sort/index.tsx
+++ extensions/tablestore/sort/index.tsx
@@ -5,10 +5,10 @@ import { Button } from "../../../component/view/button";
 import { Select } from "../../../component/view/select";
 import { PopoverSingleton } from "../../popover/popover";
 import { PopoverPosition } from "../../popover/position";
-
 import closeTick from "../../../src/assert/svg/closeTick.svg";
 import { Icon } from "../../../component/view/icon";
 import { Remark } from "../../../component/view/text";
+
 class TableSortView extends EventsComponent {
     schema: TableSchema;
     text: string;
@@ -31,47 +31,59 @@ class TableSortView extends EventsComponent {
             return {
                 text: fe.text,
                 value: fe.id
-                // value:fe.value
             }
         })
     }
     render(): ReactNode {
+        var self = this;
+        function addSort() {
+            var f = self.schema.fields.find(g => !self.sorts.some(s => s.field == g.id));
+            if (!f) f = self.schema.fields.first();
+            self.sorts.push({ field: f.id, sort: 1 });
+            self.forceUpdate()
+        }
+        function removeSort(at: number) {
+            self.sorts.splice(at, 1);
+            self.forceUpdate();
+        }
         var hasSorts = Array.isArray(this.sorts) && this.sorts.length > 0;
         return <div className="shy-table-sorts-view">
             <div className="shy-table-sorts-view-head"></div>
             <div className="shy-table-sorts-view-content">
                 {hasSorts && this.sorts.map((so, i) => {
                     return <div key={i}>
-                        <Select options={this.getFields()} onChange={e => { }}></Select>
+                        <Select options={this.getFields()} onChange={e => { so.field = e; self.forceUpdate() }}></Select>
                         <Select options={[
                             { text: '降序', value: -1 },
                             { text: '升序', value: 1 }
-                        ]}>
+                        ]} onChange={e => { so.sort = e; self.forceUpdate() }}>
                         </Select>
-                        <Icon icon={closeTick}></Icon>
+                        <Icon icon={closeTick} click={e => removeSort(i)}></Icon>
                     </div>
                 })}
-                {hasSorts && <Button>添加排序</Button>}
+                {hasSorts && <Button onClick={e => addSort()}>添加排序</Button>}
                 {!hasSorts && <Remark>
                 </Remark>}
             </div>
             {!hasSorts && <div className="shy-table-sorts-view-footer">
-                <Button>添加排序</Button>
+                <Button onClick={e => addSort()}>添加排序</Button>
             </div>}
         </div>
     }
 }
 
-export async function useTableFilterView(pos: PopoverPosition, options: {
+export async function useTableSortView(pos: PopoverPosition, options: {
     schema: TableSchema,
     text: string,
     url: string,
-    sorts?: Record<string, any>
+    sorts?: { field: string, sort: number }[]
 }) {
     let popover = await PopoverSingleton(TableSortView);
     let fv = await popover.open(pos);
     fv.open(options);
-    return new Promise((resolve: (data: Record<string, any>) => void, reject) => {
-
+    return new Promise((resolve:(sorts: { field: string, sort: number }[]) => void, reject) =>{
+        popover.only('close', () => {
+            resolve(fv.sorts)
+        })
     })
 }
