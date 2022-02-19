a1f8dd1d2697f3a69d0c514ff9f42f6c9b7dfc8e
kanhai <760489033@qq.com>
2022-02-19 23:48:17 +0800

    增加了创建row check block,row num block块

--- blocks/data-grid/view/item/service.ts
+++ blocks/data-grid/view/item/service.ts
@@ -2,51 +2,64 @@ import { Block } from "../../../../src/block";
 import { BlockFactory } from "../../../../src/block/factory/block.factory";
 import { FieldType } from "../../schema/type";
 import { ViewField } from "../../schema/view";
-export async function createFieldBlock(viewField: ViewField, row: Record<string, any>, block: Block) {
+export async function createFieldBlock(viewField: ViewField, data: { row: Record<string, any>, index: number }, block: Block) {
     var cellContent: Block;
+    var row = data.row;
     var field = viewField.field;
     var page = block.page;
-    switch (field.type) {
-        case FieldType.text:
-            cellContent = await BlockFactory.createBlock('/field/text', page, {
-                value: viewField.getValue(row),
-                viewField
-            }, block);
-            break;
-        case FieldType.autoIncrement:
-        case FieldType.number:
-            cellContent = await BlockFactory.createBlock('/field/number', page, {
-                value: viewField.getValue(row),
-                viewField
-            }, block);
-            break;
-        case FieldType.modifyDate:
-        case FieldType.createDate:
-        case FieldType.date:
-            cellContent = await BlockFactory.createBlock('/field/date', page, {
-                value: viewField.getValue(row),
-                viewField
-            },block);
-            break;
-        case FieldType.option:
-            cellContent = await BlockFactory.createBlock('/field/option', page, {
-                value: viewField.getValue(row),
-                viewField
-            }, block);
-            break;
-        case FieldType.creater:
-        case FieldType.modifyer:
-            cellContent = await BlockFactory.createBlock('/field/user', page, {
-                viewField,
-                value: viewField.getValue(row),
-            }, block);
-            break;
-        case FieldType.bool:
-            cellContent = await BlockFactory.createBlock('/field/check', page, {
-                viewField,
-                value: viewField.getValue(row),
-            }, block);
-            break;
+    if (viewField.type == 'check') {
+        cellContent = await BlockFactory.createBlock('/field/row/check', page, {
+            viewField
+        }, block);
+    }
+    else if (viewField.type == 'rowNum') {
+        cellContent = await BlockFactory.createBlock('/field/row/num', page, {
+            viewField
+        }, block);
+    }
+    else {
+        switch (field.type) {
+            case FieldType.text:
+                cellContent = await BlockFactory.createBlock('/field/text', page, {
+                    value: viewField.getValue(row),
+                    viewField
+                }, block);
+                break;
+            case FieldType.autoIncrement:
+            case FieldType.number:
+                cellContent = await BlockFactory.createBlock('/field/number', page, {
+                    value: viewField.getValue(row),
+                    viewField
+                }, block);
+                break;
+            case FieldType.modifyDate:
+            case FieldType.createDate:
+            case FieldType.date:
+                cellContent = await BlockFactory.createBlock('/field/date', page, {
+                    value: viewField.getValue(row),
+                    viewField
+                }, block);
+                break;
+            case FieldType.option:
+                cellContent = await BlockFactory.createBlock('/field/option', page, {
+                    value: viewField.getValue(row),
+                    viewField
+                }, block);
+                break;
+            case FieldType.creater:
+            case FieldType.modifyer:
+                cellContent = await BlockFactory.createBlock('/field/user', page, {
+                    viewField,
+                    value: viewField.getValue(row),
+                }, block);
+                break;
+            case FieldType.bool:
+                cellContent = await BlockFactory.createBlock('/field/check', page, {
+                    viewField,
+                    value: viewField.getValue(row),
+                }, block);
+                break;
+        }
     }
     return cellContent;
 }
