3da51879e10d18b0f38c0e46be71d2f39df0cd5e
kanhai <760489033@qq.com>
2022-02-20 16:29:23 +0800

    增加email块

---
+++ blocks/data-grid/element/field/email.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { url, view } from "../../../../src/block/factory/observable";
+import { BlockView } from "../../../../src/block/view";
+import { TextArea } from "../../../../src/block/view/appear";
+import { OriginField } from "./origin.field";
+
+@url('/field/email')
+export class FieldEmail extends OriginField {
+
+}
+@view('/field/email')
+export class FieldEmailView extends BlockView<FieldEmail>{
+    render() {
+        return <div className='sy-field-email'>
+            <TextArea rf={e => this.block.elementAppear({ el: e, prop: 'value' })} placeholder="输入文本" html={this.block.value}></TextArea>
+        </div>
+    }
+}
