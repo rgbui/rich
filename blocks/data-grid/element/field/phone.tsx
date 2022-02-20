a04ef62c83a14d03e878b2aa5307f87ac352a3f4
kanhai <760489033@qq.com>
2022-02-20 16:29:33 +0800

    增加phone块

---
+++ blocks/data-grid/element/field/phone.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { url, view } from "../../../../src/block/factory/observable";
+import { BlockView } from "../../../../src/block/view";
+import { TextArea } from "../../../../src/block/view/appear";
+import { OriginField } from "./origin.field";
+
+@url('/field/phone')
+export class FieldPhone extends OriginField {
+
+}
+@view('/field/phone')
+export class FieldPhoneView extends BlockView<FieldPhone>{
+    render() {
+        return <div className='sy-field-phone'>
+            <TextArea rf={e => this.block.elementAppear({ el: e, prop: 'value' })} placeholder="输入文本" html={this.block.value}></TextArea>
+        </div>
+    }
+}
