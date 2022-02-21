020674d22ac921d4a4a7951830015c3b84f56b3f
rgbui <760489033@qq.com>
2022-02-21 18:30:52 +0800

    button组件点击传入button

--- component/view/button.tsx
+++ component/view/button.tsx
@@ -4,7 +4,7 @@ export class Button extends React.Component<{
     children?: JSX.Element | string | React.ReactNode,
     icon?: string | SvgrComponent | JSX.Element,
     disabled?: boolean,
-    onClick?: (event: React.MouseEvent) => void,
+    onClick?: (event: React.MouseEvent,button?:Button) => void,
     block?: boolean,
     ghost?: boolean,
     link?:boolean,
@@ -26,7 +26,7 @@ export class Button extends React.Component<{
         var btn = <button style={this.props.style || {}}
             className={'shy-button'+(props.link?" shy-button-link":"") + (props.ghost ? " shy-button-ghost" : "") + (this.props.size ? " shy-button-" + this.props.size : "")}
             disabled={props.disabled || this._disabled ? props.disabled : false}
-            onClick={e => props.onClick ? props.onClick(e) : undefined}
+            onClick={e => props.onClick ? props.onClick(e,this) : undefined}
         >{props.icon && <Icon icon={props.icon}></Icon>}{props.children}</button>;
         if (props.block) return <div style={this.props.style || {}} className='shy-button-block'>{btn}</div>
         else return btn
