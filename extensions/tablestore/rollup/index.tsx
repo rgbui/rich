403fdd133fc8d7d12119f022fa738f2418cba106
kanhai <760489033@qq.com>
2022-02-24 02:03:34 +0800

    增加rollup组件

---
+++ extensions/tablestore/rollup/index.tsx
@@ -0,0 +1,132 @@
+import React, { ReactNode } from "react";
+import { FieldConfig } from "../../../blocks/data-grid/schema/field";
+import { TableSchema } from "../../../blocks/data-grid/schema/meta";
+import { FieldType } from "../../../blocks/data-grid/schema/type";
+import { EventsComponent } from "../../../component/lib/events.component";
+import { Col, Row } from "../../../component/view/grid";
+import { Select } from "../../../component/view/select";
+import { Remark } from "../../../component/view/text";
+import { channel } from "../../../net/channel";
+import { PopoverSingleton } from "../../popover/popover";
+import { PopoverPosition } from "../../popover/position";
+import "./style.less";
+
+export class RollupView extends EventsComponent {
+    render(): ReactNode {
+        return <div className="shy-rollup-view">
+            {this.notExistsRelationTable && <div>没有关联表，无法进行关联统计</div>}
+            {this.notExistsRelationTable == false && this.relationDatas && <div>
+                <Row>
+                    <Col><Remark>关联表格:</Remark></Col>
+                    <Col><Select
+                        value={this.config.rollupTableId}
+                        options={this.relationDatas.map(r => { return { text: r.text, value: r.id } })}
+                        onChange={e => { this.config.rollupTableId = e; this.loadTypeDatas() }}
+                        style={{ width: '100%' }}
+                    >
+                    </Select>
+                    </Col>
+                </Row >
+                {this.rollFields && <><Row>
+                    <Col><Remark>统计表格列:</Remark></Col>
+                    <Col><Select value={this.config.rollupFieldId} options={this.rollFields.map(c => {
+                        return { text: c.text, value: c.id }
+                    })}
+                        onChange={e => { this.config.rollupFieldId = e; this.loadTypeDatas() }}
+                        style={{ width: '100%' }}></Select>
+                    </Col>
+                </Row >
+                    {this.config.rollupFieldId && <Row>
+                        <Col><Remark>对数据进行统计:</Remark></Col>
+                        <Col><Select onChange={e => this.config.rollupStatistic = e} value={this.config.rollupStatistic} options={this.rollupStatisticOptions} style={{ width: '100%' }}> </Select>
+                        </Col>
+                    </Row>}
+                </>
+                }
+            </div>}
+        </div>
+    }
+    isChange: boolean = false;
+    private relationDatas: TableSchema[];
+    config: FieldConfig = {};
+    schema: TableSchema;
+    private notExistsRelationTable: boolean = false;
+    async open(option: {
+        schema: TableSchema,
+        config?: Record<string, any>
+    }) {
+        this.isChange = false;
+        this.schema = option.schema;
+        this.config = option.config || {};
+        this.relationDatas = null;
+        await this.loadTypeDatas();
+        this.forceUpdate();
+    }
+    async loadTypeDatas(force?: boolean) {
+        var isUpdate: boolean = false;
+        if (!Array.isArray(this.relationDatas)) {
+            var ids = this.schema.fields.findAll(g => g.config?.relationTableId ? true : false).map(g => g.config.relationTableId)
+            if (ids.length > 0) {
+                this.notExistsRelationTable = false;
+                var r = await channel.get('/schema/ids/list', {
+                    ids: ids
+                });
+                if (r.ok) {
+                    this.relationDatas = r.data.list as TableSchema[];
+                    isUpdate = true;
+                }
+            }
+            else this.notExistsRelationTable = true;
+            isUpdate = true;
+        }
+        if (force == true && isUpdate) {
+            this.forceUpdate()
+        }
+    }
+    async onChangeConfig(config: Partial<FieldConfig>) {
+        Object.assign(this.config, config);
+        this.isChange = true;
+        this.forceUpdate();
+    }
+    get rollupStatisticOptions() {
+        var rfs = this.rollFields;
+        if (rfs) {
+            if (this.config.rollupFieldId) {
+                var rf = rfs.find(g => g.id == this.config.rollupFieldId);
+                if (rf.type == FieldType.number) {
+                    return [
+                        { text: '最小', value: '$min' },
+                        { text: '最大', value: '$max' },
+                        { text: '平均', value: '$agv' },
+                        { text: '求和', value: '$sum' },
+                    ]
+                }
+                else return [{ text: '数量', value: '$count' }]
+            }
+        }
+        return [];
+    }
+    get rollFields() {
+        if (this.relationDatas)
+            return this.relationDatas?.find(g => g.id == this.config?.rollupTableId)?.fields
+    }
+}
+
+export async function useRollupView(pos: PopoverPosition,
+    option: {
+        schema: TableSchema,
+        config?: Record<string, any>
+    }) {
+    let popover = await PopoverSingleton(RollupView, { mask: true });
+    let fv = await popover.open(pos);
+    fv.open(option);
+    return new Promise((resolve: (data: { config?: FieldConfig }) => void, reject) => {
+        fv.only('close', () => {
+            popover.close();
+            resolve(fv.isChange ? { config: fv.config } : undefined);
+        })
+        popover.only('close', () => {
+            resolve(fv.isChange ? { config: fv.config } : undefined);
+        })
+    })
+}
