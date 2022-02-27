36d09b02521919c19e77961a807aecaa66e7f5f6
kanhai <760489033@qq.com>
2022-02-27 14:17:21 +0800

    调整field title button展示效果

--- blocks/data-grid/element/field/title.tsx
+++ blocks/data-grid/element/field/title.tsx
@@ -15,7 +15,7 @@ export class FieldTextView extends BlockView<FieldText>{
     render() {
         return <div className='sy-field-title'>
             <TextArea rf={e => this.block.elementAppear({ el: e, prop: 'value' })} placeholder="输入文本" html={this.block.value}></TextArea>
-            <Button className='sy-field-title-button' ghost onClick={e => this.block.openPage()}>打开页面</Button>
+            <Button size="small" className='sy-field-title-button' ghost onClick={e => this.block.openPage()}>打开</Button>
         </div>
     }
 }
