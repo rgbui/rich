d38718f7d171469625039dd91d0c191660d3f930
rgbui <760489033@qq.com>
2022-03-02 19:41:56 +0800

    text channel

--- blocks/channel/text/view/view.tsx
+++ blocks/channel/text/view/view.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { ChannelText } from "..";
+import { RichTextInput } from "../../../../component/view/text.input";
 import { view } from "../../../../src/block/factory/observable";
 import { BlockView } from "../../../../src/block/view";
-
+import { renderChannelTextContent } from "./content";
 @view('/channel/text')
 export class ChannelTextView extends BlockView<ChannelText>{
     renderHead() {
@@ -10,13 +11,19 @@ export class ChannelTextView extends BlockView<ChannelText>{
     }
     renderContent() {
         return <div className="sy-channel-text-content">
-
+            {renderChannelTextContent(this.block)}
         </div>
     }
     renderInput() {
         return <div className="sy-channel-text-input">
-
+            <RichTextInput popOpen={e => this.popOpen(e)} onInput={e => this.onInput(e)} ></RichTextInput>
         </div>
+    }
+    popOpen(cs: { char: string, span: HTMLElement }) {
+
+    }
+    onInput(data: { files?: File[], content?: string }) {
+
     }
     render() {
         return <div className='sy-channel-text' style={this.block.visibleStyle}>
