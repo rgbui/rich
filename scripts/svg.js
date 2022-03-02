53ae37d7d8d6f12b0dbf1624db8a589e81e6f5e9
kanhai <760489033@qq.com>
2022-03-02 23:16:21 +0800

    增加对shy中的svg生成引用

--- scripts/svg.js
+++ scripts/svg.js
@@ -1,28 +1,52 @@
 var path = require('path');
 var fs = require('fs');
-var svgDir = path.join(__dirname, "../src/assert/svg")
-var files = fs.readdirSync(svgDir);
-files = files.map(f => {
-    var name = f.replace('.svg', '')
-        .replace(/\.([a-z])/g, ($, $1) => { return $1.toUpperCase() })
-        .replace(/\./, "")
-        .replace(/^([a-z])/g, ($, $1) => { return $1.toUpperCase() }) + 'Svg';
-    return { name, file: f }
-});
-console.log(files);
 
 
+function makeRichSvg() {
+    var svgDir = path.join(__dirname, "../src/assert/svg")
+    var files = fs.readdirSync(svgDir);
+    files = files.map(f => {
+        var name = f.replace('.svg', '')
+            .replace(/\.([a-z])/g, ($, $1) => { return $1.toUpperCase() })
+            .replace(/\./, "")
+            .replace(/^([a-z])/g, ($, $1) => { return $1.toUpperCase() }) + 'Svg';
+        return { name, file: f }
+    });
 
-
-
-var str = `${files.map(f => {
-    return `import ${f.name} from "../src/assert/svg/${f.file}";`
-}).join("\n")}
+    var str = `${files.map(f => {
+        return `import ${f.name} from "../src/assert/svg/${f.file}";`
+    }).join("\n")}
 
 export  {
 ${files.map(f => f.name).join(",\n")}
 }`
 
-fs.writeFileSync(path.join(__dirname, "../component/svgs.ts"), str)
+    fs.writeFileSync(path.join(__dirname, "../component/svgs.ts"), str)
+}
+makeRichSvg();
+function makeShySvg() {
+    var svgDir = path.join(__dirname, "../../shy/src/assert/svg")
+    var files = fs.readdirSync(svgDir);
+    files = files.map(f => {
+        var name = f.replace('.svg', '')
+            .replace(/\.([a-z])/g, ($, $1) => { return $1.toUpperCase() })
+            .replace(/\./, "")
+            .replace(/^([a-z])/g, ($, $1) => { return $1.toUpperCase() }) + 'Svg';
+        return { name, file: f }
+    });
+
+    var str = `${files.map(f => {
+        return `import ${f.name} from "../assert/svg/${f.file}";`
+    }).join("\n")}
+
+export  {
+${files.map(f => f.name).join(",\n")}
+}`
+
+    fs.writeFileSync(path.join(__dirname, "../../shy/src/component/svgs.ts"), str)
+}
+makeShySvg()
+
+
 
 
