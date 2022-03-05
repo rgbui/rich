1fdf04d42e29f4e3ee912e5350f03b5dbae10f95
kanhai <760489033@qq.com>
2022-03-05 22:54:48 +0800

    调整userbasic 类型申明

---
+++ types/user.ts
@@ -0,0 +1,17 @@
+import { IconArguments } from "../extensions/icon/declare";
+
+export enum UserStatus {
+    offline = 0,
+    online = 1,
+    busy = 2,
+    idle = 3,
+    hidden = 4,
+}
+export type UserBasic = {
+    id: string,
+    avatar?: IconArguments,
+    cover?: IconArguments,
+    sn: number,
+    name: string,
+    status: UserStatus
+}
