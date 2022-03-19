47257ecd0f0642f4bf22f1be5164ec16914add62
kanhai <760489033@qq.com>
2022-03-20 01:01:27 +0800

    实现carousel效果

--- blocks/form/carousel/content.tsx
+++ blocks/form/carousel/content.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Carousel } from ".";
 import { Block } from "../../../src/block";
 import { url, view } from "../../../src/block/factory/observable";
 import { BlockView } from "../../../src/block/view";
@@ -7,6 +8,19 @@ import { ChildsArea } from "../../../src/block/view/appear";
 @url('/carousel/content')
 export class CarouselContent extends Block {
     partName: string = 'carousel-content';
+    get handleBlock(): Block {
+        return this.parent
+    }
+    get myCarousel() {
+        return this.parent as Carousel
+    }
+    get visibleStyle() {
+        var style = super.visibleStyle;
+        if (this.at != this.myCarousel.carouselIndex) {
+            style.display = 'none';
+        }
+        return style;
+    }
 }
 @view('/carousel/content')
 export class CarouselContentView extends BlockView<CarouselContent>{
--- blocks/form/carousel/index.tsx
+++ blocks/form/carousel/index.tsx
@@ -1,19 +1,81 @@
 import React from "react";
+import { ArrowLeftSvg, ArrowRightSvg, PlusSvg, TrashSvg } from "../../../component/svgs";
+import { Icon } from "../../../component/view/icon";
+import { useSelectMenuItem } from "../../../component/view/menu";
+import { MenuItemTypeValue } from "../../../component/view/menu/declare";
 import { Block } from "../../../src/block";
+import { BlockUrlConstant } from "../../../src/block/constant";
+import { BlockFactory } from "../../../src/block/factory/block.factory";
 import { url, view } from "../../../src/block/factory/observable";
 import { BlockView } from "../../../src/block/view";
 import { ChildsArea } from "../../../src/block/view/appear";
+import { Rect } from "../../../src/common/vector/point";
+import { ActionDirective } from "../../../src/history/declare";
+import "./style.less";
 
 @url('/carousel')
 export class Carousel extends Block {
     carouselIndex: number = 0;
+    async didMounted(): Promise<void> {
+        if (this.childs.length == 0) {
+            await this.createInitTabItems();
+            this.forceUpdate()
+        }
+    }
+    async createInitTabItems() {
+        this.blocks.childs = [];
+        this.blocks.childs.push(await BlockFactory.createBlock('/carousel/content',
+            this.page,
+            {
+                blocks: {
+                    childs: [{
+                        url: BlockUrlConstant.TextSpan,
+                        content: 'Content of Tab Pane 1'
+                    }]
+                }
+            }, this));
+    }
+    async onAddTabItem() {
+        this.onAction(ActionDirective.onCarouselAddItem, async () => {
+            await this.appendBlock({ url: '/carousel/content', blocks: { childs: [{ url: BlockUrlConstant.TextSpan, content: '内容' }] } }, this.blocks.childs.length, 'childs');
+        })
+    }
+    async onTabeItemContextmenu(event: React.MouseEvent, at: number) {
+        var um = await useSelectMenuItem(
+            { roundArea: Rect.fromEvent(event) },
+            [
+                { name: 'prev', text: '前移', disabled: at == 0 ? true : false, icon: ArrowLeftSvg },
+                { name: 'after', text: '后移', disabled: at == this.childs.length - 1 ? true : false, icon: ArrowRightSvg },
+                { type: MenuItemTypeValue.divide },
+                { name: 'delete', text: '删除', disabled: this.childs.length == 1 ? true : false, icon: TrashSvg },
+            ]
+        );
+        if (um) {
+            if (um.item.name == 'delete') {
+                this.onAction(ActionDirective.onCarouselRemoveItem, async () => {
+                    await this.blocks.childs[at].delete();
+                })
+            }
+        }
+    }
+    changeTabIndex(tabeIndex) {
+        this.carouselIndex = tabeIndex;
+        this.forceUpdate()
+    }
 }
 @view('/carousel')
 export class CarouselView extends BlockView<Carousel>{
     render() {
         return <div className='sy-block-carousel'
             style={this.block.visibleStyle}>
-            <div className="sy-block-carousel-indicators"></div>
+            <div className="sy-block-carousel-indicators" onMouseDown={e => e.stopPropagation()}>
+                {this.block.blocks.childs.map((c, index) => {
+                    return <div onContextMenu={e => this.block.onTabeItemContextmenu(e, index)} onClick={e => this.block.changeTabIndex(index)} key={c.id} className={"sy-block-carousel-indicator" + (this.block.carouselIndex == index ? " hover" : "")}></div>
+                })}
+                <div className="sy-block-carousel-indicator-plus" onClick={e => this.block.onAddTabItem()}>
+                    <Icon icon={PlusSvg} size={14}></Icon>
+                </div>
+            </div>
             <div className="sy-block-carousel-contents">
                 <ChildsArea childs={this.block.blocks.childs}></ChildsArea>
             </div>
