ecabd5d9d92e77c68b679b83ae5a990f25004191
kanhai <760489033@qq.com>
2022-03-27 17:09:59 +0800

    增加mind的排版类型、线类别显示

--- blocks/board/mind/index.tsx
+++ blocks/board/mind/index.tsx
@@ -11,12 +11,15 @@ import { BoardPointType, BoardBlockSelector } from "../../../src/block/partial/b
 import { Polygon } from "../../../src/common/vector/polygon";
 import { ActionDirective } from "../../../src/history/declare";
 import { Matrix } from "../../../src/common/matrix";
-
+import { BlockCssName } from "../../../src/block/pattern/css";
+import { BlockRenderRange } from "../../../src/block/enum";
 
 @url('/flow/mind')
 export class FlowMind extends Block {
     @prop()
     direction: 'none' | 'x' | 'y' = 'none';
+    @prop()
+    lineType: 'brokenLine' | 'cure' = 'cure';
     get mindDirection() {
         return this.mindRoot.direction;
     }
@@ -31,6 +34,12 @@ export class FlowMind extends Block {
     get allBlockKeys(): string[] {
         return ['childs', 'subChilds', 'otherChilds'];
     }
+    @prop()
+    fixedWidth: number = 60;
+    @prop()
+    fixedHeight: number = 30;
+    @prop()
+    lineColor: string = '#000';
     get fixedSize(): { width: number; height: number; } {
         if (this.mindEl) {
             return {
@@ -65,11 +74,33 @@ export class FlowMind extends Block {
                 poly: new Polygon(...pr.points.map(p => gm.transform(p)))
             }
         }))
+        pickers.push(...rect.centerPoints.map((pr, i) => {
+            var arrows: PointArrow[] = [];
+            if (i == 0) arrows = [PointArrow.top, PointArrow.center];
+            else if (i == 1) arrows = [PointArrow.middle, PointArrow.right];
+            else if (i == 2) arrows = [PointArrow.bottom, PointArrow.center]
+            else if (i == 3) arrows = [PointArrow.middle, PointArrow.left]
+            return {
+                type: BoardPointType.pathConnectPort,
+                arrows,
+                point: gm.transform(pr)
+            }
+        }))
         pickers.push(...extendRect.centerPoints.toArray((p, i) => {
             var type = BoardPointType.mindAdd;
+            if (!this.isMindRoot) {
+                if (this.parentKey == 'subChilds') {
+                    if (this.mindDirection == 'x' && i == 1) return;
+                    else if (this.mindDirection == 'y' && i == 2) return;
+                }
+                else if (this.parentKey == 'otherChilds') {
+                    if (this.mindDirection == 'x' && i == 3) return;
+                    else if (this.mindDirection == 'y' && i == 0) return;
+                }
+            }
             if (this.mindDirection != 'none') {
-                if (this.mindDirection == 'x' && (i == 1 || i == 3)) type = BoardPointType.connectPort;
-                else if (this.mindDirection == 'y' && (i == 0 || i == 2)) type = BoardPointType.connectPort;
+                if (this.mindDirection == 'x' && (i == 0 || i == 2)) type = BoardPointType.connectPort;
+                else if (this.mindDirection == 'y' && (i == 1 || i == 3)) type = BoardPointType.connectPort;
             }
             var arrows: PointArrow[] = [];
             if (i == 0) arrows = [PointArrow.top, PointArrow.center];
@@ -85,60 +116,43 @@ export class FlowMind extends Block {
         return pickers;
     }
     get mindEl() {
-        return (this.view as any).mindEl;
+        if ((this.view as any))
+            return (this.view as any).mindEl;
+    }
+    get deep() {
+        if (this.isMindRoot) return 0;
+        else return (this.parent as FlowMind).deep + 1;
     }
     async onPickerMousedown(selector: BoardBlockSelector, event: React.MouseEvent) {
         var self = this;
         if (selector.type == BoardPointType.mindAdd) {
             this.onAction(ActionDirective.onMindAddSub, async () => {
                 var keys = 'subChilds';
-                var matrix = new Matrix();
-                var size = this.fixedSize;
-                var d = 100;
                 if (selector.arrows.every(g => [PointArrow.top, PointArrow.center].includes(g))) {
-                    await this.updateProps({ direction: 'y' });
+                    if (this.isMindRoot) await this.updateProps({ direction: 'y' });
                     keys = 'subChilds';
-                    if (this.blocks.subChilds.length > 0) {
-                        var rect = RectUtility.getPointsBound(this.blocks.subChilds.map(s => (s as FlowMind).relativeFixedRect.rightBottom));
-                        matrix.translate(rect.rightBottom.x + 30, Math.min(rect.rightBottom.y, 100))
-                    }
-                    else matrix.translate(size.width / 2, 0 - d)
                 }
                 else if (selector.arrows.every(g => [PointArrow.bottom, PointArrow.center].includes(g))) {
-                    await this.updateProps({ direction: 'y' });
+                    if (this.isMindRoot) await this.updateProps({ direction: 'y' });
                     keys = 'otherChilds';
-                    if (this.blocks.otherChilds.length > 0) {
-                        var rect = RectUtility.getPointsBound(this.blocks.otherChilds.map(s => (s as FlowMind).relativeFixedRect.rightTop));
-                        matrix.translate(rect.rightTop.x + 30, Math.min(rect.rightTop.y, 100))
-                    }
-                    else matrix.translate(size.width / 2, size.height + d)
                 }
                 else if (selector.arrows.every(g => [PointArrow.middle, PointArrow.right].includes(g))) {
-                    await this.updateProps({ direction: 'x' });
+                    if (this.isMindRoot) await this.updateProps({ direction: 'x' });
                     keys = 'otherChilds';
-                    if (this.blocks.otherChilds.length > 0) {
-                        var rect = RectUtility.getPointsBound(this.blocks.otherChilds.map(s => (s as FlowMind).relativeFixedRect.leftBottom));
-                        matrix.translate(Math.min(rect.rightBottom.x, 100), rect.leftBottom.y + 30)
-                    }
-                    else matrix.translate(size.width + d, size.height / 2)
                 }
                 else if (selector.arrows.every(g => [PointArrow.middle, PointArrow.left].includes(g))) {
-                    await this.updateProps({ direction: 'x' });
+                    if (this.isMindRoot) await this.updateProps({ direction: 'x' });
                     keys = 'subChilds';
-                    if (this.blocks.subChilds.length > 0) {
-                        var rect = RectUtility.getPointsBound(this.blocks.subChilds.map(s => (s as FlowMind).relativeFixedRect.leftBottom));
-                        matrix.translate(Math.min(rect.rightBottom.x, -100), rect.leftBottom.y + 30)
-                    }
-                    else matrix.translate(0 - d, size.height / 2)
                 }
                 var newBlock = await self.page.createBlock('/flow/mind',
-                    { matrix: matrix.getValues() },
+                    {},
                     this,
                     undefined,
                     keys
                 );
+                await this.mindRoot.cacChildsFlowMind(true);
                 newBlock.mounted(() => {
-                    (self.view as any).updateFlowLine();
+                    self.mindRoot.updateAllFlowLines();
                     self.page.kit.picker.onPicker([newBlock]);
                 })
             })
@@ -158,8 +172,132 @@ export class FlowMind extends Block {
         var subRect = new Rect(point.x, point.y, s.width, s.height);
         return subRect;
     }
-    cacFlowMindLayout() {
+    cacDirectionRange(arrow: 'top' | 'bottom' | 'left' | 'right') {
+        var gap = 30;
+        if (arrow == 'left' || arrow == 'right') {
+            var h = 0;
+            this.blocks[arrow == 'left' ? "subChilds" : "otherChilds"].forEach((child, i) => {
+                var s = child.fixedSize;
+                if (i != 0) h += gap;
+                var cs = (child as FlowMind).cacDirectionRange(arrow);
+                h += Math.max(cs, s.height);
+            });
+            return h;
+        }
+        else {
+            var h = 0;
+            this.blocks[arrow == 'top' ? "subChilds" : "otherChilds"].forEach((child, i) => {
+                var s = child.fixedSize;
+                if (i != 0) h += gap;
+                var cs = (child as FlowMind).cacDirectionRange(arrow);
+                h += Math.max(cs, s.width);
+            });
+            return h;
+        }
+    }
+    async cacChildsFlowMind(deep?: boolean) {
+        var fs = this.fixedSize
+        var rect = new Rect(0, 0, fs.width, fs.height);
+        var offset = 100;
+        var gap = 30;
+        if (this.mindRoot.direction == 'x') {
+            var leftStart = 0 - (this.cacDirectionRange('left') / 2 - rect.height / 2);
+            await this.blocks.subChilds.eachAsync(async (item: FlowMind, i: number) => {
+                if (deep) await item.cacChildsFlowMind(deep);
+                var rc = item.cacDirectionRange('left');
+                var fs = item.fixedSize;
+                var matrix = new Matrix();
+                var h = Math.max(rc, fs.height);
+                matrix.translate(0 - offset - fs.width, leftStart + (h - fs.height) / 2);
+                leftStart += h;
+                leftStart += gap;
+                await item.updateMatrix(item.matrix, matrix);
+            });
+            var leftStart = 0 - (this.cacDirectionRange('right') / 2 - rect.height / 2);
+            await this.blocks.otherChilds.eachAsync(async (item: FlowMind, i: number) => {
+                if (deep) await item.cacChildsFlowMind(deep);
+                var rc = item.cacDirectionRange('right');
+                var fs = item.fixedSize;
+                var matrix = new Matrix();
+                var h = Math.max(rc, fs.height);
+                matrix.translate(offset + rect.width, leftStart + (h - fs.height) / 2);
+                leftStart += h;
+                leftStart += gap;
+                await item.updateMatrix(item.matrix, matrix);
+            });
+        }
+        else if (this.mindRoot.direction == 'y') {
+            var leftStart = rect.width / 2 - this.cacDirectionRange('top') / 2;
+            await this.blocks.subChilds.eachAsync(async (item: FlowMind, i: number) => {
+                if (deep) await item.cacChildsFlowMind(deep);
+                var rc = item.cacDirectionRange('top');
+                var fs = item.fixedSize;
+                var matrix = new Matrix();
+                var w = Math.max(rc, fs.width);
+                matrix.translate(leftStart + (w - fs.width) / 2, 0 - offset - fs.height);
+                leftStart += w;
+                leftStart += gap;
+                await item.updateMatrix(item.matrix, matrix);
+            });
+            var leftStart = rect.width / 2 - this.cacDirectionRange('bottom') / 2;
+            await this.blocks.otherChilds.eachAsync(async (item: FlowMind, i: number) => {
+                if (deep) await item.cacChildsFlowMind(deep);
+                var rc = item.cacDirectionRange('bottom');
+                var fs = item.fixedSize;
+                var matrix = new Matrix();
+                var w = Math.max(rc, fs.width);
+                matrix.translate(leftStart + (w - fs.width) / 2, offset + rect.height);
+                leftStart += w;
+                leftStart += gap;
+                await item.updateMatrix(item.matrix, matrix);
+            });
+        }
+    }
+    updateAllFlowLines() {
+        if (this.view) (this.view as any).updateFlowLine();
+        this.blocks.subChilds.forEach((c: FlowMind) => c.updateAllFlowLines());
+        this.blocks.otherChilds.forEach((c: FlowMind) => c.updateAllFlowLines());
+    }
+    async getBoardEditCommand(): Promise<{ name: string; value?: any; }[]> {
+        var bold = this.pattern.css(BlockCssName.font)?.fontWeight;
+        var cs: { name: string; value?: any; }[] = [];
+        if (this.isMindRoot) {
+            cs.push({ name: 'mindDirection', value: this.direction });
+            cs.push({ name: 'mindLineType', value: this.lineType });
+            cs.push({ name: 'mindLineColor', value: this.lineColor });
+            cs.push({ name: 'fontSize', value: Math.round(this.pattern.css(BlockCssName.font)?.fontSize || 14) });
+            cs.push({ name: 'fontWeight', value: bold == 'bold' || bold == 500 ? true : false });
+            cs.push({ name: 'fontStyle', value: this.pattern.css(BlockCssName.font)?.fontStyle == 'italic' ? true : false });
+            cs.push({ name: 'textDecoration', value: this.pattern.css(BlockCssName.font)?.textDecoration });
+            cs.push({ name: 'fontColor', value: this.pattern.css(BlockCssName.font)?.color });
+        }
+        else {
+            cs.push({ name: 'fontSize', value: Math.round(this.pattern.css(BlockCssName.font)?.fontSize || 14) });
+            cs.push({ name: 'fontWeight', value: bold == 'bold' || bold == 500 ? true : false });
+            cs.push({ name: 'fontStyle', value: this.pattern.css(BlockCssName.font)?.fontStyle == 'italic' ? true : false });
+            cs.push({ name: 'textDecoration', value: this.pattern.css(BlockCssName.font)?.textDecoration });
+            cs.push({ name: 'fontColor', value: this.pattern.css(BlockCssName.font)?.color });
+        }
+        return cs;
+    }
+    async setBoardEditCommand(name: string, value: any) {
+        if (this.isMindRoot) {
+            if (name == 'mindDirection') {
+                this.updateProps({ direction: value });
+                await this.cacChildsFlowMind(true);
+                this.updateAllFlowLines();
+            }
+            else if (name == 'mindLineType') {
+                this.updateProps({ lineType: value }, BlockRenderRange.self);
+            }
+            else if (name == 'mindLineColor') {
+                this.updateProps({ lineColor: value }, BlockRenderRange.self);
+            }
+            else (await super.setBoardEditCommand(name, value) == false)
+            {
 
+            }
+        }
     }
 }
 @view('/flow/mind')
@@ -171,21 +309,22 @@ export class FlowMindView extends BlockView<FlowMind>{
         if (this.flowMindLine && (this.block.blocks.subChilds.length > 0 || this.block.blocks.otherChilds.length > 0)) {
             var { width, height } = this.block.fixedSize;
             var rect = new Rect(0, 0, width, height);
-            var leftOrigin = this.block.direction != 'y' ? rect.leftMiddle : rect.topCenter;
+            var leftOrigin = this.block.mindDirection != 'y' ? rect.leftMiddle : rect.topCenter;
             var leftPoints = this.block.blocks.subChilds.map((sub: FlowMind) => {
                 var s = sub.fixedSize;
                 var subRect = new Rect(0, 0, s.width, s.height);
-                return sub.matrix.appended(sub.moveMatrix).transform(this.block.direction != 'y' ? subRect.rightMiddle : subRect.bottomCenter);
+                return sub.matrix.appended(sub.moveMatrix).transform(this.block.mindDirection != 'y' ? subRect.rightMiddle : subRect.bottomCenter);
             });
 
-            var rightOrigin = this.block.direction != 'y' ? rect.rightMiddle : rect.bottomCenter;
+            var rightOrigin = this.block.mindDirection != 'y' ? rect.rightMiddle : rect.bottomCenter;
             var rightPoints = this.block.blocks.otherChilds.map((sub: FlowMind) => {
                 var s = sub.fixedSize;
                 var subRect = new Rect(0, 0, s.width, s.height);
-                return sub.matrix.appended(sub.moveMatrix).transform(this.block.direction != 'y' ? subRect.leftMiddle : subRect.topCenter);
+                return sub.matrix.appended(sub.moveMatrix).transform(this.block.mindDirection != 'y' ? subRect.leftMiddle : subRect.topCenter);
             });
 
             this.flowMindLine.updateView(
+                this.block.mindDirection,
                 leftOrigin,
                 leftPoints,
                 rightOrigin,
@@ -194,7 +333,9 @@ export class FlowMindView extends BlockView<FlowMind>{
         }
     }
     renderItems() {
-        return <div className='sy-flow-mind-text' ref={e => this.mindEl = e} >
+        return <div className='sy-flow-mind-text'
+            style={{ minWidth: this.block.fixedWidth, minHeight: this.block.fixedHeight }}
+            ref={e => this.mindEl = e} >
             <TextSpanArea block={this.block}></TextSpanArea>
         </div>
     }
@@ -210,7 +351,7 @@ export class FlowMindView extends BlockView<FlowMind>{
         return <div className='sy-flow-mind' style={this.block.visibleStyle}>
             {this.renderItems()}
             {this.renderSubChilds()}
-            <FlowMindLine ref={e => this.flowMindLine = e}></FlowMindLine>
+            <FlowMindLine mind={this.block} ref={e => this.flowMindLine = e}></FlowMindLine>
         </div>
     }
 }
--- blocks/board/mind/line.tsx
+++ blocks/board/mind/line.tsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { FlowMind } from ".";
 import { Point, RectUtility, Rect } from "../../../src/common/vector/point";
-export class FlowMindLine extends React.Component {
+export class FlowMindLine extends React.Component<{ mind: FlowMind }>{
     private leftOrigin: Point;
     private leftPoints: Point[] = [];
     private rightOrigin: Point;
     private rightPoints: Point[] = [];
     private range: Rect;
+    direction: 'x' | 'y' | 'none';
     updateView(
+        direction: 'x' | 'y' | 'none',
         leftOrigin: Point,
         leftPoints: Point[],
         rightOrigin: Point,
         rightPoints: Point[]
     ) {
+        this.direction = direction;
         this.leftOrigin = leftOrigin;
         this.leftPoints = leftPoints;
         this.rightOrigin = rightOrigin;
@@ -25,23 +29,82 @@ export class FlowMindLine extends React.Component {
         this.forceUpdate();
     }
     renderLines() {
-        var d = 40;
-        return <>
-            {this.leftPoints.map((point, index) => {
-                return <path key={index} d={`M${this.leftOrigin.join(' ')} C ${this.leftOrigin.move(d, 0).join(' ')}, ${point.move(0 - d, 0).join(' ')}, ${point.join(' ')}`} >
-                </path>
-            })}
-            {this.rightPoints.map((point, index) => {
-                return <path key={'right'+index} d={`M${this.rightOrigin.join(' ')} C ${this.rightOrigin.move(d, 0).join(' ')}, ${point.move(0 - d, 0).join(' ')}, ${point.join(' ')}`} >
-                </path>
-            })}
-        </>
+        if (this.props.mind.mindRoot.lineType == 'brokenLine') {
+            var r = 0.66;
+            if (this.direction == 'x') {
+                return <>
+                    {this.leftPoints.map((point, index) => {
+                        var lx = (point.x - this.leftOrigin.x) * r;
+                        var px = (this.leftOrigin.x - point.x) * (1 - r);
+                        var dString = `M${this.leftOrigin.join(" ")}L${this.leftOrigin.move(lx, 0).join(" ")}L${point.move(px, 0).join(" ")}L${point.join(" ")}`;
+                        return <path key={index} d={dString}  ></path>
+                    })}
+                    {this.rightPoints.map((point, index) => {
+                        var lx = (point.x - this.rightOrigin.x) * r;
+                        var px = (this.rightOrigin.x - point.x) * (1 - r);
+                        var dString = `M${this.rightOrigin.join(" ")}L${this.rightOrigin.move(lx, 0).join(" ")}L${point.move(px, 0).join(" ")}L${point.join(" ")}`;
+                        return <path key={'right' + index} d={dString}></path>
+                    })}
+                </>
+            }
+            else {
+                return <>
+                    {this.leftPoints.map((point, index) => {
+                        var lx = (point.y - this.leftOrigin.y) * r;
+                        var px = (this.leftOrigin.y - point.y) * (1 - r);
+                        var dString = `M${this.leftOrigin.join(" ")}L${this.leftOrigin.move(0, lx).join(" ")}L${point.move(0, px).join(" ")}L${point.join(" ")}`;
+                        return <path key={index} d={dString} >
+                        </path>
+                    })}
+                    {this.rightPoints.map((point, index) => {
+                        var lx = (point.y - this.rightOrigin.y) * r;
+                        var px = (this.rightOrigin.y - point.y) * (1 - r);
+                        var dString = `M${this.rightOrigin.join(" ")}L${this.rightOrigin.move(0, lx).join(" ")}L${point.move(0, px).join(" ")}L${point.join(" ")}`;
+                        return <path key={'right' + index} d={dString} >
+                        </path>
+                    })}
+                </>
+            }
+        }
+        else {
+            var d = 40;
+            if (this.direction == 'x') {
+                return <>
+                    {this.leftPoints.map((point, index) => {
+                        return <path key={index} d={`M${this.leftOrigin.join(' ')} C ${this.leftOrigin.move(0 - d, 0).join(' ')}, ${point.move(d, 0).join(' ')}, ${point.join(' ')}`} >
+                        </path>
+                    })}
+                    {this.rightPoints.map((point, index) => {
+                        return <path key={'right' + index} d={`M${this.rightOrigin.join(' ')} C ${this.rightOrigin.move(d, 0).join(' ')}, ${point.move(0 - d, 0).join(' ')}, ${point.join(' ')}`} >
+                        </path>
+                    })}
+                </>
+            }
+            else {
+                return <>
+                    {this.leftPoints.map((point, index) => {
+                        return <path key={index} d={`M${this.leftOrigin.join(' ')} C ${this.leftOrigin.move(0, 0 - d).join(' ')}, ${point.move(0, d).join(' ')}, ${point.join(' ')}`} >
+                        </path>
+                    })}
+                    {this.rightPoints.map((point, index) => {
+                        return <path key={'right' + index} d={`M${this.rightOrigin.join(' ')} C ${this.rightOrigin.move(0, d).join(' ')}, ${point.move(0, 0 - d).join(' ')}, ${point.join(' ')}`} >
+                        </path>
+                    })}
+                </>
+            }
+        }
     }
     render() {
-        if (!this.leftOrigin) return <div style={{ display: 'none' }}></div>
+        if (!this.leftOrigin) return <></>;
         return <svg
             className="sy-flow-mind-line"
-            style={{ top: this.range.top, left: this.range.left, width: this.range.width, height: this.range.height }}
+            style={{
+                top: this.range.top,
+                left: this.range.left,
+                width: this.range.width,
+                height: this.range.height,
+                stroke: this.props.mind.mindRoot.lineColor
+            }}
             viewBox={`${this.range.left} ${this.range.top} ${this.range.width} ${this.range.height} `}>{this.renderLines()}</svg>
     }
 }
