cdc2d4a291bfb0eab7af1e37895584fee9cfc05d
kanhai <760489033@qq.com>
2022-04-08 00:34:21 +0800

    调整frame块

--- src/block/element/frame/index.tsx
+++ src/block/element/frame/index.tsx
@@ -8,24 +8,73 @@ import { BlockAppear } from '../../appear';
 import "./style.less";
 import { BoardPointType, BoardBlockSelector } from '../../partial/board';
 import { BlockCssName } from '../../pattern/css';
+import { Rect, RectUtility, PointArrow } from '../../../common/vector/point';
+import { Polygon } from '../../../common/vector/polygon';
 /**
  * 可以将相邻的block变成一个整体去操作，
  * 可以看成是contentBlock
  */
 @url('/frame')
 export class Frame extends Block {
-    async created(this: Block): Promise<void>{
+    async created(this: Block): Promise<void> {
         this.pattern.setFillStyle({ color: '#fff' });
     }
     @prop()
-    frameTitle: string = '';
+    content: string = '框';
     @prop()
     fixedWidth: number = 800;
     @prop()
     fixedHeight: number = 300;
     getBlockBoardSelector(this: Block, types?: BoardPointType[]): BoardBlockSelector[] {
-        var pickers = super.getBlockBoardSelector();
-        pickers.removeAll(p => p.type == BoardPointType.rotatePort);
+        var h = 30;
+        var pickers: BoardBlockSelector[] = [];
+        var { width, height } = this.fixedSize;
+        var rect = new Rect(0, h, width, height - h);
+        var gm = this.globalWindowMatrix;
+        // var gs = gm.resolveMatrixs();
+        /**
+         * 这里基本没有skew，只有scale,rotate,translate
+         * scale 水平和垂直相等
+         */
+        var pathRects = RectUtility.getRectLineRects(rect, this.realPx(1));
+        if (types.includes(BoardPointType.path))
+            pickers.push(...pathRects.map((pr, i) => {
+                var arrows: PointArrow[] = [];
+                if (i == 0) arrows = [PointArrow.top, PointArrow.center];
+                else if (i == 1) arrows = [PointArrow.middle, PointArrow.right];
+                else if (i == 2) arrows = [PointArrow.bottom, PointArrow.center]
+                else if (i == 3) arrows = [PointArrow.middle, PointArrow.left]
+                return {
+                    type: BoardPointType.path,
+                    arrows,
+                    poly: new Polygon(...pr.points.map(p => gm.transform(p)))
+                }
+            }))
+        if (types.includes(BoardPointType.pathConnectPort))
+            pickers.push(...rect.centerPoints.map((pr, i) => {
+                var arrows: PointArrow[] = [];
+                if (i == 0) arrows = [PointArrow.top, PointArrow.center];
+                else if (i == 1) arrows = [PointArrow.middle, PointArrow.right];
+                else if (i == 2) arrows = [PointArrow.bottom, PointArrow.center]
+                else if (i == 3) arrows = [PointArrow.middle, PointArrow.left]
+                return {
+                    type: BoardPointType.pathConnectPort,
+                    arrows,
+                    point: gm.transform(pr)
+                }
+            }))
+        if (types.includes(BoardPointType.resizePort)) pickers.push(...rect.points.map((p, i) => {
+            var arrows: PointArrow[] = [];
+            if (i == 0) arrows = [PointArrow.top, PointArrow.left];
+            else if (i == 1) arrows = [PointArrow.top, PointArrow.right];
+            else if (i == 2) arrows = [PointArrow.bottom, PointArrow.right]
+            else if (i == 3) arrows = [PointArrow.bottom, PointArrow.left]
+            return {
+                type: BoardPointType.resizePort,
+                arrows,
+                point: gm.transform(p)
+            }
+        }))
         return pickers;
     }
     async getBoardEditCommand(this: Block): Promise<{ name: string; value?: any; }[]> {
@@ -37,19 +86,20 @@ export class Frame extends Block {
     async setBoardEditCommand(this: Block, name: string, value: any) {
         if (name == 'backgroundColor')
             this.pattern.setFillStyle({ color: value, mode: 'color' });
-       
     }
 }
 @view('/frame')
 export class FrameView extends BlockView<Frame>{
     render() {
+        var h = 30;
+        var h20 = 20;
         var style = Object.assign({ width: this.block.fixedWidth, height: this.block.fixedHeight }, this.block.visibleStyle);
         return <div className='sy-block-frame' style={style} >
-            <div className='sy-block-frame-head'>
+            <div className='sy-block-frame-head' style={{ height: h, lineHeight: h + 'px', fontSize: h20 / 1.2 }}>
                 <TextArea placeholder='框' rf={e => this.block.elementAppear({ el: e, appear: BlockAppear.text })} html={this.block.content}></TextArea>
             </div>
-            <div className='sy-block-frame-content'>
-                <ChildsArea childs={this.block.childs}></ChildsArea>
+            <ChildsArea childs={this.block.childs}></ChildsArea>
+            <div className='sy-block-frame-content' style={{ top: h }}>
             </div>
         </div>
     }
