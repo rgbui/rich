a7f12a4376c9e76fda3e1237f401f615386eb9e9
kanhai <760489033@qq.com>
2022-05-03 11:40:50 +0800

    增加relativeEleAutoScroll 属性，弹窗依赖固定相对于某个元素

--- extensions/popover/position.ts
+++ extensions/popover/position.ts
@@ -1,6 +1,9 @@
 import { Point, Rect } from "../../src/common/vector/point";
 
 export type PopoverPosition = {
+    /**
+     * 是否为中心点的位置
+     */
     center?: boolean;
     /**
      * 围绕一个点来计算弹窗
@@ -15,6 +18,10 @@ export type PopoverPosition = {
      */
     roundArea?: Rect;
     /**
+     * 围绕多个元素
+     */
+    roundAreas?: Rect[];
+    /**
      * 元素本身的大小
      */
     elementArea?: Rect,
@@ -22,5 +29,11 @@ export type PopoverPosition = {
     offset?: number,
     align?: 'start' | 'center' | 'end',
     direction?: 'top' | 'left' | 'bottom' | 'right',
-    relativePoint?: Point
+    relativePoint?: Point,
+    /**
+     * 相对某个元素，滚动条自由滚动，
+     * 例如文本编辑器，滚动时，自适应，
+     * 这里还需要考虑平移的情况（暂时不考虑）
+     */
+    relativeEleAutoScroll?: HTMLElement,
 }
