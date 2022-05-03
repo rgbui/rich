c79d695f7c911c2098ca9fa0d830f4deb5705a69
kanhai <760489033@qq.com>
2022-05-03 16:29:52 +0800

    移除老的代码

--- src/page/interaction/refrence.selector.ts
+++
@@ -1,5 +0,0 @@
-import { Page } from "..";
-import { AtSelector } from "../../../extensions/at";
-export function PageReferenceSelector(page: Page, selector: AtSelector) {
-    selector.on('error', err => page.onError(err));
-}
