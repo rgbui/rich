aebe9c1dea9fd9b23a0289949c30324116212960
kanhai <760489033@qq.com>
2022-05-05 22:54:35 +0800

    调整只有当前文档是isBoard时才显示

--- src/kit/scale/index.tsx
+++ src/kit/scale/index.tsx
@@ -6,10 +6,12 @@ import MinusSvg from "../../assert/svg/minus.svg";
 import FitSvg from "../../assert/svg/fit.svg";
 import { Icon } from "../../../component/view/icon";
 import { Point } from "../../common/vector/point";
+
 export class BoardScale extends React.Component<{ kit: Kit }>{
     render(): ReactNode {
+        if (this.props.kit.page.isBoard) return <></>
         return <div className="shy-board-scale">
-            <Icon icon={FitSvg} mousedown={e=>this.props.kit.page.onFitZoom()}></Icon>
+            <Icon icon={FitSvg} mousedown={e => this.props.kit.page.onFitZoom()}></Icon>
             <Icon icon={PlusSvg} mousedown={e => this.props.kit.page.onZoom(25, Point.from(e))} ></Icon>
             <span>{(this.props.kit.page.scale * 100).toFixed(0)}%</span>
             <Icon icon={MinusSvg} mousedown={e => this.props.kit.page.onZoom(-25, Point.from(e))} ></Icon>
