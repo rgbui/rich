3c716d64e1b873ba7d7223f3ec1d2ddd8533f167
kanhai <760489033@qq.com>
2022-05-22 22:34:09 +0800

    实现SysAlert组件

--- component/lib/alert.tsx
+++ component/lib/alert.tsx
@@ -1,54 +1,21 @@
-import React from "react";
-import { Singleton } from "./Singleton";
-import { EventsComponent } from "./events.component";
-import { Button } from "../view/button";
-import "./style.less";
-import { Space } from "../view/grid";
-import {  tipLayer } from "./zindex";
-class SyAlert extends EventsComponent {
-    private msg: string;
-    private description: string;
-    private visible: boolean = false;
-    open(msg: string, description?: string) {
-        this.msg = msg;
-        this.description = description;
-        this.visible = true;
-        this.forceUpdate();
+
+
+var alertEle: HTMLElement;
+var time;
+export function ShyAlert(msg: string, level?: 'success' | 'info' | 'fail' | 'warn' | 'load') {
+    if (!alertEle) {
+        alertEle = document.createElement('div');
+        alertEle.classList.add('.shy-alert');
+        alertEle.innerHTML = `<div className='shy-alert-msg'>${msg}</div>`;
+        document.body.appendChild(alertEle);
     }
-    close() {
-        this.visible = false;
-        tipLayer.clear(this)
-        this.forceUpdate()
-    }
-    onConfirm() {
-        this.emit('confirm');
-        this.close()
-    }
-    onCancel() {
-        this.emit('cancel');
-        this.close()
-    }
-    render() {
-        return this.visible && <div className='shy-alert-box' style={{ zIndex: tipLayer.zoom(this) }}>
-            <div className='shy-alert-mask' onMouseDown={e => this.onCancel()}></div>
-            <div className='shy-alert'>
-                <div className='shy-alert-msg'>{this.msg}</div>
-                {this.description && <div className='shy-alert-description'>{this.description}</div>}
-                <div className='shy-alert-buttons' style={{ marginTop: 20 }}>
-                    <Space align="center">
-                        <Button block onClick={e => this.onConfirm()}>确定</Button>
-                    </Space>
-                </div>
-            </div>
-        </div>
-    }
+    alertEle.style.display = 'block';
+    alertEle.querySelector('.shy-alert-msg').innerHTML = msg;
+    if (!time) { clearTimeout(time); time = null; }
+    time = setTimeout(() => {
+        alertEle.style.display = 'none';
+    },3e3);
 }
-var syAlert: SyAlert;
-export async function Alert(msg: string, description?: string) {
-    syAlert = await Singleton(SyAlert);
-    syAlert.open(msg, description);
-    return new Promise((resolve, reject) => {
-        syAlert.only('confirm', () => resolve(true))
-        syAlert.only('cancel', () => resolve(true))
-    })
-}
+
+
+
