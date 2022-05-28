8e4166cd7775a811335eb0354a0cb7c87ecf2a49
kanhai <760489033@qq.com>
2022-05-28 22:03:09 +0800

    增加日期、字节的显示信息

--- util/util.ts
+++ util/util.ts
@@ -24,5 +24,50 @@ export var util = {
                 resolve(true);
             }, time);
         })
+    },
+    timeToString(time: number) {
+        if (typeof time != 'number' || isNaN(time)) return '';
+        var t = time;
+        var ps: string[] = [];
+        if (t > 1000 * 60 * 60 * 24) {
+            ps.push(Math.floor(t / (1000 * 60 * 60 * 24)) + '天');
+            t = t % (1000 * 60 * 60 * 24)
+        }
+        if (t > 1000 * 60 * 60) {
+            ps.push(Math.floor(t / (1000 * 60 * 60)) + '时');
+            t = t % (1000 * 60 * 60)
+        }
+        if (t > 1000 * 60) {
+            ps.push(Math.floor(t / (1000 * 60)) + '分');
+            t = t % (1000 * 60)
+        }
+        if (t >= 1000) {
+            ps.push(Math.floor(t / 1000) + '秒');
+            t = t % 1000
+        }
+        if (t >= 0 && ps.length == 0) {
+            ps.push(t + 'ms');
+        }
+        return ps.join('')
+    },
+    byteToString(byte: number) {
+        if (byte > 1024 * 1024 * 1024 * 1024 * 1024) {
+            return (byte / (1024 * 1024 * 1024 * 1024 * 1024)).toFixed(2) + "PB"
+        }
+        else if (byte > 1024 * 1024 * 1024 * 1024) {
+            return (byte / (1024 * 1024 * 1024 * 1024)).toFixed(2) + "T"
+        }
+        else if (byte > 1024 * 1024 * 1024) {
+            return (byte / (1024 * 1024 * 1024)).toFixed(2) + "G"
+        }
+        else if (byte > 1024 * 1024) {
+            return (byte / (1024 * 1024)).toFixed(2) + "M"
+        }
+        else if (byte > 1024) {
+            return (byte / 1024).toFixed(2) + "KB"
+        }
+        else if (byte > 0) {
+            return (byte).toFixed(2) + "B"
+        }
     }
 }
