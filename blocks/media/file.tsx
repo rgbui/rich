7a406777f9fb125377b5c18627f9ea22198ca0b5
kanhai <760489033@qq.com>
2022-05-28 22:50:52 +0800

    实现文件块

--- blocks/media/file.tsx
+++ blocks/media/file.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { ResourceArguments } from "../../extensions/icon/declare";
 import { Block } from "../../src/block";
-import AudioSvg from "../../src/assert/svg/audio.svg";
 import { BlockDisplay, BlockRenderRange } from "../../src/block/enum";
 import { url, prop, view } from "../../src/block/factory/observable";
-import { SolidArea } from "../../src/block/view/appear";
 import { BlockView } from "../../src/block/view";
 import { Rect } from "../../src/common/vector/point";
 import { useFilePicker } from "../../extensions/file/file.picker";
-import { LangID } from "../../i18n/declare";
-import { Sp } from "../../i18n/view";
 import { channel } from "../../net/channel";
+import { DownloadSvg, FileSvg, PropertysSvg } from "../../component/svgs";
+import { Icon } from "../../component/view/icon";
+import { util } from "../../util/util";
 
 
 @url('/file')
@@ -40,18 +39,34 @@ export class File extends Block {
             }
             if (this.initialData && this.initialData.file) {
                 var d = await channel.post('/ws/upload/file', {
-                    file: this.initialData.file, uploadProgress: (event) => {
+                    file: this.initialData.file,
+                    uploadProgress: (event) => {
                         console.log(event, 'ev');
                     }
                 });
                 if (d.ok && d.data?.file?.url) {
-                    await this.onUpdateProps({ src: { url: d.data?.file?.url, name: 'upload' } }, BlockRenderRange.self);
+                    await this.onUpdateProps({
+                        src: {
+                            url: d.data?.file?.url,
+                            text: d.data?.file?.name,
+                            size: d.data?.file?.size,
+                            name: 'upload'
+                        }
+                    }, BlockRenderRange.self);
                 }
             }
             if (this.initialData && this.initialData.url) {
                 var d = await channel.post('/ws/download/url', { url: this.initialData.url });
                 if (d.ok && d.data?.file?.url) {
-                    await this.onUpdateProps({ src: { url: d.data?.file?.url, name: 'download', source: this.initialData.url } }, BlockRenderRange.self);
+                    await this.onUpdateProps({
+                        src: {
+                            url: d.data?.file?.url,
+                            text: d.data?.file?.name,
+                            size: d.data?.file?.size,
+                            name: 'download',
+                            source: this.initialData.url
+                        }
+                    }, BlockRenderRange.self);
                 }
             }
         }
@@ -64,12 +79,16 @@ export class File extends Block {
 export class FileView extends BlockView<File>{
     render() {
         return <div className='sy-block-file' style={this.block.visibleStyle}>
-            {this.block.src.name == 'none' && <div onMouseDown={e => this.block.addFile(e)} className='sy-block-audio-nofile'>
-                <AudioSvg></AudioSvg>
-                <Sp id={LangID.AddAudioTip}></Sp>
+            {this.block.src.name == 'none' && <div onMouseDown={e => this.block.addFile(e)} className='sy-block-file-nofile'>
+                <Icon icon={FileSvg}></Icon>
+                <span>添加附件</span>
             </div>}
-            {this.block.src.name != 'none' && <SolidArea rf={e => this.block.elementAppear({ el: e, prop: 'src' })}>
-            </SolidArea>}
+            {this.block.src.name != 'none' && <div className='sy-block-file-content' >
+                <Icon icon={FileSvg} size={18} className='sy-block-file-content-icon'></Icon>
+                <span className='sy-block-file-content-title'>{this.block.src.text}</span>
+                <span className='sy-block-file-content-bytes'>{util.byteToString(this.block.src.size)}</span>
+                <a className='sy-block-file-content-link' download={this.block.src.url}><Icon  size={30} icon={DownloadSvg}></Icon></a>
+            </div>}
         </div>
     }
 }
