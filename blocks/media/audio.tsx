26767d390e22aa530eaba331f273a56d6cc1fcdf
kanhai <760489033@qq.com>
2022-05-28 22:10:02 +0800

    实现audio块

--- blocks/media/audio.tsx
+++ blocks/media/audio.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { ResourceArguments } from "../../extensions/icon/declare";
-
 import { prop, url, view } from "../../src/block/factory/observable";
 import { BlockView } from "../../src/block/view";
-import AudioSvg from "../../src/assert/svg/audio.svg";
 import { Sp } from "../../i18n/view";
 import { LangID } from "../../i18n/declare";
 import { useAudioPicker } from "../../extensions/file/audio.picker";
@@ -12,7 +10,11 @@ import { Block } from "../../src/block";
 import { BlockDisplay, BlockRenderRange } from "../../src/block/enum";
 import { SolidArea } from "../../src/block/view/appear";
 import { channel } from "../../net/channel";
-
+import { AudioSvg } from "../../component/svgs";
+import { Icon } from "../../component/view/icon";
+/*
+ * https://howlerjs.com/
+ */
 @url('/audio')
 export class Audio extends Block {
     @prop()
@@ -66,11 +68,11 @@ export class AudioView extends BlockView<Audio>{
     render() {
         return <div className='sy-block-audio' style={this.block.visibleStyle}>
             {this.block.src.name == 'none' && <div onMouseDown={e => this.block.addAudio(e)} className='sy-block-audio-nofile'>
-                <AudioSvg></AudioSvg>
+                <Icon icon={AudioSvg}></Icon>
                 <Sp id={LangID.AddAudioTip}></Sp>
             </div>}
             {this.block.src.name != 'none' && <div className='sy-block-audio-content'>
-                <SolidArea rf={e => this.block.elementAppear({ el: e, prop: "src" })}><audio src={this.block.src.url}></audio></SolidArea>
+                <audio controls style={{ width: 'inherit', height: 'inherit' }} src={this.block.src.url}></audio>
             </div>}
         </div>
     }
