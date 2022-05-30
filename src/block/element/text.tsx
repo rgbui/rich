7af629187ddbf66e3fd551ed5bb7ee4401b8bab0
kanhai <760489033@qq.com>
2022-05-31 00:16:35 +0800

    实现text文本的显示

--- src/block/element/text.tsx
+++ src/block/element/text.tsx
@@ -6,6 +6,8 @@ import { prop, url, view } from "../factory/observable";
 import { TextArea } from "../view/appear";
 import { PageLink } from "../../../extensions/link/declare";
 import { Block } from "..";
+import { channel } from "../../../net/channel";
+
 /***
  * 文字型的block，
  * 注意该文字block里面含有子文字或其它的如图像block等
@@ -39,12 +41,17 @@ export class TextContent extends Block {
 }
 @view('/text')
 export class TextContentView extends BlockView<TextContent>{
+    openPage(e: React.MouseEvent) {
+        e.stopPropagation();
+        e.preventDefault();
+        channel.air('/page/open', { item: { id: this.block.link.pageId } });
+    }
     render() {
         var ta = <TextArea block={this.block} prop='content' ></TextArea>
         var classList: string[] = ['sy-block-text-content'];
         if (this.block.link) {
             if (this.block.link.pageId) {
-                ta = <a href={'/page/' + this.block.link.pageId}>{ta}</a>
+                ta = <a onClick={e => e.preventDefault()} onMouseDown={e => this.openPage(e)} href={'/page/' + this.block.link.pageId}>{ta}</a>
             }
             else if (this.block.link.url) {
                 ta = <a target='_blank' href={this.block.link.url}>{ta}</a>
