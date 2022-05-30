23c89eb92d0a186a778c201c300e3c3004d583b6
kanhai <760489033@qq.com>
2022-05-30 21:24:27 +0800

    增加quote添加子块功能

--- blocks/general/quote.tsx
+++ blocks/general/quote.tsx
@@ -1,13 +1,29 @@
 import { BlockView } from "../../src/block/view";
 import React from 'react';
 import { url, view } from "../../src/block/factory/observable";
-import { TextArea, TextLineChilds } from "../../src/block/view/appear";
+import { ChildsArea, TextArea, TextLineChilds } from "../../src/block/view/appear";
 import { TextSpan } from "../../src/block/element/textspan";
 import { BlockDisplay } from "../../src/block/enum";
 import { TextTurns } from "../../src/block/turn/text";
+import { Block } from "../../src/block";
 @url('/quote')
 export class Quote extends TextSpan {
     display = BlockDisplay.block;
+    blocks: { childs: Block[], subChilds: Block[] } = { childs: [], subChilds: [] };
+    get allBlockKeys(): string[] {
+        return ['childs', 'subChilds'];
+    }
+    get childKey() {
+        return 'subChilds';
+    }
+    /**
+ * 表示当前元素如何接收该元素至sub,
+ * @param this 
+ * @param sub  子元素是要移动的
+ */
+    async acceptSubFromMove(sub: Block) {
+        await this.append(sub, 0, 'subChilds');
+    }
     get appearAnchors() {
         if (this.childs.length > 0) return []
         return this.__appearAnchors;
@@ -25,8 +41,13 @@ export class QuoteView extends BlockView<Quote>{
         return <div className='sy-block-quote' style={this.block.visibleStyle} >
             <div className='sy-block-quote-bar'></div>
             <div className='sy-block-quote-content'>
-                {this.block.childs.length > 0 && <TextLineChilds childs={this.block.childs} rf={e => this.block.childsEl = e}></TextLineChilds>}
-                {this.block.childs.length == 0 && <TextArea  block={this.block}   prop='content'  placeholder={'键入文字或"/"选择'}></TextArea>}
+                <div>
+                    {this.block.childs.length > 0 && <TextLineChilds childs={this.block.childs} rf={e => this.block.childsEl = e}></TextLineChilds>}
+                    {this.block.childs.length == 0 && <TextArea block={this.block} prop='content' placeholder={'键入文字或"/"选择'}></TextArea>}
+                </div>
+                <div>
+                    <ChildsArea childs={this.block.blocks.subChilds}></ChildsArea>
+                </div>
             </div>
         </div>
     }
