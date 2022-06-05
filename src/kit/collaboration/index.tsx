5ca382ddc1e9067cf73bbf07cc51855733700b5e
kanhai <760489033@qq.com>
2022-06-06 00:48:47 +0800

    修复协作时偏移的光标

--- src/kit/collaboration/index.tsx
+++ src/kit/collaboration/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Kit } from "..";
 import { Avatar } from "../../../component/view/avator/face";
 import { FixedViewScroll } from "../../common/scroll";
-import { TextEle } from "../../common/text.ele";
 import { Point } from "../../common/vector/point";
 import "./style.less";
 export class Collaboration extends React.Component<{ kit: Kit }>{
@@ -11,7 +10,8 @@ export class Collaboration extends React.Component<{ kit: Kit }>{
         this.fvs.on('change', (offset: Point) => {
             if (this.el) {
                 this.offset = offset.clone();
-                this.el.style.transform = `translate(${offset.x}px,${offset.y}px)`
+                this.forceUpdate();
+                // this.el.style.transform = `translate(${offset.x}px,${offset.y}px)`
             }
         })
     }
@@ -23,7 +23,7 @@ export class Collaboration extends React.Component<{ kit: Kit }>{
     }
     private fvs: FixedViewScroll = new FixedViewScroll();
     private offset: Point = new Point(0, 0);
-    users: { userid: string, point?: Point }[] = [];
+    users: { userid: string, point?: Point, offset?: Point }[] = [];
     push(userid: string, operate: { blockId: string, prop: string, offset: 0 }) {
         try {
             var block = this.props.kit.page.find(g => g.id == operate.blockId);
@@ -33,16 +33,17 @@ export class Collaboration extends React.Component<{ kit: Kit }>{
                     var rect = rowBlock.getVisibleBound();
                     if (this.users.some(s => s.userid == userid)) {
                         var ur = this.users.find(g => g.userid == userid);
-                        ur.point = rect.leftTop.relative(this.offset).move(-30, 0)
+                        ur.point = rect.leftTop.move(-30, 0);
+                        ur.offset = this.offset.clone();
                     }
                     else {
-                        this.users.push({ userid, point: rect.leftTop.relative(this.offset).move(-30, 0) })
+                        this.users.push({ userid, offset: this.offset.clone(), point: rect.leftTop.move(-30, 0) })
                     }
                     this.forceUpdate();
                     return;
                 }
             }
-            if (userid) {
+            else if (userid) {
                 this.users.remove(g => g.userid == userid);
                 this.forceUpdate();
             }
@@ -56,7 +57,7 @@ export class Collaboration extends React.Component<{ kit: Kit }>{
     render() {
         return <div className="shy-collaboration" ref={e => this.el = e} >
             {this.users.map(u => {
-                return <div key={u.userid} className="shy-collaboration-user" style={{ top: u.point.y, left: u.point.x }} ><Avatar size={24} userid={u.userid}></Avatar></div>
+                return <div key={u.userid} className="shy-collaboration-user" style={{ top: u.point.y + (this.offset.y - u.offset.y), left: u.point.x + (this.offset.x - u.offset.x) }} ><Avatar size={24} userid={u.userid}></Avatar></div>
             })}
         </div>
     }
