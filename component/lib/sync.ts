545d32511bc273457ece284e9bb8a02236e9394a
rgbui <760489033@qq.com>
2022-06-13 19:46:04 +0800

    增加sync load方法

---
+++ component/lib/sync.ts
@@ -0,0 +1,43 @@
+
+
+
+
+
+export class SyncLoad<T>{
+    private cs: {
+        obj: T,
+        isCreated: boolean,
+        create: (callback: (obj: T) => void) => void,
+        events: { callback: (obj: T) => void }[]
+    } = { obj: null, isCreated: false, create: null, events: [] };
+    create(create: (callback: (obj: T) => void) => void): Promise<T> {
+        var self = this;
+        if (this.cs.obj) return this.cs.obj as any;
+        else {
+            if (this.cs.isCreated == false) {
+                this.cs.isCreated = true;
+                create(function (c) {
+                    self.cs.obj = c;
+                    self.cs.events.forEach(ev => {
+                        ev.callback(c);
+                    });
+                    self.cs.events = [];
+                })
+            }
+            return new Promise((resolve, reject) => {
+                self.cs.events.push({
+                    callback: (c) => {
+                        resolve(c);
+                    }
+                });
+            })
+
+        }
+    }
+    get obj() {
+        return this.cs.obj;
+    }
+}
+
+
+
