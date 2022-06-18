8cf47718a8199e8645c218899e1d2612e045ccc6
kanhai <760489033@qq.com>
2022-06-18 16:42:18 +0800

    实现显示mention块

--- blocks/at/mention.tsx
+++ blocks/at/mention.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { Avatar } from "../../component/view/avator/face";
 import { UserBox } from "../../component/view/avator/user";
 import { Block } from "../../src/block";
 import { BlockDisplay } from "../../src/block/enum";
 import { url, prop, view } from "../../src/block/factory/observable";
 import { BlockView } from "../../src/block/view";
 import "./style.less";
-
 @url('/user/mention')
 export class ShyMention extends Block {
     @prop()
@@ -21,8 +19,7 @@ export class ShyMentionView extends BlockView<ShyMention>{
     render() {
         return <div className='sy-block-mention' onMouseDown={e => this.block.openUser(e)} >
             <UserBox userid={this.block.userid}>{(user) => {
-                console.log('user', user);
-                return <><Avatar size={30} userid={user.id} user={user}></Avatar><span>{user.name}</span></>
+                return <>@<span>{user.name}</span></>
             }}</UserBox>
         </div>
     }
