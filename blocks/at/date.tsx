e280122f6607370d05203627c42ebac69ed901cb
kanhai <760489033@qq.com>
2022-07-04 01:53:05 +0800

    支持显示日期块

--- blocks/at/date.tsx
+++ blocks/at/date.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { ResourceArguments } from "../../extensions/icon/declare";
 import { Block } from "../../src/block";
 import { SolidArea } from "../../src/block/view/appear";
 import { BlockDisplay } from "../../src/block/enum";
 import { url, prop, view } from "../../src/block/factory/observable";
 import { BlockView } from "../../src/block/view";
-
-@url('/date')
+import dayjs from "dayjs";
+import { useDatePicker } from "../../extensions/date";
+import { Rect } from "../../src/common/vector/point";
+import "./style.less";
+@url('/mention/date')
 export class ShyDate extends Block {
-    @prop()
-    src: ResourceArguments = { name: 'none' };
     @prop()
     date: number;
     @prop()
@@ -24,14 +24,19 @@ export class ShyDate extends Block {
     remind: number;
     display = BlockDisplay.inline;
     async openDate(event: React.MouseEvent) {
-
+        var r = await useDatePicker({ roundArea: Rect.fromEvent(event) },
+            new Date(this.date || Date.now()),
+            { includeTime: this.includeTime });
+        if (r) {
+            this.onUpdateProps({ date: r.getTime() })
+        }
     }
 }
-@view('/date')
+@view('/mention/date')
 export class ShyDateView extends BlockView<ShyDate>{
     render() {
         return <div className='sy-block-date' onMouseDown={e => this.block.openDate(e)} >
-            <SolidArea prop='date' block={this.block}></SolidArea>
+            <SolidArea prop='date' block={this.block}>@{dayjs(this.block.date).format('YYYY-MM-DD')}</SolidArea>
         </div>
     }
 }
