517170a1a746b7dfc6e7ca7da0105f72a54af78b
kanhai <760489033@qq.com>
2022-07-11 11:36:05 +0800

    修复单例自定义名称失效的问题

--- component/lib/Singleton.tsx
+++ component/lib/Singleton.tsx
@@ -4,7 +4,7 @@ let maps = new Map<typeof React.Component | string, React.Component>();
 export async function Singleton<T extends React.Component>(CP: { new(...args: any[]): T }, nickName?: string) {
     return new Promise((resolve: (ins: T) => void, reject) => {
         if (maps.has(nickName)) return resolve(maps.get(nickName) as T)
-        if (maps.has(CP)) return resolve(maps.get(CP) as T)
+        if (maps.has(CP) && !nickName) return resolve(maps.get(CP) as T)
         var ele = document.createElement('div');
         document.body.appendChild(ele);
         ReactDOM.render(<CP ref={e => {
