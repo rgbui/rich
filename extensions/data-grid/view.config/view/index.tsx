b3dbc54003a0b9169ea2b8233287cce7049160a0
kanhai <760489033@qq.com>
2022-07-12 23:52:13 +0800

    调整数据表格视图配置

--- extensions/data-grid/view.config/view/index.tsx
+++ extensions/data-grid/view.config/view/index.tsx
@@ -4,9 +4,6 @@ import { DataGridView } from "../../../../blocks/data-grid/view/base";
 import { EventsComponent } from "../../../../component/lib/events.component";
 import { Switch } from "../../../../component/view/switch";
 import "./style.less";
-import { Remark } from "../../../../component/view/text";
-import { Select } from "../../../../component/view/select";
-import { Col, Row } from "../../../../component/view/grid";
 import { BlockUrlConstant } from "../../../../src/block/constant";
 import { TableStoreGallery } from "../../../../blocks/data-grid/view/gallery";
 import lodash from "lodash";
@@ -14,6 +11,7 @@ import { FieldType } from "../../../../blocks/data-grid/schema/type";
 import { BlockRenderRange } from "../../../../src/block/enum";
 import { SelectBox } from "../../../../component/view/select/box";
 import { getTypeSvg } from "../../../../blocks/data-grid/schema/util";
+import { Divider } from "../../../../component/view/grid";
 
 export class DataGridViewConfig extends EventsComponent {
     get schema() {
@@ -22,7 +20,6 @@ export class DataGridViewConfig extends EventsComponent {
     block: DataGridView;
     onOpen(block: DataGridView) {
         this.block = block;
-        console.log('ggg', this.block);
         this.forceUpdate();
     }
     render(): ReactNode {
@@ -30,9 +27,9 @@ export class DataGridViewConfig extends EventsComponent {
         var fs = this.schema.fields.findAll(g => g.text ? true : false);
         var self = this;
         return <div className="shy-table-property-view">
-            <div className="shy-table-property-view-operator">
-                <Remark>每页数量</Remark>
-                <SelectBox options={[
+            <div className="shy-table-property-view-item">
+                <label>每页数量</label>
+                <div className="operator"><SelectBox options={[
                     { text: '20条', value: 20 },
                     { text: '50条', value: 50 },
                     { text: '80条', value: 80 },
@@ -43,11 +40,13 @@ export class DataGridViewConfig extends EventsComponent {
                     value={this.block.size}
                     onChange={e => { this.block.onChangeSize(e); self.forceUpdate() }}>
                 </SelectBox>
+                </div>
             </div>
             {this.block.url == BlockUrlConstant.DataGridGallery && <>
-                <Row className="shy-table-property-view-operator">
-                    <Col span={12} align={'start'}><Remark>列数</Remark></Col>
-                    <Col span={12} align={'end'}><SelectBox
+                <Divider></Divider>
+                <div className="shy-table-property-view-item">
+                    <label>列数</label>
+                    <div className="operator"><SelectBox
                         options={[
                             { text: '1', value: 1 },
                             { text: '2', value: 2 },
@@ -57,62 +56,56 @@ export class DataGridViewConfig extends EventsComponent {
                             { text: '6', value: 6 }
                         ]}
                         value={(this.block as TableStoreGallery).gallerySize}
-                        onChange={async e => { await this.block.onUpdateProps({ gallerySize: e }, { range: BlockRenderRange.self }); self.forceUpdate() }}></SelectBox>
-                    </Col>
-                </Row>
-                <Row className="shy-table-property-view-operator">
-                    <Col span={12} align={'start'}><Remark>卡片是否自适应</Remark></Col>
-                    <Col span={12} align={'end'}><Switch onChange={async e => {
+                        onChange={async e => { await this.block.onUpdateProps({ gallerySize: e }, { range: BlockRenderRange.self }); self.forceUpdate() }}></SelectBox></div>
+                </div>
+                <div className="shy-table-property-view-item">
+                    <label>卡片是否自适应</label>
+                    <div className="operator"><Switch onChange={async e => {
                         var g = lodash.cloneDeep((this.block as TableStoreGallery).cardConfig);
                         g.auto = e;
                         await this.block.onUpdateProps({ cardConfig: g }, { range: BlockRenderRange.self });
                         this.forceUpdate()
-                    }} checked={(this.block as TableStoreGallery).cardConfig.auto}></Switch>
-                    </Col>
-                </Row>
-                <Row className="shy-table-property-view-operator">
-                    <Col span={12} align={'start'}><Remark>是否显示封面</Remark></Col>
-                    <Col span={12} align={'end'}>
-                        <Switch onChange={async e => {
+                    }} checked={(this.block as TableStoreGallery).cardConfig.auto}></Switch></div>
+                </div>
+                <Divider></Divider>
+                <div className="shy-table-property-view-item">
+                    <label>是否显示封面</label>
+                    <div className="operator"><Switch onChange={async e => {
+                        var g = lodash.cloneDeep((this.block as TableStoreGallery).cardConfig);
+                        g.showCover = e;
+                        await this.block.onUpdateProps({ cardConfig: g }, { range: BlockRenderRange.self });
+                        this.forceUpdate();
+                    }} checked={(this.block as TableStoreGallery).cardConfig.showCover}></Switch></div>
+                </div>
+                {(this.block as TableStoreGallery).cardConfig.showCover && <>
+                    <div className="shy-table-property-view-item">
+                        <label>封面自适应</label>
+                        <div className="operator"><Switch onChange={async e => {
                             var g = lodash.cloneDeep((this.block as TableStoreGallery).cardConfig);
-                            g.showCover = e;
+                            g.coverAuto = e;
                             await this.block.onUpdateProps({ cardConfig: g }, { range: BlockRenderRange.self });
                             this.forceUpdate();
-                        }} checked={(this.block as TableStoreGallery).cardConfig.showCover}></Switch>
-                    </Col>
-                </Row>
-                {(this.block as TableStoreGallery).cardConfig.showCover && <>
-                    <Row className="shy-table-property-view-operator">
-                        <Col span={12} align={'start'}><Remark>封面自适应</Remark></Col>
-                        <Col span={12} align={'end'}>
-                            <Switch onChange={async e => {
-                                var g = lodash.cloneDeep((this.block as TableStoreGallery).cardConfig);
-                                g.coverAuto = e;
-                                await this.block.onUpdateProps({ cardConfig: g }, { range: BlockRenderRange.self });
-                                this.forceUpdate();
-                            }} checked={(this.block as TableStoreGallery).cardConfig.coverAuto}></Switch>
-                        </Col>
-                    </Row>
-                    <Row className="shy-table-property-view-operator">
-                        <Col span={12} align={'start'}><Remark>封面字段</Remark></Col>
-                        <Col span={12} align={'end'}>
-                            <SelectBox options={fs.filter(g => g.type == FieldType.image).map(f => {
+                        }} checked={(this.block as TableStoreGallery).cardConfig.coverAuto}></Switch></div>
+                    </div>
+                    <div className="shy-table-property-view-item">
+                        <label>封面字段</label>
+                        <div className="operator"><SelectBox
+                            options={fs.filter(g => g.type == FieldType.image).map(f => {
                                 return {
                                     icon: getTypeSvg(f.type),
                                     text: f.text,
                                     value: f.id
                                 }
                             })}
-                                value={(this.block as TableStoreGallery).cardConfig.coverFieldId}
-                                onChange={async e => {
-                                    var g = lodash.cloneDeep((this.block as TableStoreGallery).cardConfig);
-                                    g.coverFieldId = e;
-                                    await this.block.onUpdateProps({ cardConfig: g }, { range: BlockRenderRange.self });
-                                    self.forceUpdate()
-                                }}>
-                            </SelectBox>
-                        </Col>
-                    </Row>
+                            value={(this.block as TableStoreGallery).cardConfig.coverFieldId}
+                            onChange={async e => {
+                                var g = lodash.cloneDeep((this.block as TableStoreGallery).cardConfig);
+                                g.coverFieldId = e;
+                                await this.block.onUpdateProps({ cardConfig: g }, { range: BlockRenderRange.self });
+                                self.forceUpdate()
+                            }}>
+                        </SelectBox></div>
+                    </div>
                 </>}
             </>}
         </div>
