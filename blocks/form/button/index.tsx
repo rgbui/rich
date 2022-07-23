bdceb5aaa2d825a38e068a9a027a48cf3e6e95d3
kanhai <760489033@qq.com>
2022-07-23 20:42:01 +0800

    增加button块

--- blocks/form/button/index.tsx
+++ blocks/form/button/index.tsx
@@ -1,38 +1,42 @@
 import React from "react";
+import { getEmoji } from "../../../net/element.type";
 import { Block } from "../../../src/block";
+import { BlockDisplay } from "../../../src/block/enum";
 import { prop, url, view } from "../../../src/block/factory/observable";
 import { BlockView } from "../../../src/block/view";
-import { TextArea } from "../../../src/block/view/appear";
-import { ActionFlowType, ActionScope, ExcuteAction, ExcuteScope } from "./declare";
+import { SolidArea, TextArea } from "../../../src/block/view/appear";
+import { EmojiSrcType } from "../../general/emoji";
+import "./style.less";
 
 @url('/button')
 export class BlockButton extends Block {
     @prop()
+    src: EmojiSrcType = { mime: 'emoji', code: '😀' };
+    display = BlockDisplay.inline;
+    @prop()
     showIcon: boolean = false;
     @prop()
     showText: boolean = true;
-    @prop()
-    actions: ActionFlowType[] = [];
-    @prop()
-    actionScope: ActionScope = { type: ExcuteScope.none };
     async mousedown(event: MouseEvent) {
-        await ExcuteAction({
-            block: this,
-            scope: { $event: event },
-            actions: this.actions,
-            actionScope: this.actionScope
-        });
+
     }
 }
+
 @view('/button')
 export class BlockButtonView extends BlockView<BlockButton>{
     render() {
-        return <div className='sy-button'
+        return <button className='sy-button'
             onMouseDown={e => this.block.mousedown(e.nativeEvent)}
             style={this.block.visibleStyle}>
-            <TextArea block={this.block} placeholder='按钮'
-                prop='content' ></TextArea>
-        </div>
+            {this.block.showIcon && <span>
+                <SolidArea block={this.block} prop='src'>
+                    {this.block.src.mime == 'emoji' && <span dangerouslySetInnerHTML={{ __html: getEmoji(this.block.src.code) }}>{this.block.src.code}</span>}
+                    {this.block.src.mime == 'image' && <span><img src={this.block.src.url} /></span>}
+                </SolidArea>
+            </span>}
+            {this.block.showText && <span><TextArea block={this.block} placeholder='按钮'
+                prop='content'></TextArea></span>}
+        </button>
     }
 }
 
