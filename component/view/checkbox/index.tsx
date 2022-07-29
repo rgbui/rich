c2fe1fc940109bb8417986e6aea5824ea22a4ac1
kanhai <760489033@qq.com>
2022-07-29 23:21:53 +0800

    增加简单版的checkbox组件

---
+++ component/view/checkbox/index.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from "react";
+
+import "./style.less"
+
+export class CheckBox extends React.Component<{
+
+    checked: boolean,
+    onChange: (checked: boolean) => void,
+    children: ReactNode
+
+}>{
+    constructor(props) {
+        super(props);
+    }
+    render(): React.ReactNode {
+        return <div className="shy-checkbox">
+            <input type='checkbox' checked={this.props.checked} onChange={e => this.props.onChange((e.target as HTMLInputElement).checked)}></input>
+            <span  className="shy-checkbox-content">{this.props.children}</span>
+        </div>
+    }
+}
