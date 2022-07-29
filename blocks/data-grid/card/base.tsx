0ec7640306386ef7eb21f860f9cdf83f9302868d
rgbui <760489033@qq.com>
2022-07-29 19:19:18 +0800

    feat: 增加卡片的设计

---
+++ blocks/data-grid/card/base.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { DataGridView } from "../view/base";
+export class DataGridCard extends React.Component<{ dataGrid: DataGridView, record: Record<string, any> }>{
+    
+}
---
+++ extensions/data-grid/card/data.ts
@@ -0,0 +1,9 @@
+import React from "react";
+
+export var DataGridCards: {
+    url: string,
+    text: string,
+    pic: string,
+    render: (row: Record<string, any>) => any
+}[]=[];
+
