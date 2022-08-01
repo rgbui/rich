1eb623215ca6b3203f300f6e5d82501c67d91b8e
kanhai <760489033@qq.com>
2022-08-02 01:08:07 +0800

    修复拖动块是，光标位于块的三分之一处是，是位于上面，位于之下，是相当于放在下面

--- src/kit/handle/direction.ts
+++ src/kit/handle/direction.ts
@@ -155,13 +155,15 @@ export function cacDragDirection(kit: Kit, dragBlocks: Block[], dropBlock: Block
                 direction = DropDirection.right;
                 dropBlock = getOutXBlock(dropBlock);
             }
-            else if (point.y <= bound.top + bound.height / 2) {
+            else if (point.y <= bound.top + bound.height * 0.4) {
                 direction = DropDirection.top;
             }
-            else if (point.y >= bound.top + bound.height / 2) {
-                if ([BlockUrlConstant.List,
-                BlockUrlConstant.Callout,
-                BlockUrlConstant.Quote].some(s => s == dropBlock.url)
+            else if (point.y > bound.top + bound.height * 0.4) {
+                if ([
+                    BlockUrlConstant.List,
+                    BlockUrlConstant.Callout,
+                    BlockUrlConstant.Quote
+                ].some(s => s == dropBlock.url)
                 ) {
                     direction = DropDirection.sub;
                     if (point.x - bound.left < 30) {
