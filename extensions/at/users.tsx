af3263f45a196775981da0845b100e548b9b8eb8
rgbui <760489033@qq.com>
2022-08-08 19:29:49 +0800

    feature: add online users

---
+++ extensions/at/users.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Avatar } from "../../component/view/avator/face";
+import { UserBasic } from "../../types/user"
+
+class OnlineUsers extends React.Component {
+    componentDidMount():void {
+
+    }
+    users: UserBasic[] = [];
+    render(): React.ReactNode {
+        return <div className="shy-online-users">
+            {this.users.map(user => {
+                <Avatar key={user.id} userid={user.id} size={40} showName></Avatar>
+            })}
+        </div>
+    }
+}
