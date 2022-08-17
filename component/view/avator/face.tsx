dde74af9cb89c7045a2ef31f01162746f54e3488
kanhai <760489033@qq.com>
2022-08-17 19:01:40 +0800

    feature:增加用户的在线状态显示

--- component/view/avator/face.tsx
+++ component/view/avator/face.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { channel } from '../../../net/channel';
 import { autoImageUrl } from '../../../net/element.type';
 import { Rect } from '../../../src/common/vector/point';
-import { UserBasic } from '../../../types/user';
+import { UserBasic, UserStatus } from '../../../types/user';
 import { useUserCard } from './card';
 import "./style.less";
 export class Avatar extends React.Component<{
@@ -17,8 +17,11 @@ export class Avatar extends React.Component<{
     showName?: boolean
 }> {
     private user: UserBasic;
-    async componentDidMount() {
-        if (!this.user && !this.props.user) {
+    componentDidMount() {
+        this.load()
+    }
+    async load(force?:boolean) {
+        if (force==true|| !this.user && !this.props.user) {
             if (!this.props.userid) {
                 // console.trace(this.props);
                 return;
@@ -46,6 +49,35 @@ export class Avatar extends React.Component<{
                     className='shy-avatar-name'>{user.name.slice(0, 1)}</span>
             }
         }
+        var renderStatus = () => {
+            if (!user) return <></>;
+            return <div className='shy-avatar-status'>
+                {user.status == UserStatus.online && <div className='shy-avatar-status-online'>
+                    <svg x="14.5" y="17" width="25" height="15" viewBox="0 0 25 15"><mask id="c4f52791-6ee2-4a19-b7dc-a20086dccadc"><rect x="7.5" y="5" width="10" height="10" rx="5" ry="5" fill="white"></rect><rect x="12.5" y="10" width="0" height="0" rx="0" ry="0" fill="black"></rect><polygon points="-2.16506,-2.5 2.16506,0 -2.16506,2.5" fill="black" transform="scale(0) translate(13.125 10)" style={{ transformOrigin: '13.125px 10px' }} ></polygon><circle fill="black" cx="12.5" cy="10" r="0"></circle></mask><rect fill="rgb(59, 165, 93)" width="25" height="15" mask="url(#c4f52791-6ee2-4a19-b7dc-a20086dccadc)"></rect></svg>
+                </div>}
+                {user.status == UserStatus.busy && <div className='shy-avatar-status-busy'><svg x="14.5" y="17" width="25" height="15"
+                    viewBox="0 0 25 15"><mask
+                        id="c4f52791-6ee2-4a19-b7dc-a20086dccadc"><rect x="7.5" y="5" width="10" height="10"
+                            rx="5" ry="5" fill="white"></rect><rect x="8.75"
+                                y="8.75" width="7.5" height="2.5" rx="1.25" ry="1.25" fill="black"></rect><polygon
+                                    points="-2.16506,-2.5 2.16506,0 -2.16506,2.5" fill="black" transform="scale(0) translate(13.125 10)" style={{ transformOrigin: '13.125px 10px' }} ></polygon><circle fill="black" cx="12.5" cy="10" r="0"></circle></mask><rect fill="rgb(237, 66, 69)" width="25" height="15" mask="url(#c4f52791-6ee2-4a19-b7dc-a20086dccadc)"></rect></svg></div>}
+                {(user.status == UserStatus.hidden || user.status == UserStatus.offline) && <div className='shy-avatar-status-hidden'>
+                    <svg x="14.5" y="17" width="25" height="15" viewBox="0 0 25 15"><mask id="c4f52791-6ee2-4a19-b7dc-a20086dccadc"><rect x="7.5" y="5" width="10" height="10" rx="5" ry="5" fill="white"></rect><rect x="10" y="7.5" width="5" height="5" rx="2.5" ry="2.5" fill="black"></rect><polygon points="-2.16506,-2.5 2.16506,0 -2.16506,2.5" fill="black" transform="scale(0) translate(13.125 10)"
+                        style={{ transformOrigin: '13.125px 10px' }} ></polygon><circle fill="black" cx="12.5" cy="10" r="0"></circle></mask><rect fill="rgb(116, 127, 141)" width="25" height="15" mask="url(#c4f52791-6ee2-4a19-b7dc-a20086dccadc)"></rect></svg>
+                </div>}
+                {user.status == UserStatus.idle && <div className='shy-avatar-status-idle'>
+                    <svg x="14.5" y="17" width="25" height="15" viewBox="0 0 25 15"><mask
+                        id="c4f52791-6ee2-4a19-b7dc-a20086dccadc"><rect
+                            x="7.5" y="5" width="10" height="10" rx="5" ry="5"
+                            fill="white"></rect><rect x="6.25" y="3.75"
+                                width="7.5" height="7.5" rx="3.75" ry="3.75" fill="black"></rect><polygon points="-2.16506,-2.5 2.16506,0 -2.16506,2.5" fill="black" transform="scale(0) translate(13.125 10)"
+                                    style={{ transformOrigin: '13.125px 10px' }}></polygon>
+                        <circle fill="black" cx="12.5" cy="10" r="0"></circle></mask><rect
+                            fill="rgb(250, 168, 26)" width="25" height="15"
+                            mask="url(#c4f52791-6ee2-4a19-b7dc-a20086dccadc)"></rect></svg>
+                </div>}
+            </div>
+        }
         if (this.props.head || this.props.showName || this.props.children || this.props.showSn) {
             return <div className={'shy-avatar-say' + " " + this.props.className}>
                 <div className={'shy-avatar-say-face'} onMouseDown={e => this.mousedown(e)}>{renderIcon()}</div>
@@ -58,6 +90,7 @@ export class Avatar extends React.Component<{
         else
             return <div className={'shy-avatar' + " " + this.props.className} style={{ width: size, height: size }} onMouseDown={e => this.mousedown(e)}>
                 {renderIcon()}
+                {renderStatus()}
             </div>
     }
 }
