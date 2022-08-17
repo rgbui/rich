1de1a758dd616fca7474b46e5518e723c914b4bb
kanhai <760489033@qq.com>
2022-08-17 16:02:30 +0800

    fixed:调整大纲的显示效果

--- blocks/page/outline/index.tsx
+++ blocks/page/outline/index.tsx
@@ -4,14 +4,60 @@ import { url, view } from "../../../src/block/factory/observable";
 import { Block } from "../../../src/block";
 import { BlockDisplay } from "../../../src/block/enum";
 import "./style.less";
+import { BlockUrlConstant } from "../../../src/block/constant";
+import { Rect } from "../../../src/common/vector/point";
+import lodash from "lodash";
 
 @url('/outline')
 export class PageOutLine extends Block {
     display = BlockDisplay.block;
-    outlines: { id: string, deep: number, hover: boolean, text: string }[] = [];
-    async updateOutLines() {
-        this.outlines = await this.page.getOutLines();
+    outlines: { id: string, deep: number, block: Block, hover?: boolean, text: string }[] = [];
+    hoverId = '';
+    updateOutlinesHover() {
+        var hoverId = '';
+        if (this.outlines.length > 0) {
+            for (let i = 0; i < this.outlines.length - 1; i++) {
+                var r = Rect.fromEle(this.outlines[i].block.el);
+                var n = Rect.fromEle(this.outlines[i + 1].block.el);
+                if (r.top < 50 && n.top > 50) {
+                    hoverId = this.outlines[i].block.id;
+                    break;
+                }
+            }
+            if (!hoverId) {
+                var r = Rect.fromEle(this.outlines[this.outlines.length - 1].block.el);
+                if (r.top <= 50) hoverId = this.outlines[this.outlines.length - 1].block.id;
+            }
+        }
+        this.hoverId = hoverId;
         this.forceUpdate();
+    }
+    getOutLines() {
+        var outlines: { id: string, deep: number, block: Block, text: string }[] = [];
+        var bs = this.page.findAll(x => x.url == BlockUrlConstant.Head && x.el && (x.closest(c => c.isPart) ? false : true));
+        lodash.sortBy(bs, g => Rect.fromEle(g.el).top);
+        var currentDeep = 0, lastLevel;
+        if (this.page.view) {
+            outlines = bs.map((b, i) => {
+                var level = parseInt((b as any).level.replace('h', ''));
+                var deep = currentDeep;
+                if (typeof lastLevel == 'number' && level < lastLevel) deep -= 1;
+                else if (typeof lastLevel == 'number' && level > lastLevel) deep += 1;
+                currentDeep = deep;
+                lastLevel = level;
+                return {
+                    id: b.id,
+                    deep,
+                    block: b,
+                    text: b.childs.length > 0 ? b.childs.map(c => c.content).join("") : b.content
+                }
+            })
+        }
+        this.outlines = outlines;
+    }
+    updateOutLine() {
+        this.getOutLines();
+        this.updateOutlinesHover()
     }
 }
 @view('/outline')
@@ -25,10 +71,13 @@ export class PageOutLineView extends BlockView<PageOutLine>{
             });
         }
     }
+    didMount(): void {
+        this.block.updateOutLine()
+    }
     render() {
         return <div className='sy-block-outline' style={this.block.visibleStyle}>
             {this.block.outlines.map(line => {
-                return <a key={line.id} className={line.hover ? "hover" : ""} style={{ paddingLeft: 10 + line.deep * 15 }} onMouseDown={e => this.mousedownLine(line, e)}>{line.text}</a>
+                return <div className={"item" + (this.block.hoverId == line.block.id ? " hover" : "")} key={line.id}><a key={line.id} style={{ paddingLeft: 10 + line.deep * 15 }} onMouseDown={e => this.mousedownLine(line, e)}>{line.text}</a></div>
             })}
         </div>
     }
