c344b0931bc02e1bc2ebf8373f02c4a65e8e46f3
kanhai <760489033@qq.com>
2022-09-07 23:50:09 +0800

    fix: 优化头像状态显示不正常的问题

--- component/view/avator/face.tsx
+++ component/view/avator/face.tsx
@@ -5,6 +5,7 @@ import { Rect } from '../../../src/common/vector/point';
 import { UserBasic, UserStatus } from '../../../types/user';
 import { useUserCard } from './card';
 import "./style.less";
+
 export class Avatar extends React.Component<{
     size?: number,
     userid?: string,
@@ -20,8 +21,8 @@ export class Avatar extends React.Component<{
     componentDidMount() {
         this.load()
     }
-    async load(force?:boolean) {
-        if (force==true|| !this.user && !this.props.user) {
+    async load(force?: boolean) {
+        if (force == true || !this.user && !this.props.user) {
             if (!this.props.userid) {
                 // console.trace(this.props);
                 return;
@@ -61,7 +62,7 @@ export class Avatar extends React.Component<{
                             rx="5" ry="5" fill="white"></rect><rect x="8.75"
                                 y="8.75" width="7.5" height="2.5" rx="1.25" ry="1.25" fill="black"></rect><polygon
                                     points="-2.16506,-2.5 2.16506,0 -2.16506,2.5" fill="black" transform="scale(0) translate(13.125 10)" style={{ transformOrigin: '13.125px 10px' }} ></polygon><circle fill="black" cx="12.5" cy="10" r="0"></circle></mask><rect fill="rgb(237, 66, 69)" width="25" height="15" mask="url(#c4f52791-6ee2-4a19-b7dc-a20086dccadc)"></rect></svg></div>}
-                {(user.status == UserStatus.hidden || user.status == UserStatus.offline) && <div className='shy-avatar-status-hidden'>
+                {(user.status == UserStatus.hidden || !(user.status ? true : false) || user.status == UserStatus.offline) && <div className='shy-avatar-status-hidden'>
                     <svg x="14.5" y="17" width="25" height="15" viewBox="0 0 25 15"><mask id="c4f52791-6ee2-4a19-b7dc-a20086dccadc"><rect x="7.5" y="5" width="10" height="10" rx="5" ry="5" fill="white"></rect><rect x="10" y="7.5" width="5" height="5" rx="2.5" ry="2.5" fill="black"></rect><polygon points="-2.16506,-2.5 2.16506,0 -2.16506,2.5" fill="black" transform="scale(0) translate(13.125 10)"
                         style={{ transformOrigin: '13.125px 10px' }} ></polygon><circle fill="black" cx="12.5" cy="10" r="0"></circle></mask><rect fill="rgb(116, 127, 141)" width="25" height="15" mask="url(#c4f52791-6ee2-4a19-b7dc-a20086dccadc)"></rect></svg>
                 </div>}
@@ -90,7 +91,7 @@ export class Avatar extends React.Component<{
         else
             return <div className={'shy-avatar' + " " + this.props.className} style={{ width: size, height: size }} onMouseDown={e => this.mousedown(e)}>
                 {renderIcon()}
-                {renderStatus()}
+                {size > 30 && renderStatus()}
             </div>
     }
 }
