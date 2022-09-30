537798413b8ea787b60dbed694821602d99b7fe1
kanhai <760489033@qq.com>
2022-09-30 22:20:15 +0800

    修复下拉框的显示

--- component/view/menu/item.tsx
+++ component/view/menu/item.tsx
@@ -118,10 +118,10 @@ export class MenuItemView extends React.Component<{
                 {item.icon && <i className="flex-center flex-line size-20"><Icon icon={item.icon} size={item.iconSize ? item.iconSize : 16}></Icon></i>}
                 {item.renderIcon && item.renderIcon(item, this)}
                 <span className='shy-menu-box-item-option-text'>{item.text}</span>
-                {item.checkLabel && <Icon className={'shy-menu-box-item-option-label-icon'} size={14} icon={CheckSvg}></Icon>}
+                {item.checkLabel && <Icon className={'shy-menu-box-item-option-label-icon'} size={16} icon={CheckSvg}></Icon>}
                 {item.label && <label>{item.label}</label>}
                 {Array.isArray(item.btns) && item.btns.map(btn => {
-                    return <ToolTip key={btn.name} overlay={btn.overlay} placement={btn.placement || 'top'} ><em onMouseUp={e => { e.stopPropagation(); this.click(item, e, btn.name) }}><Icon icon={btn.icon}></Icon></em></ToolTip>
+                    return <ToolTip key={btn.name} overlay={btn.overlay} placement={btn.placement || 'top'} ><em onMouseUp={e => { e.stopPropagation(); this.click(item, e, btn.name) }}><Icon size={16} icon={btn.icon}></Icon></em></ToolTip>
                 })}
                 {item.childs && item.childs.length > 0 && <Icon className={'shy-menu-box-item-option-spread'} icon='arrow-right:sy'></Icon>}
             </a></ToolTip>}
@@ -144,7 +144,7 @@ export class MenuItemView extends React.Component<{
                 <span className='shy-menu-box-item-option-text flex-auto'>{item.text}</span>
                 <span className="shy-menu-box-item-select-value flex  flex-fix" onMouseDown={e => this.openSelectMenu(item, e)}>
                     <em className="text-over flex-auto max-w-100">{item?.options?.find(g => g.value == item.value)?.text}</em>
-                    <Icon className={'flex-fix'} size={14} icon={ChevronDownSvg}></Icon>
+                    <Icon className={'flex-fix'} size={16} icon={ChevronDownSvg}></Icon>
                 </span>
             </div>}
             {item.type == MenuItemType.drag && <ToolTip overlay={item.overlay} placement={item.placement || 'right'} ><div data-drag={item.drag} onMouseUp={e => this.select(item, e.nativeEvent)} className="shy-menu-box-item-drag">
