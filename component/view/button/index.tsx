dbcb5fdc9f6f423eb8596d9fb48de126d76c7bd0
kanhai <760489033@qq.com>
2022-10-02 18:40:08 +0800

    调整button中的icon默认大小

--- component/view/button/index.tsx
+++ component/view/button/index.tsx
@@ -4,14 +4,15 @@ import "./style.less";
 export class Button extends React.Component<{
     children?: JSX.Element | string | React.ReactNode,
     icon?: string | SvgrComponent | JSX.Element,
+    iconSize?: number,
     disabled?: boolean,
     onClick?: (event: React.MouseEvent, button?: Button) => void,
     block?: boolean,
     ghost?: boolean,
     link?: boolean,
     style?: CSSProperties,
-    size?: 'normal' | 'medium' | 'larger'|'small',
-    className?:string
+    size?: 'normal' | 'medium' | 'larger' | 'small',
+    className?: string
 }>{
     private _loading: boolean;
     set loading(loading: boolean) {
@@ -41,10 +42,10 @@ export class Button extends React.Component<{
                     d="M988 548c-19.9 0-36-16.1-36-36 0-59.4-11.6-117-34.6-171.3a440.45 440.45 0 00-94.3-139.9 437.71 437.71 0 00-139.9-94.3C629 83.6 571.4 72 512 72c-19.9 0-36-16.1-36-36s16.1-36 36-36c69.1 0 136.2 13.5 199.3 40.3C772.3 66 827 103 874 150c47 47 83.9 101.8 109.7 162.7 26.7 63.1 40.2 130.2 40.2 199.3.1 19.9-16 36-35.9 36z"
                 ></path>
             </svg>
-            if (props.icon) return <Icon icon={props.icon}></Icon>
+            if (props.icon) return <Icon size={props.iconSize || 16} icon={props.icon}></Icon>
         }
         var btn = <button style={this.props.style || {}}
-            className={'shy-button'+(props.className?" "+props.className:"") + (props.link ? " shy-button-link" : "") + (props.ghost ? " shy-button-ghost" : "") + (this.props.size ? " shy-button-" + this.props.size : "")}
+            className={'shy-button' + (props.className ? " " + props.className : "") + (props.link ? " shy-button-link" : "") + (props.ghost ? " shy-button-ghost" : "") + (this.props.size ? " shy-button-" + this.props.size : "")}
             disabled={props.disabled || this._disabled ? props.disabled : false}
             onClick={e => props.onClick && !this._loading ? props.onClick(e, this) : undefined}
         >{renderLoading()}{props.children}</button>;
