472c42ccae7713176799c69b50045e59e9095472
kanhai <760489033@qq.com>
2022-10-06 21:41:40 +0800

    增加视图的控制操作

--- extensions/data-grid/view.config/control/index.tsx
+++ extensions/data-grid/view.config/control/index.tsx
@@ -1,8 +1,11 @@
 
 import React, { ReactNode } from "react";
-import { Field } from "../../../../blocks/data-grid/schema/field";
+import { FieldType } from "../../../../blocks/data-grid/schema/type";
+import { getTypeSvg } from "../../../../blocks/data-grid/schema/util";
 import { DataGridView } from "../../../../blocks/data-grid/view/base";
 import { EventsComponent } from "../../../../component/lib/events.component";
+import { Icon } from "../../../../component/view/icon";
+import { Switch } from "../../../../component/view/switch";
 import "./style.less";
 
 export class DataGridControl extends EventsComponent {
@@ -17,20 +20,56 @@ export class DataGridControl extends EventsComponent {
     render(): ReactNode {
         if (!this.block) return <></>;
         if (!this.schema) return <div></div>
-        var fs = this.schema.fields.findAll(g => g.text ? true : false);
-        var self = this;
-        async function change(field: Field, checked: boolean) {
-            if (checked == true) {
-                await self.block.onShowField(field);
-            }
-            else {
-                var vf = self.block.fields.find(g => g.field.id == field.id);
-                if (vf) await self.block.onHideField(vf);
-            }
-            self.forceUpdate();
-        }
-        return <div className="shy-table-field-view">
 
+        return <div className="f-14 max-h-300 overflow-y">
+
+            <div className="remark f-12 padding-w-14  gap-t-10">扩展视图控制块</div>
+
+            <div className="flex item-hover round h-30 padding-w-14 ">
+                <span className="flex-auto">分页</span>
+                <span className="flex-fix"><Switch checked={false} onChange={e => { }}></Switch></span>
+            </div>
+
+            <div className="flex item-hover round h-30 padding-w-14 ">
+                <span className="flex-auto">显示批量选中的数据</span>
+                <span className="flex-fix"><Switch checked={false} onChange={e => { }}></Switch></span>
+            </div>
+
+            <div className="flex item-hover round h-30 padding-w-14 ">
+                <span className="flex-auto">批量删除按钮</span>
+                <span className="flex-fix"><Switch checked={false} onChange={e => { }}></Switch></span>
+            </div>
+
+            <div className="flex item-hover round h-30 padding-w-14 ">
+                <span className="flex-auto">添加数据按钮</span>
+                <span className="flex-fix"><Switch checked={false} onChange={e => { }}></Switch></span>
+            </div>
+
+            <div className="flex item-hover round h-30 padding-w-14 ">
+                <span className="flex-auto">导入数据</span>
+                <span className="flex-fix"><Switch checked={false} onChange={e => { }}></Switch></span>
+            </div>
+
+            <div className="flex item-hover round h-30 padding-w-14 ">
+                <span className="flex-auto">导出数据</span>
+                <span className="flex-fix"><Switch checked={false} onChange={e => { }}></Switch></span>
+            </div>
+
+            <div className="remark f-12 padding-w-14 gap-t-10">扩展字段控制块</div>
+            {this.block.schema.visibleFields.findAll(g => ![
+                FieldType.video,
+                FieldType.button,
+                FieldType.audio,
+                FieldType.parentId,
+                FieldType.image,
+                FieldType.file,
+                FieldType].includes(g.type)).map(f => {
+                    return <div key={f.id} className="flex item-hover round h-30 padding-w-14 ">
+                        <span className="flex-fix size-24 flex-center cursor round item-hover"><Icon size={16} icon={getTypeSvg(f.type)}></Icon></span>
+                        <span className="flex-auto">{f.text}</span>
+                        <span className="flex-fix"><Switch checked={false} onChange={e => { }}></Switch></span>
+                    </div>
+                })}
         </div>
     }
 }
