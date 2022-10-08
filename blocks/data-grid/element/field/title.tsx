fc5e8fdee4c2922af2958688a45c537030e94026
kanhai <760489033@qq.com>
2022-10-08 22:50:53 +0800

    增加title字段的icon显示

--- blocks/data-grid/element/field/title.tsx
+++ blocks/data-grid/element/field/title.tsx
@@ -3,7 +3,10 @@ import { url, view } from "../../../../src/block/factory/observable";
 import { TextArea } from "../../../../src/block/view/appear";
 import { BlockView } from "../../../../src/block/view";
 import { OriginField } from "./origin.field";
-import { Button } from "../../../../component/view/button";
+import { Icon } from "../../../../component/view/icon";
+import { getPageIcon } from "../../../../extensions/at/declare";
+import { PageLayoutType } from "../../../../src/page/declare";
+
 @url('/field/title')
 export class FieldText extends OriginField {
     async openPage() {
@@ -13,9 +16,12 @@ export class FieldText extends OriginField {
 @view('/field/title')
 export class FieldTextView extends BlockView<FieldText>{
     render() {
-        return <div className='sy-field-title'>
+        return <div className='sy-field-title f-14'>
+            {this.block.item.dataRow['icon'] && <Icon icon={getPageIcon({ pageType: PageLayoutType.doc, icon: this.block.item.dataRow['icon'] })}></Icon>}
             <TextArea block={this.block} prop='value' placeholder="输入文本" ></TextArea>
-            <Button size="small" className='sy-field-title-button' ghost onClick={e => this.block.openPage()}>打开</Button>
+            <span onClick={e => this.block.openPage()} className="sy-field-title-button flex-center f-14 text-1 border item-hover round padding-w-5 padding-h-2 cursor">
+                <em>打开</em>
+            </span>
         </div>
     }
 }
