0da84a0148c976469d11e87d667764badc88d701
kanhai <760489033@qq.com>
2022-10-08 23:36:00 +0800

    修复字段文件上传的管理与显示

--- blocks/data-grid/element/field/file.tsx
+++ blocks/data-grid/element/field/file.tsx
@@ -1,50 +1,53 @@
 import React from "react";
-import { OpenFileDialoug } from "../../../../component/file";
-import { Button } from "../../../../component/view/button";
-import { channel } from "../../../../net/channel";
+import { useDataGridFileViewer } from "../../../../extensions/data-grid/filer";
+import { ResourceArguments } from "../../../../extensions/icon/declare";
 import { url, view } from "../../../../src/block/factory/observable";
 import { BlockView } from "../../../../src/block/view";
+import { Rect } from "../../../../src/common/vector/point";
+import { util } from "../../../../util/util";
 import { OriginField } from "./origin.field";
 
 @url('/field/file')
 export class FieldFile extends OriginField {
-    async uploadFile(event: React.MouseEvent) {
-        var exts = ['*'];
-        var file = await OpenFileDialoug({ exts });
-        if (file) {
-            var r = await channel.post('/ws/upload/file', {
-                file, uploadProgress: (event) => {
-                    console.log(event, 'ev');
-                }
-            })
-            if (r.ok) {
-                if (r.data.file) {
-                    if (!Array.isArray(this.value)) this.value = [];
-                    this.value.push(r.data.file);
-                    this.onUpdateCellValue(this.value);
-                    this.forceUpdate();
-                }
-            }
+    async onCellMousedown(event: React.MouseEvent) {
+        var vs = Array.isArray(this.value) ? this.value : (this.value ? [this.value] : []);
+        if (!this.field?.config?.isMultiple) {
+            vs = vs.slice(0, 1);
+        }
+        var rs = await useDataGridFileViewer({ roundArea: Rect.fromEvent(event) }, {
+            mime: 'file',
+            resources: vs,
+            isMultiple: this.field?.config?.isMultiple ? true : false
+        });
+        if (Array.isArray(rs) && rs.length > 0) {
+            this.value = rs;
+            this.onUpdateCellValue(this.value);
+            this.forceUpdate();
         }
     }
 }
 @view('/field/file')
 export class FieldFileView extends BlockView<FieldFile>{
-    renderFiles(images: { name: string, url: string }[]) {
+    down(img: ResourceArguments, e: React.MouseEvent) {
+        e.stopPropagation();
+        util.downloadFile(img.url, img.text)
+    }
+    renderFiles(images: ResourceArguments[]) {
         return images.map((img, i) => {
-            return <div className="sy-field-file-item" key={i}>
-                <a download={img.url} href={img.url}>{img.name}</a>
+            return <div className="min-h-30 " key={i}>
+                <a onMouseDown={e => { this.down(img, e) }} className="padding-3 round item-hover-focus cursor text-1 text-overflow" download={img.url}>{img.text}</a>
             </div>
         })
     }
     render() {
         var vs = Array.isArray(this.block.value) ? this.block.value : (this.block.value ? [this.block.value] : []);
         if (!this.block.field?.config?.isMultiple && vs.length > 1) vs = [vs.first()]
-        return <div className='sy-field-file' onMouseDown={e => e.stopPropagation()}>
+        return <div className='sy-field-file'>
             {this.block.value && <div className="sy-field-files">
                 {this.renderFiles(vs)}
             </div>}
-            {(vs.length == 0 || this.block.field?.config?.isMultiple) && <Button size={'small'} ghost onClick={e => this.block.uploadFile(e)}>上传文件</Button>}
         </div>
     }
 }
+
+
