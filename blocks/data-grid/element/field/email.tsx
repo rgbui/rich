d19b916c125f235e020c9c8ea8b641f140c5b21e
kanhai <760489033@qq.com>
2022-10-10 22:24:17 +0800

    增加email字段块

--- blocks/data-grid/element/field/email.tsx
+++ blocks/data-grid/element/field/email.tsx
@@ -11,8 +11,8 @@ export class FieldEmail extends OriginField {
 @view('/field/email')
 export class FieldEmailView extends BlockView<FieldEmail>{
     render() {
-        return <div className='sy-field-email'>
-            <TextArea  block={this.block} prop='value'  placeholder="输入邮箱" ></TextArea>
+        return <div className='sy-field-email f-14'>
+            <TextArea block={this.block} prop='value' placeholder="输入邮箱" ></TextArea>
         </div>
     }
 }
