ec5f2da6fcce54e6cc7c771d24c005856427bb2f
kanhai <760489033@qq.com>
2022-10-12 23:46:24 +0800

    修复关联，统计列的显示效果

--- extensions/data-grid/relation/index.tsx
+++ extensions/data-grid/relation/index.tsx
@@ -1,49 +1,42 @@
 import React from "react";
 import { ReactNode } from "react";
-import { FieldConfig } from "../../../blocks/data-grid/schema/field";
 import { TableSchema } from "../../../blocks/data-grid/schema/meta";
 import { EventsComponent } from "../../../component/lib/events.component";
-import { Row, Col } from "../../../component/view/grid";
-import { Select } from "../../../component/view/select";
-import { Switch } from "../../../component/view/switch";
-import { Remark } from "../../../component/view/text";
+import { CheckSvg, CollectTableSvg } from "../../../component/svgs";
+import { Icon } from "../../../component/view/icon";
 import { channel } from "../../../net/channel";
+import { util } from "../../../util/util";
 import { PopoverSingleton } from "../../popover/popover";
 import { PopoverPosition } from "../../popover/position";
 import "./style.less";
 
-export class RelationView extends EventsComponent {
+export class DataSourceView extends EventsComponent {
     render(): ReactNode {
-        return <div className="shy-relation-view">
-            {this.relationDatas && <Row>
-                <Col><Remark>关联表格:</Remark></Col>
-                <Col><Select
-                    onChange={e => { this.config.relationTableId = e; this.isChange = true; this.forceUpdate() }}
-                    value={this.config.relationTableId}
-                    options={this.relationDatas.map(r => {
-                        return {
-                            text: r.text + (Array.isArray(r.views) && r.views.length > 0 ? r.views[0].text : ''),
-                            value: r.id
-                        }
-                    })}
-                    style={{ width: '100%' }}>
-                </Select>
-                </Col>
-                <Col><Remark>是否一对多:</Remark><Switch onChange={e => this.onChangeConfig({ isMultiple: e })} checked={this.config?.isMultiple ? true : false}></Switch></Col>
-            </Row >}
+        return <div className="w-200 overflow-y max-h-300 padding-h-14 round">
+            {this.relationDatas && this.relationDatas.map(r => {
+                return <div onMouseDown={e => this.onChange(r.id)} key={r.id} className="padding-w-14 f-14 item-hover round cursor">
+                    <div className="flex">
+                        <span className="flex-fixed flex-center size-24 round item-hover">
+                            <Icon size={16} icon={(r as any).icon || CollectTableSvg}></Icon>
+                        </span>
+                        <span className="flex-auto">{r.text}</span>
+                        <span className="flex-fixed size-24 round item-hover">
+                            {r.id == this.currentTableId && <Icon size={16} icon={CheckSvg}></Icon>}
+                        </span>
+                    </div>
+                    <div className="remark gap-b-10">{util.showTime(r.createDate)}</div>
+                </div>
+            })}
         </div>
     }
-    isChange: boolean = false;
     private relationDatas: TableSchema[];
-    config: FieldConfig = {};
-    async open(option: { config?: FieldConfig }) {
-        this.isChange = false;
-        this.config = option.config || {};
-        this.relationDatas = null;
-        await this.loadTypeDatas();
+    currentTableId: string = '';
+    async open(option: { tableId: string }) {
+        this.currentTableId = option.tableId;
+        if (!this.relationDatas) await this.loadTypeDatas();
         this.forceUpdate();
     }
-    async loadTypeDatas(force?: boolean) {
+    async loadTypeDatas() {
         var isUpdate: boolean = false;
         if (!Array.isArray(this.relationDatas)) {
             var r = await channel.get('/schema/list');
@@ -52,32 +45,31 @@ export class RelationView extends EventsComponent {
                 isUpdate = true;
             }
         }
-        if (force == true && isUpdate) {
-            this.forceUpdate()
-        }
     }
-    async onChangeConfig(config: Partial<FieldConfig>) {
-        Object.assign(this.config, config);
-        this.isChange = true;
-        this.forceUpdate();
+    async onChange(id: string) {
+        this.currentTableId = id;
+        this.emit('save', id);
     }
 }
 
-export async function useRelationView(pos: PopoverPosition,
+export async function useDataSourceView(pos: PopoverPosition,
     option: {
-        // type: FieldType,
-        config?: Record<string, any>
+        tableId: string,
     }) {
-    let popover = await PopoverSingleton(RelationView, { mask: true });
+    let popover = await PopoverSingleton(DataSourceView, { mask: true });
     let fv = await popover.open(pos);
     fv.open(option);
-    return new Promise((resolve: (data: { config?: FieldConfig }) => void, reject) => {
+    return new Promise((resolve: (data: string) => void, reject) => {
         fv.only('close', () => {
             popover.close();
-            resolve(fv.isChange ? { config: fv.config } : undefined);
+            resolve(undefined);
+        })
+        fv.only('save', (g) => {
+            resolve(g);
+            popover.close();
         })
         popover.only('close', () => {
-            resolve(fv.isChange ? { config: fv.config } : undefined);
+            resolve(undefined);
         })
     })
 }
