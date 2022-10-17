ac40bc02001a77259163c521319fc9b3db834d1a
kanhai <760489033@qq.com>
2022-10-17 23:43:47 +0800

    增加关联时，显示图标

--- blocks/data-grid/element/field/relation.tsx
+++ blocks/data-grid/element/field/relation.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Icon } from "../../../../component/view/icon";
+import { getPageIcon } from "../../../../extensions/at/declare";
 import { useRelationPickData } from "../../../../extensions/data-grid/relation.picker";
 import { ElementType, getElementUrl } from "../../../../net/element.type";
 import { url, view } from "../../../../src/block/factory/observable";
 import { BlockView } from "../../../../src/block/view";
 import { Rect } from "../../../../src/common/vector/point";
+import { PageLayoutType } from "../../../../src/page/declare";
 import { FieldType } from "../../schema/type";
 import { OriginField } from "./origin.field";
 
@@ -52,9 +55,15 @@ export class FieldRelationView extends BlockView<FieldRelation>{
         var rs = this.block.relationSchema;
         var f = rs?.fields?.find(g => g.type == FieldType.title);
         if (!f) f = rs?.fields.find(g => g.type == FieldType.text);
+        var icon = rs?.fields.find(g => g.type == FieldType.icon);
         return <div className='sy-field-relation-items'>{this.block.relationList?.map(r => {
             var url = getElementUrl(ElementType.SchemaData, rs.id, r.id);
-            return <a href={url} onClick={e => e.preventDefault()} key={r.id}>{r[f?.name]}</a>
+            return <a href={url} onClick={e => e.preventDefault()} key={r.id}>
+                <span className="size-24 flex-center flex-inline">
+                    <Icon size={16} icon={getPageIcon({ pageType: PageLayoutType.doc, icon: r[icon.name] })}></Icon>
+                </span>
+                <span>{r[f?.name]}</span>
+            </a>
         })}
         </div>
     }
