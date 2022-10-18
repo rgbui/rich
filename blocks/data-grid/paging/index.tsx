b19253e5de579dcc74ecd4433ae204a0917aec4c
kanhai <760489033@qq.com>
2022-10-18 21:55:47 +0800

    修复切换表格时，导致引用块丢失

--- blocks/data-grid/paging/index.tsx
+++ blocks/data-grid/paging/index.tsx
@@ -23,28 +23,32 @@ export class Paging extends Block {
 @view('/data-grid/paging')
 export class PagingView extends BlockView<Paging>{
     getPages() {
-        var page = Math.ceil(this.block.refBlock.total / this.block.refBlock.size);
-        var index: number = this.block.refBlock.index;
-        if (index <= 0) index = 1;
-        if (index > page) index = page;
-        var ps: (string | number)[] = [];
-        if (this.block.refBlock.total > 0) {
-            if (index - 3 > 1) [1, "...", index - 2, index - 1, index].forEach((p) => ps.push(p));
-            else for (var i = 1; i <= index; i++) ps.push(i);
-            if (index + 3 < page) [index + 1, index + 2, "...", page].forEach((p) => ps.push(p));
-            else for (var j = index + 1; j <= page; j++) ps.push(j);
-        } else ps = [];
-        return ps.map((p) => {
-            var classList: string[] = ["sy-paging-page"];
-            if (p == index) classList.push("sy-paging-current");
-            else if (typeof p == "number") {
-            } else classList.push("sy-paging-text");
-            return {
-                text: p,
-                index: typeof p == "number" ? p : undefined,
-                classList,
-            };
-        });
+        if (this.block.refBlock) {
+            var page = Math.ceil(this.block.refBlock.total / this.block.refBlock.size);
+            var index: number = this.block.refBlock.index;
+            if (index <= 0) index = 1;
+            if (index > page) index = page;
+            var ps: (string | number)[] = [];
+            if (this.block.refBlock.total > 0) {
+                if (index - 3 > 1) [1, "...", index - 2, index - 1, index].forEach((p) => ps.push(p));
+                else for (var i = 1; i <= index; i++) ps.push(i);
+                if (index + 3 < page) [index + 1, index + 2, "...", page].forEach((p) => ps.push(p));
+                else for (var j = index + 1; j <= page; j++) ps.push(j);
+            } else ps = [];
+            return ps.map((p) => {
+                var classList: string[] = ["sy-paging-page"];
+                if (p == index) classList.push("sy-paging-current");
+                else if (typeof p == "number") {
+                } else classList.push("sy-paging-text");
+                return {
+                    text: p,
+                    index: typeof p == "number" ? p : undefined,
+                    classList,
+                };
+            });
+        }
+        return []
+
     }
     render() {
         return <div className='sy-paging'>{this.getPages().map((pa, index) => {
