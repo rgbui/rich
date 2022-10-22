ea08f4efa491ea61f56711de21dea4243d017c81
kanhai <760489033@qq.com>
2022-10-22 22:54:24 +0800

    增加className

--- component/view/switch.tsx
+++ component/view/switch.tsx
@@ -2,17 +2,20 @@ import React, { CSSProperties } from "react";
 export function Switch(props: {
     checked: boolean,
     onChange: (checked: boolean) => void,
-    style?: CSSProperties,
+    style?: CSSProperties, className?: string | (string[]),
     disabled?: boolean
 }) {
     function down(event: React.MouseEvent) {
-       
+
         if (props.disabled !== false) {
             props.onChange(!props.checked)
         }
     }
+    var classList: string[] = ['shy-switch'];
+    if (props.checked) classList.push('checked')
+    if (props.disabled) classList.push('disabled')
     return <div style={props.style || {}}
-        className={'shy-switch' + (props.checked ? " checked" : "") + (props.disabled == true ? " disabled" : "")}
+        className={classList.join(" ")}
         onMouseDown={e => down(e)}>
         <em></em>
     </div>
