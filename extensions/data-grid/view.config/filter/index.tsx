5617fa98a4b61297ba0cbdabd6e2b88036404921
kanhai <760489033@qq.com>
2022-10-23 01:12:16 +0800

    修复表格视图的filter的样式

--- extensions/data-grid/view.config/filter/index.tsx
+++ extensions/data-grid/view.config/filter/index.tsx
@@ -5,14 +5,12 @@ import { DataGridView } from "../../../../blocks/data-grid/view/base";
 import { EventsComponent } from "../../../../component/lib/events.component";
 import { Divider } from "../../../../component/view/grid";
 import { Icon } from "../../../../component/view/icon";
-import "./style.less";
 import { Input } from "../../../../component/view/input";
-import { CloseTickSvg, PlusSvg } from "../../../../component/svgs";
+import { ArrowRightSvg, CheckSvg, CloseSvg, PlusSvg } from "../../../../component/svgs";
 import lodash from "lodash";
 import { SchemaFilter } from "../../../../blocks/data-grid/schema/declare";
 import { SelectBox } from "../../../../component/view/select/box";
 import { getTypeSvg } from "../../../../blocks/data-grid/schema/util";
-import { Remark } from "../../../../component/view/text";
 import { Switch } from "../../../../component/view/switch";
 import { useSelectMenuItem } from "../../../../component/view/menu";
 import { Rect } from "../../../../src/common/vector/point";
@@ -20,6 +18,7 @@ import dayjs from "dayjs";
 import { useDatePicker } from "../../../date";
 import { useUserPicker } from "../../../at/picker";
 import { Avatar } from "../../../../component/view/avator/face";
+import { MenuItemType } from "../../../../component/view/menu/declare";
 
 export class TableFilterView extends EventsComponent {
     get schema() {
@@ -33,7 +32,7 @@ export class TableFilterView extends EventsComponent {
         this.forceUpdate();
     }
     getFields() {
-        var fs = this.schema.fields.findAll(g => g.text && ![FieldType.formula].includes(g.type)).map(fe => {
+        var fs = this.schema.visibleFields.findAll(g => g.text && ![FieldType.formula].includes(g.type)).map(fe => {
             return {
                 icon: getTypeSvg(fe.type),
                 text: fe.text,
@@ -70,7 +69,7 @@ export class TableFilterView extends EventsComponent {
                 { text: '不为空', value: '$isNotNull' },
             ]
         }
-        else if ([FieldType.date].includes(field.type)) {
+        else if ([FieldType.date, FieldType.createDate, FieldType.modifyDate].includes(field.type)) {
             return [
                 { text: '等于', value: '$eq' },
                 { text: '不等于', value: '$ne' },
@@ -151,6 +150,7 @@ export class TableFilterView extends EventsComponent {
             ]
         var op = options.find(g => g.value == item.value);
         async function mousedown(item: SchemaFilter, event: React.MouseEvent) {
+            event.stopPropagation();
             var rect = Rect.fromEvent(event)
             var r = await useSelectMenuItem({ roundArea: rect }, options.map(op => {
                 return {
@@ -161,11 +161,21 @@ export class TableFilterView extends EventsComponent {
             }), { nickName: 'second' });
             if (r?.item) {
                 if (lodash.isNull(r.item.value)) {
-                    var rs = await useDatePicker({ roundArea: rect },
-                        item.value ? new Date(item.value) : new Date(), {});
-                    if (rs) {
-                        item.value = rs.getTime();
-                        self.onForceStore();
+                    if (item.operator == '$in') {
+                        var rs = await useDatePicker({ roundArea: rect }, new Date())
+                        if (rs) {
+                            item.value = [new Date().getTime(), rs.getTime()];
+                            self.onForceStore();
+                        }
+                        else item.value = [new Date().getTime(), new Date().getTime()]
+                    }
+                    else {
+                        var rs = await useDatePicker({ roundArea: rect },
+                            item.value ? new Date(item.value) : new Date(), {});
+                        if (rs) {
+                            item.value = rs.getTime();
+                            self.onForceStore();
+                        }
                     }
                 }
                 else {
@@ -176,12 +186,35 @@ export class TableFilterView extends EventsComponent {
         }
         var dateString = '';
         if (item.operator != '$in') {
-            if (item.value) dateString = dayjs(new Date(item.value)).format('YYYY-MM-DD');
+            if (item.value) {
+                var dd = dayjs(new Date(item.value));
+                if (dd.isValid())
+                    dateString = dayjs(new Date(item.value)).format('YYYY-MM-DD');
+                else dateString = '';
+            }
         }
         else {
 
         }
-        return <div onMouseDown={e => mousedown(item, e)} className="shy-table-filter-view-item-value">
+        async function setDateValue(pos: number, event: React.MouseEvent) {
+            var rect = Rect.fromEvent(event)
+            var rs = await useDatePicker({ roundArea: rect }, new Date(item.value[pos]))
+            if (rs) {
+                item.value[pos] = rs.getTime();
+                self.onForceStore();
+            }
+        }
+        if (item.operator == '$in' && !op) {
+            if (!Array.isArray(item.value)) item.value = [Date.now(), Date.now()];
+            var from = dayjs(new Date(item.value[0])).format('YYYY-MM-DD');
+            var to = dayjs(new Date(item.value[1])).format('YYYY-MM-DD');
+            return <div style={{ minWidth: 220 }} className="border  box-border round h-26 padding-w-14 flex-center gap-r-10">
+                <span onMouseDown={e => setDateValue(0, e)}>{from}</span>
+                <span className="gap-w-5"><Icon size={12} icon={ArrowRightSvg}></Icon></span>
+                <span onMouseDown={e => setDateValue(1, e)}>{to}</span>
+            </div>
+        }
+        return <div onClick={e => mousedown(item, e)} className="border box-border round h-26 padding-w-14 flex-center gap-r-10">
             {op?.text || dateString || '请选择日期'}
         </div>
     }
@@ -195,7 +228,17 @@ export class TableFilterView extends EventsComponent {
                 return {
                     text: op.text,
                     value: op.value,
-                    checkLabel: item.value == op.value
+                    checkLabel: item.value == op.value,
+                    type: MenuItemType.custom,
+                    render(it) {
+                        return <div className="flex padding-w-14 h-30 item-hover round cursor">
+                            <span className="flex-fixed size-20 round gap-r-10 border" style={{ backgroundColor: op.color }}></span>
+                            <span className="flex-auto text f-14">{op.text}</span>
+                            {it.value == item.value &&
+                                <span className="flex-fixed size-24 flex-center"><Icon size={16} icon={CheckSvg}></Icon></span>
+                            }
+                        </div>
+                    }
                 }
             }));
             if (r?.item) {
@@ -203,8 +246,9 @@ export class TableFilterView extends EventsComponent {
                 self.onForceStore();
             }
         }
-        return <div onMouseDown={e => mousedown(item, e)} className="shy-table-filter-view-item-value">
-            {op?.text || (item.value ? dayjs(item.value).format('YYYY-MM-DD') : undefined) || '请选择一项'}
+        return <div onMouseDown={e => mousedown(item, e)} className="border box-border round h-26 padding-w-14 flex-center gap-r-10">
+            {op.color && <span className="circle size-20 gap-r-5" style={{ background: op?.color ? op.color : undefined }}></span>}
+            <span>{op?.text || '请选择一项'}</span>
         </div>
     }
     renderUserInput(item: SchemaFilter) {
@@ -216,8 +260,8 @@ export class TableFilterView extends EventsComponent {
                 self.onForceStore();
             }
         }
-        return <div onMouseDown={e => mousedown(e)} className="shy-table-filter-view-item-user">
-            {item.value && <Avatar size={30} userid={item.value}></Avatar>}
+        return <div onMouseDown={e => mousedown(e)} className="border box-border round h-26 padding-w-14 flex-center gap-r-10">
+            {item.value && <Avatar size={20} userid={item.value}></Avatar>}
             {!item.value && <span>选择用户</span>}
         </div>
     }
@@ -237,20 +281,23 @@ export class TableFilterView extends EventsComponent {
             self.block.filter.items.remove(g => g === filter);
             self.onForceStore();
         }
-        return <div className="shy-table-filter-view">
-            <div className="shy-table-filter-view-logic">筛选符合下方<SelectBox small style={{ margin: '0px 5px' }} value={self.block.filter.logic} border options={[
-                { text: '任意', value: 'or' },
-                { text: '所有', value: 'and' }
-            ]} onChange={e => {
-                self.block.filter.logic = e;
-                self.onForceStore();
-            }}></SelectBox>条件的数据</div>
-            {self.block.filter.items.map((item, index) => {
+        return <div className="f-14">
+
+            <div className="h-30 flex padding-w-14 gap-t-10">
+                筛选符合下方<em className="gap-w-5"><SelectBox small value={self.block.filter.logic} border options={[
+                    { text: '任意', value: 'or' },
+                    { text: '所有', value: 'and' }
+                ]} onChange={e => {
+                    self.block.filter.logic = e;
+                    self.onForceStore();
+                }}></SelectBox></em>条件的数据
+            </div>
+            <div className="max-h-300 overflow-y">{self.block.filter.items.map((item, index) => {
                 var fe = self.block.schema.fields.find(g => g.id == item.field);
-                return <div className="shy-table-filter-view-item" key={index}>
-                    <div className="shy-table-filter-view-item-box">
-                        <SelectBox border options={self.getFields()} value={item.field} onChange={e => { item.field = e; self.onForceStore() }}></SelectBox>
-                        <SelectBox border options={self.getComputedFields(item.field)} value={item.operator} onChange={e => { item.operator = e; self.onForceStore() }} ></SelectBox>
+                return <div className="flex max-h-30 padding-w-14 gap-h-10" key={index}>
+                    <div className="flex-auto flex">
+                        <SelectBox small className={'gap-r-10'} border options={self.getFields()} value={item.field} onChange={e => { item.field = e; self.onForceStore() }}></SelectBox>
+                        <SelectBox  small className={'gap-r-10'} border options={self.getComputedFields(item.field)} value={item.operator} onChange={e => { item.operator = e; self.onForceStore() }} ></SelectBox>
                         {['$notContain', '$contain', '$startWidth', '$endWidth'].includes(item.operator)
                             ||
                             ([FieldType.text,
@@ -260,20 +307,21 @@ export class TableFilterView extends EventsComponent {
                             FieldType.phone,
                             FieldType.email].includes(fe.type) && ['$ne', '$eq'].includes(item.operator))
                             &&
-                            <Input style={{ width: 120 }} placeholder={'值'} value={item.value} onChange={e => { item.value = e; self.onStore(); }}></Input>}
-                        {[FieldType.number, FieldType.autoIncrement].includes(fe.type) && !['$isNull', '$isNOtNull'].includes(item.operator) && <Input type='number' style={{ width: 120 }} placeholder={'值'} value={item.value} onChange={e => { item.value = e; self.onStore(); }}></Input>}
+                            <Input className={'gap-r-10'} style={{ width: 120 }} placeholder={'值'} value={item.value} onChange={e => { item.value = e; self.onStore(); }}></Input>}
+                        {[FieldType.number, FieldType.autoIncrement].includes(fe.type) && !['$isNull', '$isNOtNull'].includes(item.operator) && <Input className={'gap-r-10'} type='number' style={{ width: 120 }} placeholder={'值'} value={item.value} onChange={e => { item.value = e; self.onStore(); }}></Input>}
                         {[FieldType.date, FieldType.createDate].includes(fe.type) && !['$isNull', '$isNOtNull'].includes(item.operator) && self.renderDateInput(item)}
                         {[FieldType.option, FieldType.options].includes(fe.type) && !['$isNull', '$isNOtNull'].includes(item.operator) && self.renderOptionInput(item)}
-                        {[FieldType.bool].includes(fe.type) && !['$isNull', '$isNOtNull'].includes(item.operator) && <Switch checked={item.value ? true : false} onChange={e => { item.value = e; self.onForceStore() }}></Switch>}
+                        {[FieldType.bool].includes(fe.type) && !['$isNull', '$isNOtNull'].includes(item.operator) && <Switch className={'gap-r-10'} checked={item.value ? true : false} onChange={e => { item.value = e; self.onForceStore() }}></Switch>}
                         {[FieldType.creater, FieldType.modifyer, FieldType.user].includes(fe.type) && !['$isNull', '$isNOtNull'].includes(item.operator) && self.renderUserInput(item)}
                     </div>
-                    <Icon style={{ padding: 5 }} size={12} onMousedown={e => removeFilter(e, item)} icon={CloseTickSvg} wrapper></Icon>
+                    <span className="flex-fixed flex-center size-24 round item-hover cursor"><Icon size={12} onMousedown={e => removeFilter(e, item)} icon={CloseSvg} ></Icon></span>
                 </div>
             })}
-            {self.block.filter.items.length == 0 && <Remark style={{ margin: 10 }}>还没有添加筛选条件</Remark>}
+                {self.block.filter.items.length == 0 && <div className="remark padding-w-14 f-12 h-30 flex">还没有添加筛选条件</div>}</div>
+
             <Divider></Divider>
-            <div className="shy-table-filter-view-footer">
-                <a onClick={e => addFilter()}><Icon size={14} style={{ marginRight: 5 }} icon={PlusSvg}></Icon>添加筛选条件</a>
+            <div onClick={e => addFilter()} className="h-30 gap-b-10 flex cursor item-hover padding-w-14">
+                <Icon size={14} style={{ marginRight: 5 }} icon={PlusSvg}></Icon>添加筛选条件
             </div>
         </div>
     }
