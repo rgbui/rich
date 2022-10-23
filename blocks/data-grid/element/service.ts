23e7de93d20b1394fb5e5406cfa5aefa722870a8
kanhai <760489033@qq.com>
2022-10-23 23:58:08 +0800

    增加获取表格字段对应的表单字段块信息

--- blocks/data-grid/element/service.ts
+++ blocks/data-grid/element/service.ts
@@ -1,40 +1,12 @@
 import { PageLayoutType } from "../../../src/page/declare";
+import { Field } from "../schema/field";
 import { TableSchema } from "../schema/meta";
 import { FieldType } from "../schema/type";
 
-export function schemaCreatePageFormData(schema: TableSchema) {
-    var cs: Record<string, any>[] = schema.fields.toArray(field => {
-        switch (field.type) {
-            case FieldType.text:
-            case FieldType.title:
-                return {
-                    url: '/form/text',
-                    fieldId: field.id
-                }
-                break;
-            case FieldType.bool:
-                return {
-                    url: '/form/check',
-                    fieldId: field.id
-                }
-            case FieldType.date:
-                return {
-                    url: '/form/date',
-                    fieldId: field.id
-                }
-            case FieldType.number:
-                return {
-                    url: '/form/number',
-                    fieldId: field.id
-                }
-                break;
-            case FieldType.option:
-                return {
-                    url: '/form/option',
-                    fieldId: field.id
-                }
-                break;
-        }
+export function SchemaCreatePageFormData(schema: TableSchema) {
+    var cs: Record<string, any>[] = schema.initUserFields.toArray(field => {
+        var r = GetFieldFormBlockInfo(field);
+        if (r) return r;
     })
     return {
         url: '/page',
@@ -49,3 +21,38 @@ export function schemaCreatePageFormData(schema: TableSchema) {
         ]
     }
 }
+
+export function GetFieldFormBlockInfo(field: Field) {
+    switch (field.type) {
+        case FieldType.text:
+        case FieldType.title:
+            return {
+                url: '/form/text',
+                fieldId: field.id
+            }
+            break;
+        case FieldType.bool:
+            return {
+                url: '/form/check',
+                fieldId: field.id
+            }
+        case FieldType.date:
+            return {
+                url: '/form/date',
+                fieldId: field.id
+            }
+        case FieldType.number:
+            return {
+                url: '/form/number',
+                fieldId: field.id
+            }
+            break;
+        case FieldType.option:
+            return {
+                url: '/form/option',
+                fieldId: field.id
+            }
+            break;
+    }
+}
+
