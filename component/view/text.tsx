64cf3ed5be2dbd359d1f9920e1a3dbe4966cd46b
kanhai <760489033@qq.com>
2022-11-01 09:07:56 +0800

    clear code

--- component/view/text.tsx
+++ component/view/text.tsx
@@ -1,9 +1,9 @@
 import React, { CSSProperties } from "react";
 import { ComponentSize } from "./declare";
 export function Remark(props: { style?: CSSProperties, size?: ComponentSize, children: React.ReactNode }) {
-    return <div style={(props.style || {})} className={'shy-remark' + (props.size ? " shy-remark-" + props.size : "")}>{props.children}</div>
+    return <div style={(props.style || {})} className={'remark' + (props.size&&props.size=='middle' ? " f-14":"f-12")}>{props.children}</div>
 }
 
 export function ErrorText(props: { children: React.ReactNode }) {
-    return <span className='shy-text-error'>{props.children}</span>
+    return <span className='error'>{props.children}</span>
 }
