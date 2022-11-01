dbbf976574d6f3cfd618ba152d418bd21010343c
kanhai <760489033@qq.com>
2022-11-01 14:50:49 +0800

    增加spin的大小调整

--- component/view/spin/index.tsx
+++ component/view/spin/index.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import "./style.less";
 
-function getCircle() {
-    return <div className="size-24 relative">
-        <i className="circle size-8 pos bg-primary" style={{ top: 0, left: 0 }}></i>
-        <i className="circle size-8 pos bg-primary op-8" style={{ top: 0, right: 0 }}></i>
-        <i className="circle size-8 pos bg-primary op-5" style={{ bottom: 0, right: 0 }}></i>
-        <i className="circle size-8 pos bg-primary op-3" style={{ bottom: 0, left: 0 }}></i>
+function getCircle(size?: 24 | 16) {
+    return <div className={"size-" + (size) + " relative"}>
+        <i className={`size-${size == 24 ? 8 : 4}` + " circle pos bg-primary"} style={{ top: 0, left: 0 }}></i>
+        <i className={`size-${size == 24 ? 8 : 4}` + " circle pos bg-primary op-8"} style={{ top: 0, right: 0 }}></i>
+        <i className={`size-${size == 24 ? 8 : 4}` + " circle pos bg-primary op-5"} style={{ bottom: 0, right: 0 }}></i>
+        <i className={`size-${size == 24 ? 8 : 4}` + " circle pos bg-primary op-3"} style={{ bottom: 0, left: 0 }}></i>
     </div>
 }
 
-export function Spin(props: { block?: boolean, children?: React.ReactNode }) {
+export function Spin(props: { block?: boolean, size?: 24 | 16, children?: React.ReactNode }) {
     var ov = props.children;
-    if (!ov) ov = getCircle();
-    var div = <div className="shy-spin-circle flex-center flex-inline" style={{ width: 24 * 1.5, height: 24 * 1.5 }}>
+    var size = props.size || 24;
+    if (!ov) ov = getCircle(size);
+    var div = <div className="shy-spin-circle flex-center flex-inline" style={{ width: size * 1.5, height: size * 1.5 }}>
         {ov}
     </div>;
     if (props.block) return <div className="flex-center gap-h-20">{div}</div>
