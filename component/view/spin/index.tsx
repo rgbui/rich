05347e75546011b4be7eaed8c77a7394bcc52db4
kanhai <760489033@qq.com>
2022-11-01 09:02:41 +0800

    修复spin的显示效果

--- component/view/spin/index.tsx
+++ component/view/spin/index.tsx
@@ -2,20 +2,22 @@ import React from "react";
 import "./style.less";
 
 function getCircle() {
-    return <div className="size-24">
+    return <div className="size-24 relative">
         <i className="circle size-8 pos bg-primary" style={{ top: 0, left: 0 }}></i>
-        <i className="circle size-8 pos bg-primary op-9" style={{ top: 0, right: 0 }}></i>
+        <i className="circle size-8 pos bg-primary op-8" style={{ top: 0, right: 0 }}></i>
+        <i className="circle size-8 pos bg-primary op-5" style={{ bottom: 0, right: 0 }}></i>
         <i className="circle size-8 pos bg-primary op-3" style={{ bottom: 0, left: 0 }}></i>
-        <i className="circle size-8 pos bg-primary op-6" style={{ bottom: 0, right: 0 }}></i>
     </div>
 }
 
-export function Spin(props: { children?: React.ReactNode }) {
+export function Spin(props: { block?: boolean, children?: React.ReactNode }) {
     var ov = props.children;
     if (!ov) ov = getCircle();
-    return <div className="shy-spin-circle" style={{ display: 'inline-block' }}>
+    var div = <div className="shy-spin-circle flex-center flex-inline" style={{ width: 24 * 1.5, height: 24 * 1.5 }}>
         {ov}
-    </div>
+    </div>;
+    if (props.block) return <div className="flex-center gap-h-20">{div}</div>
+    return div;
 }
 export function SpinBox(props: {
     children?: React.ReactNode,
