b6ac95b900fabc4f647a5a0e4f253ef7d3a075fc
kanhai <760489033@qq.com>
2022-11-03 23:47:11 +0800

    设置排序查询

--- extensions/data-grid/view.config/control/index.tsx
+++ extensions/data-grid/view.config/control/index.tsx
@@ -4,8 +4,10 @@ import { FieldType } from "../../../../blocks/data-grid/schema/type";
 import { GetFieldTypeSvg } from "../../../../blocks/data-grid/schema/util";
 import { DataGridView } from "../../../../blocks/data-grid/view/base";
 import { EventsComponent } from "../../../../component/lib/events.component";
+import { FilterSvg, SortSvg } from "../../../../component/svgs";
 import { Icon } from "../../../../component/view/icon";
 import { Switch } from "../../../../component/view/switch";
+import { ToolTip } from "../../../../component/view/tooltip";
 import { BlockUrlConstant } from "../../../../src/block/constant";
 import "./style.less";
 
@@ -53,24 +55,37 @@ export class DataGridControl extends EventsComponent {
 
             <div className="flex item-hover round h-30 padding-w-14 ">
                 <span className="flex-auto">导出数据</span>
-                <span className="flex-fix"><Switch checked={false} onChange={e => { this.block.onExtendControlBlock(BlockUrlConstant.Button, { content: '导出', action: 'export' }, e) }}></Switch></span>
+                <span className="flex-fix"><Switch checked={false}
+                    onChange={e => { this.block.onExtendControlBlock(BlockUrlConstant.Button, { content: '导出', action: 'export' }, e) }}></Switch></span>
             </div>
 
             <div className="remark f-12 padding-w-14 gap-t-10">扩展字段控制块</div>
             {this.block.schema.visibleFields.findAll(g => ![
-                FieldType.video,
+                // FieldType.video,
+                FieldType.formula,
+                FieldType.rollup,
                 FieldType.button,
-                FieldType.audio,
+                // FieldType.audio,
                 FieldType.parentId,
-                FieldType.image,
-                FieldType.file,
-                FieldType].includes(g.type)).map(f => {
-                    return <div key={f.id} className="flex item-hover round h-30 padding-w-14 ">
-                        <span className="flex-fix size-24 flex-center cursor round item-hover"><Icon size={16} icon={GetFieldTypeSvg(f.type)}></Icon></span>
-                        <span className="flex-auto">{f.text}</span>
-                        <span className="flex-fix"><Switch checked={false} onChange={e => { }}></Switch></span>
-                    </div>
-                })}
+                // FieldType.image,
+                // FieldType.file,
+
+            ].includes(g.type)).map(f => {
+                return <div key={f.id} className="flex item-hover round h-30 padding-w-14 ">
+                    <span className="flex-fix size-24 flex-center cursor round item-hover"><Icon size={16} icon={GetFieldTypeSvg(f.type)}></Icon></span>
+                    <span className="flex-auto">{f.text}</span>
+                    <ToolTip overlay={"设为过滤条件"}>
+                        <span onMouseDown={e => this.block.onExtendControlFilter(f)} className="flex-fix size-24 flex-center cursor round item-hover">
+                            <Icon size={16} icon={FilterSvg}></Icon>
+                        </span>
+                    </ToolTip>
+                    <ToolTip overlay={"设为排序条件"}>
+                        <span onMouseDown={e => this.block.onExtendControlSort(f)} className="flex-fix size-24 flex-center cursor round item-hover">
+                            <Icon size={16} icon={SortSvg}></Icon>
+                        </span>
+                    </ToolTip>
+                </div>
+            })}
         </div>
     }
 }
