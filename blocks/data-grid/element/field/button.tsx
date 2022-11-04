f5b888e1fce241298e3585a283ac4185d8e7ae6c
kanhai <760489033@qq.com>
2022-11-04 23:16:41 +0800

    实现操作按钮的编辑、删除功能

--- blocks/data-grid/element/field/button.tsx
+++ blocks/data-grid/element/field/button.tsx
@@ -1,17 +1,27 @@
 import React from "react";
+import { EditSvg, TrashSvg } from "../../../../component/svgs";
+import { Icon } from "../../../../component/view/icon";
 import { url, view } from "../../../../src/block/factory/observable";
 import { BlockView } from "../../../../src/block/view";
 import { OriginField } from "./origin.field";
 
 @url('/field/button')
 export class FieldCheck extends OriginField {
-
+    onRowEdit(e: React.MouseEvent) {
+        this.dataGrid.onOpenEditForm(this.item.dataRow.id);
+    }
+    onRowDelete(e: React.MouseEvent) {
+        this.dataGrid.onBatchDelete([this.item.dataRow.id]);
+    }
 }
 @view('/field/button')
 export class FieldCheckView extends BlockView<FieldCheck>{
     render() {
         return <div className='sy-field-button' onMouseDown={e => e.stopPropagation()}>
-            <a>编辑</a><a>删除</a>
+            <div className="flex">
+                <a className="text-1 f-14 cursor padding-w-10 padding-h-5 round item-hover flex-center" onMouseDown={e => this.block.onRowEdit(e)}><span className="size-20 flex-center"><Icon size={16} icon={EditSvg}></Icon></span><span>编辑</span></a>
+                <a className="text-1 f-14 cursor padding-w-10 padding-h-5 round item-hover  flex-center" onMouseDown={e => this.block.onRowDelete(e)}><span className="size-20 flex-center"><Icon size={16} icon={TrashSvg}></Icon></span><span>删除</span></a>
+            </div>
         </div>
     }
 }
