2a70faeadf3a7a720ef6b5cf97636617f7a9d4d0
kanhai <760489033@qq.com>
2022-11-07 00:49:39 +0800

    增加视频、音频的字段显示

--- blocks/data-grid/element/field/audio.tsx
+++ blocks/data-grid/element/field/audio.tsx
@@ -1,20 +1,19 @@
-import React from "react";
-import { CSSProperties } from "react";
+import React, { CSSProperties } from "react";
 import { useDataGridFileViewer } from "../../../../extensions/data-grid/filer";
 import { url, view } from "../../../../src/block/factory/observable";
 import { BlockView } from "../../../../src/block/view";
 import { Rect } from "../../../../src/common/vector/point";
 import { OriginField } from "./origin.field";
 
-@url('/field/video')
-export class FieldVideo extends OriginField {
+@url('/field/audio')
+export class FieldAudio extends OriginField {
     async onCellMousedown(event: React.MouseEvent) {
         var vs = Array.isArray(this.value) ? this.value : (this.value ? [this.value] : []);
         if (!this.field?.config?.isMultiple) {
             vs = vs.slice(0, 1);
         }
         var rs = await useDataGridFileViewer({ roundArea: Rect.fromEvent(event) }, {
-            mime: 'video',
+            mime: 'audio',
             resources: vs,
             isMultiple: this.field?.config?.isMultiple ? true : false
         });
@@ -26,25 +25,30 @@ export class FieldVideo extends OriginField {
     }
 }
 
-@view('/field/video')
-export class FieldVideoView extends BlockView<FieldVideo>{
+@view('/field/audio')
+export class FieldVideoView extends BlockView<FieldAudio>{
     renderImages(images: { url: string }[]) {
         var style: CSSProperties = {};
         // if (this.block.field?.config.imageFormat?.display == 'auto') {
-        style.width = '100%';
-        style.maxHeight = 300;
-        style.objectFit = 'cover';
-        style.objectPosition = '50% 50%';
-        // }
-        // else {
-        //     style.width = 50;
-        //     style.height = 50;
+        //     style.width = '100%';
+        //     style.maxHeight = 300;
         //     style.objectFit = 'cover';
         //     style.objectPosition = '50% 50%';
         // }
+        // else {
+        style.width = '100%';
+        style.height = 30;
+        // style.objectFit = 'cover';
+        // style.objectPosition = '50% 50%';
+        // }
         return images.map((img, i) => {
             return <div className="sy-field-image-item" key={i}>
-                <video controls className="round" src={img.url} style={style} ></video>
+                <audio className="round" controls src={img.url} style={style}  >
+                    <source src={img.url} type="audio/ogg" />
+                    <source src={img.url} type="audio/mpeg" />
+                    <source src={img.url} type="audio/wav" />
+                    您的浏览器不支持 audio 元素。
+                </audio>
             </div>
         })
     }
