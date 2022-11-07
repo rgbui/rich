93e4c74c00494a3c617ef51beaa5af48767088ff
kanhai <760489033@qq.com>
2022-11-08 01:48:24 +0800

    修复关联字段的显示

--- blocks/data-grid/element/field/relation.tsx
+++ blocks/data-grid/element/field/relation.tsx
@@ -1,3 +1,4 @@
+import lodash from "lodash";
 import React from "react";
 import { Icon } from "../../../../component/view/icon";
 import { getPageIcon } from "../../../../extensions/at/declare";
@@ -53,12 +54,12 @@ export class FieldRelationView extends BlockView<FieldRelation>{
         var icon = rs?.fields.find(g => g.type == FieldType.icon);
         return <div className='sy-field-relation-items'>{this.block.relationList?.map(r => {
             var url = getElementUrl(ElementType.SchemaData, rs.id, r.id);
-            return <a href={url} onClick={e => e.preventDefault()} key={r.id}>
+            return <div key={r.id}><a className="item-hover round" href={url} onClick={e => e.preventDefault()}>
                 <span className="size-24 flex-center flex-inline">
                     <Icon size={16} icon={getPageIcon({ pageType: PageLayoutType.doc, icon: r[icon.name] })}></Icon>
                 </span>
                 <span>{r[f?.name]}</span>
-            </a>
+            </a></div>
         })}
         </div>
     }
@@ -68,3 +69,55 @@ export class FieldRelationView extends BlockView<FieldRelation>{
         </div>
     }
 }
+
+@url('/field/rollup')
+export class FieldRollup extends OriginField {
+    get relationList() {
+        var gs = this.dataGrid.relationDatas.get(this.field.config?.rollupTableId) || [];
+        var item: FieldRelation = this.item.childs.find(g => (g instanceof FieldRelation) && g.field.config.relationTableId == this.field.config?.rollupTableId) as any;
+        if (item) {
+            var vs: string[] = item.value;
+            if (!Array.isArray(vs)) vs = [];
+            if (vs.length == 0) return [];
+            if (!item.field.config.isMultiple) {
+                vs = vs.slice(0, 1);
+            }
+            return gs.findAll(g => vs.includes(g.id));
+        }
+        return []
+    }
+    get relationSchema() {
+        return this.dataGrid.relationSchemas.find(g => g.id == this.field.config?.rollupTableId)
+    }
+    async onCellMousedown(event: React.MouseEvent<Element, MouseEvent>) {
+
+    }
+}
+@view('/field/rollup')
+export class FieldRollupView extends BlockView<FieldRollup>{
+    render() {
+        var str: string = '';
+        var list = this.block.relationList;
+        console.log(list, 'ggg');
+        var field = this.block.relationSchema?.fields?.find(g => g.id == this.block.field.config?.rollupFieldId);
+        if (this.block.field.config?.rollupStatistic == '$count') {
+            str = list.length.toString();
+        }
+        else if (this.block.field.config?.rollupStatistic == '$sum') {
+            str = lodash.sum(list.map(c => c[field?.name])).toString()
+        } else if (this.block.field.config?.rollupStatistic == '$agv') {
+            str = (lodash.sum(list.map(c => c[field?.name])) / list.length).toString()
+        } else if (this.block.field.config?.rollupStatistic == '$min') {
+            var va = list.min(g => g[field?.name])
+            if (typeof va != 'undefined') str = va.toString()
+        } else if (this.block.field.config?.rollupStatistic == '$max') {
+            var va = list.max(g => g[field?.name])
+            if (typeof va != 'undefined') str = va.toString()
+        }
+
+        return <div className='sy-field-relation' onMouseDown={e => this.block.onCellMousedown(e)}>
+            {str}
+        </div>
+    }
+}
+
