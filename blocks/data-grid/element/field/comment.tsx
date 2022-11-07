0f912aac74cbaeb5e5dcac5c264e9283bcfc0cff
kanhai <760489033@qq.com>
2022-11-07 22:44:51 +0800

    增加评论字段的显示

--- blocks/data-grid/element/field/comment.tsx
+++ blocks/data-grid/element/field/comment.tsx
@@ -11,8 +11,11 @@ export class FieldComment extends OriginField {
 @view('/field/comment')
 export class FieldCommentView extends BlockView<FieldComment>{
     render() {
+        var v = this.block.value;
+        if (typeof v == 'object' && typeof v.count == 'number') v = v.count;
+        var countStr = v > 0 ? `(${v})` : '';
         return <div className='sy-field-text'>
-
+            <span className="flex-center flex-inline f-14 text-1 padding-w-5 h-30 round-16 item-hover">评论<em>{countStr}</em></span>
         </div>
     }
 }
