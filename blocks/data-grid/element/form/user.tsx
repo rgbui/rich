21c7fa64b9a5e4bfbceddc04262be1c7706ff67e
kanhai <760489033@qq.com>
2022-11-12 23:48:48 +0800

    完善用户的选择字段

--- blocks/data-grid/element/form/user.tsx
+++ blocks/data-grid/element/form/user.tsx
@@ -1,23 +1,41 @@
 import React from "react";
+import { PlusSvg } from "../../../../component/svgs";
 import { UserAvatars } from "../../../../component/view/avator/users";
+import { Icon } from "../../../../component/view/icon";
+import { useDataGridFileViewer } from "../../../../extensions/data-grid/filer";
 import { url, view } from "../../../../src/block/factory/observable";
 import { BlockView } from "../../../../src/block/view";
+import { Rect } from "../../../../src/common/vector/point";
 import { FieldView, OriginFormField } from "./origin.field";
 
 @url('/form/user')
 export class FieldUser extends OriginFormField {
-
+    async onSelectUser(event: React.MouseEvent) {
+        var vs = this.value;
+        if (!Array.isArray(vs) && vs) vs = [vs];
+        if (!Array.isArray(vs)) vs = [];
+        var rs = await useDataGridFileViewer({ roundArea: Rect.fromEvent(event) }, {
+            mime: 'user',
+            resources: vs,
+            isMultiple: this.field?.config?.isMultiple ? true : false
+        });
+        if (Array.isArray(rs) && rs.length > 0) {
+            this.value = rs;
+            this.forceUpdate();
+        }
+    }
 }
 
 @view('/form/user')
 export class FieldTextView extends BlockView<FieldUser>{
     render() {
-        var self = this;
         var vs = this.block.value;
         if (!Array.isArray(vs) && vs) vs = [vs];
         if (!Array.isArray(vs)) vs = [];
         return <FieldView block={this.block}>
-            <UserAvatars size={30} users={vs}></UserAvatars>
+            <UserAvatars size={30} users={vs}>
+                {(this.block.field.config.isMultiple || vs.length < 2) && <span onMouseDown={e => this.block.onSelectUser(e)} className=" round item-hover-focus size-24 flex-center cursor"><Icon size={16} icon={PlusSvg}></Icon></span>}
+            </UserAvatars>
         </FieldView>
     }
 }
