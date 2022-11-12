ab2a409c23a573d916ba7e957b0a396c12fb9dd1
kanhai <760489033@qq.com>
2022-11-12 23:47:21 +0800

    修复用户users待选管理列表

--- component/view/avator/users.tsx
+++ component/view/avator/users.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Avatar } from "./face";
 
-
-export class UserAvatars extends React.Component<{size?:number, users: (string[]) | Set<string> }>{
-
+export class UserAvatars extends React.Component<{
+    size?: number,
+    users: (string[]) | Set<string>,
+    children?: JSX.Element | string | React.ReactNode
+}>{
     render(): React.ReactNode {
         var us = Array.isArray(this.props.users) ? this.props.users : Array.from(this.props.users);
         return <div className="flex">
-            {us.map(u =><Avatar key={u} size={this.props.size} userid={u}></Avatar>)}
+            {us.map(u => <Avatar key={u} size={this.props.size} userid={u}></Avatar>)}
+            {this.props.children && <><em className={us.length > 0 ? "gap-r-10" : ""}></em>{this.props.children}</>}
         </div>
     }
 }
