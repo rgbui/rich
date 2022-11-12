175e27afc45c2b028f479ad9536c1bea37ee7aa1
kanhai <760489033@qq.com>
2022-11-12 23:54:18 +0800

    完善表单图片上传字段

--- blocks/data-grid/element/form/image.tsx
+++ blocks/data-grid/element/form/image.tsx
@@ -1,6 +1,9 @@
+import lodash from "lodash";
 import React from "react";
 import { OpenFileDialoug } from "../../../../component/file";
+import { CloseSvg } from "../../../../component/svgs";
 import { Button } from "../../../../component/view/button";
+import { Icon } from "../../../../component/view/icon";
 import { channel } from "../../../../net/channel";
 import { url, view } from "../../../../src/block/factory/observable";
 import { BlockView } from "../../../../src/block/view";
@@ -27,12 +30,27 @@ class FormFieldImage extends OriginFormField {
         }
     }
 }
+
 @view('/form/image')
 class FormFieldImageView extends BlockView<FormFieldImage>{
+    deleteImage(img, event: React.MouseEvent) {
+        var vs = Array.isArray(this.block.value) ? this.block.value : (this.block.value ? [this.block.value] : []);
+        lodash.remove(vs, g => g == img);
+        this.block.value = vs;
+        this.forceUpdate();
+    }
     renderImages(images: { url: string }[]) {
         return images.map((img, i) => {
-            return <div className="sy-field-image-item" key={i}>
-                <img src={img.url} />
+            return <div
+                onClick={e => this.deleteImage(img, e)}
+                className="border round flex-center relative gap-r-10 gap-b-10 visible-hover size-120 round"
+                key={i}>
+                <span className="pos-top-right flex-center size-24 item-hover-focus circle cursor visible">
+                    <Icon size={16} icon={CloseSvg}></Icon>
+                </span>
+                <img src={img.url}
+                    style={{ maxWidth: "100%", maxHeight: "100%", objectFit: "cover", objectPosition: 'center center' }}
+                />
             </div>
         })
     }
@@ -41,10 +59,10 @@ class FormFieldImageView extends BlockView<FormFieldImage>{
         if (!this.block.field?.config?.isMultiple && vs.length > 1) vs = [vs.first()]
         return <FieldView block={this.block}>
             <div className="sy-form-field-image-value" >
-                {vs.length > 0 && <div className="sy-field-images">
+                {vs.length > 0 && <div className="flex">
                     {this.renderImages(vs)}
                 </div>}
-                {(vs.length == 0 || this.block.field?.config?.isMultiple) && <Button size="small" ghost onClick={e => this.block.uploadFile(e)}>上传文件</Button>}
+                {(vs.length < 2 || this.block.field?.config?.isMultiple) && <Button size="small" ghost onClick={e => this.block.uploadFile(e)}>上传图片</Button>}
             </div>
         </FieldView>
     }
