9ac08ea7e8c935b0093d409b6c2e3d99a20d183a
kanhai <760489033@qq.com>
2022-11-17 23:11:08 +0800

    feature: 实现自定义卡版模板的显示效果

--- blocks/data-grid/card/view.tsx
+++ blocks/data-grid/card/view.tsx
@@ -1,8 +1,56 @@
 import React from "react";
-import { Block } from "../../../src/block";
+
+import { TableStoreGallery } from "../view/gallery";
+import { TableStoreItem } from "../view/item";
 import { DataGridItemRecord } from "../element/record";
+import { useImageFilePicker } from "../../../extensions/file/image.picker";
+import { Rect } from "../../../src/common/vector/point";
 import { DataGridView } from "../view/base";
 
-export class CardView extends React.Component<{ item: Block, dataGrid: DataGridView|DataGridItemRecord }> {
-   
+export class CardView extends React.Component<{ item: DataGridItemRecord | TableStoreItem, dataGrid: DataGridView | DataGridItemRecord }> {
+
+    CardConfig() {
+        if (this.props.dataGrid instanceof TableStoreGallery) {
+            return this.props.dataGrid.cardConfig;
+        }
+    }
+    get schema() {
+        if (this.props.dataGrid instanceof TableStoreGallery) {
+            return this.props.dataGrid.schema;
+        }
+    }
+    getField(name: string) {
+        var n = this.CardConfig().templateProps.props?.find(g => g.name == name);
+        if (n) {
+            var f = this.schema.fields.find(c => c.id == n.bindFieldId);
+            if (f) return f;
+        }
+    }
+    getValue<T = string>(name: string): T {
+        var f = this.getField(name);
+        if (f) {
+            return this.props.item.dataRow[f.name];
+        }
+    }
+    async uploadImage(name: string, event: React.MouseEvent | MouseEvent | Rect) {
+        if (typeof (event as any).stopPropagation == 'function') (event as any).stopPropagation()
+        var resource = await useImageFilePicker({ roundArea: Rect.fromEvent(event) });
+        if (resource) {
+            var field = this.getField(name);
+            if (this.props.item instanceof TableStoreItem) {
+                await this.props.item.onUpdateCellValue(field, [resource]);
+                this.forceUpdate()
+            }
+        }
+    }
+    async deleteItem() {
+        if (this.props.item instanceof TableStoreItem) {
+            await (this.props.dataGrid as DataGridView).onRemoveRow(this.props.item.dataRow.id);
+        }
+    }
+    async openEdit(event: React.MouseEvent) {
+        if (this.props.dataGrid instanceof TableStoreGallery) {
+            this.props.dataGrid.onOpenEditForm(this.props.item.dataRow.id)
+        }
+    }
 }
