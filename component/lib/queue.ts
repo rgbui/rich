97acb1d211479a9096b72ad8a1094c9d339f13d7
kanhai <760489033@qq.com>
2022-12-04 02:16:25 +0800

    增加并发请求合并到一个队列中，然后有序请求

---
+++ component/lib/queue.ts
@@ -0,0 +1,59 @@
+import lodash from "lodash";
+import { util } from "../../util/util";
+
+
+
+export class QueueHandle {
+
+
+
+
+    acts: { id: string, notify: (data, err?) => void, action: () => Promise<any> }[] = [];
+    async create(action: () => Promise<any>, timeOut?:number) {
+        return new Promise((resolve, reject) => {
+            var id = util.guid();
+            var data = {
+                id,
+                action,
+                notify: (result, err) => {
+                    if (err) reject(err)
+                    else resolve(result);
+                },
+                timeOut: setTimeout(() => {
+                    lodash.remove(this.acts, g => g.id == id);
+                    reject('over time');
+                    this.tryExcute();
+                }, 1000 || timeOut)
+            }
+            this.acts.push(data);
+            this.notifyExcute();
+        })
+    }
+    private isExcuting: boolean = false;
+    private notifyExcute() {
+        if (this.isExcuting) return;
+        this.tryExcute();
+    }
+    private tryExcute() {
+        var g = this.acts[0];
+        if (g) {
+            if (g.action) {
+                this.isExcuting = true;
+                g.action().then(d => {
+                    g.notify(d);
+                    lodash.remove(this.acts, c => c.id == g.id);
+                    this.tryExcute();
+                }).catch(err => {
+                    g.notify(undefined, err);
+                    lodash.remove(this.acts, c => c.id == g.id);
+                    this.tryExcute();
+                })
+            }
+        }
+        else {
+            this.isExcuting = false;
+        }
+    }
+
+
+}
