6db2482476ece5b8800a0e751fbc2b2f4c506bf9
kanhai <760489033@qq.com>
2023-01-05 02:50:18 +0800

    调整用户非登录状态下的bar

--- src/page/view/bar.tsx
+++ src/page/view/bar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Page } from "..";
 import { DotsSvg, MemberSvg, PublishSvg, SearchSvg } from "../../../component/svgs";
 import { UserAvatars } from "../../../component/view/avator/users";
+import { Button } from "../../../component/view/button";
 import { Icon } from "../../../component/view/icon";
 import { Spin } from "../../../component/view/spin";
 import { getPageIcon } from "../../../extensions/at/declare";
@@ -13,9 +14,9 @@ import { PageDirective } from "../directive";
 export class PageBar extends React.Component<{ page: Page }>{
     renderTitle() {
         return <div className="flex-auto flex">
-            <span className="item-hover round flex gap-l-10 cursor padding-3 ">
-                <Icon size={18} icon={getPageIcon(this.props.page.pageInfo)}></Icon>
-                <span>{this.props.page.pageInfo.text}</span>
+            <span className="item-hover round flex gap-l-10 cursor padding-h-3 padding-w-5 ">
+                <Icon size={18} icon={getPageIcon(this.props.page?.pageInfo)}></Icon>
+                <span className="gap-l-5">{this.props.page?.pageInfo?.text}</span>
             </span>
             {this.saving && <Spin></Spin>}
         </div>
@@ -58,12 +59,21 @@ export class PageBar extends React.Component<{ page: Page }>{
             <UserAvatars users={this.users}></UserAvatars>
         </div>
     }
+    toLogin() {
+        location.href = 'https://shy.live';
+    }
     renderPropertys() {
-        return <div className="flex r-flex-center r-size-24 r-item-hover r-round r-cursor r-gap-r-10 text-1">
-            {[PageLayoutType.textChannel].includes(this.props.page.pageLayout.type) && <span onMouseDown={e => this.props.page.openMember(e)} ><Icon size={18} icon={MemberSvg}></Icon></span>}
-            {![PageLayoutType.textChannel].includes(this.props.page.pageLayout.type) && <span onMouseDown={async e => { await useSearchBox({ isNav: true }) }}><Icon size={18} icon={SearchSvg}></Icon></span>}
-            {![PageLayoutType.textChannel].includes(this.props.page.pageLayout.type) && <span onMouseDown={e => this.props.page.onOpenPublish(e)} ><Icon size={18} icon={PublishSvg}></Icon></span>}
-            {![PageLayoutType.textChannel].includes(this.props.page.pageLayout.type) && <span onMouseDown={e => this.props.page.onPageContextmenu(e)} ><Icon size={18} icon={DotsSvg}></Icon></span>}
+        var user = channel.query('/query/current/user');
+        var isSign = user.id ? true : false;
+        if (isSign)
+            return <div className="flex r-flex-center r-size-24 r-item-hover r-round r-cursor r-gap-r-10 text-1">
+                {[PageLayoutType.textChannel].includes(this.props.page.pageLayout.type) && <span onMouseDown={e => this.props.page.openMember(e)} ><Icon size={18} icon={MemberSvg}></Icon></span>}
+                {![PageLayoutType.textChannel].includes(this.props.page.pageLayout.type) && <span onMouseDown={async e => { await useSearchBox({ isNav: true }) }}><Icon size={18} icon={SearchSvg}></Icon></span>}
+                {![PageLayoutType.textChannel].includes(this.props.page.pageLayout.type) && <span onMouseDown={e => this.props.page.onOpenPublish(e)} ><Icon size={18} icon={PublishSvg}></Icon></span>}
+                {![PageLayoutType.textChannel].includes(this.props.page.pageLayout.type) && <span onMouseDown={e => this.props.page.onPageContextmenu(e)} ><Icon size={18} icon={DotsSvg}></Icon></span>}
+            </div>
+        else return <div className="flex r-flex-center r-size-24 r-item-hover r-round r-cursor r-gap-r-10 text-1">
+            <Button onClick={e => this.toLogin()}>登录</Button>
         </div>
     }
 
