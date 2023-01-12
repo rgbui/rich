12273b6e8d95fc1a120dc573e1c632109acbc287
kanhai <760489033@qq.com>
2023-01-12 22:45:44 +0800

    优化button的显示

--- blocks/data-grid/element/field/button.tsx
+++ blocks/data-grid/element/field/button.tsx
@@ -19,8 +19,8 @@ export class FieldCheckView extends BlockView<FieldCheck>{
     render() {
         return <div className='sy-field-button' onMouseDown={e => e.stopPropagation()}>
             <div className="flex">
-                <a className="text-1 f-14 cursor padding-w-10 padding-h-5 round item-hover flex-center" onMouseDown={e => this.block.onRowEdit(e)}><span className="size-20 flex-center"><Icon size={16} icon={EditSvg}></Icon></span><span>编辑</span></a>
-                <a className="text-1 f-14 cursor padding-w-10 padding-h-5 round item-hover  flex-center" onMouseDown={e => this.block.onRowDelete(e)}><span className="size-20 flex-center"><Icon size={16} icon={TrashSvg}></Icon></span><span>删除</span></a>
+                <a className="text-1 f-14 cursor padding-w-5 padding-h-3 round item-hover flex-center " onMouseDown={e => this.block.onRowEdit(e)}><span className="size-20 flex-center"><Icon size={16} icon={EditSvg}></Icon></span><span>编辑</span></a>
+                <a className="text-1 f-14 cursor padding-w-5 padding-h-3 round item-hover  flex-center" onMouseDown={e => this.block.onRowDelete(e)}><span className="size-20 flex-center"><Icon size={16} icon={TrashSvg}></Icon></span><span>删除</span></a>
             </div>
         </div>
     }
