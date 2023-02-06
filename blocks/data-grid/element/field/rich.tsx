d6d9ea0a72c0d3ca7bace936ae35b12987f5e505
kanhai <760489033@qq.com>
2023-02-07 02:16:59 +0800

    调整富文本编辑字段

--- blocks/data-grid/element/field/rich.tsx
+++ blocks/data-grid/element/field/rich.tsx
@@ -1,22 +1,33 @@
-import lodash from "lodash";
 import React from "react";
-import { RichView } from "../../../../component/view/rich";
+import { EditSvg } from "../../../../component/svgs";
+import { useShyRichEditor } from "../../../../component/view/editor";
+import { Icon } from "../../../../component/view/icon";
 import { url, view } from "../../../../src/block/factory/observable";
 import { BlockView } from "../../../../src/block/view";
 import { OriginField } from "./origin.field";
 
 @url('/field/rich')
 export class FieldRich extends OriginField {
-    onInputRichValue = lodash.debounce(async (value: string) => {
-        console.log(value);
-        await this.onUpdateCellValue({ content: value });
-    },800)
+    async onOpenEditRich(event: React.MouseEvent) {
+        var g = await useShyRichEditor({
+            html: this.value?.content || ''
+        });
+        if (g) {
+            await this.onUpdateCellValue({ content: g.content, text: g.text.slice(0, 200), pics: g.pics });
+            this.forceUpdate();
+        }
+    }
 }
 @view('/field/rich')
 export class FieldRichView extends BlockView<FieldRich>{
     render() {
+        // var content = this.block.value?.content || '';
+        // content = content.replace(/<[^>]+>/gi, "");
+        var size = 80;
         return <div className='sy-field-text f-14'>
-            <RichView onInput={e => this.block.onInputRichValue(e)} value={this.block.value?.content} ></RichView>
+            {(this.block.value?.text||"").slice(0,80)}
+            {/* {content.length > size ? content.slice(0, size) + "..." : content} */}
+            <span onMouseDown={e => this.block.onOpenEditRich(e)} className="size-20 round-4 flex-center flex-inline cursor item-hover"><Icon icon={EditSvg} size={16}></Icon></span>
         </div>
     }
 }
--- blocks/data-grid/element/form/rich.tsx
+++ blocks/data-grid/element/form/rich.tsx
@@ -1,23 +1,22 @@
 import lodash from "lodash";
 import React from "react";
-import { RichView } from "../../../../component/view/rich";
+import { RichViewEditor } from "../../../../component/view/editor/view";
 import { url, view } from "../../../../src/block/factory/observable";
 import { BlockView } from "../../../../src/block/view";
 import { FieldView, OriginFormField } from "./origin.field";
 
 @url('/form/rich')
 class FieldRich extends OriginFormField {
-    onInputRichValue = lodash.debounce(async (value: string) => {
-        console.log(value);
+    onInputRichValue = lodash.debounce(async (value: string,pics,text) => {
         var self = this;
-        self.onChange({ content: value });
+        self.onChange({ content: value,pics,text:text.slice(0,200) });
     }, 800)
 }
 @view('/form/rich')
 class FieldRichView extends BlockView<FieldRich>{
     render() {
         return <FieldView block={this.block}>
-            <RichView onInput={e => this.block.onInputRichValue(e)} value={this.block.value?.content} ></RichView>
+            <RichViewEditor onChange={(e,pics,text) => this.block.onInputRichValue(e,pics,text)} html={this.block.value?.content}></RichViewEditor>
         </FieldView>
     }
 }
