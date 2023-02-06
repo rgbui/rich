986c73fa4d61bf88a8b1f75ff979bfe3cef30c79
kanhai <760489033@qq.com>
2023-02-06 21:28:13 +0800

    修复字段喜欢的显示方式

--- blocks/data-grid/element/field/emoji.tsx
+++ blocks/data-grid/element/field/emoji.tsx
@@ -1,8 +1,5 @@
-import lodash from "lodash";
 import React from "react";
 import { Icon } from "../../../../component/view/icon";
-import { channel } from "../../../../net/channel";
-import { ElementType, getElementUrl } from "../../../../net/element.type";
 import { url, view } from "../../../../src/block/factory/observable";
 import { BlockView } from "../../../../src/block/view";
 import { FieldType } from "../../schema/type";
@@ -23,58 +20,6 @@ export class FieldEmailView extends BlockView<FieldEmoji>{
                 self.block.value = r;
                 self.forceUpdate()
             }
-            // var r = await channel.patch('/interactive/emoji', {
-            //     elementUrl: getElementUrl(
-            //         self.block.viewField.field.type == FieldType.emoji ? ElementType.SchemaFieldData : ElementType.SchemaFieldNameData,
-            //         self.block.dataGrid.schema.id,
-            //         self.block.viewField.field.type == FieldType.emoji ? self.block.field.id : self.block.field.name,
-            //         self.block.item.dataRow.id
-            //     ),
-            //     fieldName: self.block.field.name
-            // });
-            // if (r.ok) {
-            //     var ov = lodash.cloneDeep(self.block.value);
-            //     if (typeof ov == 'undefined') ov = { count: 0 };
-            //     if (typeof ov == 'number') ov = { count: ov };
-            //     ov.count = r.data.count;
-            //     var userid = self.block.page.user?.id;
-            //     if (userid) {
-            //         if (!Array.isArray(ov.users)) {
-            //             ov.users = []
-            //         }
-            //         if (r.data.exists) {
-            //             var ops = self.block.dataGrid.userEmojis[self.block.viewField.field.name];
-            //             if (!Array.isArray(ops)) self.block.dataGrid.userEmojis[self.block.viewField.field.name] = ops = []
-            //             if (!ops.exists(c => c == self.block.item.dataRow.id)) ops.push(self.block.item.dataRow.id);
-            //             if (!ov.users.exists(g => g == userid)) ov.users.push(userid)
-            //         }
-            //         else {
-            //             var ops = self.block.dataGrid.userEmojis[self.block.viewField.field.name];
-            //             if (!Array.isArray(ops)) self.block.dataGrid.userEmojis[self.block.viewField.field.name] = ops = []
-            //             if (ops.exists(c => c == self.block.item.dataRow.id)) lodash.remove(ops, c => c == self.block.item.dataRow.id);
-            //             lodash.remove(ov.users, g => (g as any) == userid);
-            //         }
-            //         if (typeof r.data.otherCount == 'number') {
-            //             var name = self.block.field.name == 'like' ? FieldType[FieldType.oppose] : FieldType[FieldType.like];
-            //             self.block.item.dataRow[name] = r.data.otherCount;
-            //             var cs = self.block.item.childs.findAll(g => (g instanceof OriginField) && g.field?.name == name);
-            //             if (!r.data.otherExists) {
-            //                 var ops = self.block.dataGrid.userEmojis[name];
-            //                 if (!Array.isArray(ops)) self.block.dataGrid.userEmojis[name] = ops = []
-            //                 if (ops.exists(c => c == self.block.item.dataRow.id)) lodash.remove(ops, c => c == self.block.item.dataRow.id);
-            //             }
-            //             if (cs.length > 0) {
-            //                 for (var i = 0; i < cs.length; i++) {
-            //                     (cs[i] as any).value = r.data.otherCount;
-            //                     cs[i].forceUpdate();
-            //                 }
-            //             }
-            //         }
-            //     }
-            //     self.block.value = ov;
-            //     self.block.item.dataRow[self.block.field.name] = ov;
-            //     self.forceUpdate();
-            // }
         }
         var svg = GetFieldTypeSvg(this.block.viewField.field.type);
         var v = this.block.value;
@@ -82,16 +27,16 @@ export class FieldEmailView extends BlockView<FieldEmoji>{
         var countStr = v > 0 ? `${v}` : '';
         var sp = <></>;
         if (this.block.viewField.field.type == FieldType.love) {
-            var isOp =self.block.dataGrid.isEmoji(this.block.viewField.field,self.block.item.dataRow.id); 
-            sp = <span className="cursor flex flex-inline h-24 f-14 text-1 padding-w-5 round-32  item-hover ">
-                <Icon className={'gap-r-3'} size={16} icon={svg}></Icon>{isOp ? "取消喜欢" : "喜欢"}</span>
+            var isOp = self.block.dataGrid.isEmoji(this.block.viewField.field, self.block.item.dataRow.id);
+            sp = <span className="cursor flex-center size-24  text-1  round-8  item-hover ">
+                <Icon className={(isOp ? " fill-primary" : " text-1")} size={16} icon={svg}></Icon></span>
         }
         else if (this.block.viewField.field.type == FieldType.like) {
-            var isOp = self.block.dataGrid.isEmoji(this.block.viewField.field,self.block.item.dataRow.id); 
+            var isOp = self.block.dataGrid.isEmoji(this.block.viewField.field, self.block.item.dataRow.id);
             sp = <span className={"cursor f-12 flex flex-inline h-24 padding-w-5 round-32 " + (isOp ? " border-primary bg-primary text-white" : " text-1 item-hover border")}>
                 <Icon className={'gap-r-3'} size={16} icon={svg}></Icon>{countStr}</span>
         } else if (this.block.viewField.field.type == FieldType.oppose) {
-            var isOp = self.block.dataGrid.isEmoji(this.block.viewField.field,self.block.item.dataRow.id); 
+            var isOp = self.block.dataGrid.isEmoji(this.block.viewField.field, self.block.item.dataRow.id);
             sp = <span className={"cursor f-12 flex flex-inline h-24 padding-w-5 round-32 " + (isOp ? " border-primary bg-primary text-white" : " text-1 item-hover border")}>
                 <Icon className={'gap-r-3'} size={16} icon={svg}></Icon>{countStr}</span>
         } else {
