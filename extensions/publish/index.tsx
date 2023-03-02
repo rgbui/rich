61513c9d389a69332e991ab40f447916867fdfab
kanhai <760489033@qq.com>
2023-03-03 02:01:41 +0800

    fix:修改用户分享协作

--- extensions/publish/index.tsx
+++ extensions/publish/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { CopyText } from "../../component/copy";
 import { ShyAlert } from "../../component/lib/alert";
 import { EventsComponent } from "../../component/lib/events.component";
-import { GlobalLinkSvg, LinkSvg } from "../../component/svgs";
+import { GlobalLinkSvg, LinkSvg, PlusSvg } from "../../component/svgs";
+import { Avatar } from "../../component/view/avator/face";
 import { Divider } from "../../component/view/grid";
 import { Icon } from "../../component/view/icon";
+import { useSelectMenuItem } from "../../component/view/menu";
 import { Switch } from "../../component/view/switch";
+import { Point } from "../../src/common/vector/point";
 import { Page } from "../../src/page";
-import { PagePermission } from "../../src/page/permission";
 import { LinkPageItem } from "../at/declare";
 import { PopoverSingleton } from "../popover/popover";
 import { PopoverPosition } from "../popover/position";
@@ -33,26 +35,69 @@ class PagePublish extends EventsComponent {
             await self.page.onUpdatePageData(data);
             self.forceUpdate()
         }
+        async function addPermission(event: React.MouseEvent) {
+            var r = await useSelectMenuItem({ roundPoint: Point.from(event) },
+                [
+                    { text: '添加角色', name: 'addRole' },
+                    { text: '添加成员', name: 'addMember' }
+                ]
+            );
+            if (r) {
+
+            }
+        }
+        function getRoles(roleId) {
+            return { text: '' }
+        }
         return <div className='shy-page-publish'>
-            <div className='shy-page-publish-access'>
-                <div className='shy-page-publish-access-icon'>
-                    <Icon size={36} icon={GlobalLinkSvg}></Icon>
-                    <div>
-                        <span>公开至互联网</span>
-                        <label>任何人都可以{this.item?.permission == PagePermission.canEdit ? "编辑" : (this.item?.permission == PagePermission.canInteraction ? "浏览、添加评论、数据" : "浏览")}</label>
-                    </div>
-                </div>
-                <Switch checked={this.item?.share == 'net' ? true : false} onChange={e => setGlobalShare({ share: e ? "net" : 'nas' })}></Switch>
+            <div className="flex">
+                <span className="flex-auto">空间成员</span>
+                <span className="flex-fixed size-24 item-hover round cursor"><Icon icon={PlusSvg}></Icon></span>
             </div>
-            {this.item?.share == 'net' && <>
-                <div className='shy-page-publish-permission'><span>可编辑</span><Switch checked={this.item.permission == PagePermission.canEdit} onChange={e => setGlobalShare({ permission: e ? PagePermission.canEdit : PagePermission.canView })}></Switch></div>
-                <div className='shy-page-publish-permission'><span>可交互<em>(评论、添加数据)</em></span><Switch checked={this.item.permission == PagePermission.canInteraction} onChange={e => setGlobalShare({ permission: e ? PagePermission.canInteraction : PagePermission.canView })}></Switch></div>
-            </>}
+            <div>
+                {this.item.memberPermissions.map((mp, id) => {
+                    return <div className="flex" key={id}>
+                        <div className="flex-fixed">
+                            {mp.userid && <Avatar userid={mp.userid}></Avatar>}
+                            {mp.roleId && <span>{getRoles(mp.roleId).text}</span>}
+                        </div>
+                        <div className="flex-auto">
+
+                        </div>
+                    </div>
+                })}
+            </div>
+
+            <Divider></Divider>
+            <div className="item-hover">
+                <span className="size-24"><Icon size={18} icon={GlobalLinkSvg}></Icon></span>
+                <span>公开至互联网</span>
+                <span>
+                    <Switch checked={this.item?.share == 'net' ? true : false} onChange={e => setGlobalShare({ share: e ? "net" : 'nas' })}></Switch>
+                </span>
+            </div>
             <Divider></Divider>
             <div className="shy-page-publish-item" onClick={e => this.copyLink()}>
                 <Icon size={18} icon={LinkSvg}></Icon><span>复制页面访问链接</span>
             </div>
         </div>
+        // return <div className='shy-page-publish'>
+        //     <div className='shy-page-publish-access'>
+        //         <div className='shy-page-publish-access-icon'>
+        //             <Icon size={36} icon={GlobalLinkSvg}></Icon>
+        //             <div>
+        //                 <span>公开至互联网</span>
+        //                 <label>任何人都可以{this.item?.permission == PagePermission.canEdit ? "编辑" : (this.item?.permission == PagePermission.canInteraction ? "浏览、添加评论、数据" : "浏览")}</label>
+        //             </div>
+        //         </div>
+        //         <Switch checked={this.item?.share == 'net' ? true : false} onChange={e => setGlobalShare({ share: e ? "net" : 'nas' })}></Switch>
+        //     </div>
+        //     {this.item?.share == 'net' && <>
+        //         <div className='shy-page-publish-permission'><span>可编辑</span><Switch checked={this.item.permission == PagePermission.canEdit} onChange={e => setGlobalShare({ permission: e ? PagePermission.canEdit : PagePermission.canView })}></Switch></div>
+        //         <div className='shy-page-publish-permission'><span>可交互<em>(评论、添加数据)</em></span><Switch checked={this.item.permission == PagePermission.canInteraction} onChange={e => setGlobalShare({ permission: e ? PagePermission.canInteraction : PagePermission.canView })}></Switch></div>
+        //     </>}
+
+        // </div>
     }
 }
 export async function usePagePublish(pos: PopoverPosition, item: LinkPageItem, page: Page) {
