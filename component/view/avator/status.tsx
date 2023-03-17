d53237fa1504d95519500f76e8e155020e32b7e3
kanhai <760489033@qq.com>
2023-03-17 23:24:28 +0800

    修复status显示不正常的问题

--- component/view/avator/status.tsx
+++ component/view/avator/status.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 export function renderAvatorStatusSvgMask() {
-    return <svg x="14.5" y="17" width="25" style={{display:'none'}} height="15" viewBox="0 0 25 15">
+    return <svg x="14.5" y="17" width="25" style={{ position:'absolute',left:-10000}} height="15" viewBox="0 0 25 15">
         <mask id="user-avator-mask-online"><rect x="7.5" y="5" width="10" height="10" rx="5" ry="5" fill="white"></rect><rect x="12.5" y="10" width="0" height="0" rx="0" ry="0" fill="black"></rect><polygon points="-2.16506,-2.5 2.16506,0 -2.16506,2.5" fill="black" transform="scale(0) translate(13.125 10)" style={{ transformOrigin: '13.125px 10px' }} ></polygon><circle fill="black" cx="12.5" cy="10" r="0"></circle></mask>
         <mask id="user-avator-mask-busy"><rect x="7.5" y="5" width="10" height="10"
             rx="5" ry="5" fill="white"></rect><rect x="8.75"
