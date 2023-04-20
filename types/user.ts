db44af1c9df23a129c03213d63f1a07217883e11
kanhai <760489033@qq.com>
2023-04-20 15:11:02 +0800

    feature: add robot task

--- types/user.ts
+++ types/user.ts
@@ -19,3 +19,42 @@ export type UserBasic = {
     online: boolean,
     slogan: string
 }
+
+
+export type RobotTask = {
+    workspaceId?: string;
+    robotId?: string;
+    id?: string,
+    name: string,
+    description?: string,
+    url?: string,
+    method?: string,
+    handle?: 'stream' | 'sync' | 'async',
+    headers?: { id?: string, name: string, value: string }[],
+    main?: boolean,
+    flag?: 'append' | 'write',
+    args: { id: string, name: string, text: string, type: string }[],
+    replys?: { id: string, mime: 'text' | 'json' | 'markdown' | 'image' | 'error', code?: string, template?: string, content?: string, data?: Record<string, any>, images?: { url: string, alt?: string }[] }[]
+    /**
+     * nextActions[0].args[0].value='$args.name|$replys['name']'
+     */
+    nextActions?: { text: string, task: string, args?: { name: string, value: string }[] }[]
+    disabled?: boolean,
+    /**
+     * args replys
+     * ```
+     * args.name
+     * args.name
+     * replys 
+     * ```
+     */
+    template?: string,
+}
+
+export type RobotInfo = UserBasic & {
+    remark?: string,
+    basePath?: string,
+    scene: string;
+    headers?: { name: string, value: string }[],
+    tasks?: RobotTask[],
+}
