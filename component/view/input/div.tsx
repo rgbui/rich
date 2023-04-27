800346cc24122836c9360488830bc715f1dfb9ca
kanhai <760489033@qq.com>
2023-04-27 16:01:10 +0800

    fix:优化divInput输入框

--- component/view/input/div.tsx
+++ component/view/input/div.tsx
@@ -1,7 +1,7 @@
 import { observer, useLocalObservable } from "mobx-react"
 import React from "react"
 import { CSSProperties } from "react"
-export var SpanInput = observer(function (props: {
+export var DivInput = observer(function (props: {
     placeholder?: string,
     onInput?: (value: string) => void,
     onChange?: (value: string) => void,
@@ -10,7 +10,9 @@ export var SpanInput = observer(function (props: {
     value?: string,
     style?: CSSProperties,
     ignoreFilterWhitespace?: boolean,
-    className?: string | string[]
+    className?: string | string[],
+    line?: boolean,
+    rf?: (e: HTMLElement) => void
 }) {
     var local = useLocalObservable<{ el: HTMLElement }>(() => {
         return {
@@ -47,7 +49,7 @@ export var SpanInput = observer(function (props: {
     }
 
     async function keydown(event: React.KeyboardEvent) {
-        if (event.key == 'Enter') {
+        if (event.key == 'Enter' && !event.shiftKey) {
             event.preventDefault();
             var text = local.el.innerText;
             if (props.ignoreFilterWhitespace !== true) {
@@ -60,12 +62,29 @@ export var SpanInput = observer(function (props: {
     async function mousedown(event: React.MouseEvent) {
         if (typeof props.onMouseDown == 'function') props.onMouseDown(event)
     }
-    var classList: string[] = ["shy-span-input"];
+    var classList: string[] = ["shy-div-input"];
     if (Array.isArray(props.className)) classList.push(...props.className)
     else if (props.className) classList.push(props.className)
+
+    var style: CSSProperties = props.style || {};
+    if (props.line !== true) return <div
+        className={classList.join(" ")}
+        style={style}
+        ref={e => { local.el = e; props.rf ? props.rf(e) : undefined; }}
+        onPaste={e => paster(e)}
+        onInput={e => input()}
+        onChange={e => change()}
+        onBlur={e => change()}
+        onKeyDown={e => keydown(e)}
+        onMouseDown={e => mousedown(e)}
+        contentEditable={true}
+        data-placeholder={props.placeholder}
+        dangerouslySetInnerHTML={{ __html: props.value }}
+    ></div>
     return <span
         className={classList.join(" ")}
-        ref={e => local.el = e}
+        style={style}
+        ref={e => { local.el = e; props.rf ? props.rf(e) : undefined; }}
         onPaste={e => paster(e)}
         onInput={e => input()}
         onChange={e => change()}
