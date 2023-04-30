c99699c7a3b4fc7fdbe4d275cbd99ff225beba3f
kanhai <760489033@qq.com>
2023-04-30 18:06:17 +0800

    fix:优化markdown的显示

--- component/view/markdown/index.tsx
+++ component/view/markdown/index.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+
+import "./style.less"
 /**
  * https://github.com/remarkjs/react-markdown
  */
-export class Markdown extends React.Component<{ md: string }>{
+export class Markdown extends React.Component<{ md: string, className?: string | (string[]) }>{
     constructor(props) { super(props) }
     render(): React.ReactNode {
-        return   <ReactMarkdown children={this.props.md}  />
+        var classList: string[] = ['md']
+        if (this.props.className) {
+            if (typeof this.props.className === 'string') {
+                classList.push(this.props.className)
+            } else {
+                classList = classList.concat(this.props.className)
+            }
+        }
+        return <div className={classList.join(" ")}><ReactMarkdown remarkPlugins={[remarkGfm]} children={this.props.md} /></div>
     }
 }
