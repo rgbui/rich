bb60287182abfe1da7e5b3d0437d842ea172743f
kanhai <760489033@qq.com>
2023-04-30 18:08:57 +0800

    fix:增加通过markdown内容获取转换的blocks 数据

--- src/import-export/markdown/parse.ts
+++ src/import-export/markdown/parse.ts
@@ -6,12 +6,18 @@
  */
 import { util } from "../../../util/util";
 import { parseHtml } from "../html/parse";
-
+import { marked } from "marked";
 export async function parseMarkdown(file: File) {
     var text = await util.readFileText(file);
     // var converter = new showdown.Converter(),
     // text      = '# hello, markdown!',
     // html      = converter.makeHtml(text);
-   var html='';
-    var blocks=await  parseHtml(html);
+    var html = '';
+    var blocks = await parseHtml(html);
+}
+
+export async function parseMarkdownContent(md: string) {
+    var html = marked.parse(md);
+    var blocks = await parseHtml(html);
+    return blocks;
 }
