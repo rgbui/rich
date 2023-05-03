f8a6e47149ff672e6da4395517a744bf45433d1f
kanhai <760489033@qq.com>
2023-05-04 00:19:42 +0800

    fix:修复remark 组件块级元素的样式

--- component/view/text.tsx
+++ component/view/text.tsx
@@ -1,7 +1,7 @@
 import React, { CSSProperties } from "react";
 import { ComponentSize } from "./declare";
 export function Remark(props: { style?: CSSProperties, size?: ComponentSize, children: React.ReactNode }) {
-    return <div style={(props.style || {})} className={'remark' + (props.size&&props.size=='middle' ? " f-14":"f-12")}>{props.children}</div>
+    return <div style={(props.style || {})} className={'remark' + (props.size&&props.size=='middle' ? " f-14":" f-12")}>{props.children}</div>
 }
 
 export function ErrorText(props: { children: React.ReactNode }) {
