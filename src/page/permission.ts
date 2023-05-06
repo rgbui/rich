7a2d06481fd6b228e2dd10ae8e5d554cde6d2a3f
kanhai <760489033@qq.com>
2023-05-06 18:49:13 +0800

    fix:基于PageLayoutType来设置权限项

--- src/page/permission.ts
+++ src/page/permission.ts
@@ -1,17 +1,35 @@
+import lodash from "lodash";
+import { PageLayoutType } from "./declare";
+
+
+/**
+ * 
+ * @returns 返回用户的基本权限
+ */
 export function getCommonPerssions() {
     return [
         AtomPermission.docInteraction,
+        AtomPermission.docView,
+        AtomPermission.docExport,
         AtomPermission.channelInteraction,
+        AtomPermission.channelView,
         AtomPermission.wsNotAllow,
         AtomPermission.dbInteraction,
+        AtomPermission.dbView,
     ]
 }
+/**
+ * 
+ * @returns 返回编辑管理的权限
+ */
 export function getEditPerssions() {
     return [
+        ...getCommonPerssions(),
         AtomPermission.docEdit,
         AtomPermission.wsEdit,
         AtomPermission.channelEdit,
-        AtomPermission.dbEdit
+        AtomPermission.dbEdit,
+        AtomPermission.dbDataEdit,
     ]
 }
 export function getAllAtomPermission() {
@@ -80,4 +98,56 @@ export enum AtomPermission {
 
 }
 
+
+export function getAtomPermissionComputedChanges(pageType: PageLayoutType, vs: AtomPermission[], v: AtomPermission) {
+    if ([PageLayoutType.board, PageLayoutType.doc, PageLayoutType.docCard].includes(pageType)) {
+        if (v == AtomPermission.docInteraction) lodash.remove(vs, g => ![AtomPermission.docExport].includes(g))
+        else if (v == AtomPermission.docExport) lodash.remove(vs, g => ![AtomPermission.docInteraction].includes(g))
+        else vs = []
+        if (!vs.includes(v)) vs.push(v)
+        return vs;
+    }
+    else if ([PageLayoutType.textChannel].includes(pageType)) {
+        vs = []
+        if (!vs.includes(v)) vs.push(v)
+        return vs;
+    }
+    else if ([PageLayoutType.db]) {
+        if (v == AtomPermission.dbDataEdit) lodash.remove(vs, g => ![AtomPermission.dbInteraction].includes(g))
+        else if (v == AtomPermission.dbInteraction) lodash.remove(vs, g => ![AtomPermission.dbDataEdit].includes(g))
+        else vs = []
+        if (!vs.includes(v)) vs.push(v)
+        return vs;
+    }
+}
+
+export function getAtomPermissionOptions(pageType: PageLayoutType) {
+
+    if ([PageLayoutType.board, PageLayoutType.doc, PageLayoutType.docCard].includes(pageType)) {
+        return [
+            { text: '可编辑', value: AtomPermission.docEdit },
+            { text: '可导出', value: AtomPermission.docExport },
+            { text: '可评论', value: AtomPermission.docInteraction },
+            { text: '可查看', value: AtomPermission.docView },
+            { text: '无权限', value: AtomPermission.docNotAllow },
+        ]
+    }
+    else if ([PageLayoutType.textChannel].includes(pageType)) {
+        return [
+            { text: '可编辑', value: AtomPermission.channelEdit },
+            { text: '可发言', value: AtomPermission.channelInteraction },
+            { text: '可查看', value: AtomPermission.channelView },
+            { text: '无权限', value: AtomPermission.channelNotAllow },
+        ]
+    }
+    else if ([PageLayoutType.db]) {
+        return [
+            { text: '可编辑表格', value: AtomPermission.dbEdit },
+            { text: '可编辑数据', value: AtomPermission.dbDataEdit },
+            { text: '可添加数据', value: AtomPermission.dbInteraction },
+            { text: '可查看', value: AtomPermission.dbView },
+            { text: '无权限', value: AtomPermission.dbNotAllow },
+        ]
+    }
+}
 
