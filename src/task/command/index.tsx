136f1e3a29c5c8d75050fcd7013ad40d52cd7eb1
kanhai <760489033@qq.com>
2023-05-13 13:48:54 +0800

    fix:优化task目录

---
+++ src/task/command/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ReactNode } from "react";
+import { EventsComponent } from "../../../component/lib/events.component";
+
+export class CommandSelector extends EventsComponent {
+    render(): ReactNode {
+        return <div className="command-selector">
+            
+        </div>
+    }
+}
