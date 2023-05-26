da7e657cc241b1592c33f04d64e55525f134834f
kanhai <760489033@qq.com>
2023-05-26 23:29:47 +0800

    fix:实现表格的点击返回功能

--- src/page/view/bar.tsx
+++ src/page/view/bar.tsx
@@ -1,6 +1,6 @@
 import React, { CSSProperties } from "react";
 import { Page } from "..";
-import { ChevronLeftSvg, ChevronRightSvg, CollectTableSvg, DetailSvg, DotsSvg, DoubleRightSvg, EditSvg, FieldsSvg, MemberSvg, OrderSvg, PageSvg, PublishSvg, SearchSvg } from "../../../component/svgs";
+import { ChevronLeftSvg, ChevronRightSvg, CollectTableSvg, DetailSvg, DotsSvg, DoubleRightSvg, EditSvg, FieldsSvg, MemberSvg, OrderSvg, PageSvg, PublishSvg, SearchSvg, TableSvg } from "../../../component/svgs";
 import { UserAvatars } from "../../../component/view/avator/users";
 import { Button } from "../../../component/view/button";
 import { Icon } from "../../../component/view/icon";
@@ -20,7 +20,12 @@ export class PageBar extends React.Component<{ page: Page }>{
         if (this.props.page.pe.type == ElementType.SchemaData) {
             return <div className="flex-auto flex">
                 {this.props.page.openSource == 'slide' && <span onMouseDown={e => this.props.page.onClose()} className="item-hover size-24 round cursor flex-center gap-l-10"><Icon size={18} icon={DoubleRightSvg}></Icon></span>}
-                <span className="item-hover round flex cursor padding-h-3 padding-w-5 ">
+                <span onMouseDown={e => this.props.page.onBack()} className="item-hover round flex cursor padding-h-3 padding-w-5">
+                    <Icon size={20} icon={this.props.page.schema?.icon || CollectTableSvg}></Icon>
+                    <span className="gap-l-5">{this.props.page.schema?.text}</span>
+                </span>
+                <span className="flex-center"><Icon icon={ChevronRightSvg} size={18}></Icon></span>
+                <span className="item-hover round flex cursor padding-h-3 padding-w-5">
                     <Icon size={20} icon={this.props.page?.formRowData?.icon || PageSvg}></Icon>
                     <span className="gap-l-5">{this.props.page?.formRowData?.title}</span>
                 </span>
@@ -31,7 +36,12 @@ export class PageBar extends React.Component<{ page: Page }>{
             var sv = this.props.page.schema.views.find(g => g.id == this.props.page.pe.id1);
             return <div className="flex-auto flex">
                 {this.props.page.openSource == 'slide' && <span onMouseDown={e => this.props.page.onClose()} className="item-hover size-24 round cursor flex-center gap-l-10"><Icon size={18} icon={DoubleRightSvg}></Icon></span>}
-                <span className="item-hover round flex gap-l-10 cursor padding-h-3 padding-w-5 ">
+                <span onMouseDown={e => this.props.page.onBack()} className="item-hover round flex cursor padding-h-3 padding-w-5">
+                    <Icon size={20} icon={this.props.page.schema?.icon || CollectTableSvg}></Icon>
+                    <span className="gap-l-5">{this.props.page.schema?.text}</span>
+                </span>
+                <span className="flex-center"><Icon icon={ChevronRightSvg} size={18}></Icon></span>
+                <span className="item-hover round flex gap-l-10 cursor padding-h-3 padding-w-5">
                     <Icon size={20} icon={sv?.icon || (sv.url == BlockUrlConstant.FormView ? OrderSvg : DetailSvg)}></Icon>
                     <span className="gap-l-5">{sv?.text}</span>
                 </span>
