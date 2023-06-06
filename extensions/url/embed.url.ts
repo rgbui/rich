c32d06638de94df7a415d14a5c775d1de30806ef
kanhai <760489033@qq.com>
2023-06-06 21:49:57 +0800

    fix:优化内嵌B站视频

--- extensions/url/embed.url.ts
+++ extensions/url/embed.url.ts
@@ -7,12 +7,12 @@ export function ConvertEmbed(url: string) {
             return { embedType: 'music.163', origin: url, url: `https://music.163.com/outchain/player?type=2&id=${rs[1]}&auto=0&height=66` }
         }
     }
-    // else if (/^https?\:\/\/www\.bilibili\.com\/[\w]+\/[\w]+\/([a-zA-Z\d]+)/.test(url)) {
-    //     var rs = url.match(/^https?\:\/\/www\.bilibili\.com\/[\w]+\/[\w]+\/([a-zA-Z\d]+)/);
-    //     if (rs[1]) {
-    //         return { embedType: 'bilibili', origin: url, url: `https://player.bilibili.com/player.html?bvid=${rs[1]}&page=1&high_quality=1&as_wide=1&allowfullscreen=true` }
-    //     }
-    // }
+    else if (/^https?\:\/\/www\.bilibili\.com\/video\/([a-zA-Z\d]+)/.test(url)) {
+        var rs = url.match(/^https?\:\/\/www\.bilibili\.com\/video\/([a-zA-Z\d]+)/);
+        if (rs[1]) {
+            return { embedType: 'bilibili', origin: url, url: `https://player.bilibili.com/player.html?bvid=${rs[1]}&page=1&high_quality=1&as_wide=1&allowfullscreen=true` }
+        }
+    }
     else {
         return { origin: url, url, embedType: '' }
     }
