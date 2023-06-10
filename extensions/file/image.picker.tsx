8f9b1b85e8fa902e9f7aaa5bed2e6c2ed53a6a25
kanhai <760489033@qq.com>
2023-06-10 17:29:50 +0800

    fix:优化文件选择器

--- extensions/file/image.picker.tsx
+++ extensions/file/image.picker.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "../../blocks/page/link";
 import { EventsComponent } from "../../component/lib/events.component";
 import { LinkSvg, UploadSvg } from "../../component/svgs";
 import { Icon } from "../../component/view/icon";
@@ -20,7 +19,7 @@ class ImageFilePicker extends EventsComponent {
         return <div className='shy-file-picker' >
             <Tab keeplive>
                 <Tab.Page item={<Tip placement='bottom' id={LangID.UploadFile}><Icon size={16} icon={UploadSvg}></Icon></Tip>}>
-                    <UploadView mine='image' change={e => this.onChange({ name: 'upload', url: e.url, text: e.name, size: e.size })}></UploadView>
+                    <UploadView mine='image' change={e => this.onChange({ name: 'upload',...e })}></UploadView>
                 </Tab.Page>
                 <Tab.Page item={<Tip placement='bottom' id={LangID.ImageLink}><Icon size={16} icon={LinkSvg}></Icon></Tip>}>
                     <OutsideUrl change={e => this.onChange({ name: 'link', url: e })}></OutsideUrl>
