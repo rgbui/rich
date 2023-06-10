cea4cbc5ae956ca79b28bb48c53b64d3ebac9e21
kanhai <760489033@qq.com>
2023-06-10 23:39:38 +0800

    fix:优化block选择器

--- extensions/block/index.tsx
+++ extensions/block/index.tsx
@@ -1,35 +1,32 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { Singleton } from "../../component/lib/Singleton";
 import { Icon } from "../../component/view/icon";
 import { LangID } from "../../i18n/declare";
 import { Sp } from "../../i18n/view";
-import { KeyboardCode } from "../../src/common/keys";
-import { Point, Rect, RectUtility } from "../../src/common/vector/point";
 import { InputTextPopSelector } from "../common/input.pop";
-import { PopoverPosition } from "../popover/position";
 import { BlockSelectorItem } from "./delcare";
 import { blockStore } from "./store";
 
-class BlockSelector extends InputTextPopSelector {
-    private get filterSelectorData() {
-        return blockStore.findAll(this.command);
+class BlockSelector extends InputTextPopSelector<{
+    childs: BlockSelectorItem[];
+    text: string;
+}> {
+    prefix: string[] = ['/', '、'];
+    lazyTime: number = 0;
+    async searchAll() {
+        await blockStore.import();
     }
-    private get filterBlocks() {
-        return blockStore.findAllBlocks(this.command);
-    }
-    private get isSelectIndex() {
-        return this.selectIndex >= 0 && this.selectIndex < this.filterBlocks.length;
+    async searchByWord(word: string) {
+        return blockStore.findAll('/' + word);
     }
     private renderSelectors() {
         var i = -1;
-        var fsd = this.filterSelectorData;
-        if (fsd.length == 0) {
+        if (this.list.length == 0) {
             return <div className="flex-center remark f-14  padding-10">
                 <Sp id={LangID.blockSelectorNoData}></Sp>
             </div>
         }
-        return fsd.map((group, g) => {
+        return this.list.map((group, g) => {
             return <div key={group.text}>
                 <div className="remark flex f-12  padding-w-10 h-24">{group.text}</div>
                 <div>
@@ -51,159 +48,55 @@ class BlockSelector extends InputTextPopSelector {
     render() {
         var style: Record<string, any> = {
             top: this.pos.y,
-            left: this.pos.x
+            left: this.pos.x,
+            display: this.visible ? "block" : 'none'
         }
-        return <div>
-            {this.visible && <div className='shy-block-selector'
-                style={style}>{this.renderSelectors()}</div>}
-        </div>
+        return <div className='shy-block-selector' style={style}>{this.renderSelectors()}</div>
     }
-    private onSelect(block?: BlockSelectorItem) {
-        if (typeof this._select == 'function') this._select(block, this.getFilterText(this.inputValue))
+    onSelect(block?: BlockSelectorItem) {
+        if (typeof this._select == 'function') this._select(block, this.getFilterText(this.text))
         this.close();
-    }
-    private visible: boolean = false;
-    private pos: Point = new Point(0, 0);
-    private round: Rect;
-    private command: string = '';
-    private selectIndex: number = 0;
-    private _select: (block: BlockSelectorItem, matchValue: string) => void;
-    async open(round: Rect, text: string, callback: BlockSelector['_select'], options: {}) {
-        var cs = text.match(/(\/|、)[^\s]*$/g);
-        if (!(cs && cs[0])) {
-            this.close();
-            return false;
-        }
-        this.round = round;
-        this.pos = round.leftBottom;
-        if (this.visible == false) {
-            this.selectIndex = 0;
-        }
-        this.visible = true;
-        await blockStore.import();
-        this.inputFilter(text);
-        this._select = callback;
-        return this.visible;
-    }
-    private inputValue: string;
-    private previsible: boolean = false;
-    private inputFilter(text: string) {
-        this.inputValue = text;
-        var cs = text.match(/(\/|、)[^\s]*$/g);
-        var command = cs ? cs[0] : "";
-        if (command) {
-            command = command.replace(/、/g, "/");
-            this.command = command;
-            if (this.selectIndex >= this.filterBlocks.length) {
-                this.selectIndex = 0;
-            }
-            if (this.filterBlocks.length == 0) {
-                if (this.previsible == true) {
-                    this.previsible = false;
-                    this.adjuctPosition();
-                }
-                else this.close()
-            }
-            else { this.previsible = true; this.adjuctPosition(); }
-        }
-        else {
-            this.close();
-        }
-    }
-    private adjuctPosition() {
-        this.forceUpdate(() => {
-            var selectorEl = this.el.querySelector('.shy-block-selector') as HTMLElement;
-            if (selectorEl) {
-                var b = Rect.fromEle(selectorEl);
-                var pos: PopoverPosition = {
-                    roundArea: this.round,
-                    elementArea: b
-                }
-                var newPoint = RectUtility.cacPopoverPosition(pos);
-                if (!this.pos.equal(newPoint)) {
-                    this.pos = newPoint;
-                    this.forceUpdate();
-                }
-            }
-        })
     }
     private getFilterText(text): string {
         return text.replace(/(\/|、)[\w \-\u4e00-\u9fa5]*$/g, '');
     }
-    private get selectBlockData() {
-        var b = this.filterBlocks[this.selectIndex];
-        return b;
-    }
-    onClose() {
-        this.close();
-    }
-    private close() {
-        this.command = '';
-        if (this.visible == true) {
-            this.visible = false;
-            this.forceUpdate();
-        }
-    }
-    /**
-     * 向上选择内容
-     */
-    private keydown() {
-        if (!this.isSelectIndex) this.selectIndex = -1;
-        if (this.selectIndex < this.filterBlocks.length - 1) {
-            this.selectIndex += 1;
-            this.forceUpdate();
-        }
-    }
-    /**
-     * 向下选择内容
-     */
-    private keyup() {
-        if (!this.isSelectIndex) this.selectIndex = this.filterBlocks.length - 1;
-        if (this.selectIndex > 0) {
-            this.selectIndex -= 1;
-            this.forceUpdate();
-        }
-    }
-    private el: HTMLElement;
-    componentDidMount() {
-        this.el = ReactDOM.findDOMNode(this) as HTMLElement;
-        document.addEventListener('mousedown', this.onGlobalMousedown);
-    }
-    componentWillUnmount() {
-        document.removeEventListener('mousedown', this.onGlobalMousedown);
-    }
-    componentDidUpdate() {
-        var el = this.el.querySelector('.item-hover-focus') as HTMLElement;
-        if (el) {
-            el.scrollIntoView({
-                block: "nearest",
-                inline: "nearest"
-            });
-        }
-    }
-    onGlobalMousedown = (event: MouseEvent) => {
-        if (this.visible == true && this.el) {
-            var target = event.target as HTMLElement;
-            if (this.el.contains(target)) return;
-            this.close();
-        }
-    }
-    onKeydown(event: KeyboardEvent) {
-        if (this.visible == true) {
-            switch (event.key) {
-                case KeyboardCode.ArrowDown:
-                    this.keydown();
-                    return true;
-                case KeyboardCode.ArrowUp:
-                    this.keyup();
-                    return true;
-                case KeyboardCode.Enter:
-                    var block = this.selectBlockData;
-                    this.onSelect(block);
-                    return true;
+    getSelectBlockData() {
+        var r;
+        var i = 0;
+        this.list.forEach((c) => {
+            if (Array.isArray(c.childs)) {
+                c.childs.forEach(cc => {
+                    if (i == this.selectIndex) {
+                        r = cc;
+                    }
+                    i += 1;
+                })
+            }
+        })
+        if (r) {
+            return {
+                ...r
             }
         }
-        return false;
+        return null;
+    }
+    enter(event: KeyboardEvent) {
+        var r = this.getSelectBlockData();
+        if (r) {
+            this.onSelect(r);
+        }
+        return true;
+    }
+    getListCount(): number {
+        var i = 0;
+        this.list.forEach((c) => {
+            if (Array.isArray(c.childs)) {
+                c.childs.forEach(cc => {
+                    i += 1;
+                })
+            }
+        })
+        return i;
     }
 }
 export async function useBlockSelector() {
--- extensions/block/store.ts
+++ extensions/block/store.ts
@@ -31,7 +31,7 @@ class BlockStore extends Events {
         var bs = this._blockGroups.map(b => {
             return {
                 ...b,
-                childs: b.childs.findAll(g => g.text && ('/' + g.text).startsWith(label) || g.label.startsWith(label) || Array.isArray(g.labels) && g.labels.exists(c => c.startsWith(label)))
+                childs: b.childs.findAll(g =>!label||label&& g.text && ('/' + g.text).startsWith(label) || g.label.startsWith(label) || Array.isArray(g.labels) && g.labels.exists(c => c.startsWith(label)))
             }
         });
         bs.removeAll(g => g.childs.length == 0);
