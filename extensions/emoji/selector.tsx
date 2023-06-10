41f0983f849588d708c84c4f08bcfaef71b6f48c
kanhai <760489033@qq.com>
2023-06-10 23:46:43 +0800

    fix:优化emoji的输入体验

--- extensions/emoji/selector.tsx
+++ extensions/emoji/selector.tsx
@@ -1,82 +1,42 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { Singleton } from "../../component/lib/Singleton";
-import { KeyboardCode } from "../../src/common/keys";
-import { Rect, Point } from "../../src/common/vector/point";
+import { Rect } from "../../src/common/vector/point";
 import { InputTextPopSelector } from "../common/input.pop";
-import { Loading } from "../../component/view/loading";
 import { EmojiCode, emojiStore, EmojiType } from "./store";
 import { dom, domVisibleSeek } from "../../src/common/dom";
 import { getEmoji } from "../../net/element.type";
 import { Tip } from "../../component/view/tooltip/tip";
 import { Spin } from "../../component/view/spin";
-import lodash from "lodash";
+
 
 /**
  * 用户输入::触发
  */
-class EmojiSelector extends InputTextPopSelector {
-    onClose(): void {
-        this.close();
-    }
-    async open(
-        round: Rect,
-        text: string,
-        callback: (...args: any[]) => void): Promise<boolean> {
-        if (!text && this.visible) {
-            this.close()
-            return this.visible
-        }
-        this._select = callback;
-        this.pos = round.leftBottom;
-        this.visible = true;
-        var t = text.replace(/^::|；；|;;|：：/, '');
-        this.text = t;
-        /**
-        * 修复输入@+空格时，自动关掉搜索框
-        */
-        if (/[\S]+[\s]+$/g.test(this.text)) {
-            this.close();
-            return this.visible;
-        }
-        /**
-         * 说明搜索了，为空，然后又输入了，就默认关闭
-         */
-        if (this.searchWord && this.text.startsWith(this.searchWord) && this.emojiCodes.length == 0) {
-            if (this.continuousSearchEmpty == true) {
-                this.close();
-                return this.visible;
-            }
-            this.continuousSearchEmpty = true;
-        }
-        else if (!this.text) {
-        }
-        this.syncSearch();
-        return this.visible;
-    }
-    emojiCodes: EmojiCode[] = [];
+class EmojiSelector extends InputTextPopSelector<EmojiCode> {
+    prefix: string[] = ['::', '：：', ';;', '；；'];
     emojis: EmojiType[] = [];
-    loading = false;
     searchCode = '';
-    searchWord = '';
-    continuousSearchEmpty: boolean = false;
-    syncSearch = lodash.debounce(async () => {
-        this.loading = true;
-        this.searchWord = this.text;
-        this.forceUpdate();
-        this.emojiCodes = [];
+    lazyTime: number = 0;
+    async searchAll() {
+        var r = await emojiStore.get();
+        this.emojis = r;
+        this.searchCode = this.emojis.first().childs.first().code;
+    }
+    async searchByWord(word: string) {
+        var list: EmojiCode[] = [];
+        if (!word) return [];
         this.emojis.forEach(ej => {
             ej.childs.forEach(c => {
-                if (c.keywords.some(k => k == this.searchWord || k.indexOf(this.searchWord) > -1))
-                    this.emojiCodes.push(c);
+                if (c.name && c.name == word || c.name && c.name.indexOf(word) > -1 || c.keywords.some(k => k == word || k.indexOf(word) > -1))
+                    list.push(c);
             })
         })
-        if (!this.emojiCodes.some(s => s.code == this.searchCode)) {
-            this.searchCode = this.emojiCodes.first()?.code;
+        if (!list.some(s => s.code == this.searchCode)) {
+            this.searchCode = list.first()?.code;
         }
-        this.loading = false;
-        this.forceUpdate();
-    }, 300)
+        console.log(list);
+        return list;
+    }
     renderEmojis() {
         if (this.loading == true) return <div className='shy-emoji-view-loading'></div>
         var els: JSX.Element[] = [];
@@ -97,23 +57,21 @@ class EmojiSelector extends InputTextPopSelector {
     private renderSearchs() {
         return <div>
             {this.loading && <Spin block></Spin>}
-            {!this.loading && this.emojiCodes.map((emoji, i) => {
-                return <a onMouseDown={e => this.onSelect(emoji)} data-code={emoji.code} className={this.searchCode == emoji.code ? "item-hover-focus" : ""} key={emoji.code}>
-                </a>
-            })}
-            {!this.loading && this.emojiCodes.length == 0 && this.searchWord && <div className="remark flex-center">没有搜索到</div>}
+            {!this.loading && <div className="flex flex-wrap padding-10" >{this.list.map((emoji, i) => {
+                return <Tip overlay={<>{emoji.name}</>} key={emoji.code}><span data-code={emoji.code} className={'size-32 round cursor flex-center ef f-24 ' + (this.searchCode == emoji.code ? "item-hover-focus" : "")} onMouseDown={e => this.onSelect(emoji)} dangerouslySetInnerHTML={{ __html: getEmoji(emoji.code) }}></span></Tip>
+            })}</div>}
+            {!this.loading && this.list.length == 0 && <div className="remark flex-center">没有搜索到</div>}
         </div>
     }
     render() {
         var style: Record<string, any> = {
             top: this.pos.y,
-            left: this.pos.x
+            left: this.pos.x,
+            display: this.visible ? "block" : 'none'
         }
-        return <div>
-            {this.visible && <div className='emoji-selectors' onScroll={e => this.onScroll(e)} style={style}>
-                {!this.text && this.renderEmojis()}
-                {this.text && this.renderSearchs()}
-            </div>}
+        return <div className='emoji-selectors' onScroll={e => this.onScroll(e)} style={style}>
+            {!this.searchWord && this.renderEmojis()}
+            {this.searchWord && this.renderSearchs()}
         </div>
     }
     private scrollIndex = 0;
@@ -131,27 +89,28 @@ class EmojiSelector extends InputTextPopSelector {
             })
         }
     }
-    private onSelect(block: EmojiCode) {
+    onSelect(block: EmojiCode) {
         this._select({ url: '/emoji', isLine: true, src: { name: 'emoji', code: block.code } })
         this.close();
     }
-    private visible: boolean = false;
-    private pos: Point = new Point(0, 0)
-    private _select: (block: Record<string, any>) => void;
-    private text: string;
-    private close() {
+    close() {
         this.searchCode = '';
-        this.searchWord = '';
-        this.loading = false;
-        if (this.visible == true) {
-            this.visible = false;
-            this.forceUpdate();
-        }
+        super.close();
+        // this.continuousSearchEmpty = false;
+        // this.loading = false;
+        // this.text = '';
+        // this.searchWord = '';
+        // this.selectIndex = 0;
+        // if (this.visible == true) {
+        //     this.visible = false;
+        // }
+        // console.log(this.visible, 'sss');
+        // this.forceUpdate();
     }
     /**
      * 向下选择内容
      */
-    private keydown() {
+    keydown(e) {
         var currentEl = this.el.querySelector(`[data-code="${this.searchCode}"]`);
         var rect = Rect.fromEle(currentEl as HTMLElement);
         var fg = domVisibleSeek({
@@ -175,11 +134,12 @@ class EmojiSelector extends InputTextPopSelector {
             s.scrollIntoView({ block: 'nearest', inline: 'nearest' });
             this.searchCode = s.getAttribute('data-code');
         }
+        return true;
     }
     /**
      * 向上选择内容
      */
-    private keyup() {
+    keyup(e) {
         var currentEl = this.el.querySelector(`[data-code="${this.searchCode}"]`);
         var rect = Rect.fromEle(currentEl as HTMLElement);
         var fg = domVisibleSeek({
@@ -203,8 +163,9 @@ class EmojiSelector extends InputTextPopSelector {
             s.scrollIntoView({ block: 'nearest', inline: 'nearest' });
             this.searchCode = s.getAttribute('data-code');
         }
+        return true;
     }
-    private keyleft() {
+    keyleft(e) {
         var currentEl = this.el.querySelector(`[data-code="${this.searchCode}"]`);
         var rect = Rect.fromEle(currentEl as HTMLElement);
         var fg = domVisibleSeek({
@@ -228,8 +189,9 @@ class EmojiSelector extends InputTextPopSelector {
             s.scrollIntoView({ block: 'nearest', inline: 'nearest' });
             this.searchCode = s.getAttribute('data-code');
         }
+        return true;
     }
-    private keyright() {
+    keyright(e) {
         var currentEl = this.el.querySelector(`[data-code="${this.searchCode}"]`);
         var rect = Rect.fromEle(currentEl as HTMLElement);
         var fg = domVisibleSeek({
@@ -253,60 +215,22 @@ class EmojiSelector extends InputTextPopSelector {
             s.scrollIntoView({ block: 'nearest', inline: 'nearest' });
             this.searchCode = s.getAttribute('data-code');
         }
+        return true;
     }
-    private el: HTMLElement;
-    componentDidMount() {
-        this.el = ReactDOM.findDOMNode(this) as HTMLElement;
-        document.addEventListener('mousedown', this.onGlobalMousedown);
-        emojiStore.get().then(r => {
-            this.emojis = r;
-            this.searchCode = this.emojis.first().childs.first().code;
-            this.forceUpdate();
-        });
-    }
-    componentWillUnmount() {
-        document.removeEventListener('mousedown', this.onGlobalMousedown);
-    }
-    componentDidUpdate() {
-        var el = this.el.querySelector('.selected') as HTMLElement;
-        if (el) {
-            el.scrollIntoView({
-                block: "nearest",
-                inline: "nearest"
-            });
-        }
-    }
-    onGlobalMousedown = (event: MouseEvent) => {
-        if (this.visible == true && this.el) {
-            var target = event.target as HTMLElement;
-            if (this.el.contains(target)) return;
-            this.close();
-        }
-    }
-    onKeydown(event: KeyboardEvent) {
-        if (this.visible == true) {
-            switch (event.key) {
-                case KeyboardCode.ArrowDown:
-                    this.keydown();
-                    return true;
-                case KeyboardCode.ArrowUp:
-                    this.keyup();
-                    return true;
-                case KeyboardCode.ArrowLeft:
-                    this.keyleft();
-                    return true;
-                case KeyboardCode.ArrowRight:
-                    this.keyright();
-                    return true;
-                case KeyboardCode.Enter:
-                    var sc=this.searchCode;
-                    this.close();
-                    console.log(this.searchCode, 'ssss');
-                    if (sc) return { blockData: { url: '/emoji', isLine: true, src: { name: 'emoji', code:sc } } };
-                    else return false;
+    getSelectBlockData() {
+        if (this.searchCode) {
+            return {
+                blockData: {
+                    url: '/emoji',
+                    isLine: true,
+                    src: {
+                        name: 'emoji',
+                        code: this.searchCode
+                    }
+                }
             }
         }
-        return false;
+        return null;
     }
 }
 export async function useEmojiSelector() {
