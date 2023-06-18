c82d3fb1672a5857d708b1a107b33192966c7ddb
kanhai <760489033@qq.com>
2023-06-18 12:39:31 +0800

    增加helptext组件

--- component/view/text.tsx
+++ component/view/text.tsx
@@ -1,9 +1,18 @@
 import React, { CSSProperties } from "react";
 import { ComponentSize } from "./declare";
+import { HelpSvg } from "../svgs";
+import { Icon } from "./icon";
 export function Remark(props: { style?: CSSProperties, size?: ComponentSize, children: React.ReactNode }) {
-    return <div style={(props.style || {})} className={'remark' + (props.size&&props.size=='middle' ? " f-14":" f-12")}>{props.children}</div>
+    return <div style={(props.style || {})} className={'remark' + (props.size && props.size == 'middle' ? " f-14" : " f-12")}>{props.children}</div>
 }
 
 export function ErrorText(props: { children: React.ReactNode }) {
     return <span className='error'>{props.children}</span>
+}
+
+export function HelpText(props: { text: string, url: string }) {
+    return <a className="remark f-14 cursor" target="_blank" href={props.url}>
+        <span className="flex flex-inline"><Icon size={16} icon={HelpSvg}></Icon>
+            <span className="gap-l-5">{props.text}</span></span>
+    </a>
 }
