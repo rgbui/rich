421b33e93d747eab3e8fd3e54b5dff67ac894df5
kanhai <760489033@qq.com>
2023-06-27 16:00:48 +0800

    fix:修复编辑title时，打开按钮该显示则显示

--- blocks/data-grid/element/field/title.tsx
+++ blocks/data-grid/element/field/title.tsx
@@ -6,29 +6,38 @@ import { OriginField } from "./origin.field";
 import { Icon } from "../../../../component/view/icon";
 import { PageLayoutType, getPageIcon } from "../../../../src/page/declare";
 import { BlockUrlConstant } from "../../../../src/block/constant";
-import { AppearAnchor } from "../../../../src/block/appear";
+import { Rect } from "../../../../src/common/vector/point";
 
 @url('/field/title')
 export class FieldText extends OriginField {
     async openPage() {
         this.dataGrid.onOpenEditForm(this.item.dataRow.id);
     }
-    focusAnchor(anchor: AppearAnchor) {
-        if (this.asView<FieldTextView>()?.span) {
-            this.asView<FieldTextView>().span.style.display = 'none';
-        }
-    }
-    blurAnchor(anchor: AppearAnchor) {
-        if (this.asView<FieldTextView>()?.span) {
-            this.asView<FieldTextView>().span.style.display = 'block';
-        }
-    }
 }
 @view('/field/title')
 export class FieldTextView extends BlockView<FieldText>{
     span: HTMLElement;
+    move(e?: React.MouseEvent) {
+        if (this.span) {
+            this.span.style.display = 'block';
+            var sel = window.getSelection();
+            var eg = sel?.focusNode;
+            var range = sel.getRangeAt(0);
+            if (eg && this.span.parentNode.contains(eg) && range) {
+                var sg = Rect.fromEle(range);
+                var r = Rect.fromEle(this.span);
+                r = r.extend(20);
+                if (sg.isCross(r)) {
+                    this.span.style.display = 'none';
+                }
+            }
+        }
+    }
+    keydown(e: React.KeyboardEvent) {
+        this.move()
+    }
     render() {
-        return <div className='flex l-20 flex-top sy-field-title f-14'>
+        return <div className='flex l-20 flex-top sy-field-title f-14' onKeyDown={e => this.keydown(e)} onMouseMove={e => this.move(e)}>
             <span className="size-20 flex-center inline-flex text-1 gap-r-3"><Icon size={20} icon={getPageIcon({
                 pageType: PageLayoutType.doc,
                 icon: this.block.item?.dataRow?.icon
