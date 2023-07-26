c47e16509729bd60434397b7f2658b5814523931
kanhai <760489033@qq.com>
2023-07-26 19:06:07 +0800

    fix:实现图片的蒙板效果、图片的对齐

--- blocks/media/image.tsx
+++ blocks/media/image.tsx
@@ -1,22 +1,24 @@
 import { BlockView } from "../../src/block/view";
 import { prop, url, view } from "../../src/block/factory/observable";
-import React from 'react';
-import { BlockDisplay, BlockRenderRange } from "../../src/block/enum";
+import React, { CSSProperties } from 'react';
+import { BlockDirective, BlockDisplay, BlockRenderRange } from "../../src/block/enum";
 import { TextArea } from "../../src/block/view/appear";
 import { Rect } from "../../src/common/vector/point";
 import { ResourceArguments } from "../../extensions/icon/declare";
 import { Block } from "../../src/block";
-
 import { MouseDragger } from "../../src/common/dragger";
-
 import { Icon } from "../../component/view/icon";
 import { useImagePicker } from "../../extensions/image/picker";
 import { getImageSize } from "../../component/file";
 import { channel } from "../../net/channel";
 import { autoImageUrl } from "../../net/element.type";
 import { util } from "../../util/util";
-import { ImageErrorSvg, PicSvg } from "../../component/svgs";
+import { DotsSvg, DownloadSvg, DuplicateSvg, ImageErrorSvg, LinkSvg, PicSvg, RefreshSvg, TrashSvg } from "../../component/svgs";
 import { Spin } from "../../component/view/spin";
+import { MenuItem, MenuItemType } from "../../component/view/menu/declare";
+import { LangID } from "../../i18n/declare";
+import { langProvider } from "../../i18n/provider";
+import { MenuItemView } from "../../component/view/menu/item";
 
 @url('/image')
 export class Image extends Block {
@@ -28,24 +30,42 @@ export class Image extends Block {
     caption: string = '';
     @prop()
     allowCaption: boolean = false;
+    @prop()
+    originSize: { width: number, height: number } = null;
+    @prop()
+    align: 'left' | 'right' | 'center' = 'center';
+    @prop()
+    mask: 'rect' | 'circle' | 'radius' | 'rhombus' | 'pentagon' | "star" = 'rect';
     display = BlockDisplay.block;
     speed = '';
     async onOpenUploadImage(event: React.MouseEvent) {
         var r = await useImagePicker({ roundArea: Rect.fromEvent(event) });
         if (r) {
-            await this.onUpdateProps({ src: r }, { range: BlockRenderRange.self, merge: true });
+            await this.onSaveImageSize(r, false);
         }
     }
     get appearAnchors() {
         if (this.src?.name != 'none') return this.__appearAnchors;
         return [];
     }
+    async onSaveImageSize(d: { url?: string }, merge: boolean) {
+        var imgSize = await getImageSize(d?.url);
+        var width = this.el.getBoundingClientRect().width;
+        var per = Math.min(70, parseInt((imgSize.width * 100 / width).toString()));
+        per = Math.max(30, per);
+        this.speed = '';
+        await this.onUpdateProps({
+            imageWidthPercent: per,
+            originSize: imgSize,
+            src: { ...d }
+        }, { range: BlockRenderRange.self, merge });
+    }
     async didMounted() {
         try {
             if (this.createSource == 'InputBlockSelector') {
                 var r = await useImagePicker({ roundArea: Rect.fromEle(this.el) });
                 if (r) {
-                    await this.onUpdateProps({ src: r }, { range: BlockRenderRange.self, merge: true });
+                    await this.onSaveImageSize(r, true);
                 }
             }
             if (this.initialData && this.initialData.file) {
@@ -61,24 +81,13 @@ export class Image extends Block {
                     }
                 });
                 if (d.ok && d.data?.file?.url) {
-                    var imgSize = await getImageSize(d.data?.file?.url);
-                    var width = this.el.getBoundingClientRect().width;
-                    var per = Math.min(70, parseInt((imgSize.width * 100 / width).toString()));
-                    per = Math.max(30, per);
-                    this.speed = '';
-                    await this.onUpdateProps({
-                        imageWidthPercent: per,
-                        src: { ...d.data?.file }
-                    }, { range: BlockRenderRange.self, merge: true });
+                    await this.onSaveImageSize(d.data?.file, true);
                 }
             }
             if (this.initialData && this.initialData.url) {
                 var d = await channel.post('/ws/download/url', { url: this.initialData.url });
                 if (d.ok && d.data?.file?.url) {
-                    var imgSize = await getImageSize(d.data?.file?.url);
-                    var width = this.el.getBoundingClientRect().width;
-                    var per = Math.min(100, parseInt((imgSize.width * 100 / width).toString()));
-                    await this.onUpdateProps({ imageWidthPercent: per, src: { ...d.data?.file, source: this.initialData.url } }, { range: BlockRenderRange.self, merge: true });
+                    await this.onSaveImageSize(d.data?.file, true);
                 }
             }
         }
@@ -109,6 +118,166 @@ export class Image extends Block {
     async getMd() {
         return `![${this.caption || '图片'}](${this.src?.url})`;
     }
+    async onGetContextMenus() {
+        if (this.isFreeBlock) {
+            return await this.onGetBoardContextMenus()
+        }
+        var rc = (item: MenuItem<string>, view?: MenuItemView) => {
+            return <span className="flex-inline flex-center size-20"><span style={{ border: '1px solid var(--text-color)' }} className="round w-10 h-12 inline-block"></span></span>
+        }
+        var items: MenuItem<BlockDirective | string>[] = [];
+        items.push({
+            name: BlockDirective.copy,
+            text: '拷贝副本',
+            label: "Ctrl+D",
+            icon: DuplicateSvg
+        });
+        items.push({
+            type: MenuItemType.divide
+        });
+        items.push({
+            name: BlockDirective.link,
+            text: langProvider.getText(LangID.menuCopyLink),
+            icon: LinkSvg
+        });
+        items.push({
+            type: MenuItemType.divide
+        });
+        items.push({
+            name: 'replace',
+            text: '替换',
+            icon: RefreshSvg
+        });
+        items.push({
+            name: 'origin',
+            text: '原图',
+            icon: { name: 'bytedance-icon', code: 'arrow-right-up' }
+        });
+        items.push({
+            name: 'download',
+            text: '下载',
+            icon: DownloadSvg
+        });
+        items.push({
+            type: MenuItemType.divide
+        });
+        items.push({
+            text: '对齐',
+            icon: { name: 'bytedance-icon', code: 'align-text-both' },
+            childs: [
+                {
+                    name: 'align',
+                    icon: { name: 'bytedance-icon', code: 'align-text-left' },
+                    text: '居左',
+                    value: 'left',
+                    checkLabel: this.align == 'left'
+                },
+                {
+                    name: 'align',
+                    icon: { name: 'bytedance-icon', code: 'align-text-center' },
+                    text: '居中', value: 'center', checkLabel: this.align == 'center'
+                },
+                {
+                    name: 'align',
+                    icon: {
+                        name: 'bytedance-icon',
+                        code: 'align-text-right'
+                    },
+                    text: '居右',
+                    value: 'right',
+                    checkLabel: this.align == 'right'
+                }
+            ]
+        });
+        items.push({
+
+            text: '蒙版',
+            icon: { name: 'bytedance-icon', code: 'mask-two' },
+            childs: [
+                {
+                    name: 'mask',
+                    icon: {
+                        name: 'bytedance-icon',
+                        code: 'rectangle'
+                    },
+                    text: '无',
+                    value: 'rect',
+                    checkLabel: this.mask == 'rect'
+                },
+                {
+                    name: 'mask',
+                    renderIcon: rc,
+                    text: '圆角',
+                    value: 'radius',
+                    checkLabel: this.mask == 'radius'
+                },
+                {
+                    name: 'mask',
+                    icon: {
+                        name: 'bytedance-icon',
+                        code: 'oval-one'
+                    },
+                    text: '圆',
+                    value: 'circle',
+                    checkLabel: this.mask == 'circle'
+                },
+                {
+                    name: 'mask',
+                    icon: { name: 'bytedance-icon', code: 'diamond-three' },
+                    text: '菱形',
+                    value: 'rhombus',
+                    checkLabel: this.mask == 'rhombus'
+                },
+                {
+                    name: 'mask',
+                    icon: { name: 'bytedance-icon', code: 'pentagon-one' },
+                    text: '五边形',
+                    value: 'pentagon',
+                    checkLabel: this.mask == 'pentagon'
+                },
+                {
+                    name: 'mask',
+                    icon: { name: 'bytedance-icon', code: 'star' },
+                    text: '星形',
+                    value: 'star',
+                    checkLabel: this.mask == 'star'
+                }
+            ]
+        });
+        items.push({
+            type: MenuItemType.divide
+        });
+        items.push({
+            name: BlockDirective.delete,
+            icon: TrashSvg,
+            text: langProvider.getText(LangID.menuDelete),
+            label: "Del"
+        });
+        return items;
+    }
+    async onClickContextMenu(item, event) {
+        switch (item.name) {
+            case 'replace':
+                var r = await useImagePicker({ roundArea: this.getVisibleBound() });
+                if (r) {
+                    await this.onSaveImageSize(r, false);
+                }
+                return;
+            case 'origin':
+                window.open(this.src?.url)
+                return;
+            case 'download':
+                util.downloadFile(this.src?.url, (this.src?.filename || this.caption) + (this.src.ext || '.jpg'))
+                return;
+            case 'align':
+                await this.onUpdateProps({ align: item.value }, { range: BlockRenderRange.self })
+                return;
+            case 'mask':
+                await this.onUpdateProps({ mask: item.value }, { range: BlockRenderRange.self })
+                return;
+        }
+        await super.onClickContextMenu(item, event);
+    }
 }
 @view('/image')
 export class ImageView extends BlockView<Image>{
@@ -135,7 +304,7 @@ export class ImageView extends BlockView<Image>{
                 var width: number;
                 if (operator == 'right') width = data.realWidth + dx * 2;
                 else width = data.realWidth - dx * 2;
-                width = Math.max(100, width);
+                width = Math.max(30, width);
                 width = Math.min(bound.width, width);
                 self.imageWrapper.style.width = width + "px";
                 if (isEnd) {
@@ -160,16 +329,50 @@ export class ImageView extends BlockView<Image>{
         </div>
     }
     renderImage() {
+        var style: CSSProperties = {
+            justifyContent: 'center'
+        }
+        if (this.block.align == 'left') style.justifyContent = 'flex-start'
+        else if (this.block.align == 'right') style.justifyContent = 'flex-end'
+        var imageMaskStyle: CSSProperties = {}
+        if (this.block.mask == 'radius') imageMaskStyle.borderRadius = '10%';
+        else if (this.block.mask == 'circle') imageMaskStyle.borderRadius = '50%';
+        else if (this.block.mask == 'rhombus') {
+            imageMaskStyle.clipPath = 'polygon(50% 0, 100% 50%, 50% 100%, 0 50%)';
+        }
+        else if (this.block.mask == 'pentagon') imageMaskStyle.clipPath = 'polygon(50% 0%, 100% 38%, 82% 100%, 18% 100%, 0% 38%)'
+        else if (this.block.mask == 'star') imageMaskStyle.clipPath = `polygon(
+            50% 0%,
+            64.43% 25%,
+            93.3% 25%,
+            78.87% 50%,
+            93.3% 75%,
+            64.43% 75%,
+            50% 100%,
+            35.57% 75%,
+            6.7% 75%,
+            21.13% 50%,
+            6.7% 25%,
+            35.57% 25%)`
+        else if (this.block.mask == 'rect') imageMaskStyle.borderRadius = '0%';
         return <>{this.isLoadError && <div className='sy-block-image-error flex r-gap-r-10 text-1'>
             <Icon icon={ImageErrorSvg}></Icon>
             <span className="f-14">图片{this.errorUrl}加载失败</span>
             <span className="f-14 link cursor" onMouseDown={e => this.block.onOpenUploadImage(e)}>重新添加图片</span>
         </div>}
-            {!this.isLoadError && <div className='sy-block-image-content-view'>
-                <div className='sy-block-image-content-view-wrapper' ref={e => this.imageWrapper = e} style={{ width: this.block.imageWidthPercent ? this.block.imageWidthPercent + "%" : undefined }}>
-                    {this.block.src.name != 'none' && <img onError={e => this.onError(e)} src={autoImageUrl(this.block?.src?.url)} />}
-                    {this.block.isCanEdit() && <><div className='sy-block-image-left-resize' onMouseDown={e => this.onMousedown(e, 'left')}></div>
-                        <div className='sy-block-image-right-resize' onMouseDown={e => this.onMousedown(e, 'right')}></div></>}
+            {!this.isLoadError && <div className='sy-block-image-content-view flex-center' style={style}>
+                <div className='sy-block-image-content-view-wrapper visible-hover' ref={e => this.imageWrapper = e} style={{ width: this.block.imageWidthPercent ? this.block.imageWidthPercent + "%" : undefined }}>
+                    {this.block.src.name != 'none' && <img style={imageMaskStyle} onError={e => this.onError(e)} src={autoImageUrl(this.block?.src?.url)} />}
+                    {this.block.isCanEdit() && <>
+                        <div className='sy-block-image-left-resize' onMouseDown={e => this.onMousedown(e, 'left')}></div>
+                        <div className='sy-block-image-right-resize' onMouseDown={e => this.onMousedown(e, 'right')}></div>
+                        <div onMouseDown={e => {
+                            e.stopPropagation();
+                            this.block.onContextmenu(e.nativeEvent)
+                        }} className="bg-dark cursor visible text-white pos-top-right gap-10 size-24 round flex-center ">
+                            <Icon size={18} icon={DotsSvg}></Icon>
+                        </div>
+                    </>}
                     {this.block.allowCaption && <div className='sy-block-image-caption'>
                         {<TextArea block={this.block} prop='content' placeholder={'键入图片描述'}></TextArea>}
                     </div>}
