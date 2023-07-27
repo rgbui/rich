3871639390dbdfd7c47d2935ce21def625fbd820
kanhai <760489033@qq.com>
2023-07-28 00:31:36 +0800

    fix:修复gridMap索引时，没有索引具有子的gridMap的块

--- src/page/grid/index.ts
+++ src/page/grid/index.ts
@@ -61,7 +61,7 @@ export class GridMap {
         this.relativePanelPoint = this.getRelativeRect(new Rect(0, 0, 0, 0)).leftTop;
         if (this.panel instanceof Page) {
             this.panel.each((b) => {
-                if (b.panelGridMap === this) {
+                if (b.panelGridMap === this || b.gridMap && b.parent.panelGridMap === this) {
                     if (!b.isLine && !b.isLayout && !b.isPart)
                         this.sync(b);
                 }
@@ -69,7 +69,7 @@ export class GridMap {
         }
         else if (this.panel instanceof Block) {
             this.panel.each(b => {
-                if (b.panelGridMap === this) {
+                if (b.panelGridMap === this || b.gridMap && b.parent.panelGridMap === this) {
                     if (!b.isLine && !b.isLayout && !b.isPart)
                         this.sync(b);
                 }
