8258366307fde021e539846c9a83b1593f22ea50
kanhai <760489033@qq.com>
2023-08-03 14:57:47 +0800

    fix:增加textarea触发enter的机制及autoHeight的支持

--- component/view/input/textarea.tsx
+++ component/view/input/textarea.tsx
@@ -9,9 +9,15 @@ export class Textarea extends React.Component<{
     placeholder?: string,
     onChange?: (value: string) => void,
     onEnter?: (value) => void,
+    shiftEnter?: boolean,
+    ctrlEnter?: boolean,
+    metaEnter?: boolean,
     clear?: boolean,
     maxLength?: number,
-    ignoreFilterWhitespace?: boolean
+    ignoreFilterWhitespace?: boolean,
+    round?: boolean,
+    transparent?: boolean,
+    autoHeight?: boolean,
 }>{
     updateValue(value) {
         this.inputEl.value = value;
@@ -25,13 +31,35 @@ export class Textarea extends React.Component<{
         }
         function keydown(e: React.KeyboardEvent) {
             if (e.key == 'Enter' && props.onEnter) {
-                //  props.onEnter(filterValue((e.target as HTMLInputElement).value));
+                if (e.shiftKey && props.shiftEnter) {
+                    props.onEnter(filterValue((e.target as HTMLInputElement).value));
+                }
+                else if (e.ctrlKey && props.ctrlEnter) {
+                    props.onEnter(filterValue((e.target as HTMLInputElement).value));
+                }
+                else if (e.metaKey && props.metaEnter) {
+                    props.onEnter(filterValue((e.target as HTMLInputElement).value));
+                }
             }
         }
-        return <div className='shy-textarea' style={props.style || {}}>
-            <textarea ref={e => this.inputEl = e} maxLength={props.maxLength || undefined} defaultValue={props.value || ''}
+        var style = Object.assign({}, props.style || {});
+        if (props.autoHeight) { style.height = 'auto'; style.minHeight = 80 }
+        return <div className='shy-textarea' style={style}>
+            <textarea
+                ref={e => this.inputEl = e}
+                maxLength={props.maxLength || undefined}
+                defaultValue={props.value || ''}
                 disabled={props.disabled ? true : false}
-                placeholder={props.placeholder} onInput={e => props.onChange && props.onChange(filterValue((e.target as HTMLInputElement).value))}
+                placeholder={props.placeholder}
+                style={{
+                    borderRadius: props.round === false ? 0 : undefined,
+                    boxShadow: props.transparent ? 'none' : undefined,
+                    backgroundColor: props.transparent ? 'transparent' : undefined,
+                    height: props.autoHeight ? 'auto' : undefined,
+                    resize: props.autoHeight ? 'vertical' : undefined,
+                    minHeight: props.autoHeight ? 80 : undefined
+                }}
+                onInput={e => props.onChange && props.onChange(filterValue((e.target as HTMLInputElement).value))}
                 onKeyDown={e => keydown(e)}></textarea>
         </div>
     }
