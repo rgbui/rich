a9781e655874c5fcc14704eb86541633d11d39f5
kanhai <760489033@qq.com>
2023-08-03 15:04:42 +0800

    fix:增加helpText组件

--- component/view/text.tsx
+++ component/view/text.tsx
@@ -2,7 +2,13 @@ import React, { CSSProperties } from "react";
 import { ComponentSize } from "./declare";
 import { HelpSvg } from "../svgs";
 import { Icon } from "./icon";
-export function Remark(props: { style?: CSSProperties, size?: ComponentSize, children: React.ReactNode }) {
+import { util } from "../../util/util";
+
+export function Remark(props: {
+    style?: CSSProperties,
+    size?: ComponentSize,
+    children: React.ReactNode
+}) {
     return <div style={(props.style || {})} className={'remark' + (props.size && props.size == 'middle' ? " f-14" : " f-12")}>{props.children}</div>
 }
 
@@ -10,9 +16,15 @@ export function ErrorText(props: { children: React.ReactNode }) {
     return <span className='error'>{props.children}</span>
 }
 
-export function HelpText(props: { text: string, url: string }) {
-    return <a className="remark f-14 cursor" target="_blank" href={props.url}>
-        <span className="flex flex-inline"><Icon size={16} icon={HelpSvg}></Icon>
-            <span className="gap-l-5">{props.text}</span></span>
-    </a>
+export function HelpText(props: {
+    text?: string,
+    children?: React.ReactNode,
+    url: string,
+    className?: string | (string[])
+}) {
+    var c = util.covertToArray(props.className);
+    return <a
+        className={"padding-w-3 remark f-12 cursor item-hover round l-20 flex flex-inline" + (c.join(" "))}
+        target="_blank"
+        href={props.url}><Icon size={14} icon={HelpSvg}></Icon><span >{props.text || props.children}</span></a>
 }
