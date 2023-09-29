fc47bcf471c695ddd9452d030e932ffde605d354
kanhai <760489033@qq.com>
2023-09-29 23:24:20 +0800

    fix:优化input links显示

--- extensions/link/outside.tsx
+++ extensions/link/outside.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Button } from "../../component/view/button";
 import { Input } from "../../component/view/input";
-import { S, Sp } from "../../i18n/view";
-import "./style.less";
+import { S} from "../../i18n/view";
 import { ls } from "../../i18n/store";
+import "./style.less";
 
 export class OutsideUrl extends React.Component<{ url?: string, remark?: React.ReactNode, change: (url: string) => void }>{
     constructor(props) {
@@ -18,7 +18,9 @@ export class OutsideUrl extends React.Component<{ url?: string, remark?: React.R
     render() {
         return <div className='shy-outside-url'>
             <Input placeholder={ls.t('请输入网址')} value={this.url} onChange={e => this.url = e} onEnter={e => { this.url = e; this.save() }}></Input>
-            <Button block style={{ width: 200 }} onClick={() => this.save()}><S>保存</S></Button>
+            <div className="flex-center">
+                <Button block style={{ width: 200 }} onClick={() => this.save()}><S>保存</S></Button>
+            </div>
             {this.error && <div className='shy-outside-url-error'>{this.error}</div>}
             {this.props.remark && <div className='shy-outside-url-remark'>{this.props.remark}</div>}
         </div>
