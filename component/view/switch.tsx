444db8fb8cd458841ecd4b237a256ff8c48524dd
kanhai <760489033@qq.com>
2023-09-29 22:03:41 +0800

    fix:增加switchText的align

--- component/view/switch.tsx
+++ component/view/switch.tsx
@@ -1,4 +1,5 @@
 import React, { CSSProperties } from "react";
+
 export function Switch(props: {
     checked: boolean,
     onChange: (checked: boolean) => void,
@@ -29,14 +30,22 @@ export function SwitchText(props: {
     style?: CSSProperties, className?: string | (string[]),
     disabled?: boolean,
     size?: 'small',
-    children?: React.ReactNode
+    children?: React.ReactNode,
+    align?: 'right' | 'left'
 }) {
     var classList: string[] = ['inline-flex'];
     var style: CSSProperties = {
         // display: 'inline-block'
     }
-    return <div className={classList.join(' ')} style={style}>
-        <label className="gap-r-5">{props.text || props.children}</label>
-        <Switch {...props}></Switch>
-    </div>
+    if (props.align == 'left' || !props.align)
+        return <div className={classList.join(' ')} style={style}>
+            <label className="gap-r-5">{props.text || props.children}</label>
+            <Switch {...props}></Switch>
+        </div>
+    else if (props.align == 'right') {
+        return <div className={classList.join(' ')} style={style}>
+            <Switch {...props}></Switch>
+            <label className="gap-l-5">{props.text || props.children}</label>
+        </div>
+    }
 }
