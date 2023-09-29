9d355d9b12a7eb9f1569b32d80e16895a47af277
kanhai <760489033@qq.com>
2023-09-29 22:06:51 +0800

    fix:增加全局ai的开启关闭功能

--- src/kit/write/ai.ts
+++ src/kit/write/ai.ts
@@ -3,6 +3,7 @@ import { useAITool } from "../../../extensions/ai";
 import { AppearAnchor } from "../../block/appear";
 
 export function AiInput(write: PageWrite, aa: AppearAnchor, event: React.KeyboardEvent) {
+    if (aa.block.page.ws.aiConfig?.disabled === true) return false;
     if (event.key.toLowerCase() == ' ' || event.keyCode === 32) {
         var sel = window.getSelection();
         if (sel.focusOffset == 0 && aa.isRowStart) {
