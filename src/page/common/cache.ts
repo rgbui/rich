2ed7620f16147b70d01f38a310e26d04d24e6e01
kanhai <760489033@qq.com>
2023-10-02 10:18:50 +0800

    fix：增加blockcahe get方法

--- src/page/common/cache.ts
+++ src/page/common/cache.ts
@@ -8,10 +8,14 @@ export class BlockCache {
         return BLOCK_CACHE_KEY + key;
     }
     static async set(key: string | object, value?: any) {
-        if (typeof key == 'string') await channel.act('/cache/set', { key, value })
+        if (typeof key == 'string') await channel.act('/cache/set', { key: this.getCacheKey(key), value })
         else if (lodash.isObject(key))
             for (let n in (key as object))
                 await channel.act('/cache/set', { key: this.getCacheKey(n), value: key[n] })
+    }
+    static async get(key:string){
+        var d = await channel.query('/cache/get', { key: this.getCacheKey(key) });
+        return d;
     }
     static async getJSON(...keys: string[]) {
         var json: Record<string, any> = {};
