4d2a332b41102879b072410248917a9a9381bc75
kanhai <760489033@qq.com>
2023-10-08 03:20:29 +0800

    fix:增加block cache处理block board功能

--- src/page/common/cache.ts
+++ src/page/common/cache.ts
@@ -1,6 +1,8 @@
 import lodash from "lodash";
 import { channel } from "../../../net/channel";
-
+import { Block } from "../../block";
+import { BlockUrlConstant } from "../../block/constant";
+import { BlockRenderRange } from "../../block/enum";
 const BLOCK_CACHE_KEY = 'block_prop_';
 
 export class BlockCache {
@@ -13,7 +15,7 @@ export class BlockCache {
             for (let n in (key as object))
                 await channel.act('/cache/set', { key: this.getCacheKey(n), value: key[n] })
     }
-    static async get(key:string){
+    static async get(key: string) {
         var d = await channel.query('/cache/get', { key: this.getCacheKey(key) });
         return d;
     }
@@ -21,8 +23,98 @@ export class BlockCache {
         var json: Record<string, any> = {};
         await keys.eachAsync(async k => {
             var d = await channel.query('/cache/get', { key: this.getCacheKey(k) });
-            json[k] = d;
+            if (typeof d != 'undefined')
+                json[k] = d;
         })
         return json;
+    }
+}
+
+export async function setBoardBlockCache(block: Block) {
+    async function setFontStyle() {
+        var ps = await BlockCache.getJSON('fontSize', 'fontWeight', 'fontFamily', 'textDecoration');
+        block.pattern.setFontStyle(ps);
+        var fs = await BlockCache.get('fontStyle');
+        if (!lodash.isUndefined(fs)) {
+            block.pattern.setFontStyle({ fontStyle: fs ? 'italic' : 'normal' });
+        }
+        var fc = await BlockCache.get('fontColor');
+        if (!lodash.isUndefined(fc)) {
+            block.pattern.setFontStyle({ color: fc });
+        }
+    }
+    switch (block.url) {
+        case BlockUrlConstant.Line:
+            var bg = await BlockCache.get('backgroundColor');
+            if (!lodash.isUndefined(bg)) {
+                block.pattern.setSvgStyle({ stroke: bg })
+            }
+            var ps = await BlockCache.getJSON('lineStart', 'lineEnd', 'lineType');
+            await block.updateProps(ps, BlockRenderRange.self);
+            var svg = await BlockCache.getJSON('strokeWidth', 'strokeDasharray');
+            block.pattern.setSvgStyle(svg);
+            break;
+        case BlockUrlConstant.Shape:
+            var svg = await BlockCache.getJSON('stroke', 'strokeDasharray', 'strokeOpacity', 'strokeWidth', 'fillOpacity');
+            block.pattern.setSvgStyle(svg);
+            var g = await BlockCache.get('fillColor');
+            if (!lodash.isUndefined(g)) {
+                block.pattern.setSvgStyle({ fill: g })
+            }
+            await setFontStyle();
+            break;
+        case BlockUrlConstant.Pen:
+            var g = await BlockCache.get('tickness');
+            if (typeof g != 'undefined') {
+                block.pattern.setSvgStyle({ strokeWidth: g })
+            }
+            var bg = await BlockCache.get('backgroundColor');
+            if (!lodash.isUndefined(bg)) {
+                block.pattern.setSvgStyle({ stroke: bg })
+            }
+            break;
+        case BlockUrlConstant.Note:
+            var g = await BlockCache.get('backgroundNoTransparentColor');
+            if (!lodash.isUndefined(g)) {
+                await block.updateProps({ color: g })
+            }
+            var c = await BlockCache.get('stickerSize');
+            if (!lodash.isUndefined(c)) {
+                if (c == 'big') { await block.updateProps({ fixedWidth: 400, fixedHeight: 400 }), BlockRenderRange.self }
+                else if (c == 'medium') { await block.updateProps({ fixedWidth: 200, fixedHeight: 200 }), BlockRenderRange.self }
+                else if (c == 'small') { await block.updateProps({ fixedWidth: 100, fixedHeight: 100 }), BlockRenderRange.self }
+            }
+            await setFontStyle();
+            break;
+        case BlockUrlConstant.Mind:
+
+            break;
+        case BlockUrlConstant.Frame:
+            var bg = await BlockCache.get('backgroundColor');
+            if (!lodash.isUndefined(bg)) {
+                block.pattern.setFillStyle({ color: bg, mode: 'color' });
+            }
+            break;
+        case BlockUrlConstant.TextSpan:
+            var bg = await BlockCache.get('backgroundColor');
+            if (!lodash.isUndefined(bg)) {
+                block.pattern.setFillStyle({ color: bg, mode: 'color' });
+            }
+            var ps = await BlockCache.getJSON('fontWeight', 'fontFamily', 'textDecoration');
+            block.pattern.setFontStyle(ps);
+            var fs = await BlockCache.get('fontStyle');
+            if (!lodash.isUndefined(fs)) {
+                block.pattern.setFontStyle({ fontStyle: fs ? 'italic' : 'normal' });
+            }
+            var fc = await BlockCache.get('fontColor');
+            if (!lodash.isUndefined(fc)) {
+                block.pattern.setFontStyle({ color: fc });
+            }
+            var fss = await BlockCache.get('fontSize');
+            if (typeof fss != 'undefined') {
+                await block.updateProps({ fontScale: fss / 14 })
+            }
+            break;
+
     }
 }
