054c57fa8d0c17962614ec3807011f297834a017
kanhai <760489033@qq.com>
2023-10-07 23:17:46 +0800

    fix:优化字体的加载

--- extensions/board.edit.tool/fontfamily/index.tsx
+++ extensions/board.edit.tool/fontfamily/index.tsx
@@ -2,35 +2,59 @@ import React from "react"
 import { BoardEditTool } from ".."
 import { CheckSvg } from "../../../component/svgs";
 import { Icon } from "../../../component/view/icon";
-import { FontFamilyStyle, FontStores, loadFontFamily } from "./store"
+import { FontFamilyStyle, GetFontStores, loadFontFamily } from "./store"
 import { lst } from "../../../i18n/store";
+import { Tip } from "../../../component/view/tooltip/tip";
+import { Loading2 } from "../../../component/view/spin";
 
-export function FontFamily(props: {
+export class FontFamily extends React.Component<{
     tool: BoardEditTool,
     value: string,
     change?(value: string): void
-}) {
-
-    async function changeFont(ft: FontFamilyStyle) {
-        props.change(ft.name);
-        loadFontFamily(ft.name)
-    }
-    return <div className="relative" >
-        <div className="h-20" style={{ marginTop: 2, minWidth: 20, minHeight: 20 }} onMouseDown={e => props.tool.showDrop('fontFamily')}>
-            {FontStores().find(c => c.name == props.value)?.text||lst('默认')}
+}>{
+    render() {
+        var props = this.props;
+        var self = this;
+        async function changeFont(ft: FontFamilyStyle) {
+            props.change(ft.name);
+            loadFontFamily(ft.name);
+        }
+        async function load(ft: FontFamilyStyle) {
+            await loadFontFamily(ft.name)
+            self.forceUpdate();
+        }
+        return <div className="relative" >
+            <div className="h-20" style={{ marginTop: 2, minWidth: 20, minHeight: 20 }} onMouseDown={e => props.tool.showDrop('fontFamily')}>
+                {GetFontStores().find(c => c.name == props.value)?.text || lst('默认')}
+            </div>
+            {props.tool.isShowDrop('fontFamily') && <div style={{ top: 30 }} className="w-250 z-2 bg-white max-h-300 overflow-y pos shadow padding-h-10 round ">
+                {GetFontStores().map(c => {
+                    return <div className="flex min-h-30 padding-w-10 item-hover round cursor"
+                        onMouseDown={e => changeFont(c)}
+                        key={c.name}
+                    >
+                        <span className="flex-fixed size-24">
+                            {(c.name == props.value || c.name == '' && !props.value) && <span className="flex-center size-24"><Icon size={18} icon={CheckSvg}></Icon></span>}
+                        </span>
+                        <span className="flex-auto text-overflow" style={{ fontFamily: c.name }}>{c.text}</span>
+                        {c.name!='inherit' && <span className="flex-fixed flex">
+                            {
+                                c.loaded && <span className="flex-center size-24 cursor"><Icon icon={{ name: 'bytedance-icon', code: 'local-pin' }} size={18}></Icon></span>
+                            }
+                            {c.loaded !== true && c.loading !== true && <Tip text='下载字体'><span onMouseDown={e => {
+                                e.stopPropagation();
+                                if (c.loading !== true) { c.loading = true; self.forceUpdate(); load(c) }
+                            }} className="flex-center size-24 cursor">
+                                <Icon icon={{ name: 'bytedance-icon', code: 'download-one' }} size={18}></Icon>
+                            </span></Tip>}
+                            {c.loaded !== true && c.loading == true && <span className="flex-center size-24">
+                                <Loading2 size={20}></Loading2>
+                            </span>}
+                        </span>}
+                    </div>
+                })}
+            </div>}
         </div>
-        {props.tool.isShowDrop('fontFamily') && <div style={{ top: 30 }} className="w-180 z-2 bg-white max-h-250 overflow-y pos shadow padding-h-10 round ">
-            {FontStores().map(c => {
-                return <div className="flex min-h-30 padding-w-10 item-hover round cursor"
-                    onMouseDown={e => changeFont(c)}
-                    key={c.name}
-                >
-                    <span className="flex-auto text-overflow">{c.text}</span>
-                    <span className="flex-fixed size-24">
-                        {(c.name == props.value || c.name == '' && !props.value) && <Icon size={18} icon={CheckSvg}></Icon>}
-                    </span>
-                </div>
-            })}
-        </div>}
-    </div>
+    }
 }
+
--- extensions/board.edit.tool/fontfamily/store.ts
+++ extensions/board.edit.tool/fontfamily/store.ts
@@ -5,28 +5,42 @@ import { lst } from "../../../i18n/store";
 export type FontFamilyStyle = {
     name: string,
     url?: string,
-    text: string
+    text: string,
+    loading?: boolean,
+    loaded?: boolean
 }
 
-export var FontStores: () => FontFamilyStyle[] = () => [
-    { name: '', text: lst('默认') },
-    { url: 'https://resources.shy.live/fonts/SourceHanSansSC-Normal-2.otf', name: 'SourceHanSansSC', text: '思源黑体' },
-    { url: 'https://resources.shy.live/fonts/SiYuanSongTiRegular/SourceHanSerifCN-Regular-1.otf', name: 'SourceHanSerifCN-Regular', text: '思源宋体' },
-    { url: 'https://resources.shy.live/fonts/SmileySans-Oblique-2.ttf', name: 'SmileySans-Oblique', text: '得意黑' },
-    { url: 'https://resources.shy.live/fonts/XiaoKeNaiLaoTiShangYongMianFei@QingKeZiTi-2.ttf', name: 'XiaoKeNaiLaoTiShangYongMianFei', text: '小可奶酪体' },
-    { url: 'https://resources.shy.live/fonts/PangMenZhengDaoXiXianTi-2.ttf', name: 'PangMenZhengDaoXiXianTi', text: '庞门正道细线体' },
-    { url: 'https://resources.shy.live/fonts/FeiHuaSongTi-2', name: 'FeiHuaSongTi-2', text: '飞花宋体' },
-    { url: 'https://resources.shy.live/fonts/SourceHanSansCN-VF-2.otf', name: 'SourceHanSansCN-VF-2', text: '思源黑体 CN VF' },
-    { url: 'https://resources.shy.live/fonts/JustFontFenYuanZiTi-2.ttf', name: 'JustFontFenYuanZiTi-2', text: 'jf open 粉圓 1.0' },
-    { url: 'https://resources.shy.live/fonts/YangRenDongZhuShiTi-Light-2.ttf', name: 'YangRenDongZhuShiTi-Light-2', text: '杨任东竹石体 Light' },
-    { url: 'https://resources.shy.live/fonts/ZhanKuKuaiLeTi2016XiuDingBan-2.ttf', name: 'ZhanKuKuaiLeTi2016XiuDingBan-2', text: '站酷快乐体' },
-    { url: 'https://resources.shy.live/fonts/ZhanKuWenYiTi-2', name: 'ZhanKuWenYiTi-2', text: '站酷文艺体' },
-    { url: 'https://resources.shy.live/fonts/ZhanKuXiaoLOGOTi-2.otf', name: 'ZhanKuXiaoLOGOTi-2', text: '站酷小薇LOGO体' },
-    { url: 'https://resources.shy.live/fonts/RiBenHuaYuanMingChaoTi-2.ttf', name: 'RiBenHuaYuanMingChaoTi-2', text: '日本花园明朝体' },
-    { url: 'https://resources.shy.live/fonts/Kosugi-Regular-2.ttf', name: 'Kosugi-Regular-2', text: '小杉黑体' },
-    { url: 'https://resources.shy.live/fonts/CangErShuYuanTiW02-2.ttf', name: 'CangErShuYuanTiW02-2', text: '仓耳舒圆体W02' },
 
-];
+var FontCaches: FontFamilyStyle[];
+
+
+export var GetFontStores: () => FontFamilyStyle[] = () => {
+    if (typeof FontCaches == 'undefined') {
+        FontCaches = [
+
+            { name: 'inherit', text: lst('默认') },
+            { url: 'https://resources.shy.live/fonts/SourceHanSansSC-Normal-2.otf', name: 'SourceHanSansSC', text: '思源黑体' },
+            { url: 'https://resources.shy.live/fonts/SiYuanSongTiRegular/SourceHanSerifCN-Regular-1.otf', name: 'SourceHanSerifCN-Regular', text: '思源宋体' },
+            { url: 'https://resources.shy.live/fonts/SmileySans-Oblique-2.ttf', name: 'SmileySans-Oblique', text: '得意黑' },
+            { url: 'https://resources.shy.live/fonts/XiaoKeNaiLaoTiShangYongMianFei@QingKeZiTi-2.ttf', name: 'XiaoKeNaiLaoTiShangYongMianFei', text: '小可奶酪体' },
+            { url: 'https://resources.shy.live/fonts/PangMenZhengDaoXiXianTi-2.ttf', name: 'PangMenZhengDaoXiXianTi', text: '庞门正道细线体' },
+            // { url: 'https://resources.shy.live/fonts/FeiHuaSongTi-2', name: 'FeiHuaSongTi-2', text: '飞花宋体' },
+            { url: 'https://resources.shy.live/fonts/SourceHanSansCN-VF-2.otf', name: 'SourceHanSansCN-VF-2', text: '思源黑体 CN VF' },
+            { url: 'https://resources.shy.live/fonts/JustFontFenYuanZiTi-2.ttf', name: 'JustFontFenYuanZiTi-2', text: 'jf open 粉圓 1.0' },
+            { url: 'https://resources.shy.live/fonts/YangRenDongZhuShiTi-Light-2.ttf', name: 'YangRenDongZhuShiTi-Light-2', text: '杨任东竹石体 Light' },
+            { url: 'https://resources.shy.live/fonts/ZhanKuKuaiLeTi2016XiuDingBan-2.ttf', name: 'ZhanKuKuaiLeTi2016XiuDingBan-2', text: '站酷快乐体' },
+            // { url: 'https://resources.shy.live/fonts/ZhanKuWenYiTi-2', name: 'ZhanKuWenYiTi-2', text: '站酷文艺体' },
+            { url: 'https://resources.shy.live/fonts/ZhanKuXiaoLOGOTi-2.otf', name: 'ZhanKuXiaoLOGOTi-2', text: '站酷小薇LOGO体' },
+            { url: 'https://resources.shy.live/fonts/RiBenHuaYuanMingChaoTi-2.ttf', name: 'RiBenHuaYuanMingChaoTi-2', text: '日本花园明朝体' },
+            { url: 'https://resources.shy.live/fonts/Kosugi-Regular-2.ttf', name: 'Kosugi-Regular-2', text: '小杉黑体' },
+            { url: 'https://resources.shy.live/fonts/CangErShuYuanTiW02-2.ttf', name: 'CangErShuYuanTiW02-2', text: '仓耳舒圆体W02' },
+
+        ];
+    }
+    return FontCaches;
+}
+
+
 
 var loadFonts: string[] = [];
 async function handle(batchs: {
@@ -36,14 +50,24 @@ async function handle(batchs: {
     lodash.remove(batchs, g => loadFonts.includes(g.id));
     for (let i = 0; i < batchs.length; i++) {
         var batch = batchs[i];
-        var bn = FontStores().find(c => c.name == batch.id)
+        var bn = GetFontStores().find(c => c.name == batch.id)
         var nf = new FontFace(bn.name, `url(${bn.url})`);
-        await nf.load();
+        bn.loading = true;
+        try {
+            await nf.load();
+            bn.loaded = true;
+        }
+        catch (ex) {
+
+        }
+        finally {
+            bn.loading = false;
+        }
         loadFonts.push(bn.name);
     }
     if (batchs.length > 0) {
         let code = batchs.reduce((accumulator, currentValue) => {
-            var f = FontStores().find(c => c.name == currentValue.id);
+            var f = GetFontStores().find(c => c.name == currentValue.id);
             return accumulator + `@font-face { font-family: ${f.name};src: url('${f.url}'); }`;
         }, "");
         var style = document.createElement("style") as any;
