57ff10d006bcde79ee3741b10a0e0fe6a2914427
kanhai <760489033@qq.com>
2023-10-12 23:13:08 +0800

    fix:增加user的数量显示

--- component/view/avator/users.tsx
+++ component/view/avator/users.tsx
@@ -4,10 +4,12 @@ import { Avatar } from "./face";
 export class UserAvatars extends React.Component<{
     size?: number,
     users: (string[]) | Set<string>,
-    children?: JSX.Element | string | React.ReactNode
+    children?: JSX.Element | string | React.ReactNode,
+    limit?: number
 }>{
     render(): React.ReactNode {
         var us = Array.isArray(this.props.users) ? this.props.users : Array.from(this.props.users);
+        if (this.props.limit) us = us.slice(us.length - this.props.limit)
         return <div className="flex">
             {us.map(u => <Avatar key={u} size={this.props.size} userid={u}></Avatar>)}
             {this.props.children && <><em className={us.length > 0 ? "gap-r-10" : ""}></em>{this.props.children}</>}
