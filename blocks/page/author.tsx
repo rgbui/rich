53ca2cea44c62c2c8bdcbdf393762528f1aa1243
kanhai <760489033@qq.com>
2023-10-28 15:16:52 +0800

    fix:增加page author 的align

--- blocks/page/author.tsx
+++ blocks/page/author.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import { Block } from "../../src/block";
-import { url, view } from "../../src/block/factory/observable";
+import { prop, url, view } from "../../src/block/factory/observable";
 import { BlockView } from "../../src/block/view";
 import React from "react";
 import { UserAvatars } from "../../component/view/avator/users";
@@ -9,7 +9,8 @@ import dayjs from "dayjs";
 
 @url('/page/author')
 export class PageAuthor extends Block {
-
+    @prop()
+    align: 'left' | 'center' = 'left';
 }
 @view('/page/author')
 export class PageAuthorView extends BlockView<PageAuthor>{
@@ -31,10 +32,10 @@ export class PageAuthorView extends BlockView<PageAuthor>{
         }
         if (authors.length == 0) return <></>
         return <div style={this.block.visibleStyle}>
-            <div className="flex">
-                <span className="flex-fixed gap-r-10"><UserAvatars size={24} users={authors}></UserAvatars></span>
-                {editDate && <span className="flex-fixed remark f-12 gap-r-10"  ><S>编辑于</S>&nbsp;{dayjs(editDate).format("YYYY-MM-DD HH:mm")}</span>}
-                {viewCount && viewCount > 0 && <span className="flex-fixed remark f-12"><S>浏览</S>&nbsp;{viewCount}</span>}
+            <div className={this.props.block.align == 'center' ? "flex-center" : "flex"}>
+                <span className="flex-fixed gap-r-10 flex-center"><UserAvatars size={24} users={authors}></UserAvatars></span>
+                {editDate && <span className="flex-fixed remark f-12 gap-r-10" style={{lineHeight:1.3}} ><S>编辑于</S>&nbsp;{dayjs(editDate).format("YYYY-MM-DD HH:mm")}</span>}
+                {viewCount && viewCount > 0 && <span className="flex-fixed remark f-12" style={{lineHeight:1.3}}><S>浏览</S>&nbsp;{viewCount}</span>}
             </div>
         </div>
     }
