d2d18e3779088f15a6aa7b265a6c700a392d9dc4
kanhai <760489033@qq.com>
2023-11-09 20:22:08 +0800

    fix:增加input的drops效果

--- component/view/input/index.tsx
+++ component/view/input/index.tsx
@@ -2,6 +2,9 @@ import React, { CSSProperties } from "react";
 import { CloseSvg } from "../../svgs";
 import { Icon } from "../icon";
 import "./style.less";
+import { MenuItem } from "../menu/declare";
+import { KeyboardCode } from "../../../src/common/keys";
+
 export class Input extends React.Component<{
     style?: CSSProperties,
     disabled?: boolean,
@@ -22,6 +25,7 @@ export class Input extends React.Component<{
     className?: string | (string[]),
     onMousedown?: (event: React.MouseEvent) => void,
     prefix?: React.ReactNode,
+    onSearchDrop?: (value: string) => Promise<MenuItem[]>
 }>{
     private inputEl: HTMLInputElement;
     onClear() {
@@ -32,6 +36,15 @@ export class Input extends React.Component<{
         props.onChange && props.onChange('');
         props.onClear && props.onClear()
     }
+    drop: {
+        items: MenuItem[],
+        index: number,
+        visible: boolean
+    } = {
+            items: [],
+            index: 0,
+            visible: false
+        }
     render() {
         var props = this.props;
         var self = this;
@@ -46,13 +59,49 @@ export class Input extends React.Component<{
                 if (!value) self.forceUpdate()
             }
         }
-        function keydown(e: React.KeyboardEvent) {
-            if (e.key == 'Enter' && props.onEnter) {
+        async function keydown(e: React.KeyboardEvent) {
+            if (self.drop.visible) {
+                if (e.key == KeyboardCode.ArrowDown) {
+                    self.drop.index++;
+                    if (self.drop.index >= self.drop.items.length) self.drop.index = 0;
+                    self.forceUpdate();
+                    return;
+                }
+                else if (e.key == KeyboardCode.ArrowUp) {
+                    self.drop.index--;
+                    if (self.drop.index < 0) self.drop.index = self.drop.items.length - 1;
+                    self.forceUpdate();
+                    return;
+                }
+            }
+            if (self.props.onSearchDrop) {
+                var items = await self.props.onSearchDrop((e.target as HTMLInputElement).value);
+                if (items) {
+                    self.drop.items = items;
+                    self.drop.visible = true;
+                }
+                else {
+                    self.drop.items = [];
+                    self.drop.visible = false;
+                }
+                self.forceUpdate();
+            }
+            if (self.drop.visible && e.key == KeyboardCode.Enter) {
+                if (self.drop.items[self.drop.index]) {
+                    self.updateValue(self.drop.items[self.drop.index].value);
+                    props.onEnter && props.onEnter(self.drop.items[self.drop.index].value);
+                    self.drop.visible = false;
+                    self.forceUpdate()
+                    return;
+                }
+            }
+            if (e.key == KeyboardCode.Enter && props.onEnter) {
                 props.onEnter(filterValue((e.target as HTMLInputElement).value), e);
             }
             else if (props.onKeydown) props.onKeydown(e);
         }
         var classList: string[] = ['shy-input', 'flex', 'round-3'];
+        if (this.props.onSearchDrop) classList.push('relative');
         if (this.props.size == 'small') classList.push('small')
         else if (this.props.size == 'larger') classList.push('larger')
         if (Array.isArray(this.props.className)) this.props.className.each(c => { classList.push(c) })
@@ -65,7 +114,8 @@ export class Input extends React.Component<{
             </div>}
             <div className="shy-input-wrapper flex-auto">
                 <input
-                    ref={e => this.inputEl = e} type={props.type || 'text'}
+                    ref={e => this.inputEl = e}
+                    type={props.type || 'text'}
                     defaultValue={props.value || ''}
                     disabled={props.disabled ? true : false}
                     placeholder={props.placeholder}
@@ -79,6 +129,17 @@ export class Input extends React.Component<{
             </div>
             {props.clear && this.props.value && <div className="shy-input-clear flex-fixed" onClick={e => this.onClear()}>
                 <div className="size-20 flex-center item-hover circle cursor "><Icon size={10} icon={CloseSvg}></Icon></div>
+            </div>}
+            {this.drop?.items && this.drop?.items?.length > 0 && this.drop.visible && <div style={{ top: 30 }} className="pos pos-top-full padding-h-10 round bg-white shadow">
+                {this.drop.items.map((item, i) => {
+                    return <div onMouseDown={e => {
+                        e.stopPropagation();
+                        this.updateValue(item.value);
+                        props.onChange && props.onChange(item.value);
+                        this.drop.visible = false;
+                        this.forceUpdate()
+                    }} key={i} className={"flex padding-w-10 round item-light-hover h-24 gap-h-3 " + (this.drop.index == i ? " item-hover-focus" : "")}>{item.icon && <span className="size-24 flex-center"><Icon size={16} icon={item.icon}></Icon></span>}<span className="flex-auto">{item.text}</span></div>
+                })}
             </div>}
         </div>
     }
