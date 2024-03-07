182d04c76d405d2cb2a1230977641b66e20bd04c
kanhai <760489033@qq.com>
2024-03-07 23:13:29 +0800

    fix:优化颜色

--- extensions/color/data.ts
+++ extensions/color/data.ts
@@ -7,13 +7,13 @@ export type ColorType = { color: string, text?: string };
  */
 export var FontColorList = () => [
     { color: 'inherit', text: lst('默认') },
-    { color: 'rgb(120, 119, 116)', text: lst('灰色') },
+    { color: 'rgb(120,119,116)', text: lst('灰色') },
     { color: 'rgb(159,107,83)', text: lst('棕色') },
     { color: 'rgb(217,115,13)', text: lst('橙色') },
     { color: 'rgb(203,145,47)', text: lst('黄色') },
-    { color: 'rgb(68, 131, 97)', text: lst('绿色') },
-    { color: 'rgb(51, 126, 169)', text: lst('蓝色') },
-    { color: 'rgb(144, 101, 176)', text: lst('紫色') },
+    { color: 'rgb(68,131,97)', text: lst('绿色') },
+    { color: 'rgb(51,126,169)', text: lst('蓝色') },
+    { color: 'rgb(144,101,176)', text: lst('紫色') },
     { color: 'rgb(193,76,138)', text: lst('粉色') },
     { color: 'rgb(212,76,71)', text: lst('红色') },
     // { color: { grad: 'linear-gradient(90deg, rgb(40, 188, 190) 0%, rgb(57, 53, 221) 100%)', color: 'rgb(49, 121, 206)' }, text: lst('渐变') },
@@ -40,9 +40,9 @@ export var BackgroundColorList = () => {
         { color: 'rgba(239,218,251,0.5)', text: lst('轻紫') },
         { color: 'rgba(234,202,220,0.5)', text: lst('熏粉') },
         { color: 'rgba(253,198,200,0.5)', text: lst('将红') },
-    ].map(c => {
+    ].map(c =>{
         return {
-            color: c.color.replace('0.5', '0.8'),
+            color: c.color.replace('0.5', '1'),
             text: c.text
         }
     })
@@ -53,7 +53,25 @@ export var BackgroundColorList = () => {
  * 
  */
 export var OptionBackgroundColorList = () => {
-    var cs = BackgroundColorList();
+    var cs = [
+        { color: 'rgba(255,255,255,0)', text: lst('默认') },
+        { color: 'rgba(237,233,235,0.5)', text: lst('白灰') },
+        { color: 'rgba(217,211,215,0.5)', text: lst('暗银') },
+        { color: 'rgba(247,214,183,0.5)', text: lst('幼杏') },
+        { color: 'rgba(255,193,153,0.5)', text: lst('鲜橘') },
+        { color: 'rgba(252,246,189,0.5)', text: lst('淡黄') },
+        { color: 'rgba(205,243,220,0.5)', text: lst('浅绿') },
+        { color: 'rgba(169,222,249,0.5)', text: lst('天蓝') },
+        { color: 'rgba(189,201,255,0.5)', text: lst('雾蓝') },
+        { color: 'rgba(239,218,251,0.5)', text: lst('轻紫') },
+        { color: 'rgba(234,202,220,0.5)', text: lst('熏粉') },
+        { color: 'rgba(253,198,200,0.5)', text: lst('将红') },
+    ].map(c => {
+        return {
+            color: c.color.replace('0.5', '1'),
+            text: c.text
+        }
+    })
     lodash.remove(cs, c => c.color == 'rgba(255,255,255,0)');
     return cs;
 }
