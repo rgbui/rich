c4f8ac5d762ea5f5998cec1d1886a35967ad988c
kanhai <760489033@qq.com>
2024-04-14 21:01:50 +0800

    fix:优化公式对话框类型显示样式

--- extensions/data-grid/formula/index.tsx
+++ extensions/data-grid/formula/index.tsx
@@ -187,7 +187,7 @@ export default class FormulaSelector extends EventsComponent {
                         {this.typeSpread && this.formulaLangs.map((fl, k) => {
                             return <div className="gap-h-10" key={k}>
                                 <div onClick={e => { fl.spread = fl.spread ? false : true; this.forceUpdate() }}
-                                    className="flex font-14 padding-l-10">
+                                    className="flex font-14 padding-l-10 cursor">
                                     <span className="flex-fixed size-20 item-hover flex-center round cursor ts" style={{ transform: fl.spread ? 'rotateZ(90deg)' : 'rotateZ(0deg)' }}><Icon size={16} icon={ChevronRightSvg}></Icon></span>
                                     <span className="flex-fixed flex-center size-24"><Icon size={16} icon={GetFieldTypeSvg(fl.type)}></Icon></span>
                                     <span className="flex-auto text-overflow f-14">{fl.text}</span>
