f37d41f619502cf9eb577c0d3c88e2be6a8ecdef
kanhai <760489033@qq.com>
2024-05-03 22:45:23 +0800

    fix:优化加载进度条

--- component/view/spin/index.tsx
+++ component/view/spin/index.tsx
@@ -2,37 +2,39 @@ import React, { CSSProperties } from "react";
 import "./style.less";
 import { util } from "../../../util/util";
 
-function getCircle(size?: 24 | 16) {
-    return <div className={"size-" + (size) + " relative"}>
-        <i className={`size-${size == 24 ? 8 : 4}` + " circle pos bg-primary"} style={{ top: 0, left: 0 }}></i>
-        <i className={`size-${size == 24 ? 8 : 4}` + " circle pos bg-primary op-8"} style={{ top: 0, right: 0 }}></i>
-        <i className={`size-${size == 24 ? 8 : 4}` + " circle pos bg-primary op-5"} style={{ bottom: 0, right: 0 }}></i>
-        <i className={`size-${size == 24 ? 8 : 4}` + " circle pos bg-primary op-3"} style={{ bottom: 0, left: 0 }}></i>
-    </div>
-}
+// function getCircle(size?: 24 | 16) {
+//     return <div className={"size-" + (size) + " relative"}>
+//         <i className={`size-${size == 24 ? 8 : 4}` + " circle pos bg-primary"} style={{ top: 0, left: 0 }}></i>
+//         <i className={`size-${size == 24 ? 8 : 4}` + " circle pos bg-primary op-8"} style={{ top: 0, right: 0 }}></i>
+//         <i className={`size-${size == 24 ? 8 : 4}` + " circle pos bg-primary op-5"} style={{ bottom: 0, right: 0 }}></i>
+//         <i className={`size-${size == 24 ? 8 : 4}` + " circle pos bg-primary op-3"} style={{ bottom: 0, left: 0 }}></i>
+//     </div>
+// }
 
 export function Spin(props: { block?: boolean, size?: 24 | 16, children?: React.ReactNode }) {
     var ov = props.children;
     var size = props.size || 16;
-    if (!ov) ov = getCircle(size);
-    var div = <div className="shy-spin-circle flex-center flex-inline" style={{ width: size * 1.5, height: size * 1.5 }}>
-        {ov}
-    </div>;
-    if (props.block) return <div className="flex-center gap-h-20">{div}</div>
-    return div;
+    return Loading4(size);
+    // if (!ov) ov = getCircle(size);
+    // var div = <div className="shy-spin-circle flex-center flex-inline" style={{ width: size * 1.5, height: size * 1.5 }}>
+    //     {ov}
+    // </div>;
+    // if (props.block) return <div className="flex-center gap-h-20">{div}</div>
+    // return div;
 }
+
 export function SpinBox(props: {
     children?: React.ReactNode,
-    overlay?: React.ReactNode,
+    // overlay?: React.ReactNode,
     spin?: boolean,
     mask?: boolean
 }) {
-    var ov = props.overlay;
-    if (!ov) ov = getCircle(24);
+    // var ov = props.overlay;
+    // if (!ov) ov = getCircle(24);
     return <div className="relative">
         {props.children}
         {props.mask && props.spin && <div className="pos-full" style={{ background: 'rgba(0,0,0,.1)', opacity: .4 }}></div>}
-        {props.spin && <div className="pos-center"><div className="shy-spin-circle">{ov}</div></div>}
+        {props.spin && <div className="pos-center">{Loading4(24)}</div>}
     </div>
 }
 
@@ -77,7 +79,6 @@ export function Loading3() {
 
 }
 
-
 export function SK(props: { children?: React.ReactNode, style?: CSSProperties, className?: (string[]) | string }) {
     var classList: string[] = ['sketelon'];
     var style: CSSProperties = props.style || {};
@@ -113,4 +114,20 @@ export class Ring extends React.Component<{
             <circle r={r} cx={c} cy={c} strokeWidth={d} stroke={hoverColor} strokeLinecap="round" strokeDasharray={cc} strokeDashoffset={off} fill="transparent"></circle>
         </svg>
     }
+}
+
+export function Loading4(size = 32)
+{
+    return <svg
+        style={{
+            boxSizing: 'content-box',
+            color: 'var(--text-color)'
+        }}
+        width={size}
+        height={size}
+        viewBox="0 0 16 16"
+        fill="none" className="anim-rotate">
+        <circle cx="8" cy="8" r="7" stroke="currentColor" strokeOpacity="0.25" strokeWidth="2" vectorEffect="non-scaling-stroke" fill="none"></circle>
+        <path d="M15 8a7.002 7.002 0 00-7-7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" vectorEffect="non-scaling-stroke"></path>
+    </svg>
 }
