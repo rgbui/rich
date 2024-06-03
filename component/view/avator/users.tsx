ffd7d4ffdb5fd3a2051600ece8e491c949c64cbe
kanhai <760489033@qq.com>
2024-06-03 10:16:35 +0800

    fix:优化user的字段显示

--- component/view/avator/users.tsx
+++ component/view/avator/users.tsx
@@ -3,16 +3,17 @@ import { Avatar } from "./face";
 
 export class UserAvatars extends React.Component<{
     size?: number,
+    showName?: boolean,
     users: (string[]) | Set<string>,
     children?: JSX.Element | string | React.ReactNode,
     limit?: number
-}>{
+}> {
     render(): React.ReactNode {
         var us = Array.isArray(this.props.users) ? this.props.users : Array.from(this.props.users);
         if (this.props.limit) us = us.slice(us.length - this.props.limit)
         return <div className="flex">
-            {us.map(u => <Avatar key={u} size={this.props.size} userid={u}></Avatar>)}
-            {this.props.children && <><em className={us.length > 0 ? "gap-r-10" : ""}></em>{this.props.children}</>}
+            {us.map(u => <Avatar key={u} showName={us.length == 1 && this.props.showName ? true : false} size={this.props.size} userid={u}></Avatar>)}
+            {this.props.children && <><em className={us.length > 0 ? "gap-r-5" : ""}></em>{this.props.children}</>}
         </div>
     }
 }
