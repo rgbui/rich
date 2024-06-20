472ba7fdb6a759d796846bf8f39c532a36817afa
kanhai <760489033@qq.com>
2024-06-20 16:56:03 +0800

    fix:增加data-grid中groupView排序

--- blocks/data-grid/view/base/left.cycle.ts
+++ blocks/data-grid/view/base/left.cycle.ts
@@ -114,7 +114,6 @@ export class DataGridViewLife {
                     if (rc.ok) {
                         this.dataGroupHeads = []
                         this.data = [];
-                        var gf = this.schema.fields.find(g => g.id == this.groupView.groupId);
                         rc.data.groupList.forEach(gl => {
                             this.dataGroupHeads.push({
                                 id: gl.id,
@@ -129,6 +128,34 @@ export class DataGridViewLife {
                                 this.data.push(g);
                             })
                         })
+                        if (this.groupView.sort == 'asc') {
+                            this.dataGroupHeads.sort((a, b) => {
+                                if (lodash.isNull(b.id)) return -1;
+                                if (typeof (a.id as any)?.min == 'number') {
+                                    return (a.id as any).min > (b.id as any).min ? 1 : -1
+                                }
+                                return a.id > b.id ? 1 : -1
+                            })
+                            var nd = this.dataGroupHeads.find(g => lodash.isNull(g.id));
+                            if (nd) {
+                                lodash.remove(this.dataGroupHeads, c => lodash.isNull(c.id));
+                                this.dataGroupHeads.splice(0, 0, nd);
+                            }
+                        }
+                        else if (this.groupView.sort == 'desc') {
+                            this.dataGroupHeads.sort((a, b) => {
+                                if (lodash.isNull(a.id)) return -1;
+                                if (typeof (a.id as any)?.min == 'number') {
+                                    return (a.id as any).min < (b.id as any).min ? 1 : -1
+                                }
+                                return a.id < b.id ? 1 : -1
+                            })
+                            var nd = this.dataGroupHeads.find(g => lodash.isNull(g.id));
+                            if (nd) {
+                                lodash.remove(this.dataGroupHeads, c => lodash.isNull(c.id));
+                                this.dataGroupHeads.push(nd);
+                            }
+                        }
                     }
                 }
                 else {
--- blocks/data-grid/view/board/content.tsx
+++ blocks/data-grid/view/board/content.tsx
@@ -9,7 +9,7 @@ import { useSelectMenuItem } from "../../../../component/view/menu"
 import { MenuItem, MenuItemType } from "../../../../component/view/menu/declare"
 import { Tip } from "../../../../component/view/tooltip/tip"
 import { lst } from "../../../../i18n/store"
-import { S } from "../../../../i18n/view"
+import { S, Sp } from "../../../../i18n/view"
 import { BlockRenderRange } from "../../../../src/block/enum"
 import { Rect } from "../../../../src/common/vector/point"
 import { FieldType } from "../../schema/type"
@@ -17,6 +17,7 @@ import { GetFieldTypeSvg } from "../../schema/util"
 import { ViewField } from "../../schema/view"
 import { CardFactory } from "../../template/card/factory/factory"
 import { Icon } from "../../../../component/view/icon"
+import { ToolTip } from "../../../../component/view/tooltip"
 
 export class BoardContent extends React.Component<{
     block: Block,
@@ -103,7 +104,12 @@ export class BoardContent extends React.Component<{
 
         return <div className="sy-data-grid-board-group visible-hover" key={index}>
             <div className="sy-data-grid-board-group-head">
-                <span className="flex-auto"><span className="text-overflow padding-w-6 f-14 padding-h-2  l-16" style={{ backgroundColor: dg.color || undefined }}>{dg.group || lst('未定义')}</span><label>{ds?.count}</label></span>
+                <span className="flex-auto">
+                    {!dg.group && <span className="text-1"><Sp text={'无{group}的数据'}
+                        view={{ group: <em className="bold padding-w-3">{this.block.groupField.text}</em> }}></Sp></span>}
+                    {dg.group && <span className="text-overflow padding-w-6 f-14 padding-h-2  l-16" style={{ backgroundColor: dg.color || undefined }}>{dg.group || lst('未定义')}</span>}
+                    <label>{ds?.count}</label>
+                </span>
                 {this.block.isCanEdit() && <div className="flex-fixed flex">
                     <div onMouseDown={e => {
                         e.stopPropagation();
@@ -119,10 +125,10 @@ export class BoardContent extends React.Component<{
                 {cs.map(c => {
                     return <div onMouseDown={e => this.onDrag(e, c)} key={c.id}>{this.renderItem(c)}</div>
                 })}
-                {this.block.isCanEdit() && <div onMouseDown={e => this.block.onAddGroup(dg)} className="f-12 gap-b-10 visible item-hover item-hover-light-focus flex text-1 round h-30 cursor">
-                    <Icon size={18} className={'gap-l-10'} icon={PlusSvg}></Icon>
-                    <S>新增</S>
-                </div>}
+                {this.block.isCanEdit() && <ToolTip overlay={lst('新增数据')}><div onMouseDown={e => this.block.onAddGroup(dg)} className=" gap-b-10 visible item-hover item-hover-light-focus flex-center text-1 round h-30 cursor">
+                    <Icon size={18} icon={PlusSvg}></Icon>
+                </div></ToolTip>
+                }
             </div>
         </div>
     }
@@ -134,16 +140,25 @@ export class BoardContent extends React.Component<{
         return <itemBlock.viewComponent block={itemBlock}></itemBlock.viewComponent>
     }
     renderAddGroup() {
+        var hgs = this.block.hideGroups;
+        if (hgs.includes(null)) {
+            hgs.remove(c => c === null);
+            hgs.push(null)
+        }
+
+        var gf = this.block.groupField
         return <div className="sy-data-grid-board-group visible-hover" >
             <div className="item-hover flex-center round h-30 cursor remark" onMouseDown={e => this.block.onAddNewGroup(e)}><Icon icon={PlusSvg}></Icon><S>新增分组</S></div>
-            {this.block.hideGroups.length > 0 && this.block.hideGroups.map(hg => {
+            {hgs.length > 0 && hgs.map(hg => {
                 var dg = this.block.dataGroups.find(c => c.value == hg)
                 var ds = this.block.groupStats.find(g => g.groupId == dg.value)
                 if (this.props.groupHead) ds = this.block.groupStats.find(g => g.groupId == dg.value && lodash.isEqual(g.groupViewId, this.props.groupHead.id));
 
-                return <div className="flex" key={hg}>
+                return <div className="flex min-h-30 visible-hover" key={hg}>
                     <span className="flex-auto">
-                        <span className="text-overflow padding-w-6  f-14 padding-h-2  l-16 round " style={{ backgroundColor: dg?.color }}>{dg?.group || lst('未定义')}</span>
+                        {!dg.group && <span className="text-1"><Sp text={'无{group}的数据'}
+                            view={{ group: <em className="bold padding-w-3">{this.block.groupField.text}</em> }}></Sp></span>}
+                        {dg.group && <span className="text-overflow padding-w-6  f-14 padding-h-2  l-16 round " style={{ backgroundColor: dg?.color }}>{dg?.group}</span>}
                         <label className="remark f-12 gap-l-5">{ds?.count}</label>
                     </span>
                     <Tip text='取消隐藏分组'><span onMouseDown={e => {
@@ -151,13 +166,13 @@ export class BoardContent extends React.Component<{
                         var hgs = lodash.cloneDeep(this.block.hideGroups);
                         lodash.remove(hgs, c => c == hg);
                         this.block.onUpdateProps({ hideGroups: hgs }, { range: BlockRenderRange.self });
-                    }} className="flex-fixed size-24 round item-hover cursor flex-center">
+                    }} className="flex-fixed visible  size-24 remark round item-hover cursor flex-center">
                         <Icon icon={EyeHideSvg} size={18}></Icon>
                     </span></Tip>
                     <span onMouseDown={e => {
                         e.stopPropagation();
                         this.block.onOpenGroupEdit(dg, e, this.props.groupHead);
-                    }} className="flex-fixed size-24 round item-hover cursor  flex-center">
+                    }} className="flex-fixed visible size-24 remark round item-hover cursor  flex-center">
                         <Icon icon={DotsSvg} size={18}></Icon>
                     </span>
                 </div>
