46ed189d5e9447211be86452b77a9c981a2c9e58
kanhai <760489033@qq.com>
2024-07-10 11:11:58 +0800

    fix:优化filter input停靠位置

--- component/view/input/filter.tsx
+++ component/view/input/filter.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { EventsComponent } from "../../lib/events.component";
 import { Divider } from "../grid";
-import { MenuItem } from "../menu/declare";
+import { MenuItem, MenuItemType } from "../menu/declare";
 import { Input } from ".";
 import { Icon } from "../icon";
 import { KeyboardCode } from "../../../src/common/keys";
@@ -17,13 +17,16 @@ export class FilterInput extends EventsComponent {
     focusIndex: number = 1;
     word: string = '';
     placeholder = '';
+    width: number;
+    height: number;
     onSelect(item: MenuItem & { words: string[] }) {
         this.emit('select', item);
     }
     render() {
-        return <div className="bg-white w-200 round">
+        return <div className="bg-white w-200 round" style={{ width: this.width || undefined }}>
             <div className="gap-w-5 gap-t-10">
                 <Input
+                    focusSelectionAll
                     size="small"
                     placeholder={this.placeholder}
                     value={this.word}
@@ -32,7 +35,9 @@ export class FilterInput extends EventsComponent {
                         this.word = '';
                         this.items = this.options.map(o => o);
                         if (this.focusIndex >= this.items.length) this.focusIndex = 0;
-                        this.forceUpdate();
+                        this.forceUpdate(() => {
+                            this.emit('update')
+                        });
                     }}
                     onKeydown={e => {
                         if (e.key == KeyboardCode.ArrowDown) {
@@ -51,47 +56,61 @@ export class FilterInput extends EventsComponent {
                     }}
                     onChange={e => {
                         this.word = e;
-                        this.items = this.options.filter(g => Array.isArray(g.words) && g.words.some(c => c, toLower().indexOf(e.toLowerCase()) > -1) || g.text.toLowerCase().indexOf(e.toLowerCase()) >= 0);
-                        this.forceUpdate();
+                        this.items = this.options.filter(g => g.type != MenuItemType.text && Array.isArray(g.words) && g.words.some(c => c, toLower().indexOf(e.toLowerCase()) > -1) || g.type != MenuItemType.text && g.text.toLowerCase().indexOf(e.toLowerCase()) >= 0);
+                        this.forceUpdate(() => {
+                            this.emit('update')
+                        });
                     }}></Input>
             </div>
 
             <Divider></Divider>
-            <div className="max-h-200 overflow-y">
+            <div className="max-h-200 overflow-y" style={{ maxHeight: this.height || undefined }}>
                 {this.items.length == 0 && <div className="padding-w-5 flex-center h-24 f-12 remark"><S>无匹配项</S></div>}
                 {this.items.map((it, i) => {
+                    if (it.type == MenuItemType.text) return <div key={i} className="padding-w-5 gap-w-5 remark f-12 gap-h-5">{it.text}</div>
                     return <div
-                        className={" flex round h-28 gap-h-3 padding-w-10 gap-w-5 cursor " + (i == this.focusIndex ? "item-hover-light-focus" : "item-hover")}
+                        className={" flex round h-28 gap-h-3 padding-w-5 gap-w-5 cursor " + (i == this.focusIndex ? "item-hover-light-focus" : "item-hover")}
                         key={i}
                         onMouseDown={e => this.onSelect(it)}
                     >
-                        {it.icon && <span className="flex-fixed"><Icon size={16} icon={it.icon}></Icon></span>}
-                        <span className="flex-auto text-overflow">{it.text}</span>
+                        {it.icon && <span className="flex-fixed size-20 flex-center text-1"><Icon size={16} icon={it.icon}></Icon></span>}
+                        <span className="flex-auto text-overflow gap-l-3">{it.text}</span>
                         {it.checkLabel && <span className="flex-fixed">
                             <Icon icon={CheckSvg} size={16}></Icon>
                         </span>}
-
                     </div>
                 })}
             </div>
         </div>
     }
-    open(options: { options: MenuItem & { words: string[] }[], placeholder: string }) {
+    open(options: {
+        options: MenuItem & { words: string[] }[], height?: number,
+        width?: number, placeholder: string
+    }) {
         this.options = options.options;
         this.placeholder = options.placeholder || '';
         this.items = this.options.map(o => o);
         this.word = '';
         this.focusIndex = 0;
-        this.forceUpdate();
+        this.width = options.width;
+        this.height = options.height;
+        this.forceUpdate(() => {
+            this.emit('update', { width: this.width || 200, height: (this.height || 200) + 30 })
+        });
     }
 }
 
 export async function useFilterInput(pos: PopoverPosition, options: {
     options: (MenuItem & { words: string[] })[],
+    height?: number,
+    width?: number,
     placeholder: string
 }) {
     let popover = await PopoverSingleton(FilterInput, { mask: true });
     let fv = await popover.open(pos);
+    fv.only('update', (d) => {
+        popover.updateLayout(d)
+    })
     fv.open(options);
     return new Promise((resolve: (data: MenuItem) => void, reject) => {
         fv.only('select', (value) => {
