a2ee906c3abe80fdd70e11f8ede251d0544a62ac
kanhai <760489033@qq.com>
2024-07-30 16:04:40 +0800

    fix:实现group的组合与解组合，及块的操作变化范围自动更新

--- src/block/element/group.tsx
+++ src/block/element/group.tsx
@@ -4,7 +4,10 @@ import { Block } from '..';
 import { BlockView } from '../view';
 import { BlockDisplay } from '../enum';
 import { url, view } from '../factory/observable';
+import { Point, PointArrow, Rect, RectUtility } from '../../common/vector/point';
+import lodash from 'lodash';
 import { Matrix } from '../../common/matrix';
+
 
 /**
  * 可以将相邻的block变成一个整体去操作，
@@ -16,34 +19,131 @@ export class Group extends Block {
     async getBoardEditCommand(): Promise<{ name: string; value?: any; }[]> {
         var cs: { name: string; value?: any; }[] = [];
         cs.push({ name: "ungroup" });
-
         return cs;
     }
-    async setBoardEditCommand(this: Block, name: string, value: any) {
+    async setBoardEditCommand(name: string, value: any) {
         if (name == 'ungroup') {
-            var bs = this.blocks.childs.map(c => c);
-            for (let i = 0; i < bs.length; i++) {
-                var b = bs[i];
-                var r = b.getTranslation().base(this.getTranslation());
-                var nm = new Matrix();
-                nm.translate(r);
-                nm.rotate(b.matrix.getRotation(), { x: 0, y: 0 });
-                await b.updateMatrix(b.matrix, nm);
-            }
-            await this.parent.appendArray(bs, this.at, this.parentKey);
-            await this.delete();
-            setTimeout(() => {
-                this.page.kit.picker.onPicker(bs);
-            }, 200);
+            await this.unmerge();
         }
     }
+    get fixedSize() {
+        // var b = this.getBounds();
+        // return {
+        //     width: b.width,
+        //     height: b.height
+        // }
+        return {
+            width: this.fixedWidth,
+            height: this.fixedHeight
+        }
+    }
+    onResizeBoardSelector(arrows: PointArrow[], event: React.MouseEvent) {
+        this.onResizeScaleBoardSelector(arrows, event);
+    }
+    getBounds() {
+        var cs = this.childs;
+        var bs = cs.map(c => {
+            var size = c.fixedSize;
+            var rect = new Rect(0, 0, size.width, size.height);
+            rect = rect.transformToBound(c.currentMatrix);
+            return rect;
+        });
+        lodash.remove(bs, g => g ? false : true);
+        var points = bs.map(b => b.points).flat();
+        return RectUtility.getPointsBound(points)
+    }
+    async updateGroupRange() {
+        var blocks = this.childs.map(c => c);
+        var bs: Rect[] = [];
+        blocks.forEach(c => {
+            var { width, height } = c.fixedSize;
+            var rect = new Rect(0, 0, width, height);
+            var ma = this.matrix.clone();
+            var gm = ma.append(c.currentMatrix);
+            var nb = rect.transformToBound(gm);
+            bs.push(nb);
+        });
+        var nr = bs.first();
+        bs.forEach((b, i) => {
+            if (i > 0) {
+                nr = nr.merge(b);
+            }
+        })
+        nr = nr.transformInverseToBound(this.matrix);
+        console.log('nnn', nr);
+        var ma = new Matrix();
+        ma.translate(nr.leftTop.x, nr.leftTop.y);
+        var newMa = this.matrix.clone().append(ma);
+        console.log('updateGroupRange', ma, nr.width, nr.height, this.fixedWidth, this.fixedHeight);
+        await this.updateProps({
+            fixedWidth: nr.width,
+            fixedHeight: nr.height
+        });
+        var oldMa = this.matrix.clone();
+        await this.updateMatrix(this.matrix, newMa);
+        for (let i = 0; i < blocks.length; i++) {
+            var b = blocks[i];
+            var oc = oldMa.clone().append(b.matrix).transform(new Point(0, 0));
+            var nc = this.matrix.clone().append(b.matrix).inverseTransform(oc);
+            var mb = new Matrix();
+            mb.translate(nc.x, nc.y);
+            var nm = b.matrix.clone().append(mb);
+            await b.updateMatrix(b.matrix, nm);
+        }
+    }
+    async unmerge() {
+        var bs = this.blocks.childs.map(c => c);
+        for (let i = 0; i < bs.length; i++) {
+            var b = bs[i];
+            var ma = this.matrix.clone();
+            var nm = ma.append(b.matrix);
+            await b.updateMatrix(b.matrix, nm);
+        }
+        await this.parent.appendArray(bs, this.at, this.parentKey);
+        await this.delete();
+        setTimeout(() => {
+            this.page.kit.picker.onPicker(bs);
+        }, 200);
+    }
+    async mergeBlocks(blocks: Block[]) {
+        var bs: Rect[] = [];
+        blocks.forEach(c => {
+            var { width, height } = c.fixedSize;
+            var rect = new Rect(0, 0, width, height);
+            var gm = c.currentMatrix.clone();
+            var nb = rect.transformToBound(gm);
+            bs.push(nb);
+        });
+        var nr = bs.first();
+        bs.forEach((b, i) => {
+            if (i > 0) {
+                nr = nr.merge(b);
+            }
+        })
+        var ma = new Matrix();
+        ma.translate(nr.leftTop.x, nr.leftTop.y);
+        await this.updateProps({
+            fixedWidth: nr.width,
+            fixedHeight: nr.height
+        });
+        await this.updateMatrix(this.matrix, ma);
+        for (let i = 0; i < blocks.length; i++) {
+            var b = blocks[i];
+            var mc = ma.clone() as Matrix;
+            var nm = mc.inverted().append(b.matrix);
+            await b.updateMatrix(b.matrix, nm);
+        }
+        await this.appendArray(blocks, this.childs.length);
+    }
 }
+
 @view('/group')
 export class GroupView extends BlockView<Group> {
     renderView() {
+        var fs = this.block.fixedSize;
         var style = Object.assign({
-            width: this.block.fixedWidth,
-            height: this.block.fixedHeight
+            width: fs.width,
+            height: fs.height
         }, this.block.visibleStyle);
         return <div className='sy-block-group' style={style} >{this.block.childs.map(x =>
             <x.viewComponent key={x.id} block={x}></x.viewComponent>
