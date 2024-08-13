8553d006f69a07b79174b669d3268d393533d380
kanhai <760489033@qq.com>
2024-08-14 00:27:46 +0800

    fix:优化小于1的透明度数字显示

--- extensions/board.edit.tool/common/measure.tsx
+++ extensions/board.edit.tool/common/measure.tsx
@@ -10,11 +10,14 @@ export class MeasureText extends React.Component<{
     value: number,
     onChange?: (value: number) => void
 }> {
-
     refValue: HTMLElement;
     render() {
+        var gv = (v) => {
+            if (v >= 0 && v <= 1) return (Math.round(v * 10) * 0.1).toFixed(1);
+            else return Math.round(v).toString();
+        }
         return <>
-            <div className="shy-measure-view-label"><label className="f-12 remark">{this.props.label || <S>透明度</S>}</label><span className="f-12 remark" ref={e => this.refValue = e} style={{ float: 'right' }}>{Math.round(this.props.value)}</span></div>
+            <div className="shy-measure-view-label"><label className="f-12 remark">{this.props.label || <S>透明度</S>}</label><span className="f-12 remark" ref={e => this.refValue = e} style={{ float: 'right' }}>{gv(this.props.value)}</span></div>
             <MeasureView
                 theme="light"
                 min={this.props.min || 0}
@@ -24,7 +27,7 @@ export class MeasureText extends React.Component<{
                 value={this.props.value}
                 inputting={false}
                 onInput={e => {
-                    this.refValue.innerText = Math.round(e).toString();
+                    this.refValue.innerText = gv(e);
                 }}
                 onChange={e => {
                     this.props.onChange(e);
